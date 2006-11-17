f42550600f93aba5b4163c285e40d8b0aea11e51
plegall <plegall@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-11-17 14:13:02 +0000

    feature 250 added: new component tAggregateRow. This is a virtual component based on new components tAggregateOut and tArrayIn. In the GUI, define the "group by" columns and a list of operations on input data to build output fields. Available operations are: count, min, max, avg, sum. tAggregateOut fills a Perl array of array references. tArrayIn is in charge of reading this array.
    New: a Stringutils java class was added to make easier string operations
    such as repeating strings.
    
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@593 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.commons/src/main/java/org/talend/commons/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package org.talend.commons.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtils {
+	
+    public static String repeat(String str, int repeat) {
+    	return org.apache.commons.lang.StringUtils.repeat(str, repeat);
+    }
+
+}
