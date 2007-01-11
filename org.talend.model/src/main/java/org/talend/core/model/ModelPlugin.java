11f0a37cb39bc70ed161412ef78d59b7055fb35c
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-01-11 15:20:51 +0000

    Modification: ModelPlugin activator class created
    git-svn-id: http://talendforge.org/svn/tos/trunk@1412 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.model/src/main/java/org/talend/core/model/ModelPlugin.java
@@ -0,0 +1,68 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.core.model;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class ModelPlugin extends AbstractUIPlugin {
+
+    public static final String MODEL_PLUGIN_ID = "org.talend.model";
+
+    // The shared instance
+    private static ModelPlugin plugin;
+
+    /**
+     * The constructor.
+     */
+    public ModelPlugin() {
+        plugin = this;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance.
+     * 
+     * @return the shared instance
+     */
+    public static ModelPlugin getDefault() {
+        return plugin;
+    }
+}
