f90fc3b14e9e7d0f59f7e1c6f06f8867209c20ee
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-01-31 16:02:15 +0000

    Feature 432 partially completed : Java code checker added for internal components, only FILENAME fields are now checked
    git-svn-id: http://talendforge.org/svn/tos/trunk@1748 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.commons/src/main/java/org/talend/commons/utils/generation/CodeGenerationUtils.java
@@ -0,0 +1,82 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.commons.utils.generation;
+
+
+/**
+ * DOC amaumont  class global comment. Detailled comment
+ * <br/>
+ *
+ * $Id$
+ *
+ */
+public class CodeGenerationUtils {
+
+    public static final String PROBLEM_KEY_FIELD_SEPARATOR = ":";
+
+    /**
+     * DOC amaumont Comment method "buildProblemKey".
+     * 
+     * @param mapperComponent
+     * @param problemKeyField
+     * @param tableName
+     * @param entryName
+     */
+    public static String buildProblemKey(String... valuesOfKey) {
+        String key = "";
+        for (int i = 0; i < valuesOfKey.length; i++) {
+            String value = valuesOfKey[i];
+            if (value != null) {
+                if (i != 0) {
+                    key += PROBLEM_KEY_FIELD_SEPARATOR;
+                }
+                key += value;
+            }
+        }
+        return key;
+    }
+
+
+    /**
+     * DOC amaumont Comment method "insertFieldKey".
+     * 
+     * @param string
+     * @param expression
+     * @return
+     */
+    public static String buildStartFieldKey(String key) {
+        return "\n// Start field " + key + "\n";
+    }
+
+    /**
+     * DOC amaumont Comment method "buildEndFieldKey".
+     * 
+     * @param key
+     * @return
+     */
+    public static String buildEndFieldKey(String key) {
+        return "\n// End field " + key + "\n";
+    }
+
+
+    
+}
