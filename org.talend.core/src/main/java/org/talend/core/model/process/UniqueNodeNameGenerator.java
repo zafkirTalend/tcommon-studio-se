5340aa7f1edd0313701a0c13d02dfc5a20d012d5
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-02-12 14:47:34 +0000

    Modification: Externalize Node unique name generator from org.designer.core.Process to org.talend.core.UniqueNodeNameGenerator
    Modification: Add IComponentFilter and IComponentConversion to manage components renaming (part 1)
    
    Bug 540 fixed: tDBInput to tMysqlInput migration
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@2014 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/core/model/process/UniqueNodeNameGenerator.java
@@ -0,0 +1,56 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.core.model.process;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.talend.core.model.properties.ProcessItem;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+
+/**
+ * DOC smallet class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 1 2006-09-29 17:06:40 +0000 (ven., 29 sept. 2006) nrousseau $
+ * 
+ */
+public class UniqueNodeNameGenerator {
+
+    public static String generateUniqueNodeName(String baseName, List<String> uniqueNodeNameList) {
+        if (baseName == null) {
+            throw new IllegalArgumentException("Component name can't be null"); //$NON-NLS-1$
+        }
+        String uniqueName = baseName + "_" + 1; //$NON-NLS-1$
+
+        int counter = 1;
+        boolean exists = true;
+        while (exists) {
+            exists = uniqueNodeNameList.contains(uniqueName);
+            if (!exists) {
+                break;
+            }
+            uniqueName = baseName + "_" + counter++; //$NON-NLS-1$
+        }
+        return uniqueName;
+    }
+
+}
