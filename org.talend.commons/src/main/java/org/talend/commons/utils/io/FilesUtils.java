e55c8026724f3b285a8469ed8b2df7dcbc8d3a3d
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-02-16 17:38:46 +0000

    Feature 608 added: "Import project as ..."  + add a wizard, wizard page to import a "project as..."  + centralize functions in ImportProjectUtilities  + move actions into a related package "importProject"  + replaceInFile(String regex, String fileName, String replacement) method created in FilesUtils
    git-svn-id: http://talendforge.org/svn/tos/trunk@2090 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons/src/main/java/org/talend/commons/utils/io/FilesUtils.java
+++ org.talend.commons/src/main/java/org/talend/commons/utils/io/FilesUtils.java
@@ -21,11 +21,15 @@
 // ============================================================================
 package org.talend.commons.utils.io;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 /**
  * DOC smallet class global comment. Detailled comment <br/>
@@ -86,6 +90,25 @@ public class FilesUtils {
         }
         fis.close();
         fos.close();
+    }
+
+    public static void replaceInFile(String regex, String fileName, String replacement) throws IOException {
+        InputStream in = new FileInputStream(fileName);
+        StringBuffer buffer = new StringBuffer();
+        try {
+            InputStreamReader inR = new InputStreamReader(in);
+            BufferedReader buf = new BufferedReader(inR);
+            String line;
+            while ((line = buf.readLine()) != null) {
+                buffer.append(line.replaceAll(regex, replacement)).append("\n");
+            }
+        } finally {
+            in.close();
+        }
+
+        OutputStream os = new FileOutputStream(fileName);
+        os.write(buffer.toString().getBytes());
+        os.close();
     }
 
 }
