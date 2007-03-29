4a3d16c78ae20e715f71adb85a3be44e5db865c8
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-03-29 08:19:11 +0000

    feature 816: add comment to method replaceSpecialCharForXML in TalendString.
    git-svn-id: http://talendforge.org/svn/tos/trunk@2758 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/TalendString.java
+++ org.talend.librariesmanager/resources/java/routines/system/TalendString.java
@@ -1,72 +1,81 @@
 package routines;
 
-//# formatString return the input string formatted as requested. This function
-//# takes a hash as input. We have 4 different cases:
-//#
-//# 1. "asis => 1", the string is returned "as is"
-//#
-//# 2. the string is shorter than the requested size, "align" parameter can be
-//# set to 'R', 'L' or 'C' (center).
-//#
-//# 3. the string is longer than the requested size, "keep" parameter can be
-//# 'All', 'Right', 'Left' or 'Middle', depending on you want to keep all
-//# letters, only right letters, only left letters or only middle letters.
-//#
-//# 4. the length of the string is the requested size
-//#
-//# You can change the padding char with the "padding_char" parameter.
-//#
-//# Examples:
-//# formatString(string => 'foobar', size => 10, align => 'L');
-//# formatString(string => 'foobar', size => 3, keep => 'M');
-//# formatString(string => 'foobar', size => 20, padding_char => '0');
+// # formatString return the input string formatted as requested. This function
+// # takes a hash as input. We have 4 different cases:
+// #
+// # 1. "asis => 1", the string is returned "as is"
+// #
+// # 2. the string is shorter than the requested size, "align" parameter can be
+// # set to 'R', 'L' or 'C' (center).
+// #
+// # 3. the string is longer than the requested size, "keep" parameter can be
+// # 'All', 'Right', 'Left' or 'Middle', depending on you want to keep all
+// # letters, only right letters, only left letters or only middle letters.
+// #
+// # 4. the length of the string is the requested size
+// #
+// # You can change the padding char with the "padding_char" parameter.
+// #
+// # Examples:
+// # formatString(string => 'foobar', size => 10, align => 'L');
+// # formatString(string => 'foobar', size => 3, keep => 'M');
+// # formatString(string => 'foobar', size => 20, padding_char => '0');
 
-
-//sub getRandomString {
-//    my ($length, $letters) = @_;
+// sub getRandomString {
+// my ($length, $letters) = @_;
 //
-//    my $string = '';
-//    for (1..$length) {
-//        $string.= $letters->[int rand scalar @$letters];
-//    }
+// my $string = '';
+// for (1..$length) {
+// $string.= $letters->[int rand scalar @$letters];
+// }
 //
-//    return $string;
-//}
+// return $string;
+// }
 
-//sub getAsciiRandomString {
-//    my ($length) = @_;
+// sub getAsciiRandomString {
+// my ($length) = @_;
 //
-//    return getRandomString(
-//        $length,
-//        ['a'..'z', 'A'..'Z', 0..9]
-//    );
-//}
+// return getRandomString(
+// $length,
+// ['a'..'z', 'A'..'Z', 0..9]
+// );
+// }
 
-//sub getHexRandomString {
-//    my ($length) = @_;
+// sub getHexRandomString {
+// my ($length) = @_;
 //
-//    return getRandomString(
-//        $length,
-//        ['a'..'f', 0..9]
-//    );
-//}
+// return getRandomString(
+// $length,
+// ['a'..'f', 0..9]
+// );
+// }
 
-//##
-//# return a random english firstname
-//#
-//# {talendTypes} String
-//# {param} list(undef, 'uppercase', 'lowercase', 'uppercase first') transformation : processing
-//#
-//# {example} firstName(undef) # Hugh, Andrew, John
-//# {example} firstName('uppercase') # MIKE, ANDREW, HUGH
+// ##
+// # return a random english firstname
+// #
+// # {talendTypes} String
+// # {param} list(undef, 'uppercase', 'lowercase', 'uppercase first') transformation : processing
+// #
+// # {example} firstName(undef) # Hugh, Andrew, John
+// # {example} firstName('uppercase') # MIKE, ANDREW, HUGH
 
 public class TalendString {
-    public static String replaceSpecialCharForXML(String input){
-        input = input.replaceAll("&","&amp;");
-        input = input.replaceAll("<","&lt;");
-        input = input.replaceAll(">","&gt;");
-        input = input.replaceAll("'","&apos;");
-        input = input.replaceAll("\"","&quot;");
+
+    /**
+     * return Replace the special character(e.g. <,>,& etc) within a string for XML file.
+     * 
+     * {talendTypes} String
+     * 
+     * {param} input: The string with the special character(s) need to be replaced.
+     * 
+     * {example} replaceSpecialCharForXML("<title>Empire <>Burlesque</title>") # <title>Empire &lt;&gt;Burlesque</title>
+     */
+    public static String replaceSpecialCharForXML(String input) {
+        input = input.replaceAll("&", "&amp;");
+        input = input.replaceAll("<", "&lt;");
+        input = input.replaceAll(">", "&gt;");
+        input = input.replaceAll("'", "&apos;");
+        input = input.replaceAll("\"", "&quot;");
         return input;
     }
 }
