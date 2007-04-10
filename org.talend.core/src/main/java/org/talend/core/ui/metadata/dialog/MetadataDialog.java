521eb2bbba53166ae0156e25779be94b47a07677
qzhang <qzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-04-10 09:54:38 +0000

    fixed bug 890:keep the output/input metadatatable name. only modify its cloumns
    git-svn-id: http://talendforge.org/svn/tos/trunk@3011 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/ui/metadata/dialog/MetadataDialog.java
+++ org.talend.core/src/main/java/org/talend/core/ui/metadata/dialog/MetadataDialog.java
@@ -151,8 +151,8 @@ public class MetadataDialog extends Dialog {
         if (inputMetaTable == null) {
             composite.setLayout(new FillLayout());
             metadataTableEditor = new MetadataTableEditor(outputMetaTable, titleOutput);
-            outputMetaView = new MetadataTableEditorView(composite, SWT.NONE, metadataTableEditor, outputReadOnly,
-                    true, true, false);
+            outputMetaView = new MetadataTableEditorView(composite, SWT.NONE, metadataTableEditor, outputReadOnly, true, true,
+                    false);
             outputMetaView.setDbTypeColumnsState(DATABASE_LABEL.equals(outputFamily), true, false);
 
             outputMetaView.initGraphicComponents();
@@ -174,8 +174,8 @@ public class MetadataDialog extends Dialog {
             composite.setLayoutData(gridData);
 
             metadataTableEditor = new MetadataTableEditor(inputMetaTable, titleInput + " (Input)"); //$NON-NLS-1$
-            inputMetaView = new MetadataTableEditorView(compositesSachForm.getLeftComposite(), SWT.NONE,
-                    metadataTableEditor, inputReadOnly, true, true, false);
+            inputMetaView = new MetadataTableEditorView(compositesSachForm.getLeftComposite(), SWT.NONE, metadataTableEditor,
+                    inputReadOnly, true, true, false);
             inputMetaView.setDbTypeColumnsState(DATABASE_LABEL.equals(inputFamily), true, false);
             inputMetaView.initGraphicComponents();
             inputMetaView.getExtendedTableViewer().setCommandStack(commandStack);
@@ -222,14 +222,14 @@ public class MetadataDialog extends Dialog {
             copyToOutput.addListener(SWT.Selection, new Listener() {
 
                 public void handleEvent(Event event) {
-                    MessageBox messageBox = new MessageBox(parent.getShell(), SWT.APPLICATION_MODAL | SWT.OK
-                            | SWT.CANCEL);
+                    MessageBox messageBox = new MessageBox(parent.getShell(), SWT.APPLICATION_MODAL | SWT.OK | SWT.CANCEL);
                     messageBox.setText(Messages.getString("MetadataDialog.SchemaModification")); //$NON-NLS-1$
                     messageBox.setMessage(Messages.getString("MetadataDialog.Message")); //$NON-NLS-1$
                     if (messageBox.open() == SWT.OK) {
+                        IMetadataTable metadataTable = inputMetaView.getMetadataTableEditor().getMetadataTable().clone();
+                        // add by qzhang. keep the output metadatatable name. only modify its cloumns
+                        metadataTable.setTableName(outputMetaView.getMetadataTableEditor().getMetadataTable().getTableName());
                         outputMetaView.getMetadataTableEditor().removeAll();
-                        IMetadataTable metadataTable = inputMetaView.getMetadataTableEditor().getMetadataTable()
-                                .clone();
                         List<IMetadataColumn> listColumns = metadataTable.getListColumns();
                         for (IMetadataColumn column : listColumns) {
                             column.setPattern(null);
@@ -249,14 +249,14 @@ public class MetadataDialog extends Dialog {
             copyToInput.addListener(SWT.Selection, new Listener() {
 
                 public void handleEvent(Event event) {
-                    MessageBox messageBox = new MessageBox(parent.getShell(), SWT.APPLICATION_MODAL | SWT.OK
-                            | SWT.CANCEL);
+                    MessageBox messageBox = new MessageBox(parent.getShell(), SWT.APPLICATION_MODAL | SWT.OK | SWT.CANCEL);
                     messageBox.setText(Messages.getString("MetadataDialog.SchemaModification")); //$NON-NLS-1$
                     messageBox.setMessage(Messages.getString("MetadataDialog.TransferMessage")); //$NON-NLS-1$
                     if (messageBox.open() == SWT.OK) {
+                        IMetadataTable metadataTable = outputMetaView.getMetadataTableEditor().getMetadataTable().clone();
+                        // add by qzhang. keep the input metadatatable name. only modify its cloumns
+                        metadataTable.setTableName(inputMetaView.getMetadataTableEditor().getMetadataTable().getTableName());
                         inputMetaView.getMetadataTableEditor().removeAll();
-                        IMetadataTable metadataTable = outputMetaView.getMetadataTableEditor().getMetadataTable()
-                                .clone();
                         List<IMetadataColumn> listColumns = metadataTable.getListColumns();
                         for (IMetadataColumn column : listColumns) {
                             column.setPattern(null);
