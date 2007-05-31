46ed3c6cfb69124f833820a57f83de97646e9b48
mhirt <mhirt@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-05-31 00:11:47 +0000

    Fix bug 1180 : Class name of routine is always "Template", it should be the name of the routine typed at creation
    git-svn-id: http://talendforge.org/svn/tos/trunk@3616 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/Template.java
+++ org.talend.librariesmanager/resources/java/routines/Template.java
@@ -1,7 +1,7 @@
 //template routine Java
 package routines;
 
-public class Template {
+public class __CLASS_NAME__ {
     public static void helloExemple(String message) {
         if (message == null) {
             message = "World";
