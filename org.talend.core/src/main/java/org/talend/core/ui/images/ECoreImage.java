dda02e882cfa39f046821f012d99f62a0a2744c9
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-06-11 12:33:38 +0000

    Icons: update context icon + context wizard  + refrerence this icons in designer.core plugin.xml as context view icon
    git-svn-id: http://talendforge.org/svn/tos/trunk@3764 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/ui/images/ECoreImage.java
+++ org.talend.core/src/main/java/org/talend/core/ui/images/ECoreImage.java
@@ -47,7 +47,7 @@ public enum ECoreImage implements IImage {
     BUSINESS_PROCESS_WIZ("/icons/business_wiz.png"), //$NON-NLS-1$
     PROCESS_ICON("/icons/process_icon.gif"), //$NON-NLS-1$
     PROCESS_WIZ("/icons/process_wiz.png"), //$NON-NLS-1$
-    CONTEXT_ICON("/icons/JobContexts3bySML.png"), //$NON-NLS-1$
+    CONTEXT_ICON("/icons/context_icon.gif"), //$NON-NLS-1$
     CONTEXT_WIZ("/icons/context_wiz.png"), //$NON-NLS-1$
     CODE_ICON("/icons/code.png"), //$NON-NLS-1$
     ROUTINE_ICON("/icons/routine.gif"), //$NON-NLS-1$
