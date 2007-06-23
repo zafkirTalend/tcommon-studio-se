59979690d4c590f33c9e9eaba36e5e4ec5cca963
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-06-23 10:33:08 +0000

    minor 1098 modified: solved the problem of deleting java and perl job class file and script after exportation.
    git-svn-id: http://talendforge.org/svn/tos/trunk@3998 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/designer/core/ui/editor/ITalendEditorService.java
@@ -0,0 +1,38 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.designer.core.ui.editor;
+
+import org.talend.core.IService;
+
+/**
+ * Service of Talend editor.
+ * 
+ * yzhang class global comment. Detailled comment <br/>
+ * 
+ * $Id: ITalendEditorService.java 下午03:13:30 2007-6-19 +0000 (2007-6-19) yzhang $
+ * 
+ */
+public interface ITalendEditorService extends IService {
+
+    public boolean isProcessAlive(String processName);
+
+}
