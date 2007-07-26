62a600931ee40daeced89b7009e527004f87802b
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-07-26 02:17:05 +0000

    bug 1614 fixed: Remove the quotes in the tRunJob fields (for the process and context).
    git-svn-id: http://talendforge.org/svn/tos/trunk@4798 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/core/model/components/conversions/RemoveQuotesInPropertyComponentConversion.java
@@ -0,0 +1,63 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.core.model.components.conversions;
+
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+
+/**
+ * DOC smallet class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 1 2006-09-29 17:06:40 +0000 (ven., 29 sept. 2006) nrousseau $
+ * 
+ */
+public class RemoveQuotesInPropertyComponentConversion implements IComponentConversion {
+
+    private String propertyToModify;
+
+    public RemoveQuotesInPropertyComponentConversion(String propertyToModify) {
+        super();
+        this.propertyToModify = propertyToModify;
+    }
+
+    public void transform(NodeType node) {
+        String value = ComponentUtilities.getNodePropertyValue(node, propertyToModify);
+        if (isWithinQuote(value)) {
+            String newValue = value.substring(1, value.length() - 1);
+            ComponentUtilities.setNodeValue(node, propertyToModify, newValue);
+        }
+    }
+
+    private boolean isWithinQuote(String string) {
+        if (string == null) {
+            return true;
+        }
+        boolean isWithin = false;
+        if (string.startsWith("\"") && string.endsWith("\"") && (string.length() > 1)) {
+            isWithin = true;
+        } else if (string.startsWith("\'") && string.endsWith("\'") && (string.length() > 1)) {
+            isWithin = true;
+        }
+        return isWithin;
+    }
+
+}
