6b9496571634fe09eb077c6bcaf2b786826ea565
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-08-01 10:49:31 +0000

    feature 1182 modified: integrated expression builder with tRowGenerator component.
    git-svn-id: http://talendforge.org/svn/tos/trunk@4945 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/expressionbuilder/IExpressionConsumer.java
@@ -0,0 +1,34 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.expressionbuilder;
+
+/**
+ * yzhang class global comment. Detailled comment <br/>
+ * 
+ * $Id: IExpressionConsumer.java 下午04:21:19 2007-8-1 +0000 (2007-8-1) yzhang $
+ * 
+ */
+public interface IExpressionConsumer {
+
+    public void setExpression(String expression);
+
+}
