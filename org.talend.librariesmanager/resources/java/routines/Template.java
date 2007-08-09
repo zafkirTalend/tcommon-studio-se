bf9cb6edad1542801bb9b04e3af7d6d7e0ef0fdc
qzhang <qzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-08-09 07:26:20 +0000

    Modification: add correctly comment in the Runtine Template.java ,and user can modify conveniently.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5081 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/Template.java
+++ org.talend.librariesmanager/resources/java/routines/Template.java
@@ -2,7 +2,19 @@
 package routines;
 
 public class __CLASS_NAME__ {
-    public static void helloExemple(String message) {
+    /**
+     * helloExample: not return value, only print "hello" + message.
+     * 
+     * 
+     * {talendTypes} String
+     * 
+     * {Category} User Defined
+     * 
+     * {param} string("world") input: The string need to be printed.
+     * 
+     * {example} helloExemple("world") # hello world !.
+     */
+    public static void helloExample(String message) {
         if (message == null) {
             message = "World";
         }
