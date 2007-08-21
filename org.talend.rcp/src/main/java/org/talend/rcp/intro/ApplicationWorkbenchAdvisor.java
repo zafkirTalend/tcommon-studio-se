2a083fdca0024f67a21a10f267d812f9a7740997
mhirt <mhirt@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-08-21 23:22:01 +0000

    Move Registration Wizard after first project creation Sned more informations through the wizard : projectLanguage, osName, osVersion, javaVersion, totalMemory, ram and nbProcessor for statistics
    git-svn-id: http://talendforge.org/svn/tos/trunk@5262 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp/src/main/java/org/talend/rcp/intro/ApplicationWorkbenchAdvisor.java
+++ org.talend.rcp/src/main/java/org/talend/rcp/intro/ApplicationWorkbenchAdvisor.java
@@ -21,16 +21,30 @@
 // ============================================================================
 package org.talend.rcp.intro;
 
+import java.lang.management.ManagementFactory;
+
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.talend.commons.exception.BusinessException;
+import org.talend.core.CorePlugin;
 import org.talend.core.GlobalServiceRegister;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.language.ECodeLanguage;
 import org.talend.designer.codegen.CodeGeneratorActivator;
 import org.talend.designer.codegen.ICodeGeneratorService;
 import org.talend.designer.runprocess.RunProcessPlugin;
+import org.talend.rcp.Activator;
+import org.talend.repository.i18n.Messages;
+import org.talend.repository.registeruser.RegisterManagement;
+import org.talend.repository.ui.wizards.register.RegisterWizard;
+import org.talend.repository.ui.wizards.register.RegisterWizardDialog;
 
 /**
  * DOC ccarbone class global comment. Detailled comment <br/>
@@ -46,19 +60,23 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
     private static final String PERSPECTIVE_ID = "org.talend.rcp.perspective"; //$NON-NLS-1$
 
+    @Override
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
     }
 
+    @Override
     public void initialize(IWorkbenchConfigurer configurer) {
         super.initialize(configurer);
         configurer.setSaveAndRestore(true);
 
         PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.CLOSE_EDITORS_ON_EXIT, true);
         PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS, false);
-        PlatformUI.getPreferenceStore().setDefault(IWorkbenchPreferenceConstants.DOCK_PERSPECTIVE_BAR, IWorkbenchPreferenceConstants.TOP_RIGHT);
+        PlatformUI.getPreferenceStore().setDefault(IWorkbenchPreferenceConstants.DOCK_PERSPECTIVE_BAR,
+                IWorkbenchPreferenceConstants.TOP_RIGHT);
     }
 
+    @Override
     public String getInitialWindowPerspectiveId() {
         return PERSPECTIVE_ID;
     }
@@ -78,9 +96,56 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
     @Override
     public void postStartup() {
         super.postStartup();
+        // Start Code Generation Init
         ICodeGeneratorService codeGenService = (ICodeGeneratorService) GlobalServiceRegister.getDefault().getService(
                 ICodeGeneratorService.class);
         codeGenService.initializeTemplates();
+
+        // Start Web Service Registration
+        try {
+            if (!RegisterManagement.isProductRegistered()) {
+                RegisterWizard registerWizard = new RegisterWizard();
+                Shell shell = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell();
+                WizardDialog dialog = new RegisterWizardDialog(shell, registerWizard);
+                dialog.setTitle(Messages.getString("RegisterWizard.windowTitle")); //$NON-NLS-1$
+                if (dialog.open() == WizardDialog.OK) {
+
+                    // project language
+                    RepositoryContext repositoryContext = (RepositoryContext) CorePlugin.getContext().getProperty(
+                            Context.REPOSITORY_CONTEXT_KEY);
+                    ECodeLanguage codeLanguage = repositoryContext.getProject().getLanguage();
+                    String projectLanguage = codeLanguage.getName();
+
+                    // OS
+                    String osName = System.getProperty("os.name");
+                    String osVersion = System.getProperty("os.version");
+
+                    // Java version
+                    String javaVersion = System.getProperty("java.version");
+
+                    // Java Memory
+                    long totalMemory = Runtime.getRuntime().totalMemory();
+
+                    // RAM
+                    com.sun.management.OperatingSystemMXBean composantSystem = (com.sun.management.OperatingSystemMXBean) ManagementFactory
+                            .getOperatingSystemMXBean();
+                    Long memRAM = new Long(composantSystem.getTotalPhysicalMemorySize() / 1024);
+
+                    // CPU
+                    int nbProc = Runtime.getRuntime().availableProcessors();
+
+                    RegisterManagement.register(registerWizard.getEmail(), registerWizard.getCountry(), registerWizard
+                            .isProxyEnabled(), registerWizard.getProxyHost(), registerWizard.getProxyPort(),
+                            org.talend.core.CorePlugin.getDefault().getBundle().getHeaders().get(
+                                    org.osgi.framework.Constants.BUNDLE_VERSION).toString(), projectLanguage, osName,
+                            osVersion, javaVersion, totalMemory, memRAM, nbProc);
+                } else {
+                    RegisterManagement.decrementTry();
+                }
+            }
+        } catch (BusinessException e) {
+            // Do nothing : registration web service error is not a problem
+        }
     }
 
 }
