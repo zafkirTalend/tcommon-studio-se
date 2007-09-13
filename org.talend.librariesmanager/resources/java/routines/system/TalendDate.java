5b523d86b45613b718362f2b5b0f5afac07e1d8b
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-13 17:26:03 +0000

    Java : "parse" and "format" date methods added in TalendDate routines class
    git-svn-id: http://talendforge.org/svn/tos/trunk@5593 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/TalendDate.java
+++ org.talend.librariesmanager/resources/java/routines/system/TalendDate.java
@@ -1,13 +1,64 @@
 package routines;
 
 import java.text.FieldPosition;
+import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Random;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import routines.system.FastDateParser;
 
 public class TalendDate {
 
+    /**
+     * Formats a Date into a date/time string.
+     * @param date the time value to be formatted into a time string.
+     * @return the formatted time string.
+     * 
+     * {talendTypes} String
+     * 
+     * {Category} TalendDate
+     * 
+     * {param} string("yyyy-MM-dd HH:mm:ss")
+     */
+    public synchronized static String format(String pattern, java.util.Date date) {
+        return FastDateParser.getInstance(pattern).format(date);
+    }
+
+    public synchronized static String format(String pattern, java.util.Date date, Locale locale) {
+        return FastDateParser.getInstance(pattern, locale).format(date);
+    }
+    
+    /**
+     * Parses text from the beginning of the given string to produce a date.
+     * The method may not use the entire text of the given string.
+     * <p>
+     * See the {@link #parse(String, ParsePosition)} method for more information
+     * on date parsing.
+     *
+     * @param source A <code>String</code> whose beginning should be parsed.
+     * @return A <code>Date</code> parsed from the string.
+     * @throws ParseException 
+     * @exception ParseException if the beginning of the specified string
+     *            cannot be parsed.
+     * 
+     * {talendTypes} String
+     * 
+     * {Category} TalendDate
+     * 
+     * {param} string("yyyy-MM-dd HH:mm:ss")
+     */
+    public synchronized static Date parse(String pattern, String stringDate) throws ParseException {
+        return FastDateParser.getInstance(pattern).parse(stringDate);
+    }
+    
+    public synchronized static Date parse(String pattern, String stringDate, Locale locale) throws ParseException {
+        return FastDateParser.getInstance(pattern, locale).parse(stringDate);
+    }
+    
     /**
      * getDate : return the current datetime with the given display format format : (optional) string representing the
      * wished format of the date. This string contains fixed strings and variables related to the date. By default, the
@@ -85,5 +136,104 @@ public class TalendDate {
         cal.set(Calendar.MONTH, mm - 1);
         cal.set(Calendar.DAY_OF_MONTH, dd);
         return cal.getTime();
+    }
+    
+    /**
+     * 
+     * Method used for tests only.
+     * @param args
+     */
+    public static void main(String[] args) {
+        final int LOOPS = 100000;
+        final String dateTimeRef_Test1 = "1979-03-23 mars 12:30";
+        Thread test1 = new Thread() {
+            public void run() {
+                Calendar calendar = GregorianCalendar.getInstance();
+                calendar.set(1979, 2, 23, 12, 30, 40);
+                Date dateCalendar = calendar.getTime();
+                for (int i = 0; i < LOOPS; i++) {
+                    String date = TalendDate.format("yyyy-MM-dd MMM HH:mm", dateCalendar);
+//                    System.out.println("Test1:" + date + " # " + dateTimeRef_Test1);
+                    if(!dateTimeRef_Test1.equals(date)) {
+                        throw new IllegalStateException("Test1: Date ref : '" + dateTimeRef_Test1 + "' is different of '" + date + "'");
+                    }
+                }
+                System.out.println("test1 ok");
+            }
+        };
+        final String dateTimeRef_Test2 = "1980-03-23 mars 12:30";
+        Thread test2 = new Thread() {
+            public void run() {
+                Calendar calendar = GregorianCalendar.getInstance();
+                calendar.set(1980, 2, 23, 12, 30, 40);
+                Date dateCalendar = calendar.getTime();
+                for (int i = 0; i < LOOPS; i++) {
+                    String date = TalendDate.format("yyyy-MM-dd MMM HH:mm", dateCalendar);
+//                    System.out.println("Test2:" + date + " # " + dateTimeRef_Test2);
+                    if(!dateTimeRef_Test2.equals(date)) {
+                        throw new IllegalStateException("Test2: Date ref : '" + dateTimeRef_Test2 + "' is different of '" + date + "'");
+                    }
+                }
+                System.out.println("test2 ok");
+            }
+        };
+        
+        final String dateTimeRef_Test3 = "1979-03-23 mars 12:30";
+        Thread test3 = new Thread() {
+            public void run() {
+                Calendar calendar = GregorianCalendar.getInstance();
+                calendar.set(1979, 2, 23, 12, 30, 40);
+                Date dateCalendar = calendar.getTime();
+                for (int i = 0; i < LOOPS; i++) {
+                    String date = TalendDate.format("yyyy-MM-dd MMM HH:mm", dateCalendar, Locale.FRANCE);
+//                    System.out.println("Test3:" + date + " # " + dateTimeRef_Test3);
+                    if(!dateTimeRef_Test3.equals(date)) {
+                        throw new IllegalStateException("Test3: Date ref : '" + dateTimeRef_Test3 + "' is different of '" + date + "'");
+                    }
+                }
+                System.out.println("test3 ok");
+            }
+        };
+        final String dateTimeRef_Test4 = "1980-03-23 Mar 12:30";
+        Thread test4 = new Thread() {
+            public void run() {
+                Calendar calendar = GregorianCalendar.getInstance();
+                calendar.set(1980, 2, 23, 12, 30, 40);
+                Date dateCalendar = calendar.getTime();
+                for (int i = 0; i < LOOPS; i++) {
+                    String date = TalendDate.format("yyyy-MM-dd MMM HH:mm", dateCalendar, Locale.ENGLISH);
+//                    System.out.println("Test4:" + date + " # " + dateTimeRef_Test4);
+                    if(!dateTimeRef_Test4.equals(date)) {
+                        throw new IllegalStateException("Test4: Date ref : '" + dateTimeRef_Test4 + "' is different of '" + date + "'");
+                    }
+                }
+                System.out.println("test4 ok");
+            }
+        };
+        
+        final String dateTimeRef_Test5 = "1979-03-23";
+        Thread test5 = new Thread() {
+            public void run() {
+                Calendar calendar = GregorianCalendar.getInstance();
+                calendar.set(1979, 2, 23, 12, 30, 40);
+                Date dateCalendar = calendar.getTime();
+                for (int i = 0; i < LOOPS; i++) {
+                    String date = TalendDate.format("yyyy-MM-dd", dateCalendar);
+//                    System.out.println("Test5:" + date + " # " + dateTimeRef_Test5);
+                    if(!dateTimeRef_Test5.equals(date)) {
+                        throw new IllegalStateException("Test5: Date ref : '" + dateTimeRef_Test5 + "' is different of '" + date + "'");
+                    }
+                    
+                }
+                System.out.println("test5 ok");
+            }
+        };
+
+        
+        test1.start();
+        test2.start();
+        test3.start();
+        test4.start();
+        test5.start();
     }
 }
--- org.talend.librariesmanager/resources/java/talend/FastDateParser.java
+++ org.talend.librariesmanager/resources/java/talend/FastDateParser.java
@@ -21,158 +21,245 @@
 // ============================================================================
 package routines.system;
 
+import java.util.Locale;
+
 public class FastDateParser {
-	private static java.util.HashMap<String, java.text.DateFormat> cache =
-		new java.util.HashMap<String, java.text.DateFormat>();
-	
-	// Warning : DateFormat objects returned by this method are not thread safe
-	public static java.text.DateFormat getInstance(String pattern) {
-		java.text.DateFormat format = cache.get(pattern);
-		if (format == null) {
-			if (pattern.equals("yyyy-MM-dd")) {
-				format = new DateParser();
-			} else if (pattern.equals("yyyy-MM-dd HH:mm:ss")) {
-				format = new DateTimeParser();
-			} else {
-				format = new java.text.SimpleDateFormat(pattern);
-			}
-			cache.put(pattern, format);
-		}
-		return format;
-	}
-	
-	// Parse and format dates with yyyy-MM-dd format
-	private static class DateParser extends java.text.DateFormat {
-		private int year, month, day;
-		
-		public DateParser() {
-			calendar = java.util.Calendar.getInstance();
-		}
-		
-		public StringBuffer format(java.util.Date date, StringBuffer toAppendTo, java.text.FieldPosition fieldPosition) {
-			calendar.setTime(date);
-			
-			// Year
-			toAppendTo.append(calendar.get(java.util.Calendar.YEAR));
-			while (toAppendTo.length() < 4)
-				toAppendTo.insert(0, "0");
-			toAppendTo.append("-");
-			
-			// Month
-			month = calendar.get(java.util.Calendar.MONTH) + 1;
-			if (month < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(month);
-			toAppendTo.append("-");
-				
-			// Day
-			day = calendar.get(java.util.Calendar.DAY_OF_MONTH);
-			if (day < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(day);
-			
-			return toAppendTo;
-		}
-		
-		public java.util.Date parse(String source, java.text.ParsePosition pos) {
-			int index = 0;
-			try {
-				year = Integer.parseInt(source.substring(0, 4));
-				index = 5;
-				month = Integer.parseInt(source.substring(5, 7)) - 1;
-				index = 8;
-				day = Integer.parseInt(source.substring(8, 10));
-				
-				pos.setIndex(source.length());
-				
-				calendar.clear();
-				calendar.set(year, month, day, 0, 0, 0);
-				return calendar.getTime();
-			} catch (Exception e) {
-				pos.setErrorIndex(index);
-			}
-			return null;
-		}
-	}
-	
-	// Parse dates with yyyy-MM-dd HH:mm:ss format
-	private static class DateTimeParser extends java.text.DateFormat {
-		private int year, month, day, hour, minute, second;
-		
-		public DateTimeParser() {
-			calendar = java.util.Calendar.getInstance();
-		}
 
-		public StringBuffer format(java.util.Date date, StringBuffer toAppendTo, java.text.FieldPosition fieldPosition) {
-			calendar.setTime(date);
-			
-			// Year
-			toAppendTo.append(calendar.get(java.util.Calendar.YEAR));
-			while (toAppendTo.length() < 4)
-				toAppendTo.insert(0, "0");
-			toAppendTo.append("-");
-			
-			// Month
-			month = calendar.get(java.util.Calendar.MONTH) + 1;
-			if (month < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(month);
-			toAppendTo.append("-");
-			
-			// Day
-			day = calendar.get(java.util.Calendar.DAY_OF_MONTH);
-			if (day < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(day);
-			toAppendTo.append(" ");
-			
-			// Hour
-			hour = calendar.get(java.util.Calendar.HOUR);
-			if (hour < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(hour);
-			toAppendTo.append(":");
-			
-			// Minute
-			minute = calendar.get(java.util.Calendar.MINUTE);
-			if (minute < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(minute);
-			toAppendTo.append(":");
-			
-			// Second
-			second = calendar.get(java.util.Calendar.SECOND);
-			if (second < 10)
-				toAppendTo.append("0");
-			toAppendTo.append(second);
-			
-			return toAppendTo;
-		}
-		
-		public java.util.Date parse(String source, java.text.ParsePosition pos) {
-			int index = 0;
-			try {
-				year = Integer.parseInt(source.substring(0, 4));
-				index = 5;
-				month = Integer.parseInt(source.substring(5, 7)) - 1;
-				index = 8;
-				day = Integer.parseInt(source.substring(8, 10));
-				index = 11;
-				hour = Integer.parseInt(source.substring(11, 13));
-				index = 14;
-				minute = Integer.parseInt(source.substring(14, 16));
-				index = 17;
-				second = Integer.parseInt(source.substring(17, 19));
-				
-				pos.setIndex(source.length());
+    private static FastDateParser instance;
 
-				calendar.clear();
-				calendar.set(year, month, day, hour, minute, second);
-				return calendar.getTime();
-			} catch (Exception e) {
-				pos.setErrorIndex(index);
-			}
-			return null;
-		}
-	}
+    public static FastDateParser getInstance() {
+        if (instance == null) {
+            instance = new FastDateParser();
+        }
+        return instance;
+    }
+
+    private FastDateParser() {
+        super();
+    }
+    
+    private static java.util.HashMap<DateFormatKey, java.text.DateFormat> cache = new java.util.HashMap<DateFormatKey, java.text.DateFormat>();
+
+    private static DateFormatKey dateFormatKey = getInstance().new DateFormatKey();
+    
+    // Warning : DateFormat objects returned by this method are not thread safe
+    public static java.text.DateFormat getInstance(String pattern) {
+        return getInstance(pattern, null);
+    }
+
+    public static java.text.DateFormat getInstance(String pattern, Locale locale) {
+        dateFormatKey.pattern = pattern;
+        dateFormatKey.locale = locale;
+        java.text.DateFormat format = cache.get(dateFormatKey);
+        if (format == null) {
+            if (pattern.equals("yyyy-MM-dd")) {
+                format = new DateParser();
+            } else if (pattern.equals("yyyy-MM-dd HH:mm:ss")) {
+                format = new DateTimeParser();
+            } else {
+                if(locale != null) {
+                    format = new java.text.SimpleDateFormat(pattern, locale);
+                } else {
+                    format = new java.text.SimpleDateFormat(pattern);
+                }
+            }
+            cache.put(getInstance().new DateFormatKey(pattern, locale), format);
+        }
+        return format;
+    }
+
+    // Parse and format dates with yyyy-MM-dd format
+    private static class DateParser extends java.text.DateFormat {
+
+        private int year, month, day;
+
+        public DateParser() {
+            calendar = java.util.Calendar.getInstance();
+        }
+
+        public StringBuffer format(java.util.Date date, StringBuffer toAppendTo, java.text.FieldPosition fieldPosition) {
+            calendar.setTime(date);
+
+            // Year
+            toAppendTo.append(calendar.get(java.util.Calendar.YEAR));
+            while (toAppendTo.length() < 4)
+                toAppendTo.insert(0, "0");
+            toAppendTo.append("-");
+
+            // Month
+            month = calendar.get(java.util.Calendar.MONTH) + 1;
+            if (month < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(month);
+            toAppendTo.append("-");
+
+            // Day
+            day = calendar.get(java.util.Calendar.DAY_OF_MONTH);
+            if (day < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(day);
+
+            return toAppendTo;
+        }
+
+        public java.util.Date parse(String source, java.text.ParsePosition pos) {
+            int index = 0;
+            try {
+                year = Integer.parseInt(source.substring(0, 4));
+                index = 5;
+                month = Integer.parseInt(source.substring(5, 7)) - 1;
+                index = 8;
+                day = Integer.parseInt(source.substring(8, 10));
+
+                pos.setIndex(source.length());
+
+                calendar.clear();
+                calendar.set(year, month, day, 0, 0, 0);
+                return calendar.getTime();
+            } catch (Exception e) {
+                pos.setErrorIndex(index);
+            }
+            return null;
+        }
+    }
+
+    // Parse dates with yyyy-MM-dd HH:mm:ss format
+    private static class DateTimeParser extends java.text.DateFormat {
+
+        private int year, month, day, hour, minute, second;
+
+        public DateTimeParser() {
+            calendar = java.util.Calendar.getInstance();
+        }
+
+        public StringBuffer format(java.util.Date date, StringBuffer toAppendTo, java.text.FieldPosition fieldPosition) {
+            calendar.setTime(date);
+
+            // Year
+            toAppendTo.append(calendar.get(java.util.Calendar.YEAR));
+            while (toAppendTo.length() < 4)
+                toAppendTo.insert(0, "0");
+            toAppendTo.append("-");
+
+            // Month
+            month = calendar.get(java.util.Calendar.MONTH) + 1;
+            if (month < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(month);
+            toAppendTo.append("-");
+
+            // Day
+            day = calendar.get(java.util.Calendar.DAY_OF_MONTH);
+            if (day < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(day);
+            toAppendTo.append(" ");
+
+            // Hour
+            hour = calendar.get(java.util.Calendar.HOUR);
+            if (hour < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(hour);
+            toAppendTo.append(":");
+
+            // Minute
+            minute = calendar.get(java.util.Calendar.MINUTE);
+            if (minute < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(minute);
+            toAppendTo.append(":");
+
+            // Second
+            second = calendar.get(java.util.Calendar.SECOND);
+            if (second < 10)
+                toAppendTo.append("0");
+            toAppendTo.append(second);
+
+            return toAppendTo;
+        }
+
+        public java.util.Date parse(String source, java.text.ParsePosition pos) {
+            int index = 0;
+            try {
+                year = Integer.parseInt(source.substring(0, 4));
+                index = 5;
+                month = Integer.parseInt(source.substring(5, 7)) - 1;
+                index = 8;
+                day = Integer.parseInt(source.substring(8, 10));
+                index = 11;
+                hour = Integer.parseInt(source.substring(11, 13));
+                index = 14;
+                minute = Integer.parseInt(source.substring(14, 16));
+                index = 17;
+                second = Integer.parseInt(source.substring(17, 19));
+
+                pos.setIndex(source.length());
+
+                calendar.clear();
+                calendar.set(year, month, day, hour, minute, second);
+                return calendar.getTime();
+            } catch (Exception e) {
+                pos.setErrorIndex(index);
+            }
+            return null;
+        }
+    }
+
+    private class DateFormatKey {
+
+        private String pattern;
+
+        private Locale locale;
+
+        public DateFormatKey() {
+        }
+        
+        public DateFormatKey(String pattern, Locale locale) {
+            this.pattern = pattern;
+            this.locale = locale;
+        }
+        
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Object#hashCode()
+         */
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((this.locale == null) ? 0 : this.locale.hashCode());
+            result = prime * result + ((this.pattern == null) ? 0 : this.pattern.hashCode());
+            return result;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            final DateFormatKey other = (DateFormatKey) obj;
+            if (this.locale == null) {
+                if (other.locale != null)
+                    return false;
+            } else if (!this.locale.equals(other.locale))
+                return false;
+            if (this.pattern == null) {
+                if (other.pattern != null)
+                    return false;
+            } else if (!this.pattern.equals(other.pattern))
+                return false;
+            return true;
+        }
+
+    }
+
 }
