484a07d09cf069d00fb6152f3185e2bcaab0af1f
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-14 14:46:26 +0000

    Integration of TalendDate routine in Talend improved to be used.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5598 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.librariesmanager/resources/java/talend/LocaleProvider.java
@@ -0,0 +1,84 @@
+package routines.system;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+
+/**
+ * DOC amaumont class global comment. Detailled comment <br/>
+ * 
+ */
+public class LocaleProvider {
+
+    private static Map<String, Locale> cache;
+
+    public static void main(String[] args) {
+
+        System.out.println("FR => " + getLocale("FR"));
+        System.out.println("fr => " + getLocale("fr"));
+        System.out.println("tw => " + getLocale("tw"));
+        System.out.println("TW => " + getLocale("TW"));
+        System.out.println("zh => " + getLocale("zh"));
+        System.out.println("ZH => " + getLocale("ZH"));
+
+    }
+
+    public static Locale getLocale(String languageOrCountyCode) {
+        if (cache == null) {
+            initCache();
+        }
+        Locale locale = null;
+        if (languageOrCountyCode != null) {
+            locale = cache.get(languageOrCountyCode.toLowerCase());
+        }
+        if (locale == null) {
+            locale = Locale.ENGLISH;
+        }
+        return locale;
+    }
+
+    /**
+     * DOC amaumont Comment method "initCache".
+     */
+    private static void initCache() {
+        cache = new HashMap<String, Locale>();
+        Locale[] availableLocales = Locale.getAvailableLocales();
+        for (int i = 0; i < availableLocales.length; i++) {
+            Locale locale = availableLocales[i];
+            String country = locale.getCountry();
+            String language = locale.getLanguage();
+            String key = null;
+            if (country.length() > 0) {
+                key = country;
+            } else if (language.length() > 0) {
+                key = language;
+            }
+            if (key != null) {
+                cache.put(key.toLowerCase(), locale);
+            }
+        }
+    }
+
+}
