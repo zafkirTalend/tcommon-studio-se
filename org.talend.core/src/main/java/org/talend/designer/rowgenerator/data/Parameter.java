87ed46b776f22fe15722098ba81ad0b601e7cb15
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-20 08:11:12 +0000

    modification: remove the modification that store variables from EB due to the mechanism need to be changed.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5745 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/designer/rowgenerator/data/Parameter.java
+++ org.talend.core/src/main/java/org/talend/designer/rowgenerator/data/Parameter.java
@@ -23,173 +23,150 @@ package org.talend.designer.rowgenerator.data;
 
 import java.util.List;
 
-import org.talend.commons.ui.swt.tableviewer.celleditor.IDataBean;
 import org.talend.core.i18n.Messages;
 import org.talend.expressionbuilder.test.shadow.Variable;
 
 /**
- * class global comment. Detailled comment <br/> $Id: Parameter.java,v 1.4 2007/02/02 03:04:21 pub Exp $
+ * class global comment. Detailled comment <br/> $Id: Parameter.java,v 1.4
+ * 2007/02/02 03:04:21 pub Exp $
  */
-public abstract class Parameter implements IDataBean {
+public abstract class Parameter {
 
-    private List<Variable> vars;
+	/**
+	 * @uml.property name="name"
+	 */
+	protected String name = ""; //$NON-NLS-1$
 
-    /**
-     * @uml.property name="name"
-     */
-    protected String name = ""; //$NON-NLS-1$
+	/**
+	 * Getter of the property <tt>name</tt>.
+	 * 
+	 * @return Returns the name.
+	 * @uml.property name="name"
+	 */
+	public String getName() {
+		return this.name;
+	}
 
-    /**
-     * Getter of the property <tt>name</tt>.
-     * 
-     * @return Returns the name.
-     * @uml.property name="name"
-     */
-    public String getName() {
-        return this.name;
-    }
+	/**
+	 * Setter of the property <tt>name</tt>.
+	 * 
+	 * @param name
+	 *            The name to set.
+	 * @uml.property name="name"
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * Setter of the property <tt>name</tt>.
-     * 
-     * @param name The name to set.
-     * @uml.property name="name"
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * @uml.property name="value"
+	 */
+	protected String value = ""; //$NON-NLS-1$
 
-    /**
-     * @uml.property name="value"
-     */
-    protected String value = ""; //$NON-NLS-1$
+	/**
+	 * Getter of the property <tt>value</tt>.
+	 * 
+	 * @return Returns the value.
+	 * @uml.property name="value"
+	 */
+	public String getValue() {
+		int index = this.value.indexOf("{variables}");
+		if (index != -1) {
+			return this.value.substring(0, index);
+		} else {
+			return this.value;
+		}
+	}
 
-    /**
-     * Getter of the property <tt>value</tt>.
-     * 
-     * @return Returns the value.
-     * @uml.property name="value"
-     */
-    public String getValue() {
-        int index = this.value.indexOf("{variables}");
-        if (index != -1) {
-            return this.value.substring(0, index);
-        } else {
-            return this.value;
-        }
-    }
+	/**
+	 * Setter of the property <tt>value</tt>.
+	 * 
+	 * @param value
+	 *            The value to set.
+	 * @uml.property name="value"
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    /**
-     * Setter of the property <tt>value</tt>.
-     * 
-     * @param value The value to set.
-     * @uml.property name="value"
-     */
-    public void setValue(String value) {
-        this.value = value;
-    }
+	/**
+	 * @uml.property name="comment"
+	 */
+	protected String comment = ""; //$NON-NLS-1$
 
-    /**
-     * @uml.property name="comment"
-     */
-    protected String comment = ""; //$NON-NLS-1$
+	/**
+	 * Getter of the property <tt>comment</tt>.
+	 * 
+	 * @return Returns the comment.
+	 * @uml.property name="comment"
+	 */
+	public String getComment() {
+		return this.comment;
+	}
 
-    /**
-     * Getter of the property <tt>comment</tt>.
-     * 
-     * @return Returns the comment.
-     * @uml.property name="comment"
-     */
-    public String getComment() {
-        return this.comment;
-    }
+	/**
+	 * Setter of the property <tt>comment</tt>.
+	 * 
+	 * @param comment
+	 *            The comment to set.
+	 * @uml.property name="comment"
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 
-    /**
-     * Setter of the property <tt>comment</tt>.
-     * 
-     * @param comment The comment to set.
-     * @uml.property name="comment"
-     */
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+	/**
+	 * @uml.property name="type"
+	 */
+	protected ParameterType type;
 
-    /**
-     * @uml.property name="type"
-     */
-    protected ParameterType type;
+	/**
+	 * Getter of the property <tt>type</tt>.
+	 * 
+	 * @return Returns the type.
+	 * @uml.property name="type"
+	 */
+	public ParameterType getType() {
+		return this.type;
+	}
 
-    /**
-     * Getter of the property <tt>type</tt>.
-     * 
-     * @return Returns the type.
-     * @uml.property name="type"
-     */
-    public ParameterType getType() {
-        return this.type;
-    }
+	/**
+	 * Setter of the property <tt>type</tt>.
+	 * 
+	 * @param type
+	 *            The type to set.
+	 * @uml.property name="type"
+	 */
+	public void setType(ParameterType type) {
+		this.type = type;
+	}
 
-    /**
-     * Setter of the property <tt>type</tt>.
-     * 
-     * @param type The type to set.
-     * @uml.property name="type"
-     */
-    public void setType(ParameterType type) {
-        this.type = type;
-    }
+	/**
+	 * Sets the properties from the input String.
+	 */
+	public void parseProperties(String str) {
 
-    /**
-     * Sets the properties from the input String.
-     */
-    public void parseProperties(String str) {
+	}
 
-    }
+	/**
+	 * qzhang Comment method "sameParameterAs".
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public abstract boolean sameParameterAs(Parameter obj);
 
-    /**
-     * qzhang Comment method "sameParameterAs".
-     * 
-     * @param obj
-     * @return
-     */
-    public abstract boolean sameParameterAs(Parameter obj);
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(Messages.getString("Parameter.Name") + name).append(
+				Messages.getString("Parameter.Value") + this.getValue())
+				.append(
+						Messages.getString("Parameter.Comment")
+								+ this.getComment()).append(
+						Messages.getString("Parameter.Type") + this.getType());
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Messages.getString("Parameter.Name") + name).append(
-                Messages.getString("Parameter.Value") + this.getValue()).append(
-                Messages.getString("Parameter.Comment") + this.getComment()).append(
-                Messages.getString("Parameter.Type") + this.getType());
-
-        return sb.toString();
-    }
-
-    /**
-     * Getter for vars.
-     * 
-     * @return the vars
-     */
-    public List<Variable> getVars() {
-        return this.vars;
-    }
-
-    /**
-     * Sets the vars.
-     * 
-     * @param vars the vars to set
-     */
-    public void setVars(List<Variable> vars) {
-        this.vars = vars;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.talend.commons.ui.swt.tableviewer.celleditor.IDataBean#getBeanData()
-     */
-    public Object getBeanData() {
-        return getVars();
-    }
+		return sb.toString();
+	}
 
 }
