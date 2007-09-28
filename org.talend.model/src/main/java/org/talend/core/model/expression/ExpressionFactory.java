6c53146c2adb28f3b7a42b50c9d75b4306374296
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-28 03:38:46 +0000

    feature 1182 modified: add talend type and nullable field in emf for EB varialbes persistance.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5981 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.model/src/main/java/org/talend/core/model/expression/ExpressionFactory.java
+++ org.talend.model/src/main/java/org/talend/core/model/expression/ExpressionFactory.java
@@ -1,7 +1,6 @@
 /**
- * <copyright>
- * </copyright>
- *
+ * <copyright> </copyright>
+ * 
  * $Id$
  */
 package org.talend.core.model.expression;
@@ -9,26 +8,23 @@ package org.talend.core.model.expression;
 import org.eclipse.emf.ecore.EFactory;
 
 /**
- * <!-- begin-user-doc -->
- * The <b>Factory</b> for the model.
- * It provides a create method for each non-abstract class of the model.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> The <b>Factory</b> for the model. It provides a create method for each non-abstract class of
+ * the model. <!-- end-user-doc -->
  * @see org.talend.core.model.expression.ExpressionPackage
  * @generated
  */
 public interface ExpressionFactory extends EFactory {
+
     /**
      * The singleton instance of the factory.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
      * @generated
      */
     ExpressionFactory eINSTANCE = org.talend.core.model.expression.impl.ExpressionFactoryImpl.init();
 
     /**
      * Returns a new object of class '<em>EMF Expression</em>'.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
      * @return a new object of class '<em>EMF Expression</em>'.
      * @generated
      */
@@ -36,8 +32,7 @@ public interface ExpressionFactory extends EFactory {
 
     /**
      * Returns a new object of class '<em>EMF Variable</em>'.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
      * @return a new object of class '<em>EMF Variable</em>'.
      * @generated
      */
@@ -45,11 +40,10 @@ public interface ExpressionFactory extends EFactory {
 
     /**
      * Returns the package supported by this factory.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
+     * <!-- begin-user-doc --> <!-- end-user-doc -->
      * @return the package supported by this factory.
      * @generated
      */
     ExpressionPackage getExpressionPackage();
 
-} //ExpressionFactory
+} // ExpressionFactory
