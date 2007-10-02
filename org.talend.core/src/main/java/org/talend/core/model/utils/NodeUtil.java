c09496b89a612a6ac4fdff1879b1043fc0364481
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-10-02 04:50:12 +0000

    bug 2154 fixed: correct ThenRun order (modification of the sort order)
    git-svn-id: http://talendforge.org/svn/tos/trunk@6054 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/utils/NodeUtil.java
+++ org.talend.core/src/main/java/org/talend/core/model/utils/NodeUtil.java
@@ -58,7 +58,7 @@ public class NodeUtil {
                     if (lineStyle == EConnectionType.FLOW_MAIN || lineStyle == EConnectionType.FLOW_MERGE) {
                         return -1;
                     } else {
-                        return 1;
+                        return 0;
                     }
                 }
             });
