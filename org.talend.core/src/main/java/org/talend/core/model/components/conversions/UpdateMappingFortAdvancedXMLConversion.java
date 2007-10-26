71c090569590f843a56cc12dcda292fc5af7cb58
xzhang <xzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-10-26 05:27:30 +0000

    bug 2298 fixed: add a migration task in org.talend.repository plugin to migrate 2.1 version to 2.2.
    git-svn-id: http://talendforge.org/svn/tos/trunk@6565 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/core/model/components/conversions/UpdateMappingFortAdvancedXMLConversion.java
@@ -0,0 +1,212 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.core.model.components.conversions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementValueType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.TalendFileFactory;
+
+/**
+ * DOC s class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 1 2007-10-25 17:06:40 +0000 xzhang $
+ */
+public class UpdateMappingFortAdvancedXMLConversion implements IComponentConversion {
+
+    private static final String OLD_ROOT_TABLE_NAME = "ROOT_TAGS";
+
+    private static final String OLD_GROUPING_TABLE_NAME = "GROUPING";
+
+    private static final String OLD_MAPPING_TABLE_NAME = "MAPPING";
+
+    private static final String OLD_TAG = "TAG";
+
+    private static final String OLD_LABEL = "LABEL";
+
+    private static final String OLD_COLUMN = "COLUMN";
+
+    private static final String OLD_ATTRIBUTE = "ATTRIBUTE";
+
+    private static final String OLD_DEPTH = "DEPTH";
+
+    private static final String NEW_ROOT_NAME = "ROOT";
+
+    private static final String NEW_GROUP_NAME = "GROUP";
+
+    private static final String NEW_LOOP_NAME = "LOOP";
+
+    private static final String NEW_PATH = "PATH";
+
+    private static final String NEW_COLUMN = "COLUMN";
+
+    private static final String NEW_ATTRIBUTE = "ATTRIBUTE";
+
+    private static final String NEW_VALUE = "VALUE";
+
+    public void transform(NodeType node) {
+        ElementParameterType eleParaRoot = ComponentUtilities.getNodeProperty(node, OLD_ROOT_TABLE_NAME);
+        ElementParameterType eleParaGroup = ComponentUtilities.getNodeProperty(node, OLD_GROUPING_TABLE_NAME);
+        ElementParameterType eleParaMap = ComponentUtilities.getNodeProperty(node, OLD_MAPPING_TABLE_NAME);
+        if (eleParaRoot == null || eleParaGroup == null || eleParaMap == null) {
+            return;
+        }
+        
+        StringBuffer path = new StringBuffer();
+
+        // count the values of "ROOT_TAGS" to "ROOT".
+        EList elist = eleParaRoot.getElementValue();
+        List<ElementValueType> newValueListForRoot = new ArrayList<ElementValueType>();
+        for (Object value : elist) {
+            ElementValueType eValue = (ElementValueType) value;
+            if (eValue.getElementRef().equals(OLD_TAG)) {
+                path.append("/").append(eValue.getValue());
+                addOneRecord(newValueListForRoot, path.toString(), "", "", "");
+            }
+        }
+
+        // count the values of "GROUPING" to "GROUP".
+        elist = eleParaGroup.getElementValue();
+        List<ElementValueType> newValueListForGroup = new ArrayList<ElementValueType>();
+        for (int i = 0; i < elist.size(); i += 3) {
+            String pathValue = "";
+            String columnValue = "";
+            String attributeValue = "";
+            for (Object value : elist.subList(i, i + 3)) {
+                ElementValueType eValue = (ElementValueType) value;
+                if (eValue.getElementRef().equals(OLD_LABEL)) {
+                    pathValue = eValue.getValue();
+                } else if (eValue.getElementRef().equals(OLD_COLUMN)) {
+                    columnValue = eValue.getValue();
+                } else if (eValue.getElementRef().equals(OLD_ATTRIBUTE)) {
+                    attributeValue = eValue.getValue();
+                }
+            }
+            if (attributeValue.equals("false")) {
+                pathValue = path.append("/").append(pathValue).toString();
+            }
+            addOneRecord(newValueListForGroup, pathValue, columnValue, "", attributeValue);
+        }
+
+        // count the values of "MAPPING" to "LOOP".
+        elist = eleParaMap.getElementValue();
+        List<ElementValueType> newValueListForLoop = new ArrayList<ElementValueType>();
+        ArrayList<String> pathStrs = new ArrayList<String>();
+        for (int i = 0; i < elist.size(); i += 4) {
+            String pathValue = "";
+            String columnValue = "";
+            String attributeValue = "";
+            String depthValue = "";
+            // loop the elist in 4, it's a record of "MAPPING"
+            for (Object value : elist.subList(i, i + 4)) {
+                ElementValueType eValue = (ElementValueType) value;
+                if (eValue.getElementRef().equals(OLD_LABEL)) {
+                    pathValue = eValue.getValue();
+                } else if (eValue.getElementRef().equals(OLD_COLUMN)) {
+                    columnValue = eValue.getValue();
+                } else if (eValue.getElementRef().equals(OLD_ATTRIBUTE)) {
+                    attributeValue = eValue.getValue();
+                } else if (eValue.getElementRef().equals(OLD_DEPTH)) {
+                    depthValue = eValue.getValue();
+                }
+            }
+            if (attributeValue.equals("false")) {
+                pathValue = getPathByDepth(pathStrs, pathValue, Integer.valueOf(depthValue));
+                pathValue = path.toString() + pathValue;
+            }
+            addOneRecord(newValueListForLoop, pathValue, columnValue, "", attributeValue);
+        }
+        
+        // add new Property
+        ComponentUtilities.addNodeProperty(node, NEW_ROOT_NAME, "TABLE");
+        ComponentUtilities.setNodeProperty(node, NEW_ROOT_NAME, newValueListForRoot);
+        ComponentUtilities.addNodeProperty(node, NEW_GROUP_NAME, "TABLE");
+        ComponentUtilities.setNodeProperty(node, NEW_GROUP_NAME, newValueListForGroup);
+        ComponentUtilities.addNodeProperty(node, NEW_LOOP_NAME, "TABLE");
+        ComponentUtilities.setNodeProperty(node, NEW_LOOP_NAME, newValueListForLoop);
+
+        // remove old table
+        ComponentUtilities.removeNodeProperty(node, OLD_ROOT_TABLE_NAME);
+        ComponentUtilities.removeNodeProperty(node, OLD_GROUPING_TABLE_NAME);
+        ComponentUtilities.removeNodeProperty(node, OLD_MAPPING_TABLE_NAME);
+    }
+
+    /**
+     * change the depth to path DOC s Comment method "getPathByDepth".
+     * 
+     * @param pathStrs store the pathValue
+     * @param pathValue the LABEL of MAPPING table
+     * @param depthValue the DEPTH of MAPPING table
+     * @return the path in MAPPING table
+     */
+    private String getPathByDepth(ArrayList<String> pathStrs, String pathValue, int depthValue) {
+        String result = "";
+        if (depthValue <= pathStrs.size()) {
+            pathStrs.set(depthValue - 1, pathValue);
+        } else {
+            pathStrs.add(pathValue);
+        }
+
+        for (int i = 0; i < depthValue; i++) {
+            result += "/" + pathStrs.get(i);
+        }
+        return result;
+    }
+
+    /**
+     * 
+     * DOC s Comment method "addOneRecord".
+     * 
+     * @param values
+     * @param pathValue
+     * @param columnValue
+     * @param value
+     * @param attributeValue
+     */
+    private void addOneRecord(List<ElementValueType> values, String pathValue, String columnValue, String value,
+            String attributeValue) {
+        ElementValueType eValue = TalendFileFactory.eINSTANCE.createElementValueType();
+        eValue.setElementRef(NEW_PATH);
+        eValue.setValue(pathValue);
+        values.add(eValue);
+
+        eValue = TalendFileFactory.eINSTANCE.createElementValueType();
+        eValue.setElementRef(NEW_COLUMN);
+        eValue.setValue(columnValue);
+        values.add(eValue);
+
+        eValue = TalendFileFactory.eINSTANCE.createElementValueType();
+        eValue.setElementRef(NEW_VALUE);
+        eValue.setValue(value);
+        values.add(eValue);
+
+        eValue = TalendFileFactory.eINSTANCE.createElementValueType();
+        eValue.setElementRef(NEW_ATTRIBUTE);
+        eValue.setValue(attributeValue);
+        values.add(eValue);
+    }
+}
