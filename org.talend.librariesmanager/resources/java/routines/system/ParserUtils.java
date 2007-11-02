9d9ccb1d9912002c9ec12cd988d664bcde7d046c
mhirt <mhirt@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-11-02 22:58:32 +0000

    Clarify Java error messages generated by ParserUtils
    git-svn-id: http://talendforge.org/svn/tos/trunk@6737 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/ParserUtils.java
+++ org.talend.librariesmanager/resources/java/routines/system/ParserUtils.java
@@ -21,18 +21,20 @@
 // ============================================================================
 package routines.system;
 
-import java.util.List; 
-import java.util.ArrayList; 
+import java.util.List;
+import java.util.ArrayList;
 
 public class ParserUtils {
-    public static List parseTo_List(String s) { 
-                  if (s != null) { 
-                                  List list = new ArrayList(); 
-                                  list.add(s); 
-                                  return list; 
-                          } 
-                  return null; 
-              } 
+
+    public static List parseTo_List(String s) {
+        if (s != null) {
+            List list = new ArrayList();
+            list.add(s);
+            return list;
+        }
+        return null;
+    }
+
     public static char parseTo_Character(String s) {
         return (s != null) ? s.charAt(0) : null;
     }
@@ -40,7 +42,7 @@ public class ParserUtils {
     public static char parseTo_char(String s) {
         return (s != null) ? s.charAt(0) : null;
     }
-    
+
     public static byte parseTo_Byte(String s) {
         return Byte.decode(s).byteValue();
     }
@@ -48,56 +50,96 @@ public class ParserUtils {
     public static byte parseTo_byte(String s) {
         return Byte.decode(s).byteValue();
     }
-    
+
     public static Double parseTo_Double(String s) {
-        return Double.parseDouble(s);
+        try {
+            return Double.parseDouble(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static double parseTo_double(String s) {
-        return Double.parseDouble(s);
+        try {
+            return Double.parseDouble(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static float parseTo_float(String s) {
-        return Float.parseFloat(s);
+        try {
+            return Float.parseFloat(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static Float parseTo_Float(String s) {
-        return Float.parseFloat(s);
+        try {
+            return Float.parseFloat(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static int parseTo_int(String s) {
-        return Integer.parseInt(s);
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static Integer parseTo_Integer(String s) {
-        return Integer.parseInt(s);
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static short parseTo_short(String s) {
-        return Short.parseShort(s);
+        try {
+            return Short.parseShort(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static Short parseTo_Short(String s) {
-        return Short.parseShort(s);
+        try {
+            return Short.parseShort(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static long parseTo_long(String s) {
-        return Long.parseLong(s);
+        try {
+            return Long.parseLong(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
 
     public static Long parseTo_Long(String s) {
-        return Long.parseLong(s);
+        try {
+            return Long.parseLong(s);
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException("NumberFormatException " + nfe.getMessage());
+        }
     }
-    
+
     public static Boolean parseTo_Boolean(String s) {
-        if(s.equals("1")){
+        if (s.equals("1")) {
             return Boolean.parseBoolean("true");
         }
         return Boolean.parseBoolean(s);
     }
-    
+
     public static boolean parseTo_boolean(String s) {
-        if(s.equals("1")){
+        if (s.equals("1")) {
             return Boolean.parseBoolean("true");
         }
         return Boolean.parseBoolean(s);
@@ -107,12 +149,12 @@ public class ParserUtils {
         return s;
     }
 
-    public static java.util.Date parseTo_Date(String s, String pattern) {        
-        //check the parameter for supporting " ","2007-09-13"," 2007-09-13 "
+    public static java.util.Date parseTo_Date(String s, String pattern) {
+        // check the parameter for supporting " ","2007-09-13"," 2007-09-13 "
         if (s != null) {
             s = s.trim();
         }
-        if(s==null || s.length() == 0) {        
+        if (s == null || s.length() == 0) {
             return null;
         }
         java.util.Date date = null;
@@ -124,8 +166,8 @@ public class ParserUtils {
         }
         return date;
     }
-    
-    public static java.util.Date parseTo_Date(java.util.Date date, String pattern){
+
+    public static java.util.Date parseTo_Date(java.util.Date date, String pattern) {
         // java.text.SimpleDateFormat simpleDateFormat = new java.text.SimpleDateFormat(pattern);
         // java.util.Date date = null;
         // try {
@@ -133,7 +175,7 @@ public class ParserUtils {
         // } catch (java.text.ParseException e) {
         // e.printStackTrace();
         // System.err.println("Current string to parse '" + s + "'");
-        //        }
+        // }
         return date;
     }
 }
