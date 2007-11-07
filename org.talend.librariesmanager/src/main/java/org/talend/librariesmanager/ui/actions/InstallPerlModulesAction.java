4dec4624b3ff6c7c3f24f35e4e8892368f5e20bd
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-11-07 16:57:18 +0000

    Bug fixed: try to fix a bad character 3
    git-svn-id: http://talendforge.org/svn/tos/trunk@6826 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/ui/actions/InstallPerlModulesAction.java
+++ org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/ui/actions/InstallPerlModulesAction.java
@@ -1,4 +1,25 @@
-﻿package org.talend.librariesmanager.ui.actions;
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.librariesmanager.ui.actions;
 
 import java.io.IOException;
 import java.util.ArrayList;
