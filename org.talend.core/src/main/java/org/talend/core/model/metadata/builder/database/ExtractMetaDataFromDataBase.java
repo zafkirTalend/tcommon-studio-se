ce3b13ca94c878a556d02e259cc0de59c2040db9
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-11-13 07:09:43 +0000

    Modification: resolve a problem when trying to resolve unique keys with a table name with specific characters.
    git-svn-id: http://talendforge.org/svn/tos/trunk@6948 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataFromDataBase.java
+++ org.talend.core/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataFromDataBase.java
@@ -5,7 +5,7 @@
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-// You should have received a copy of the  agreement
+// You should have received a copy of the agreement
 // along with this program; if not, write to Talend SA
 // 9 rue Pages 92150 Suresnes, France
 //   
@@ -352,8 +352,8 @@ public class ExtractMetaDataFromDataBase {
             Connection connection) {
         if (connection instanceof com.mysql.jdbc.Connection) {// MySql
             try {
-                PreparedStatement statement = ExtractMetaDataUtils.conn.prepareStatement("SHOW INDEX FROM "
-                        + medataTable.getLabel() + " WHERE Non_unique=0 AND Key_name != \'PRIMARY\';");
+                PreparedStatement statement = ExtractMetaDataUtils.conn.prepareStatement("SHOW INDEX FROM `"
+                        + medataTable.getLabel() + "` WHERE Non_unique=0 AND Key_name != \'PRIMARY\';");
                 ResultSet keys = null;
                 if (statement.execute()) {
                     keys = statement.getResultSet();
