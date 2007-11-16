f5486b450a17584e17f60d2d4e4508ca23dcd916
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-11-16 02:40:17 +0000

    Modification: correct some problems of header to avoid checkstyle errors.
    git-svn-id: http://talendforge.org/svn/tos/trunk@7051 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.migrationTool/src/org/talend/migrationtool/MigrationToolPlugin.java
+++ org.talend.migrationTool/src/org/talend/migrationtool/MigrationToolPlugin.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.migrationtool;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -8,43 +20,45 @@ import org.osgi.framework.BundleContext;
  */
 public class MigrationToolPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.talend.migrationTool"; //$NON-NLS-1$
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.talend.migrationTool"; //$NON-NLS-1$
 
-	// The shared instance
-	private static MigrationToolPlugin plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public MigrationToolPlugin() {
-		plugin = this;
-	}
+    // The shared instance
+    private static MigrationToolPlugin plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+    /**
+     * The constructor
+     */
+    public MigrationToolPlugin() {
+        plugin = this;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static MigrationToolPlugin getDefault() {
-		return plugin;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static MigrationToolPlugin getDefault() {
+        return plugin;
+    }
 
 }
