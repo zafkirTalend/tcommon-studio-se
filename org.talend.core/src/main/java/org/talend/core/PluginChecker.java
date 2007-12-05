6202e1120d34589f0f5c9cffe16a5c1d9240f502
bqian <bqian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-12-05 06:31:03 +0000

    Modification: Add a plugin-checker tools.
    git-svn-id: http://talendforge.org/svn/tos/trunk@7396 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/core/PluginChecker.java
@@ -0,0 +1,60 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core;
+
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+/**
+ * This class can check whether some specific plugins are loaded or not. <br/>
+ * 
+ */
+public class PluginChecker {
+
+    private static final String DOCUMENTATION_PLUGIN_ID = "org.talend.repository.documentation";
+
+    private static final String SNIPPET_PLUGIN_ID = "org.talend.snippets";
+
+    /**
+     * Check if specific plug-in is loaded.
+     * 
+     * @return isLoaded
+     */
+    public static boolean isPluginLoaded(String pluginID) {
+        boolean isLoaded = true;
+        Bundle bundle = Platform.getBundle(pluginID);
+        if (bundle == null || (bundle != null && bundle.getState() == Bundle.UNINSTALLED)) {
+            isLoaded = false;
+        }
+        return isLoaded;
+    }
+
+    /**
+     * Checks if documentation plug-in is loaded.
+     * 
+     * @return isLoaded
+     */
+    public static boolean isDocumentationPluginLoaded() {
+        return isPluginLoaded(DOCUMENTATION_PLUGIN_ID);
+    }
+
+    /**
+     * Check if snippets plug-in is loaded.
+     * 
+     * @return isLoaded
+     */
+    public static boolean isSnippetsPluginLoaded() {
+        return isPluginLoaded(SNIPPET_PLUGIN_ID);
+    }
+
+}
