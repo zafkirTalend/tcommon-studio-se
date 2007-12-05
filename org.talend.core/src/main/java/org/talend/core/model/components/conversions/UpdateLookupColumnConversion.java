2b93f378d0db7d70d1df619ab71e8d3ddcbfd666
qwei <qwei@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-12-05 07:10:34 +0000

    Minor 2514 fixed:
    change the class check style header.
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@7399 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/components/conversions/UpdateLookupColumnConversion.java
+++ org.talend.core/src/main/java/org/talend/core/model/components/conversions/UpdateLookupColumnConversion.java
@@ -1,22 +1,13 @@
 // ============================================================================
 //
-// Talend Community Edition
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
 //
-// Copyright (C) 2006 Talend – www.talend.com
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
 package org.talend.core.model.components.conversions;
@@ -30,11 +21,11 @@ import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
  * $Id: talend.epf 1 2006-09-29 17:06:40 +0000 (ææäº, 29 ä¹æ 2006) nrousseau $
  * 
  */
-public class UpdateLookupColumnCoversion implements IComponentConversion {
+public class UpdateLookupColumnConversion implements IComponentConversion {
 
     private String propertyToModify;
 
-    public UpdateLookupColumnCoversion(String propertyToModify) {
+    public UpdateLookupColumnConversion(String propertyToModify) {
         super();
         this.propertyToModify = propertyToModify;
     }
