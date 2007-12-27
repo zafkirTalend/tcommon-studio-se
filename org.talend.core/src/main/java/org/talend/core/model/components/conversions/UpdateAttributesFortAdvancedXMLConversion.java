2d4a3ea49b94fbd8180937f6c722ca0341b50c3c
xzhang <xzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-12-27 07:18:48 +0000

    bug 2675 fixed: Support multi element defined with the same path. Add "main" and "branch" type to attributes. Add a migration task to change the old version jobs.
    git-svn-id: http://talendforge.org/svn/tos/trunk@7785 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core/src/main/java/org/talend/core/model/components/conversions/UpdateAttributesFortAdvancedXMLConversion.java
@@ -0,0 +1,88 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.components.conversions;
+
+import org.eclipse.emf.common.util.EList;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementValueType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+
+/**
+ * DOC s class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 1 2007-10-25 17:06:40 +0000 xzhang $
+ */
+public class UpdateAttributesFortAdvancedXMLConversion implements IComponentConversion {
+
+    private static final String ROOT_NAME = "ROOT";
+
+    private static final String GROUP_NAME = "GROUP";
+
+    private static final String LOOP_NAME = "LOOP";
+
+    private static final String PATH = "PATH";
+
+    private static final String ATTRIBUTE = "ATTRIBUTE";
+
+    public void transform(NodeType node) {
+        ElementParameterType eleParaRoot = ComponentUtilities.getNodeProperty(node, ROOT_NAME);
+        ElementParameterType eleParaGroup = ComponentUtilities.getNodeProperty(node, GROUP_NAME);
+        ElementParameterType eleParaLoop = ComponentUtilities.getNodeProperty(node, LOOP_NAME);
+        if (eleParaRoot == null || eleParaGroup == null || eleParaLoop == null) {
+            return;
+        }
+
+        String loopRootPath = null;
+        EList elist = eleParaLoop.getElementValue();
+        if (elist.size() > 0) {
+            ElementValueType ePath = (ElementValueType) elist.get(0);
+            loopRootPath = ePath.getValue();
+        }
+        modifyAttribute(eleParaRoot, loopRootPath);
+        modifyAttribute(eleParaGroup, loopRootPath);
+        modifyAttribute(eleParaLoop, loopRootPath);
+    }
+
+    /**
+     * DOC s Comment method "modifyAttribute".
+     * 
+     * @param eleParaGroup
+     * @param loopRootPath
+     * @param path
+     * @return
+     */
+    private void modifyAttribute(ElementParameterType eleParaGroup, String loopRootPath) {
+        String path = null;
+        EList elist = eleParaGroup.getElementValue();
+        for (int i = 0; i < elist.size(); i += 4) {
+            for (Object value : elist.subList(i, i + 4)) {
+                ElementValueType eValue = (ElementValueType) value;
+                if (eValue.getElementRef().equals(ATTRIBUTE)) {
+                    String attri = eValue.getValue();
+                    if (attri.equals("false") || attri.equals("")) {
+                        if ((loopRootPath != null && loopRootPath.startsWith(path)) || (loopRootPath == null && i == 0)) {
+                            eValue.setValue("main");
+                        } else {
+                            eValue.setValue("branch");
+                        }
+                    } else if (attri.equals("true")) {
+                        eValue.setValue("attri");
+                    }
+                } else if (eValue.getElementRef().equals(PATH)) {
+                    path = eValue.getValue();
+                }
+            }
+        }
+    }
+}
