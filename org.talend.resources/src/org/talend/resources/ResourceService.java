3d3f72dc1e98d8b745e99ab09b1d86db1d4dfa80
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-02-21 03:23:56 +0000

    major 3132 added: the libraries under plugin org.talend.resorces need to copy to lib folder of tos automatically.
    git-svn-id: http://talendforge.org/svn/tos/trunk@11484 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.resources/src/org/talend/resources/ResourceService.java
@@ -0,0 +1,69 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend ¨C www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.resources;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+import org.talend.commons.exception.RuntimeExceptionHandler;
+import org.talend.resource.IResourceService;
+
+/**
+ * yzhang class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 1 2006-09-29 17:06:40Z nrousseau $
+ * 
+ */
+public class ResourceService implements IResourceService {
+
+    private final static String RESOURCE_LOCATION = "org.talend.resources";
+
+    private final static String JAVA_LIBRARIE = "resources/demoprojects/java/TALENDDEMOSJAVA/lib/java";
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.resource.IResourceService#getJavaLibraryPath()
+     */
+    public String getJavaLibraryPath() {
+        // TODO Auto-generated method stub
+
+        Bundle b = Platform.getBundle(RESOURCE_LOCATION);
+        URL url = null;
+        try {
+            url = FileLocator.toFileURL(FileLocator.find(b, new Path(JAVA_LIBRARIE), null));
+        } catch (IOException e) {
+            RuntimeExceptionHandler.process(e);
+        }
+
+        if (url != null) {
+            return url.getFile();
+        } else {
+            return null;
+        }
+
+    }
+}
