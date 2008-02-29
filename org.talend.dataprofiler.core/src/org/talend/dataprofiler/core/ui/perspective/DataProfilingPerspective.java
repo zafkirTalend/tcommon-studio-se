a1c099d1e30ad73edad839e1836eb99f33579fa8
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-02-29 10:44:28 +0000

    ADD:add the perspectives and views to dataprofiler core plugin.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@11892 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/perspective/DataProfilingPerspective.java
@@ -0,0 +1,27 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * DOC rli  class global comment. Detailled comment
+ * <br/>
+ *
+ */
+public class DataProfilingPerspective implements IPerspectiveFactory {
+
+    public void createInitialLayout(IPageLayout layout) {
+    }
+}
