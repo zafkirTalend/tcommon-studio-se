a76f7393d13a9c434d507ee682f7b5935589b682
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-03-11 10:15:35 +0000

    Modification: change the emf model file display pattern, add the label provider class to custum the .prv extension file.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@12180 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/views/ResourceViewLabelProvider.java
@@ -0,0 +1,46 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.views;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.ui.navigator.ICommonContentExtensionSite;
+import org.eclipse.ui.navigator.ICommonLabelProvider;
+
+
+/**
+ * @author rli
+ *
+ */
+public class ResourceViewLabelProvider extends WorkbenchLabelProvider implements ICommonLabelProvider {
+
+    public void init(ICommonContentExtensionSite aConfig) {
+        //init
+    }
+
+    public String getDescription(Object anElement) {
+
+        if (anElement instanceof IResource) {
+            return ((IResource) anElement).getFullPath().makeRelative().toString();
+        }
+        return null;
+    }
+
+    public void restoreState(IMemento aMemento) {
+
+    }
+
+    public void saveState(IMemento aMemento) {
+    }
+}
