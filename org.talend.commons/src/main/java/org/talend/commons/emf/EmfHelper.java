0f40c93dcb2de184ef62f0221d9f8c5c0d8baa1a
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-03-14 12:09:04 +0000

    merge -c12326 from branch 2.3 to trunk bug fix : avoid loading problems of the model in remote repository
    git-svn-id: http://talendforge.org/svn/tos/trunk@12329 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.commons/src/main/java/org/talend/commons/emf/EmfHelper.java
@@ -0,0 +1,46 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons.emf;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+
+/***/
+public class EmfHelper {
+
+	@SuppressWarnings("unchecked")
+	public static void visitChilds(EObject object) {
+	    for (Iterator iterator = object.eClass().getEAllReferences().iterator(); iterator.hasNext();) {
+	        EReference reference = (EReference) iterator.next();
+	        if (reference.isMany()) {
+	            List list = (List) object.eGet(reference);
+	            for (Iterator iterator2 = list.iterator(); iterator2.hasNext();) {
+	                Object get = iterator2.next();
+	                if (get instanceof EObject) {
+	                    visitChilds((EObject) get);
+	                }
+	            }
+	        } else {
+	            Object get = object.eGet(reference);
+	            if (get instanceof EObject) {
+	                visitChilds((EObject) get);
+	            }
+	        }
+	
+	    }
+	}
+
+}
