cd9b586ef5c9b62f1879be7447c6117eccdf4ea7
ggu <ggu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-03-27 09:57:47 +0000

    feature 3311 modification: 1. modified the editing parameter using repository dialog.                            2. fixed that when opened a job, the parameters were not set by using repository mode. such as the parameters for job settings,the encoding and the csv field for node.                            3. fixed that if the repository node is not expand, we can't find repository nod and open the connection dialog.
    git-svn-id: http://talendforge.org/svn/tos/trunk@12765 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/dialogs/ModelSelectionDialog.java
+++ org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/dialogs/ModelSelectionDialog.java
@@ -12,19 +12,26 @@
 // ============================================================================
 package org.talend.commons.ui.swt.dialogs;
 
-import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.SelectionDialog;
+import org.talend.commons.ui.i18n.Messages;
 
 /**
  * DOC zqin class global comment. Detailled comment
  */
-public class ModelSelectionDialog extends TitleAreaDialog {
+public class ModelSelectionDialog extends SelectionDialog {
+
+    private static final String TITLE = Messages.getString("ModelSelectionDialog.Title"); //$NON-NLS-1$
+
+    private static final String MESSAGE = Messages.getString("ModelSelectionDialog.Message"); //$NON-NLS-1$
 
     private int optionValue = -1;
 
@@ -32,18 +39,9 @@ public class ModelSelectionDialog extends TitleAreaDialog {
 
     public ModelSelectionDialog(Shell parentShell) {
         super(parentShell);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
-     */
-    @Override
-    protected void configureShell(Shell newShell) {
-        // TODO Auto-generated method stub
-        super.configureShell(newShell);
-
+        setHelpAvailable(false);
+        setTitle(TITLE);
+        setMessage(MESSAGE);
     }
 
     /*
@@ -53,26 +51,36 @@ public class ModelSelectionDialog extends TitleAreaDialog {
      */
     @Override
     protected Control createDialogArea(Composite parent) {
+        Composite composite = (Composite) super.createDialogArea(parent);
+        createMessageArea(composite);
+        Label titleBarSeparator = new Label(composite, SWT.HORIZONTAL | SWT.SEPARATOR);
+        titleBarSeparator.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        createOptionArea(composite);
+        return composite;
+    }
 
-        setTitle("Select the model:");
+    protected Control createOptionArea(Composite composite) {
+        Composite inner = new Composite(composite, SWT.NONE);
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.marginHeight = 0;
+        inner.setLayout(gridLayout);
+        GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+        gridData.minimumWidth = 300;
+        inner.setLayoutData(gridData);
 
-        setMessage("switch to Built-In Mode, or update the repository connection, or cancel:");
+        Group group = new Group(inner, SWT.NONE);
+        group.setText(Messages.getString("ModelSelectionDialog.Option")); //$NON-NLS-1$
+        gridLayout = new GridLayout();
+        gridLayout.horizontalSpacing = 10;
+        group.setLayout(gridLayout);
+        group.setLayoutData(gridData);
 
-        Composite comp = (Composite) super.createDialogArea(parent);
+        buildIn = new Button(group, SWT.RADIO);
+        buildIn.setText(Messages.getString("ModelSelectionDialog.BuiltIn")); //$NON-NLS-1$
 
-        buildIn = new Button(comp, SWT.RADIO);
-        buildIn.setText("Change to Build-In Model");
-
-        repository = new Button(comp, SWT.RADIO);
-        repository.setText("Refresh the repository connection");
-
-        comp.setLayout(new GridLayout());
-
-        GridData data = new GridData();
-        data.verticalSpan = 70;
-        comp.setLayoutData(data);
-
-        return comp;
+        repository = new Button(group, SWT.RADIO);
+        repository.setText(Messages.getString("ModelSelectionDialog.Update")); //$NON-NLS-1$
+        return inner;
     }
 
     /*
@@ -82,7 +90,6 @@ public class ModelSelectionDialog extends TitleAreaDialog {
      */
     @Override
     protected void okPressed() {
-        // TODO Auto-generated method stub
         if (buildIn.getSelection())
             setOptionValue(0);
         if (repository.getSelection())
