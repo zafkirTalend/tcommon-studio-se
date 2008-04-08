f78ffaae18ed4611d1bf5bd77439fe1b9cc72a6b
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-04-08 15:04:57 +0000

    merge -c13115 from branch 2.3 to trunk format code
    git-svn-id: http://talendforge.org/svn/tos/trunk@13118 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons/src/main/java/org/talend/commons/emf/EmfHelper.java
+++ org.talend.commons/src/main/java/org/talend/commons/emf/EmfHelper.java
@@ -26,32 +26,29 @@ public class EmfHelper {
      * WARNING : you may face StackOverflowError if you have bidirectional relationships or circular references it was
      * only tested with containment references.
      */
-	   @SuppressWarnings("unchecked")
-	    public static void visitChilds(EObject object) {
-	        List<EObject> toVisit = new ArrayList<EObject>(); 
-	        for (Iterator iterator = object.eClass().getEAllReferences().iterator(); iterator.hasNext();) {
-	            EReference reference = (EReference) iterator.next();
-	            if (reference.isMany()) {
-	                List list = (List) object.eGet(reference);
-	                for (Iterator iterator2 = list.iterator(); iterator2.hasNext();) {
-	                    Object get = iterator2.next();
-	                    if (get instanceof EObject) {
-	                        toVisit.add((EObject) get);
-	                    }
-	                }
-	            } else {
-	                Object get = object.eGet(reference);
-	                if (get instanceof EObject) {
-	                    toVisit.add((EObject) get);
-	                }
-	            }
-	        }
-	        
-	        for (EObject eObject : toVisit) {
-	            visitChilds(eObject);
-	        }
-	    }
+    @SuppressWarnings("unchecked")
+    public static void visitChilds(EObject object) {
+        List<EObject> toVisit = new ArrayList<EObject>();
+        for (Iterator iterator = object.eClass().getEAllReferences().iterator(); iterator.hasNext();) {
+            EReference reference = (EReference) iterator.next();
+            if (reference.isMany()) {
+                List list = (List) object.eGet(reference);
+                for (Iterator iterator2 = list.iterator(); iterator2.hasNext();) {
+                    Object get = iterator2.next();
+                    if (get instanceof EObject) {
+                        toVisit.add((EObject) get);
+                    }
+                }
+            } else {
+                Object get = object.eGet(reference);
+                if (get instanceof EObject) {
+                    toVisit.add((EObject) get);
+                }
+            }
+        }
+
+        for (EObject eObject : toVisit) {
+            visitChilds(eObject);
         }
     }
-
 }
