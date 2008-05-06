3cc7bb70eb294851856c960d25408d61c0746d11
chuang <chuang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-06 05:52:41 +0000

    modification: refactor the load and save methods for emf models to EmfHelper
    git-svn-id: http://talendforge.org/svn/tos/trunk@13816 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons/src/main/java/org/talend/commons/emf/EmfHelper.java
+++ org.talend.commons/src/main/java/org/talend/commons/emf/EmfHelper.java
@@ -12,12 +12,21 @@
 // ============================================================================
 package org.talend.commons.emf;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 
 /***/
 public class EmfHelper {
@@ -51,4 +60,68 @@ public class EmfHelper {
             visitChilds(eObject);
         }
     }
+
+    /**
+     * Load emf model from file.
+     * 
+     * 
+     * @param pkg EPackage of your emf model.
+     * @param file The file that contains your emf model.
+     * @return A list of emf object.
+     * @throws IOException
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> loadEmfModel(EPackage pkg, String file) throws IOException {
+        ResourceSet resourceSet = new ResourceSetImpl();
+
+        Resource.Factory.Registry registry = resourceSet.getResourceFactoryRegistry();
+        registry.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+        registry.getExtensionToFactoryMap().put("xml", new XMLResourceFactoryImpl());
+        registry.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+
+        EPackage.Registry reg = resourceSet.getPackageRegistry();
+        reg.put(pkg.getNsURI(), pkg);
+
+        List<T> list = new ArrayList<T>();
+
+        URI uri = URI.createFileURI(file);
+        Resource resource = resourceSet.getResource(uri, true);
+        resource.load(null);
+
+        for (EObject obj : resource.getContents()) {
+            list.add((T) obj);
+        }
+        return list;
+    }
+
+    /**
+     * Save emf model to file.
+     * 
+     * @param pkg EPackage of your emf model.
+     * @param models A list of emf object.
+     * @param file The file that will store your emf model.
+     * @throws IOException
+     */
+    public static void saveEmfModel(EPackage pkg, List<? extends EObject> models, String file) throws IOException {
+        ResourceSet resourceSet = new ResourceSetImpl();
+
+        Resource.Factory.Registry registry = resourceSet.getResourceFactoryRegistry();
+        registry.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+        registry.getExtensionToFactoryMap().put("xml", new XMLResourceFactoryImpl());
+        registry.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+
+        EPackage.Registry reg = resourceSet.getPackageRegistry();
+        reg.put(pkg.getNsURI(), pkg);
+
+        URI uri = URI.createFileURI(file);
+
+        Resource resource = resourceSet.createResource(uri);
+
+        for (EObject model : models) {
+            resource.getContents().add(model);
+        }
+
+        resource.save(null);
+    }
+
 }
