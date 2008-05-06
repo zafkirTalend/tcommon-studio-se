12b9fcb9d9836061c1a51cd638d7df7608edd8e4
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-06 09:46:47 +0000

    Feature 3626 added: 1. add the IS_LAST column on the sql file and modification the corresponding files. 2. persist the TdqAnalysis object.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@13837 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/HistorizeReportProvider.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/HistorizeReportProvider.java
@@ -12,14 +12,23 @@
 // ============================================================================
 package org.talend.dataprofiler.core.ui.action.provider;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 import org.talend.dataprofiler.core.ImageLib;
+import org.talend.dataprofiler.core.helper.RepResourceFileHelper;
+import org.talend.dataprofiler.core.ui.progress.ProgressUI;
+import org.talend.dq.persistence.DatabasePersistence;
 
 
 /**
@@ -27,7 +36,7 @@ import org.talend.dataprofiler.core.ImageLib;
  */
 public class HistorizeReportProvider extends CommonActionProvider {
 
-//    private IFile selectedFile;
+    private IFile selectedFile;
 
     /**
      * DOC rli HistorizeReportProvider constructor comment.
@@ -48,11 +57,11 @@ public class HistorizeReportProvider extends CommonActionProvider {
      * Adds a submenu to the given menu with the name "New Component".
      */
     public void fillContextMenu(IMenuManager menu) {
-//        IStructuredSelection selection = (IStructuredSelection) getContext().getSelection();
-//        Object firstElement = selection.getFirstElement();
-//        if (firstElement instanceof IFile) {
-//            selectedFile = (IFile) firstElement;
-//        }
+        IStructuredSelection selection = (IStructuredSelection) getContext().getSelection();
+        Object firstElement = selection.getFirstElement();
+        if (firstElement instanceof IFile) {
+            selectedFile = (IFile) firstElement;
+        }
         menu.add(historizeReportAction);
     }
 
@@ -71,7 +80,21 @@ public class HistorizeReportProvider extends CommonActionProvider {
          * (non-Javadoc) Method declared on IAction.
          */
         public void run() {
-            super.run();
+            final Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+            IRunnableWithProgress op = new IRunnableWithProgress() {
+
+                public void run(IProgressMonitor monitor) throws InvocationTargetException {
+                    DatabasePersistence databasePersistence = new DatabasePersistence();
+                    databasePersistence.persist(RepResourceFileHelper.getInstance().findReport(selectedFile));
+                }
+            };
+            try {
+                ProgressUI.popProgressDialog(op, shell);
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
