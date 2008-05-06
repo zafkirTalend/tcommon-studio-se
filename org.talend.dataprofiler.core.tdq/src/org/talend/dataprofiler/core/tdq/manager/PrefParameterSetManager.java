fe5520d6779e8fb2948a1aa3e952e5afe53d5ed5
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-06 03:08:49 +0000

    Share project org.talend.dataprofiler.core.tdq
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@13814 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core.tdq/src/org/talend/dataprofiler/core/tdq/manager/PrefParameterSetManager.java
@@ -0,0 +1,51 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.tdq.manager;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.talend.dataprofiler.core.tdq.Activator;
+import org.talend.dataprofiler.core.tdq.pref.PrefParameterBean;
+import org.talend.dataprofiler.core.tdq.pref.PreferenceConstant;
+
+/**
+ * This class can get the parameter value from eclipse preference.
+ */
+public final class PrefParameterSetManager {
+
+    private static PrefParameterSetManager instance;
+
+    private IPreferenceStore preferenceStore;
+
+    private PrefParameterSetManager() {
+        preferenceStore = Activator.getDefault().getPreferenceStore();
+    }
+
+    public static PrefParameterSetManager getInstance() {
+        if (instance == null) {
+            instance = new PrefParameterSetManager();
+            return instance;
+        }
+        return instance;
+    }
+
+    public PrefParameterBean getPrefParameterBean() {
+        PrefParameterBean prefParameterBean = new PrefParameterBean();
+        prefParameterBean.setReportEnable(preferenceStore.getBoolean(PreferenceConstant.REPORTINGENABLE));
+        return prefParameterBean;
+    }
+
+    public void savePreParameterBean(PrefParameterBean prefBean) {
+        preferenceStore.setValue(PreferenceConstant.REPORTINGENABLE, prefBean.isReportEnable());
+    }
+
+}
---
+++ org.talend.dataprofiler.core.tdq/src/org/talend/dataprofiler/core/tdq/pref/PrefParameterBean.java
@@ -0,0 +1,41 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.tdq.pref;
+
+
+/**
+ * DOC rli  class global comment. Detailled comment
+ */
+public class PrefParameterBean {
+    
+    private boolean reportEnable;
+
+    
+    /**
+     * Getter for reportEnable.
+     * @return the reportEnable
+     */
+    public boolean isReportEnable() {
+        return reportEnable;
+    }
+
+    
+    /**
+     * Sets the reportEnable.
+     * @param reportEnable the reportEnable to set
+     */
+    public void setReportEnable(boolean reportEnable) {
+        this.reportEnable = reportEnable;
+    }
+
+}
