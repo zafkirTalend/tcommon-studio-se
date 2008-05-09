ff6e25ad28e279dc8e74a4029240b07d87e94c35
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-09 09:51:56 +0000

    Bug 3716 fixed: When delete a resource, only one delete pop-ups.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@13952 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/DeleteResourceProvider.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/DeleteResourceProvider.java
@@ -15,7 +15,14 @@ package org.talend.dataprofiler.core.ui.action.provider;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -23,15 +30,20 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.DeleteResourceAction;
+import org.eclipse.ui.ide.undo.DeleteResourcesOperation;
+import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
+import org.eclipse.ui.progress.WorkbenchJob;
 import org.talend.cwm.softwaredeployment.TdDataProvider;
 import org.talend.dataprofiler.core.ImageLib;
 import org.talend.dataprofiler.core.PluginConstant;
 import org.talend.dataprofiler.core.helper.AnaResourceFileHelper;
 import org.talend.dataprofiler.core.helper.PrvResourceFileHelper;
+import org.talend.dataprofiler.core.helper.RepResourceFileHelper;
 import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.reports.TdReport;
 import org.talend.utils.sugars.TypedReturnCode;
 import orgomg.cwm.objectmodel.core.Dependency;
 import orgomg.cwm.objectmodel.core.ModelElement;
@@ -73,8 +85,17 @@ public class DeleteResourceProvider extends CommonActionProvider {
      */
     class DeleteDataResourceAction extends DeleteResourceAction {
 
+        private static final String REPORTS = "reports";
+
+        private static final String ANALYSES = "analyses";
+
+        protected boolean isDeleteContent = false;
+
+        private Shell shell;
+
         public DeleteDataResourceAction(Shell shell) {
             super(shell);
+            this.shell = shell;
             setImageDescriptor(ImageLib.getImageDescriptor(ImageLib.ACTION_DELETE));
         }
 
@@ -82,6 +103,69 @@ public class DeleteResourceProvider extends CommonActionProvider {
          * (non-Javadoc) Method declared on IAction.
          */
         public void run() {
+            deleteResource();
+        }
+
+        public void deleteResource() {
+
+            final IResource[] resources = getSelectedResourcesArray();
+            // WARNING: do not query the selected resources more than once
+            // since the selection may change during the run,
+            // e.g. due to window activation when the prompt dialog is dismissed.
+            // For more details, see Bug 60606 [Navigator] (data loss) Navigator
+            // deletes/moves the wrong file
+            if (!checkDeleteContent()) {
+                return;
+            }
+
+            Job deletionCheckJob = new Job("Checking resources") {
+
+                /*
+                 * (non-Javadoc)
+                 * 
+                 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+                 */
+                protected IStatus run(IProgressMonitor monitor) {
+                    if (resources.length == 0) {
+                        return Status.CANCEL_STATUS;
+                    }
+                    scheduleDeleteJob(resources);
+                    return Status.OK_STATUS;
+                }
+
+                /*
+                 * (non-Javadoc)
+                 * 
+                 * @see org.eclipse.core.runtime.jobs.Job#belongsTo(java.lang.Object)
+                 */
+                public boolean belongsTo(Object family) {
+                    if ("Deleting resources".equals(family)) {
+                        return true;
+                    }
+                    return super.belongsTo(family);
+                }
+            };
+
+            deletionCheckJob.schedule();
+        }
+
+        /**
+         * Return an array of the currently selected resources.
+         * 
+         * @return the selected resources
+         */
+        @SuppressWarnings("unchecked")
+        private IResource[] getSelectedResourcesArray() {
+            List selection = getSelectedResources();
+            IResource[] resources = new IResource[selection.size()];
+            selection.toArray(resources);
+            return resources;
+        }
+
+        /**
+         * DOC rli Comment method "getCheckImpactString".
+         */
+        private boolean checkDeleteContent() {
             List<String> impactNames = new ArrayList<String>();
             if (selectedFile.getName().endsWith(PluginConstant.PRV_SUFFIX)) {
                 TypedReturnCode<TdDataProvider> returnValue = PrvResourceFileHelper.getInstance().readFromFile(selectedFile);
@@ -96,11 +180,10 @@ public class DeleteResourceProvider extends CommonActionProvider {
                         impactNames.add(modelElement.getName());
                     }
                 }
-                if (impactNames.size() != 0) {
-                    MessageDialog.openWarning(null, "Impacted  analyses", "The following analyses will be unusable!\n"
-                            + impactNames);
+                popConfirmDialog(impactNames, provider.getName(), ANALYSES);
+                if (isDeleteContent) {
+                    PrvResourceFileHelper.getInstance().clear();
                 }
-                PrvResourceFileHelper.getInstance().clear();
             } else if (selectedFile.getName().endsWith(PluginConstant.ANA_SUFFIX)) {
                 Analysis findAnalysis = AnaResourceFileHelper.getInstance().findAnalysis(selectedFile);
                 EList<Dependency> clientDependency = findAnalysis.getSupplierDependency();
@@ -113,14 +196,116 @@ public class DeleteResourceProvider extends CommonActionProvider {
                         impactNames.add(modelElement.getName());
                     }
                 }
-                if (impactNames.size() != 0) {
-                    MessageDialog.openWarning(null, "Impacted  reports", "The following reports will be unusable!\n"
-                            + impactNames);
+                popConfirmDialog(impactNames, findAnalysis.getName(), REPORTS);
+                if (isDeleteContent) {
+                    AnaResourceFileHelper.getInstance().clear();
                 }
-                AnaResourceFileHelper.getInstance().clear();
 
+            } else if (selectedFile.getName().endsWith(PluginConstant.REP_SUFFIX)) {
+                TdReport findReport = RepResourceFileHelper.getInstance().findReport(selectedFile);
+                popConfirmDialog(impactNames, findReport.getName(), null);
+                if (isDeleteContent) {
+                    AnaResourceFileHelper.getInstance().clear();
+                }
+            } else {
+                isDeleteContent = MessageDialog.openConfirm(null, "Confirm Resource Delete", "Are you sure you want to delele"
+                        + "\"" + selectedFile.getName() + "\"?");
             }
-            super.run();
+            return isDeleteContent;
         }
+
+        /**
+         * DOC rli Comment method "popConfirmDialog".
+         * 
+         * @param impactNames
+         * @param provider
+         */
+        private void popConfirmDialog(List<String> impactNames, String resourceName, String relatedResourceType) {
+            if (impactNames.size() != 0) {
+                isDeleteContent = MessageDialog.openConfirm(null, "Confirm Resource Delete", "The following "
+                        + relatedResourceType + " will be unusable!\n" + impactNames + "\n\n" + "Are you sure you want to delele"
+                        + "\"" + resourceName + "\"?");
+            } else {
+                isDeleteContent = MessageDialog.openConfirm(null, "Confirm Resource Delete", "Are you sure you want to delele"
+                        + "\"" + resourceName + "\"?");
+            }
+        }
+
+        /**
+         * Schedule a job to delete the resources to delete.
+         * 
+         * @param resourcesToDelete
+         */
+        private void scheduleDeleteJob(final IResource[] resourcesToDelete) {
+            // use a non-workspace job with a runnable inside so we can avoid
+            // periodic updates
+            Job deleteJob = new Job("Deleting resources") {
+
+                public IStatus run(final IProgressMonitor monitor) {
+                    try {
+                        final DeleteResourcesOperation op = new DeleteResourcesOperation(resourcesToDelete, "Delete Resources",
+                                isDeleteContent);
+                        op.setModelProviderIds(getModelProviderIds());
+                        // If we are deleting projects and their content, do not
+                        // execute the operation in the undo history, since it cannot be
+                        // properly restored. Just execute it directly so it won't be
+                        // added to the undo history.
+                        if (isDeleteContent) {
+                            // We must compute the execution status first so that any user prompting
+                            // or validation checking occurs. Do it in a syncExec because
+                            // we are calling this from a Job.
+                            WorkbenchJob statusJob = new WorkbenchJob("Status checking") { //$NON-NLS-1$
+
+                                /*
+                                 * (non-Javadoc)
+                                 * 
+                                 * @see org.eclipse.ui.progress.UIJob#runInUIThread(org.eclipse.core.runtime.IProgressMonitor)
+                                 */
+                                public IStatus runInUIThread(IProgressMonitor monitor) {
+                                    return op.computeExecutionStatus(monitor);
+                                }
+
+                            };
+
+                            statusJob.setSystem(true);
+                            statusJob.schedule();
+                            try { // block until the status is ready
+                                statusJob.join();
+                            } catch (InterruptedException e) {
+                                // Do nothing as status will be a cancel
+                            }
+
+                            if (statusJob.getResult().isOK()) {
+                                return op.execute(monitor, WorkspaceUndoUtil.getUIInfoAdapter(shell));
+                            }
+                            return statusJob.getResult();
+                        }
+                        return PlatformUI.getWorkbench().getOperationSupport().getOperationHistory().execute(op, monitor,
+                                WorkspaceUndoUtil.getUIInfoAdapter(shell));
+                    } catch (ExecutionException e) {
+                        if (e.getCause() instanceof CoreException) {
+                            return ((CoreException) e.getCause()).getStatus();
+                        }
+                        return new Status(IStatus.ERROR, "The IDE workbench plugin ID.", e.getMessage(), e);
+                    }
+                }
+
+                /*
+                 * (non-Javadoc)
+                 * 
+                 * @see org.eclipse.core.runtime.jobs.Job#belongsTo(java.lang.Object)
+                 */
+                public boolean belongsTo(Object family) {
+                    if ("Deleting resources".equals(family)) {
+                        return true;
+                    }
+                    return super.belongsTo(family);
+                }
+
+            };
+            deleteJob.setUser(true);
+            deleteJob.schedule();
+        }
+
     }
 }
