4f01adc93ebeb5226d15f7eaa3f9342a118edf1b
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-12 13:57:51 +0000

    Added realvalue and integer value and instance value methods in data quality model for facilitating historization of reports + reorganize some code in AnalysisHelper and ColumnAnalysisHandler
    git-svn-id: http://talendforge.org/svn/tos/trunk@13976 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.utils/src/main/java/org/talend/utils/sql/Java2SqlType.java
+++ org.talend.utils/src/main/java/org/talend/utils/sql/Java2SqlType.java
@@ -14,30 +14,31 @@ package org.talend.utils.sql;
 
 import java.sql.Types;
 
-
 /**
- * DOC zqin  class global comment. Detailled comment
- * <br/>
- *
+ * DOC zqin class global comment. Detailled comment <br/>
+ * 
  * $Id: talend.epf 1 2006-09-29 17:06:40Z zqin $
- *
+ * 
  */
-public class Java2SqlType {
+public final class Java2SqlType {
+
+    private Java2SqlType() {
+    }
 
     public static boolean isTextInSQL(int type) {
-        
+
         switch (type) {
         case Types.CHAR:
         case Types.VARCHAR:
         case Types.LONGVARCHAR:
         case Types.CLOB:
-            
+
             return true;
         default:
             return false;
         }
     }
-    
+
     public static boolean isNumbericInSQL(int type) {
         switch (type) {
         case Types.DOUBLE:
@@ -49,40 +50,40 @@ public class Java2SqlType {
         case Types.BIGINT:
         case Types.DECIMAL:
         case Types.NUMERIC:
-            
+
             return true;
         default:
             return false;
         }
     }
-    
+
     public static boolean isDateInSQL(int type) {
         switch (type) {
         case Types.DATE:
         case Types.TIME:
         case Types.TIMESTAMP:
-            
+
             return true;
         default:
             return false;
         }
     }
-    
+
     public static boolean isOtheTypeInSQL(int type) {
-        
+
         if (isTextInSQL(type)) {
             return false;
         }
-        
+
         if (isNumbericInSQL(type)) {
             return false;
         }
-        
+
         if (isDateInSQL(type)) {
-            
+
             return false;
         }
-        
+
         return true;
     }
 }
