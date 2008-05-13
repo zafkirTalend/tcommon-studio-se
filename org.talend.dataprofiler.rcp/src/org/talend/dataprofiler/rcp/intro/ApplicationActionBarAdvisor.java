6d1eb19d254c5bcad728a09eebbe201c2b44ef7f
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-13 02:30:36 +0000

    Featrue 3757 added:   	 Add the Eclipse menus "close" and "close all" in the menu "File"
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@13987 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/intro/ApplicationActionBarAdvisor.java
+++ org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/intro/ApplicationActionBarAdvisor.java
@@ -19,8 +19,10 @@ import java.util.List;
 
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
@@ -33,112 +35,120 @@ import org.eclipse.ui.internal.registry.IActionSetDescriptor;
 import org.talend.dataprofiler.core.ui.perspective.PerspectiveMenuManager;
 
 /**
- * DOC rli  class global comment. Detailled comment
- * <br/>
- *
+ * DOC rli class global comment. Detailled comment <br/>
+ * 
  */
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
-	private IWorkbenchAction exitAction;
+    private IWorkbenchAction exitAction;
 
-	private IWorkbenchAction preferenceAction;
+    private IWorkbenchAction preferenceAction;
 
-	private IWorkbenchAction aboutAction;
+    private IWorkbenchAction aboutAction;
 
     private IWorkbenchAction saveAction;
-    
+
     private IWorkbenchAction saveAllAction;
 
     private IWorkbenchAction helpAction;
-    
-    private IWorkbenchAction resetPerspectiveAction;
-    
-	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
-		super(configurer);
-	}
 
-	protected void makeActions(IWorkbenchWindow window) {
-		exitAction = ActionFactory.QUIT.create(window);
-		register(exitAction);
-		
-		saveAction = ActionFactory.SAVE.create(window);
+    private IWorkbenchAction resetPerspectiveAction;
+
+    private IWorkbenchAction colseAction;
+
+    private IWorkbenchAction colseAllAction;
+
+    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+        super(configurer);
+    }
+
+    protected void makeActions(IWorkbenchWindow window) {
+        exitAction = ActionFactory.QUIT.create(window);
+        register(exitAction);
+
+        colseAction = ActionFactory.CLOSE.create(window);
+        colseAllAction = ActionFactory.CLOSE_ALL.create(window);
+
+        saveAction = ActionFactory.SAVE.create(window);
         register(saveAction);
-        
+
         saveAllAction = ActionFactory.SAVE_ALL.create(window);
         register(saveAllAction);
 
-		preferenceAction = ActionFactory.PREFERENCES.create(window);
-		register(preferenceAction);
+        preferenceAction = ActionFactory.PREFERENCES.create(window);
+        register(preferenceAction);
 
-		helpAction = ActionFactory.HELP_CONTENTS.create(window);
-		register(helpAction);
-		
-		aboutAction = ActionFactory.ABOUT.create(window);
-		register(aboutAction);
-		
-		resetPerspectiveAction = ActionFactory.RESET_PERSPECTIVE.create(window);
-		register(resetPerspectiveAction);
-	}
+        helpAction = ActionFactory.HELP_CONTENTS.create(window);
+        register(helpAction);
 
-	protected void fillMenuBar(IMenuManager menuBar) {
-		this.beforefillMenuBar();
-		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
-		MenuManager windowMenu = new MenuManager("&Window", IWorkbenchActionConstants.M_WINDOW); //$NON-NLS-1$
-		MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP); //$NON-NLS-1$
+        aboutAction = ActionFactory.ABOUT.create(window);
+        register(aboutAction);
 
-		menuBar.add(fileMenu);
-		menuBar.add(windowMenu);
-		// Add a group marker indicating where action set menus will appear.
-		menuBar.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
-		menuBar.add(helpMenu);
+        resetPerspectiveAction = ActionFactory.RESET_PERSPECTIVE.create(window);
+        register(resetPerspectiveAction);
+    }
 
-		// File
-		fileMenu.add(saveAction);
-		fileMenu.add(saveAllAction);
+    protected void fillMenuBar(IMenuManager menuBar) {
+        this.beforefillMenuBar();
+        MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
+        MenuManager windowMenu = new MenuManager("&Window", IWorkbenchActionConstants.M_WINDOW); //$NON-NLS-1$
+        MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP); //$NON-NLS-1$
+
+        menuBar.add(fileMenu);
+        menuBar.add(windowMenu);
+        // Add a group marker indicating where action set menus will appear.
+        menuBar.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+        menuBar.add(helpMenu);
+
+        fileMenu.add(colseAction);
+        fileMenu.add(colseAllAction);
+        fileMenu.add(new Separator());
+
+        // File
+        fileMenu.add(saveAction);
+        fileMenu.add(saveAllAction);
         fileMenu.add(exitAction);
 
-		// Window
-		
-		MenuManager perspMenu = new PerspectiveMenuManager();
-		menuBar.add(windowMenu);
-		windowMenu.add(perspMenu);
-		windowMenu.add(preferenceAction);
-		windowMenu.add(resetPerspectiveAction);
-		windowMenu.add(new ShowViewAction());
-		// Help
-		helpMenu.add(helpAction);
-		helpMenu.add(aboutAction);
-	}
+        // Window
 
-	private static final String[] ACTIONSETID = new String[] {
-//        "org.eclipse.ui.NavigateActionSet", //$NON-NLS-1$ //$NON-NLS-2$
-        "org.eclipse.ui.WorkingSetActionSet", //$NON-NLS-1$ //$NON-NLS-2$
-        "org.eclipse.update.ui.softwareUpdates",
-//        "org.eclipse.ui.actionSet.keyBindings", //$NON-NLS-1$ //$NON-NLS-2$
-        "org.eclipse.ui.actionSet.openFiles", //$NON-NLS-1$
-        "org.eclipse.ui.edit.text.actionSet.convertLineDelimitersTo" }; //$NON-NLS-1$ 
-	
-	private void beforefillMenuBar() {
-		this.removeAction();
-	}
-	
-	/**
-	 * remove the unnecessary actions.
-	 */
-	@SuppressWarnings("restriction") //$NON-NLS-1$
-	private void removeAction() {
-		ActionSetRegistry reg = WorkbenchPlugin.getDefault()
-				.getActionSetRegistry();
-		IActionSetDescriptor[] actionSets = reg.getActionSets();
-		List<String> list = Arrays.asList(ACTIONSETID);
-		for (int i = 0; i < actionSets.length; i++) {
-			if (list.contains(actionSets[i].getId())) {
-				IExtension ext = actionSets[i].getConfigurationElement()
-						.getDeclaringExtension();
-				reg.removeExtension(ext, new Object[] { actionSets[i] });
-			}
-		}
+        MenuManager perspMenu = new PerspectiveMenuManager();
+        menuBar.add(windowMenu);
+        windowMenu.add(perspMenu);
+        windowMenu.add(preferenceAction);
+        windowMenu.add(resetPerspectiveAction);
+        windowMenu.add(new ShowViewAction());
+        // Help
+        helpMenu.add(helpAction);
+        helpMenu.add(aboutAction);
+    }
 
-	}
+    private static final String[] ACTIONSETID = new String[] { // "org.eclipse.ui.NavigateActionSet", //$NON-NLS-1$
+                                                                // //$NON-NLS-2$
+            "org.eclipse.ui.WorkingSetActionSet", //$NON-NLS-1$ //$NON-NLS-2$
+            "org.eclipse.update.ui.softwareUpdates", // "org.eclipse.ui.actionSet.keyBindings", //$NON-NLS-1$
+                                                        // //$NON-NLS-2$
+            "org.eclipse.ui.actionSet.openFiles", //$NON-NLS-1$
+            "org.eclipse.ui.edit.text.actionSet.convertLineDelimitersTo" }; //$NON-NLS-1$ 
+
+    private void beforefillMenuBar() {
+        this.removeAction();
+    }
+
+    /**
+     * remove the unnecessary actions.
+     */
+    @SuppressWarnings("restriction")//$NON-NLS-1$
+    private void removeAction() {
+        ActionSetRegistry reg = WorkbenchPlugin.getDefault().getActionSetRegistry();
+        IActionSetDescriptor[] actionSets = reg.getActionSets();
+        List<String> list = Arrays.asList(ACTIONSETID);
+        for (int i = 0; i < actionSets.length; i++) {
+            if (list.contains(actionSets[i].getId())) {
+                IExtension ext = actionSets[i].getConfigurationElement().getDeclaringExtension();
+                reg.removeExtension(ext, new Object[] { actionSets[i] });
+            }
+        }
+
+    }
 
 }
