8f2ea85e82743c9bffe165f20bd985d89105d266
qzhang <qzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-20 06:21:46 +0000

    fixed feature 3519: Make Data Discovery perspective usable. and add "Create SQL File" and  "Open SQL File" actions.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@14188 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- sqlexplorer/src/net/sourceforge/sqlexplorer/connections/ConnectionTreeActionGroup.java
+++ sqlexplorer/src/net/sourceforge/sqlexplorer/connections/ConnectionTreeActionGroup.java
@@ -1,40 +1,22 @@
 /*
- * Copyright (C) 2006 SQL Explorer Development Team
- * http://sourceforge.net/projects/eclipsesql
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Copyright (C) 2006 SQL Explorer Development Team http://sourceforge.net/projects/eclipsesql
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package net.sourceforge.sqlexplorer.connections;
 
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-
 import net.sourceforge.sqlexplorer.connections.actions.AbstractConnectionTreeAction;
-import net.sourceforge.sqlexplorer.connections.actions.AutoCommitAction;
-import net.sourceforge.sqlexplorer.connections.actions.ChangeAliasAction;
-import net.sourceforge.sqlexplorer.connections.actions.CloseAllConnectionsAction;
-import net.sourceforge.sqlexplorer.connections.actions.CloseConnectionAction;
 import net.sourceforge.sqlexplorer.connections.actions.CommitAction;
-import net.sourceforge.sqlexplorer.connections.actions.CommitOnCloseAction;
 import net.sourceforge.sqlexplorer.connections.actions.ConnectAliasAction;
-import net.sourceforge.sqlexplorer.connections.actions.NewUserAction;
-import net.sourceforge.sqlexplorer.connections.actions.CopyAliasAction;
-import net.sourceforge.sqlexplorer.connections.actions.CopyUserAction;
-import net.sourceforge.sqlexplorer.connections.actions.DeleteAction;
-import net.sourceforge.sqlexplorer.connections.actions.EditUserAction;
-import net.sourceforge.sqlexplorer.connections.actions.NewAliasAction;
 import net.sourceforge.sqlexplorer.connections.actions.NewDatabaseStructureViewAction;
 import net.sourceforge.sqlexplorer.connections.actions.NewEditorAction;
 import net.sourceforge.sqlexplorer.connections.actions.RollbackAction;
@@ -48,13 +30,13 @@ import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.actions.ActionGroup;
 
 /**
- * ActionGroup for Database Structure View. This group controls what context
- * menu actions are being shown for which node.
+ * ActionGroup for Database Structure View. This group controls what context menu actions are being shown for which
+ * node.
  * 
  * @author Davy Vanherbergen
  */
 public class ConnectionTreeActionGroup extends ActionGroup {
-	
+
     /**
      * Fill the node context menu with all the correct actions.
      * 
@@ -62,78 +44,79 @@ public class ConnectionTreeActionGroup extends ActionGroup {
      */
     public void fillContextMenu(IMenuManager menu) {
 
-    	ConnectionsView view = SQLExplorerPlugin.getDefault().getConnectionsView();
-    	Object[] selection = (view == null) ? null : view.getSelected();
+        ConnectionsView view = SQLExplorerPlugin.getDefault().getConnectionsView();
+        Object[] selection = (view == null) ? null : view.getSelected();
 
         // If nothing is selected, then show the default ones
         if (selection == null || selection.length != 1) {
-        	addAction(menu, new NewAliasAction());
-            addAction(menu, new CloseConnectionAction());
-            addAction(menu, new CloseAllConnectionsAction());
+            // PTODO qzhang delete the Context meun in the Connections View for feature 3519.
+            // addAction(menu, new NewAliasAction());
+            // addAction(menu, new CloseConnectionAction());
+            // addAction(menu, new CloseAllConnectionsAction());
             return;
         }
 
         if (selection[0] instanceof Alias) {
-        	Alias alias = (Alias)selection[0];
-        	
+            Alias alias = (Alias) selection[0];
+
             addAction(menu, new NewEditorAction());
             addAction(menu, new NewDatabaseStructureViewAction());
             addAction(menu, new ConnectAliasAction());
             menu.add(new Separator());
             for (User user : alias.getUsers())
-            	if (!user.isAutoCommit()) {
+                if (!user.isAutoCommit()) {
                     addAction(menu, new CommitAction());
                     addAction(menu, new RollbackAction());
                     menu.add(new Separator());
-            		break;
-            	}
-            
-            addAction(menu, new NewUserAction());
-            addAction(menu, new ChangeAliasAction());
-            addAction(menu, new CopyAliasAction());
-            addAction(menu, new DeleteAction());
-            
+                    break;
+                }
+
+            // addAction(menu, new NewUserAction());
+            // addAction(menu, new ChangeAliasAction());
+            // addAction(menu, new CopyAliasAction());
+            // addAction(menu, new DeleteAction());
+
         } else if (selection[0] instanceof User) {
-        	User user = (User) selection[0];
-        	
+            User user = (User) selection[0];
+
             addAction(menu, new NewEditorAction());
             addAction(menu, new NewDatabaseStructureViewAction());
             addAction(menu, new ConnectAliasAction());
             menu.add(new Separator());
-        	if (!user.isAutoCommit()) {
+            if (!user.isAutoCommit()) {
                 addAction(menu, new CommitAction());
                 addAction(menu, new RollbackAction());
                 menu.add(new Separator());
-        	}
-        	
-            addAction(menu, new NewUserAction());
-            addAction(menu, new EditUserAction());
-            addAction(menu, new CopyUserAction());
-            addAction(menu, new DeleteAction());
-            
+            }
+
+            // addAction(menu, new NewUserAction());
+            // addAction(menu, new EditUserAction());
+            // addAction(menu, new CopyUserAction());
+            // addAction(menu, new DeleteAction());
+
         } else if (selection[0] instanceof SQLConnection) {
-        	SQLConnection connection = (SQLConnection)selection[0];
-        	
+            SQLConnection connection = (SQLConnection) selection[0];
+
             addAction(menu, new NewEditorAction());
             addAction(menu, new NewDatabaseStructureViewAction());
             menu.add(new Separator());
-            
-        	if (!connection.getUser().isAutoCommit()) {
+
+            if (!connection.getUser().isAutoCommit()) {
                 addAction(menu, new CommitAction());
                 addAction(menu, new RollbackAction());
                 menu.add(new Separator());
-        	}
-        	
-            addAction(menu, new CloseConnectionAction());
+            }
+
+            // addAction(menu, new CloseConnectionAction());
         }
     }
-    
+
     private boolean addAction(IMenuManager menu, AbstractConnectionTreeAction action) {
-		if (action.isAvailable()){
-			menu.add(action);
-			action.setEnabled(true);
-			return true;
-		}
-		return false;
+        if (action.isAvailable()) {
+            menu.add(action);
+            action.setEnabled(true);
+            return true;
+        }
+        return false;
     }
 }
