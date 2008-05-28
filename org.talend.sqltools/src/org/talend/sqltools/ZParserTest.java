82f52feef2e6074a9c10cfb3450f1b229e08b6ac
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-28 17:55:15 +0000

    other sql statements that are needed and cannot be parsed!
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@14494 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.sqltools/src/org/talend/sqltools/ZParserTest.java
+++ org.talend.sqltools/src/org/talend/sqltools/ZParserTest.java
@@ -29,6 +29,13 @@ public class ZParserTest {
 
     private static final String SELECT_WITH_QUOTES = "SELECT T.`INO_COUNT_NULLS` FROM TDQ_INDICATOR_OPTIONS T";
 
+    private static final String EXTRACT_ORACLE_DATE = "SELECT * FROM (SELECT TO_NUMBER(TO_CHAR(POST_DATE, 'YYYY')) , TO_NUMBER(TO_CHAR(POST_DATE,'Q')) , TO_NUMBER(TO_CHAR(POST_DATE,'MM')) , TO_NUMBER(TO_CHAR(POST_DATE, 'IW')), COUNT(*) c FROM ROOT.DATES r GROUP BY TO_NUMBER(TO_CHAR(POST_DATE, 'YYYY')) , TO_NUMBER(TO_CHAR(POST_DATE,'Q')) , TO_NUMBER(TO_CHAR(POST_DATE,'MM')) , TO_NUMBER(TO_CHAR(POST_DATE, 'IW')) ORDER BY c DESC) AS subquery WHERE ROWNUM <= 20;";
+
+    private static final String MEDIAN_ORACLE_INNER_QUERY = "SELECT \"TEST_DOUBLE\", COUNT(*) OVER( ) total, CAST(COUNT(*) OVER( ) AS DECIMAL)/2 mid, CEIL(CAST(COUNT(*) OVER( ) AS DECIMAL)/2) next, ROW_NUMBER() OVER ( ORDER BY \"TEST_DOUBLE\") rn FROM \"ROOT\".\"TEST_COUNT\" ";
+
+    private static final String MEDIAN_ORACLE = "SELECT AVG(\"TEST_DOUBLE\") FROM ( " + MEDIAN_ORACLE_INNER_QUERY + ") x"
+            + "WHERE ( MOD(total,2) = 0     AND rn IN ( mid, mid+1 ) ) OR ( MOD(total,2) = 1 AND rn = next );";
+
     /**
      * DOC scorreia Comment method "main".
      * 
@@ -36,9 +43,12 @@ public class ZParserTest {
      */
     public static void main(String[] args) {
         try {
-            String sqlString = SELECT_WITH_QUOTES;
+            // String sqlString = SELECT_WITH_QUOTES;
             // String sqlString = SELECT_EXTRACT;
             // String sqlString = SELECT_LIMIT;
+            // String sqlString = EXTRACT_ORACLE_DATE;
+            String sqlString = MEDIAN_ORACLE_INNER_QUERY;
+
             ZParserTest myTest = new ZParserTest();
 
             if (args != null && args.length > 0) {
@@ -59,6 +69,8 @@ public class ZParserTest {
         System.out.println("Parsing query: " + safeZqlString);
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(safeZqlString.getBytes());
         ZqlParser parser = new ZqlParser();
+        parser.addCustomFunction("OVER", 0);
+        parser.addCustomFunction("OVER", 1);
 
         parser.initParser(byteArrayInputStream);
         ZQuery zQuery = (ZQuery) parser.readStatement();
