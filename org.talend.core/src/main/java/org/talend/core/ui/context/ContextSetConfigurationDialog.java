e7d61d9f4a4b46989bb2a4fc929e0197a9f7c004
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-06-03 02:02:32 +0000

    bug 4024 fixed: it was impossible to rename the default context in the repository context.
    git-svn-id: http://talendforge.org/svn/tos/trunk@14637 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/ui/context/ContextSetConfigurationDialog.java
+++ org.talend.core/src/main/java/org/talend/core/ui/context/ContextSetConfigurationDialog.java
@@ -28,7 +28,6 @@ import org.talend.core.i18n.Messages;
 import org.talend.core.model.context.JobContext;
 import org.talend.core.model.context.JobContextParameter;
 import org.talend.core.model.process.IContext;
-import org.talend.core.model.process.IContextManager;
 import org.talend.core.model.process.IContextParameter;
 import org.talend.core.ui.images.ECoreImage;
 import org.talend.repository.model.RepositoryConstants;
@@ -37,165 +36,151 @@ import org.talend.repository.model.RepositoryConstants;
  * A dialog that config the context value sets.
  * 
  */
-public class ContextSetConfigurationDialog extends
-		ObjectSelectionDialog<IContext> {
+public class ContextSetConfigurationDialog extends ObjectSelectionDialog<IContext> {
 
-	private static String defaultMesage = "Configure Contexts for Job                                      ";
+    private static String defaultMesage = "Configure Contexts for Job                                      ";
 
-	IContextModelManager manager = null;
+    IContextModelManager manager = null;
 
-	/**
-	 * DOC bqian ContextSetConfigurationDialog class global comment. Detailled
-	 * comment <br/>
-	 * 
-	 */
-	public class ContextLabelProvider extends LabelProvider {
+    /**
+     * DOC bqian ContextSetConfigurationDialog class global comment. Detailled comment <br/>
+     * 
+     */
+    public class ContextLabelProvider extends LabelProvider {
 
-		public Image getImage(Object object) {
-			return ImageProvider.getImage(ECoreImage.CONTEXT_ICON);
-		}
+        public Image getImage(Object object) {
+            return ImageProvider.getImage(ECoreImage.CONTEXT_ICON);
+        }
 
-		public String getText(Object object) {
-			IContext context = (IContext) object;
-			return context.getName();
-		}
-	}
+        public String getText(Object object) {
+            IContext context = (IContext) object;
+            return context.getName();
+        }
+    }
 
-	@SuppressWarnings("restriction")
-	public ContextSetConfigurationDialog(Shell parentShell,
-			IContextModelManager manager) {
-		super(parentShell, "Configure Contexts", defaultMesage, null);
-		this.manager = manager;
-		setLabelProvider(getLabelProvider());
-		List<IContext> list = new ArrayList<IContext>(manager
-				.getContextManager().getListContext());
-		setData(list);
-		setShellStyle(getShellStyle() | SWT.RESIZE);
-	}
+    @SuppressWarnings("restriction")
+    public ContextSetConfigurationDialog(Shell parentShell, IContextModelManager manager) {
+        super(parentShell, "Configure Contexts", defaultMesage, null);
+        this.manager = manager;
+        setLabelProvider(getLabelProvider());
+        List<IContext> list = new ArrayList<IContext>(manager.getContextManager().getListContext());
+        setData(list);
+        setShellStyle(getShellStyle() | SWT.RESIZE);
+    }
 
-	public List<IContext> getResultContexts() {
-		return getData();
-	}
+    public List<IContext> getResultContexts() {
+        return getData();
+    }
 
-	LabelProvider getLabelProvider() {
-		return new ContextLabelProvider();
-	}
+    LabelProvider getLabelProvider() {
+        return new ContextLabelProvider();
+    }
 
-	public void createElement() {
-		InputDialog inputDial = new InputDialog(getShell(), Messages
-				.getString("ContextProcessSection.6"), //$NON-NLS-1$
-				Messages.getString("ContextProcessSection.7"), "", null); //$NON-NLS-1$ //$NON-NLS-2$
+    public void createElement() {
+        InputDialog inputDial = new InputDialog(getShell(), Messages.getString("ContextProcessSection.6"), //$NON-NLS-1$
+                Messages.getString("ContextProcessSection.7"), "", null); //$NON-NLS-1$ //$NON-NLS-2$
 
-		inputDial.open();
-		String returnValue = inputDial.getValue();
-		if (returnValue == null) {
-			return;
-		}
-		if (!validateContextName(returnValue)) {
-			return;
-		}
-		createContext(returnValue);
-		refreshViewer();
-	}
+        inputDial.open();
+        String returnValue = inputDial.getValue();
+        if (returnValue == null) {
+            return;
+        }
+        if (!validateContextName(returnValue)) {
+            return;
+        }
+        createContext(returnValue);
+        refreshViewer();
+    }
 
-	private boolean validateContextName(String name) {
-		if (name.equals("")
-				|| !name.matches(RepositoryConstants.CODE_ITEM_PATTERN)) {
-			MessageDialog.openWarning(new Shell(), Messages.getString(Messages
-					.getString("ContextProcessSection.50")), Messages //$NON-NLS-1$
-					.getString(Messages.getString("ContextProcessSection.51"))); //$NON-NLS-1$
-			return false;
-		}
+    private boolean validateContextName(String name) {
+        if (name.equals("") || !name.matches(RepositoryConstants.CODE_ITEM_PATTERN)) {
+            MessageDialog.openWarning(new Shell(), Messages.getString(Messages.getString("ContextProcessSection.50")), Messages //$NON-NLS-1$
+                    .getString(Messages.getString("ContextProcessSection.51"))); //$NON-NLS-1$
+            return false;
+        }
 
-		return !isContextExisting(name);
-	}
+        return !isContextExisting(name);
+    }
 
-	public List<IContext> getAllContexts() {
-		return (List<IContext>) getData();
-	}
+    public List<IContext> getAllContexts() {
+        return (List<IContext>) getData();
+    }
 
-	private boolean isContextExisting(String name) {
-		for (IContext context : getAllContexts()) {
-			if (context.getName().equalsIgnoreCase(name)) {
-				MessageBox mBox = new MessageBox(this.getShell(),
-						SWT.ICON_ERROR);
-				mBox.setText(Messages.getString("ContextProcessSection.29")); //$NON-NLS-1$
-				mBox.setMessage(Messages.getString("ContextProcessSection.30")); //$NON-NLS-1$
-				mBox.open();
-				return true;
-			}
-		}
-		return false;
-	}
+    private boolean isContextExisting(String name) {
+        for (IContext context : getAllContexts()) {
+            if (context.getName().equalsIgnoreCase(name)) {
+                MessageBox mBox = new MessageBox(this.getShell(), SWT.ICON_ERROR);
+                mBox.setText(Messages.getString("ContextProcessSection.29")); //$NON-NLS-1$
+                mBox.setMessage(Messages.getString("ContextProcessSection.30")); //$NON-NLS-1$
+                mBox.open();
+                return true;
+            }
+        }
+        return false;
+    }
 
-	private void createContext(final String name) {
-		IContext context = manager.getContextManager().getDefaultContext();
+    private void createContext(final String name) {
+        IContext context = manager.getContextManager().getDefaultContext();
 
-		JobContext newContext = new JobContext(name);
+        JobContext newContext = new JobContext(name);
 
-		List<IContextParameter> newParamList = new ArrayList<IContextParameter>();
-		newContext.setContextParameterList(newParamList);
-		JobContextParameter param;
-		for (int i = 0; i < context.getContextParameterList().size(); i++) {
-			param = (JobContextParameter) context.getContextParameterList()
-					.get(i).clone();
-			param.setContext(newContext);
-			newParamList.add(param);
-		}
-		getAllContexts().add(newContext);
-	}
+        List<IContextParameter> newParamList = new ArrayList<IContextParameter>();
+        newContext.setContextParameterList(newParamList);
+        JobContextParameter param;
+        for (int i = 0; i < context.getContextParameterList().size(); i++) {
+            param = (JobContextParameter) context.getContextParameterList().get(i).clone();
+            param.setContext(newContext);
+            newParamList.add(param);
+        }
+        getAllContexts().add(newContext);
+    }
 
-	protected void editSelectedElement() {
-		IContext selectedContext = (IContext) (getSelection())
-				.getFirstElement();
-		String contextName = selectedContext.getName();
-		InputDialog inputDial = new InputDialog(
-				getShell(),
-				Messages.getString("ContextProcessSection.12"), //$NON-NLS-1$
-				Messages.getString("ContextProcessSection.13", contextName), "", null); //$NON-NLS-1$ //$NON-NLS-2$
-		inputDial.open();
-		String returnValue = inputDial.getValue();
-		if (returnValue == null) {
-			return;
-		}
-		if (!validateContextName(returnValue)) {
-			return;
-		}
-		if (manager.getProcess().getLastRunContext().sameAs(selectedContext)) {
-			manager.getProcess().setLastRunContext(selectedContext);
-		}
-		renameContext(selectedContext, returnValue);
-		refreshViewer();
-	}
+    protected void editSelectedElement() {
+        IContext selectedContext = (IContext) (getSelection()).getFirstElement();
+        String contextName = selectedContext.getName();
+        InputDialog inputDial = new InputDialog(getShell(), Messages.getString("ContextProcessSection.12"), //$NON-NLS-1$
+                Messages.getString("ContextProcessSection.13", contextName), "", null); //$NON-NLS-1$ //$NON-NLS-2$
+        inputDial.open();
+        String returnValue = inputDial.getValue();
+        if (returnValue == null) {
+            return;
+        }
+        if (!validateContextName(returnValue)) {
+            return;
+        }
+        if (manager.getProcess() != null && manager.getProcess().getLastRunContext().sameAs(selectedContext)) {
+            manager.getProcess().setLastRunContext(selectedContext);
+        }
+        renameContext(selectedContext, returnValue);
+        refreshViewer();
+    }
 
-	private void renameContext(IContext context, String newName) {
-		context.setName(newName);
-	}
+    private void renameContext(IContext context, String newName) {
+        context.setName(newName);
+    }
 
-	/**
-	 * Updates the modify buttons' enabled state based on the current seleciton.
-	 */
-	protected void updateButtonAvailability() {
-		super.updateButtonAvailability();
+    /**
+     * Updates the modify buttons' enabled state based on the current seleciton.
+     */
+    protected void updateButtonAvailability() {
+        super.updateButtonAvailability();
 
-		boolean selectDefaultContext = false;
-		for (Iterator iterator = getSelection().iterator(); iterator.hasNext();) {
-			IContext context = (IContext) iterator.next();
-			if (context == manager.getContextManager().getDefaultContext()) {
-				selectDefaultContext = true;
-				break;
-			}
+        boolean selectDefaultContext = false;
+        for (Iterator iterator = getSelection().iterator(); iterator.hasNext();) {
+            IContext context = (IContext) iterator.next();
+            if (context == manager.getContextManager().getDefaultContext()) {
+                selectDefaultContext = true;
+                break;
+            }
 
-		}
-		if (selectDefaultContext) {
-			fRemoveButton.setEnabled(false);
-			String contextNname = manager.getContextManager()
-					.getDefaultContext().getName();
-			setDisplayMessage(Messages.getString(
-					"ContextProcessSection.RemoveInformation", contextNname));
-		} else {
-			setDisplayMessage(defaultMesage);
-		}
+        }
+        if (selectDefaultContext) {
+            fRemoveButton.setEnabled(false);
+            String contextNname = manager.getContextManager().getDefaultContext().getName();
+            setDisplayMessage(Messages.getString("ContextProcessSection.RemoveInformation", contextNname));
+        } else {
+            setDisplayMessage(defaultMesage);
+        }
 
-	}
+    }
 }
