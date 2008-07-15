f7e1f722cdc9d6ad943626fd6e1f4a097ac7effe
slanglois <slanglois@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-07-15 16:31:21 +0000

    fix issue 4496: add new API function to write SQLPattern
    git-svn-id: http://talendforge.org/svn/tos/trunk@16212 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/StringUtils.java
+++ org.talend.librariesmanager/resources/java/routines/system/StringUtils.java
@@ -20,7 +20,6 @@ public class StringUtils {
 
     public static String deleteChar(String s, char delChar) {
         int len = s.length();
-        int i = -1;
         char[] val = s.toCharArray();
         char buf[] = new char[len];
         int m = 0;
@@ -33,5 +32,63 @@ public class StringUtils {
         }
 
         return new String(buf, 0, m);
+    }
+
+    public static String list(String[] stringArray) {
+        return list(stringArray, null, null, null, null);
+    }
+
+    public static String list(String[] stringArray, String separator) {
+        return list(stringArray, separator, null, null, null);
+    }
+
+    public static String list(String[] stringArray, String separator, String startEnclosure, String endEnclosure) {
+        return list(stringArray, separator, startEnclosure, endEnclosure, null);
+    }
+
+    public static String list(String[] stringArray, String separator, String escaper) {
+        return list(stringArray, separator, null, null, escaper);
+    }
+
+    public static String list(String[] stringArray, String separator, String startEnclosure, String endEnclosure, String escaper) {
+        if (separator == null) {
+            separator = "";
+        } else {
+            separator = separator.trim();
+        }
+        if (startEnclosure == null) {
+            startEnclosure = "";
+        } else {
+            startEnclosure = startEnclosure.trim();
+        }
+        if (endEnclosure == null) {
+            endEnclosure = "";
+        } else {
+            endEnclosure = endEnclosure.trim();
+        }
+        if (escaper == null) {
+            escaper = "";
+        } else {
+            escaper = escaper.trim();
+        }
+        StringBuilder result = new StringBuilder();
+
+        result.append(startEnclosure);
+
+        boolean flag = false;
+        for (String item : stringArray) {
+            item = item.trim();
+            if (flag) {
+                result.append(separator);
+            } else {
+                flag = true;
+            }
+            result.append(escaper);
+            result.append(item);
+            result.append(escaper);
+        }
+        result.append(endEnclosure);
+
+        return result.toString();
     }
 }
