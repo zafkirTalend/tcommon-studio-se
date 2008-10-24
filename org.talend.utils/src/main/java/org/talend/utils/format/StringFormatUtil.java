dbde1cbbb2a466be154605206d543fd88fc84a7d
zqin <zqin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-24 05:37:38 +0000

    bug 5273  fixed : change format of average length and mean indicators
    git-svn-id: http://talendforge.org/svn/tos/trunk@19323 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.utils/src/main/java/org/talend/utils/format/StringFormatUtil.java
+++ org.talend.utils/src/main/java/org/talend/utils/format/StringFormatUtil.java
@@ -12,12 +12,21 @@
 // ============================================================================
 package org.talend.utils.format;
 
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 /**
  * @author scorreia
  * 
  * This class contains utilities for formatting strings.
  */
 public final class StringFormatUtil {
+
+    public static final int PERCENT = 0;
+
+    public static final int NUMBER = 1;
+
+    public static final int OTHER = 99999;
 
     private StringFormatUtil() {
     }
@@ -31,5 +40,38 @@ public final class StringFormatUtil {
      */
     public static String padString(String stringToPad, int size) {
         return String.format("%" + size + "s", stringToPad);
+    }
+
+    public static Object format(Object input, int style) {
+        if (input == null) {
+            return "";
+        }
+
+        try {
+            Double dbl = new Double(input.toString());
+            if (dbl.equals(Double.NaN)) {
+                return String.valueOf(dbl);
+            }
+
+            DecimalFormat format = null;
+
+            switch (style) {
+            case 0:
+                format = (DecimalFormat) DecimalFormat.getPercentInstance(Locale.ENGLISH);
+                format.applyPattern("0.00%");
+                break;
+            case 1:
+                format = (DecimalFormat) DecimalFormat.getNumberInstance(Locale.ENGLISH);
+                format.applyPattern("0.00");
+                break;
+            default:
+                format = (DecimalFormat) DecimalFormat.getInstance(Locale.getDefault());
+                return format.parse(input.toString());
+            }
+
+            return format.format(dbl);
+        } catch (Exception e) {
+            return input;
+        }
     }
 }
