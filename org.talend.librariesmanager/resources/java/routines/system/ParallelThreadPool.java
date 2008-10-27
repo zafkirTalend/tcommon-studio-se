e8a7c206141a72e9f79c1b21a470aba26f0fb728
xzhang <xzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-27 10:07:35 +0000

    Parallelize output for database Add the new ThreadPool and new Thread classes.
    git-svn-id: http://talendforge.org/svn/tos/trunk@19392 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.librariesmanager/resources/java/routines/system/ParallelThreadPool.java
@@ -0,0 +1,159 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2008 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package routines.system;
+
+import java.util.ArrayList;
+
+/**
+ * DOC xzhang class global comment. Detailled comment
+ */
+public class ParallelThreadPool {
+
+	private volatile boolean stopAllWorkers = false;
+
+	private ParallelThread errorThread = null;
+
+	private TalendThreadResult threadResult = null;
+
+	private ParallelThread[] threads;
+
+	private int poolMaxSize = 0;
+
+	private int poolCurrentSize = 0;
+
+	/**
+	 * init the thread list
+	 * 
+	 * @param poolSize
+	 *            the size of pool
+	 */
+	public ParallelThreadPool(int poolSize) {
+		this.threadResult = new TalendThreadResult();
+		this.poolMaxSize = Math.max(1, poolSize);
+		this.threads = new ParallelThread[this.poolMaxSize];
+	}
+
+	/**
+	 * add and run the thread
+	 * 
+	 * @param pt
+	 */
+	public void execThread(ParallelThread pt) {
+		if (!stopAllWorkers) {
+			pt.setThreadID(poolCurrentSize);
+			this.threads[poolCurrentSize++] = pt;
+			pt.start();
+		}
+	}
+
+	public boolean isFull() {
+		return this.poolCurrentSize >= this.poolMaxSize;
+	}
+
+	/**
+	 * get free thread for setting new rows
+	 * 
+	 * @return
+	 */
+
+	public ParallelThread getFreeThread() {
+		while (!stopAllWorkers) {
+			for (ParallelThread tmp : this.threads) {
+				if (tmp.isFree()) {
+					return tmp;
+				}
+			}
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * 
+	 * DOC stan_zhang Comment method "waitForEnd".
+	 */
+	public void waitForEnd() {
+		try {
+
+			for (ParallelThread tmp : this.threads) {
+				// make sure the parallel thread is waiting for buffer
+				tmp.waitForFree();
+				// set finish
+				tmp.finish();
+				// insert a empty buffer to break the waiting buffer
+				tmp.putBuffer(new ArrayList<String[]>());
+			}
+			while (!stopAllWorkers) {
+				boolean hasThreadWork = false;
+				for (ParallelThread tmp : this.threads) {
+					if (tmp.isAlive()) {
+						hasThreadWork = true;
+					}
+				}
+				if (hasThreadWork) {
+					Thread.sleep(100);
+				} else {
+					break;
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void stopAllThreads() {
+		if (!stopAllWorkers) {
+			try {
+				stopAllWorkers = true;
+				for (ParallelThread tmp : this.threads) {
+					tmp.finish();
+					tmp.interrupt();
+					while (tmp.isAlive()) {
+						Thread.sleep(100);
+					}
+				}
+			} catch (InterruptedException x) {
+			}
+
+		}
+	}
+
+	/**
+	 * get ErrorThread.
+	 * 
+	 * @return
+	 */
+	public ParallelThread getErrorThread() {
+		return errorThread;
+	}
+
+	/**
+	 * only keep the first ErrorThread
+	 * 
+	 * @param errorThread
+	 */
+	public void setErrorThread(ParallelThread errorThread) {
+		if (this.errorThread == null) {
+			this.errorThread = errorThread;
+		}
+	}
+
+	public TalendThreadResult getTalendThreadResult() {
+		return threadResult;
+	}
+}
