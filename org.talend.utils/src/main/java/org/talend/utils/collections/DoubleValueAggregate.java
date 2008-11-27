556f15429616ce884a2cc524b17f60f1c9350006
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-11-27 17:36:31 +0000

    classes for aggregation of data
    git-svn-id: http://talendforge.org/svn/tos/trunk@20447 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.utils/src/main/java/org/talend/utils/collections/DoubleValueAggregate.java
@@ -0,0 +1,44 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.utils.collections;
+
+import java.util.Arrays;
+
+/**
+ * @author scorreia
+ * 
+ * This class aggregates doubles.
+ * @param <T> the key class
+ */
+public class DoubleValueAggregate<T> extends ValueAggregate<T, Double> {
+
+    public void addValue(T key, Double[] values) {
+        Double[] doubles = keyToVal.get(key);
+        if (doubles == null) {
+            doubles = new Double[values.length];
+            Arrays.fill(doubles, 0.0);           
+        }
+
+        for (int i = 0; i < values.length; i++) {
+            Double d = values[i];
+            if (d == null) {
+                nullResults.add(key);
+                return;
+            }
+            // else add values
+            doubles[i] += d;
+        }
+        keyToVal.put(key, doubles);
+    }
+
+}
