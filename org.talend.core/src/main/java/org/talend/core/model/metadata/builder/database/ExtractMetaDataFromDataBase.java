f32308f1bdd891976e0080efaef81585885bbe86
gcui <gcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-02-09 10:02:14 +0000

    bug 6065 fixed: Comments not retrieve when uploading a schema from Oracle 10g.
    git-svn-id: http://talendforge.org/svn/tos/trunk@21725 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataFromDataBase.java
+++ org.talend.core/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataFromDataBase.java
@@ -400,32 +400,8 @@ public class ExtractMetaDataFromDataBase {
                     boolean isNullable = ExtractMetaDataUtils.getBooleanMetaDataInfo(columns, "IS_NULLABLE"); //$NON-NLS-1$
                     metadataColumn.setNullable(isNullable);
 
-                    // gcui:if it is oracle database, use this SQL select comments.
-
-                    if (EDatabaseTypeName.ORACLEFORSID.getDisplayName().equals(metadataConnection.getDbType())
-                            || EDatabaseTypeName.ORACLESN.getDisplayName().equals(metadataConnection.getDbType())) {
-                        try {
-                            PreparedStatement statement = ExtractMetaDataUtils.conn
-                                    .prepareStatement("SELECT COMMENTS FROM USER_COL_COMMENTS WHERE TABLE_NAME='" //$NON-NLS-1$
-                                            + medataTable.getLabel() + "' AND COLUMN_NAME='" + metadataColumn.getLabel() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
-                            ResultSet keys = null;
-                            ExtractMetaDataUtils.setQueryStatementTimeout(statement);
-                            if (statement.execute()) {
-                                keys = statement.getResultSet();
-                                while (keys.next()) {
-                                    metadataColumn.setComment(keys.getString("COMMENTS")); //$NON-NLS-1$
-                                }
-                            }
-                            keys.close();
-
-                        } catch (Exception e) {
-                            System.out.print(e);
-                            log.error(e.toString());
-                        }
-                    } else {
-                        // gcui:if not oracle database use "REMARKS" select comments
-                        metadataColumn.setComment(ExtractMetaDataUtils.getStringMetaDataInfo(columns, "REMARKS")); //$NON-NLS-1$
-                    }
+                    // gcui:if not oracle database use "REMARKS" select comments
+                    metadataColumn.setComment(ExtractMetaDataUtils.getStringMetaDataInfo(columns, "REMARKS")); //$NON-NLS-1$
                     metadataColumns.add(metadataColumn);
 
                     // cantoine : patch to fix 0x0 pb cause by Bad Schema
@@ -436,6 +412,33 @@ public class ExtractMetaDataFromDataBase {
                     }
                     metadataColumn.setDefaultValue(stringMetaDataInfo);
 
+                }
+            }
+
+            // gcui:if it is oracle database, use this SQL select comments.
+            // there will do one query to retrieve all comments on the table.
+
+            if (EDatabaseTypeName.ORACLEFORSID.getDisplayName().equals(metadataConnection.getDbType())
+                    || EDatabaseTypeName.ORACLESN.getDisplayName().equals(metadataConnection.getDbType())) {
+                try {
+                    PreparedStatement statement = ExtractMetaDataUtils.conn
+                            .prepareStatement("SELECT COMMENTS FROM USER_COL_COMMENTS WHERE TABLE_NAME='" //$NON-NLS-1$
+                                    + medataTable.getLabel() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+                    ResultSet keys = null;
+                    ExtractMetaDataUtils.setQueryStatementTimeout(statement);
+                    if (statement.execute()) {
+                        keys = statement.getResultSet();
+                        int i = 0;
+                        while (keys.next()) {
+                            MetadataColumn metadataColumn = (MetadataColumn) metadataColumns.get(i++);
+                            metadataColumn.setComment(keys.getString("COMMENTS")); //$NON-NLS-1$
+                        }
+                    }
+                    keys.close();
+
+                } catch (Exception e) {
+                    System.out.print(e);
+                    log.error(e.toString());
                 }
             }
 
