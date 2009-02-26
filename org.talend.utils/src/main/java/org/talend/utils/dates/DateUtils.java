1d68d787dc624fe3228307f301d8d6378f547f99
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-02-26 07:59:03 +0000

    feature 4715 create a jrxml file in order to be able to generate a report file (pdf, html...) for this type of analysis.
    git-svn-id: http://talendforge.org/svn/tos/trunk@22177 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.utils/src/main/java/org/talend/utils/dates/DateUtils.java
@@ -0,0 +1,34 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.utils.dates;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * DOC mzhao class global comment. Detailled comment
+ */
+public final class DateUtils {
+
+    public static final String PATTERN_1 = "MM/dd/yyyy";//$NON-NLS-1$
+
+    public static Date parse(String pattern, String dateText) throws ParseException {
+        Date date = null;
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        date = sdf.parse(dateText);
+        return date;
+    }
+}
