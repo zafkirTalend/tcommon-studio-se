7acff8abe04dae036f11a1301057306328ddd377
hwang <hwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-03-09 02:04:17 +0000

    feature 6482 added: Enhance the display of errors, to allow to find easily the source component of error.
    git-svn-id: http://talendforge.org/svn/tos/trunk@22511 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/ui/images/ECoreImage.java
+++ org.talend.core/src/main/java/org/talend/core/ui/images/ECoreImage.java
@@ -130,7 +130,8 @@ public enum ECoreImage implements IImage {
     PALETTE_OK_ICON("/icons/palette_ok.png"), //$NON-NLS-1$
     PALETTE_CLEAR_ICON("/icons/clear_toolbox.png"), //$NON-NLS-1$
     JOBLET_COMPONENT_ICON("/icons/joblet_component.png"), //$NON-NLS-1$
-    UNKNOWN();
+    UNKNOWN(),
+    TRIANGLE("/icons/triangle.gif");// triangle.gif
 
     private String path;
 
