8565daed3fb004533ae7bb826448bfc54ceb67ae
wyang <wyang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-03-23 09:27:55 +0000

    modification: 1. modify SharedDBConnection, make it support MultiThread, Singleton, Debug. 2. modify the "Shared DB ConnectionName", default value is blank, it means user must input by manual.
    git-svn-id: http://talendforge.org/svn/tos/trunk@22952 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/SharedDBConnection.java
+++ org.talend.librariesmanager/resources/java/routines/system/SharedDBConnection.java
@@ -1,24 +1,109 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package routines.system;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
+/**
+ * A buffer to keep all the DB connections, make it reusable between the different jobs.
+ */
 public class SharedDBConnection {
 
-    private static Map<Integer, Connection> insDBConnections = new HashMap<Integer, java.sql.Connection>();
+    private static boolean DEBUG = false;
 
-    public static java.sql.Connection getDBConnection(String dbDriver, String url, String userName, String password,
+    private static SharedDBConnection instance = null;
+
+    private static Map<String, Connection> sharedConnections = new HashMap<String, java.sql.Connection>();
+
+    private SharedDBConnection() {
+
+    }
+
+    private static synchronized SharedDBConnection getInstance() {
+        if (instance == null) {
+            instance = new SharedDBConnection();
+        }
+        return instance;
+    }
+
+    private synchronized Connection getConnection(String dbDriver, String url, String userName, String password,
             String dbConnectionName) throws ClassNotFoundException, SQLException {
-        if (null == insDBConnections.get((url + userName + password + dbConnectionName).hashCode())
-                || insDBConnections.get((url + userName + password + dbConnectionName).hashCode()).isClosed()) {
-            java.lang.Class.forName(dbDriver);
-            synchronized (insDBConnections) {
-                insDBConnections.put((url + userName + password + dbConnectionName).hashCode(), java.sql.DriverManager
-                        .getConnection(url, userName, password));
+
+        if (DEBUG) {
+            Set<String> keySet = sharedConnections.keySet();
+            System.out.print("SharedDBConnection, current shared connections list is:");
+            for (String key : keySet) {
+                System.out.print(" " + key);
+            }
+            System.out.println();
+        }
+
+        Connection connection = sharedConnections.get(dbConnectionName);
+        if (connection == null) {
+            if (DEBUG) {
+                System.out.println("SharedDBConnection, can't find the key:" + dbConnectionName + " "
+                        + "so create a new one and share it.");
+            }
+            Class.forName(dbDriver);
+            connection = DriverManager.getConnection(url, userName, password);
+            sharedConnections.put(dbConnectionName, connection);
+        } else if (connection.isClosed()) {
+            if (DEBUG) {
+                System.out.println("SharedDBConnection, find the key: " + dbConnectionName + " "
+                        + "But it is closed. So create a new one and share it.");
+            }
+            connection = DriverManager.getConnection(url, userName, password);
+            sharedConnections.put(dbConnectionName, connection);
+        } else {
+            if (DEBUG) {
+                System.out.println("SharedDBConnection, find the key: " + dbConnectionName + " " + "it is OK.");
             }
         }
-        return insDBConnections.get((url + userName + password + dbConnectionName).hashCode());
+        return connection;
+    }
+
+    /**
+     * If there don't exist the connection or it is closed, create and store it.
+     * 
+     * @param dbDriver
+     * @param url
+     * @param userName
+     * @param password
+     * @param dbConnectionName
+     * @return
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public static Connection getDBConnection(String dbDriver, String url, String userName, String password,
+            String dbConnectionName) throws ClassNotFoundException, SQLException {
+        SharedDBConnection instanceLocal = getInstance();
+        Connection connection = instanceLocal.getConnection(dbDriver, url, userName, password, dbConnectionName);
+        return connection;
+    }
+
+    /**
+     * Set the buffer as null, make it recyclable.
+     */
+    public static void clear() {
+        instance = null;
+    }
+
+    public static void setDebugMode(boolean debug) {
+        DEBUG = debug;
     }
 }
