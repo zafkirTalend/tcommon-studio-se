df84c4b0fa49bfcd680167b9e199b554a0c98bac
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-09 10:48:39 +0000

    bug 7417 fixed: remove characters in the routine that causes problems of encoding.
    git-svn-id: http://talendforge.org/svn/tos/trunk@26455 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/TalendString.java
+++ org.talend.librariesmanager/resources/java/routines/TalendString.java
@@ -157,97 +157,97 @@ public class TalendString {
         String car = null;
 
         car = new String("A"); //$NON-NLS-1$
-        result.add(car); /* '\u00C0' À alt-0192 */
-        result.add(car); /* '\u00C1' Á alt-0193 */
-        result.add(car); /* '\u00C2' Â alt-0194 */
-        result.add(car); /* '\u00C3' Ã alt-0195 */
-        result.add(car); /* '\u00C4' Ä alt-0196 */
-        result.add(car); /* '\u00C5' Å alt-0197 */
+        result.add(car); /* '\u00C0' alt-0192 */
+        result.add(car); /* '\u00C1' alt-0193 */
+        result.add(car); /* '\u00C2' alt-0194 */
+        result.add(car); /* '\u00C3' alt-0195 */
+        result.add(car); /* '\u00C4' alt-0196 */
+        result.add(car); /* '\u00C5' alt-0197 */
         car = new String("AE"); //$NON-NLS-1$
-        result.add(car); /* '\u00C6' Æ alt-0198 */
+        result.add(car); /* '\u00C6' alt-0198 */
         car = new String("C"); //$NON-NLS-1$
-        result.add(car); /* '\u00C7' Ç alt-0199 */
+        result.add(car); /* '\u00C7' alt-0199 */
         car = new String("E"); //$NON-NLS-1$
-        result.add(car); /* '\u00C8' È alt-0200 */
-        result.add(car); /* '\u00C9' É alt-0201 */
-        result.add(car); /* '\u00CA' Ê alt-0202 */
-        result.add(car); /* '\u00CB' Ë alt-0203 */
+        result.add(car); /* '\u00C8' alt-0200 */
+        result.add(car); /* '\u00C9' alt-0201 */
+        result.add(car); /* '\u00CA' alt-0202 */
+        result.add(car); /* '\u00CB' alt-0203 */
         car = new String("I"); //$NON-NLS-1$
-        result.add(car); /* '\u00CC' Ì alt-0204 */
-        result.add(car); /* '\u00CD' Í alt-0205 */
-        result.add(car); /* '\u00CE' Î alt-0206 */
-        result.add(car); /* '\u00CF' Ï alt-0207 */
+        result.add(car); /* '\u00CC' alt-0204 */
+        result.add(car); /* '\u00CD' alt-0205 */
+        result.add(car); /* '\u00CE' alt-0206 */
+        result.add(car); /* '\u00CF' alt-0207 */
         car = new String("D"); //$NON-NLS-1$
-        result.add(car); /* '\u00D0' Ð alt-0208 */
+        result.add(car); /* '\u00D0' alt-0208 */
         car = new String("N"); //$NON-NLS-1$
-        result.add(car); /* '\u00D1' Ñ alt-0209 */
+        result.add(car); /* '\u00D1' alt-0209 */
         car = new String("O"); //$NON-NLS-1$
-        result.add(car); /* '\u00D2' Ò alt-0210 */
-        result.add(car); /* '\u00D3' Ó alt-0211 */
-        result.add(car); /* '\u00D4' Ô alt-0212 */
-        result.add(car); /* '\u00D5' Õ alt-0213 */
-        result.add(car); /* '\u00D6' Ö alt-0214 */
+        result.add(car); /* '\u00D2' alt-0210 */
+        result.add(car); /* '\u00D3' alt-0211 */
+        result.add(car); /* '\u00D4' alt-0212 */
+        result.add(car); /* '\u00D5' alt-0213 */
+        result.add(car); /* '\u00D6' alt-0214 */
         car = new String("*"); //$NON-NLS-1$
-        result.add(car); /* '\u00D7' × alt-0215 */
+        result.add(car); /* '\u00D7' alt-0215 */
         car = new String("0"); //$NON-NLS-1$
-        result.add(car); /* '\u00D8' Ø alt-0216 */
+        result.add(car); /* '\u00D8' alt-0216 */
         car = new String("U"); //$NON-NLS-1$
-        result.add(car); /* '\u00D9' Ù alt-0217 */
-        result.add(car); /* '\u00DA' Ú alt-0218 */
-        result.add(car); /* '\u00DB' Û alt-0219 */
-        result.add(car); /* '\u00DC' Ü alt-0220 */
+        result.add(car); /* '\u00D9' alt-0217 */
+        result.add(car); /* '\u00DA' alt-0218 */
+        result.add(car); /* '\u00DB' alt-0219 */
+        result.add(car); /* '\u00DC' alt-0220 */
         car = new String("Y"); //$NON-NLS-1$
-        result.add(car); /* '\u00DD' Ý alt-0221 */
+        result.add(car); /* '\u00DD' alt-0221 */
         car = new String("Þ"); //$NON-NLS-1$
-        result.add(car); /* '\u00DE' Þ alt-0222 */
+        result.add(car); /* '\u00DE' alt-0222 */
         car = new String("B"); //$NON-NLS-1$
-        result.add(car); /* '\u00DF' ß alt-0223 */
+        result.add(car); /* '\u00DF' alt-0223 */
         car = new String("a"); //$NON-NLS-1$
-        result.add(car); /* '\u00E0' à alt-0224 */
-        result.add(car); /* '\u00E1' á alt-0225 */
-        result.add(car); /* '\u00E2' â alt-0226 */
-        result.add(car); /* '\u00E3' ã alt-0227 */
-        result.add(car); /* '\u00E4' ä alt-0228 */
-        result.add(car); /* '\u00E5' å alt-0229 */
+        result.add(car); /* '\u00E0' alt-0224 */
+        result.add(car); /* '\u00E1' alt-0225 */
+        result.add(car); /* '\u00E2' alt-0226 */
+        result.add(car); /* '\u00E3' alt-0227 */
+        result.add(car); /* '\u00E4' alt-0228 */
+        result.add(car); /* '\u00E5' alt-0229 */
         car = new String("ae"); //$NON-NLS-1$
-        result.add(car); /* '\u00E6' æ alt-0230 */
+        result.add(car); /* '\u00E6' alt-0230 */
         car = new String("c"); //$NON-NLS-1$
-        result.add(car); /* '\u00E7' ç alt-0231 */
+        result.add(car); /* '\u00E7' alt-0231 */
         car = new String("e"); //$NON-NLS-1$
-        result.add(car); /* '\u00E8' è alt-0232 */
-        result.add(car); /* '\u00E9' é alt-0233 */
-        result.add(car); /* '\u00EA' ê alt-0234 */
-        result.add(car); /* '\u00EB' ë alt-0235 */
+        result.add(car); /* '\u00E8' alt-0232 */
+        result.add(car); /* '\u00E9' alt-0233 */
+        result.add(car); /* '\u00EA' alt-0234 */
+        result.add(car); /* '\u00EB' alt-0235 */
         car = new String("i"); //$NON-NLS-1$
-        result.add(car); /* '\u00EC' ì alt-0236 */
-        result.add(car); /* '\u00ED' í alt-0237 */
-        result.add(car); /* '\u00EE' î alt-0238 */
-        result.add(car); /* '\u00EF' ï alt-0239 */
+        result.add(car); /* '\u00EC' alt-0236 */
+        result.add(car); /* '\u00ED' alt-0237 */
+        result.add(car); /* '\u00EE' alt-0238 */
+        result.add(car); /* '\u00EF' alt-0239 */
         car = new String("d"); //$NON-NLS-1$
-        result.add(car); /* '\u00F0' ð alt-0240 */
+        result.add(car); /* '\u00F0' alt-0240 */
         car = new String("n"); //$NON-NLS-1$
-        result.add(car); /* '\u00F1' ñ alt-0241 */
+        result.add(car); /* '\u00F1' alt-0241 */
         car = new String("o"); //$NON-NLS-1$
-        result.add(car); /* '\u00F2' ò alt-0242 */
-        result.add(car); /* '\u00F3' ó alt-0243 */
-        result.add(car); /* '\u00F4' ô alt-0244 */
-        result.add(car); /* '\u00F5' õ alt-0245 */
-        result.add(car); /* '\u00F6' ö alt-0246 */
+        result.add(car); /* '\u00F2' alt-0242 */
+        result.add(car); /* '\u00F3' alt-0243 */
+        result.add(car); /* '\u00F4' alt-0244 */
+        result.add(car); /* '\u00F5' alt-0245 */
+        result.add(car); /* '\u00F6' alt-0246 */
         car = new String("/"); //$NON-NLS-1$
-        result.add(car); /* '\u00F7' ÷ alt-0247 */
+        result.add(car); /* '\u00F7' alt-0247 */
         car = new String("0"); //$NON-NLS-1$
-        result.add(car); /* '\u00F8' ø alt-0248 */
+        result.add(car); /* '\u00F8' alt-0248 */
         car = new String("u"); //$NON-NLS-1$
-        result.add(car); /* '\u00F9' ù alt-0249 */
-        result.add(car); /* '\u00FA' ú alt-0250 */
-        result.add(car); /* '\u00FB' û alt-0251 */
-        result.add(car); /* '\u00FC' ü alt-0252 */
+        result.add(car); /* '\u00F9' alt-0249 */
+        result.add(car); /* '\u00FA' alt-0250 */
+        result.add(car); /* '\u00FB' alt-0251 */
+        result.add(car); /* '\u00FC' alt-0252 */
         car = new String("y"); //$NON-NLS-1$
-        result.add(car); /* '\u00FD' ý alt-0253 */
+        result.add(car); /* '\u00FD' alt-0253 */
         car = new String("þ"); //$NON-NLS-1$
-        result.add(car); /* '\u00FE' þ alt-0254 */
+        result.add(car); /* '\u00FE' alt-0254 */
         car = new String("y"); //$NON-NLS-1$
-        result.add(car); /* '\u00FF' ÿ alt-0255 */
+        result.add(car); /* '\u00FF' alt-0255 */
         result.add(car); /* '\u00FF' alt-0255 */
 
         return result;
