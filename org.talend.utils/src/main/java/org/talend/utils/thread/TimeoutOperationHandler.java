be3cc833bfe339580dd9e97ef11dee544d83055b
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-20 08:36:36 +0000

    backport fix of r26631 after merge r26610 from branch 3.1 to trunk Bug 8326 fixed : Performances on JobConductor? has to be improved particularly when many tasks are existing
    git-svn-id: http://talendforge.org/svn/tos/trunk@26668 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.utils/src/main/java/org/talend/utils/thread/TimeoutOperationHandler.java
@@ -0,0 +1,130 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.utils.thread;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 
+ * Handler for an operation which processes with a timeout.
+ * @param <R> type of result
+ */
+public abstract class TimeoutOperationHandler<R> {
+
+    private static Logger log = Logger.getLogger(TimeoutOperationHandler.class);
+    
+    private R result;
+
+    private boolean timeoutReached;
+
+    private String labelOperation;
+
+    private long timeout;
+
+    private Throwable operationError;
+
+    private R defaultResult;
+    
+    public TimeoutOperationHandler(long timeout) {
+        super();
+        this.timeout = timeout;
+    }
+
+    public TimeoutOperationHandler(long timeout, String labelOperation) {
+        super();
+        this.timeout = timeout;
+        this.labelOperation = labelOperation;
+    }
+
+    public void start() {
+        
+        internalStart();
+
+    }
+
+    protected void internalStart() {
+        Runnable runnable = new Runnable() {
+            public void run() {
+                try {
+                    result = TimeoutOperationHandler.this.internalRun();
+                } catch (Throwable t) {
+                    log.error(t.getMessage());
+                    operationError = t;
+                } finally {
+                    finalizeOperation();
+                }
+            }
+
+        };
+        
+        if(labelOperation != null) {
+            new Thread(runnable, labelOperation).start();
+        } else {
+            new Thread(runnable).start();
+        }
+
+        long timeStart = System.currentTimeMillis();
+
+        while (true) {
+            
+            if(System.currentTimeMillis() - timeStart > timeout) {
+                timeoutReached = true;
+            }
+            
+            if (timeoutReached || hasValidResultOperation()) {
+                break;
+            }
+            ThreadUtils.waitTimeBool(100);
+        }
+    }
+    
+    protected R internalRun() {
+        return run();
+    }
+
+    public boolean hasValidResultOperation() {
+        return result != null;
+    }
+    
+    public abstract R run();
+
+    public R getResult() {
+        if(hasValidResultOperation()) {
+            return result;
+        } else {
+            return getDefaultResult();
+        }
+    }
+    
+    public Throwable getOperationError() {
+        return operationError;
+    }
+    
+    public void finalizeOperation() {
+        
+    }
+    
+    public R getDefaultResult() {
+        return defaultResult;
+    }
+
+    public void setDefaultResult(R defaultResult) {
+        this.defaultResult = defaultResult;
+    }
+
+    
+    public void setLabelOperation(String labelOperation) {
+        this.labelOperation = labelOperation;
+    }
+    
+}
