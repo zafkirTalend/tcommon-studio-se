8535719b64bf3e171f71583001bcb4d480ecb3ca
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-12 15:09:04 +0000

    Modification: add method using Base64 encoding
    git-svn-id: http://talendforge.org/svn/tos/trunk@27380 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons/src/main/java/org/talend/commons/utils/PasswordHelper.java
+++ org.talend.commons/src/main/java/org/talend/commons/utils/PasswordHelper.java
@@ -15,13 +15,16 @@ package org.talend.commons.utils;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.codec.binary.Base64;
+
 /**
  */
 public class PasswordHelper {
 
     private static final String ALGORITHM = "MD5"; //$NON-NLS-1$
 
-    public static synchronized boolean verifyPassword(byte[] encryptedPassword, byte[] encryptedPassword2) throws NoSuchAlgorithmException {
+    public static synchronized boolean verifyPassword(byte[] encryptedPassword, byte[] encryptedPassword2)
+            throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
         return messageDigest.isEqual(encryptedPassword, encryptedPassword2);
     }
@@ -29,5 +32,15 @@ public class PasswordHelper {
     public static synchronized byte[] encryptPasswd(String password) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance(ALGORITHM);
         return messageDigest.digest(password.getBytes());
+    }
+
+    public static synchronized String encryptPasswd64(String password) throws NoSuchAlgorithmException {
+        return new String(Base64.encodeBase64(encryptPasswd(password)));
+    }
+
+    public static synchronized boolean verifyPassword64(String encryptedPassword, String encryptedPassword2)
+            throws NoSuchAlgorithmException {
+        return verifyPassword(Base64.decodeBase64(encryptedPassword.getBytes()), Base64.decodeBase64(encryptedPassword2
+                .getBytes()));
     }
 }
