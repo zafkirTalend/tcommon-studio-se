83e4d125e26a815fc0be60b8bfd10173d16cd847
hwang <hwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-19 09:59:11 +0000

    bug 8380 modify: Drag&drop of SAP Connection doesn't set the set information in the component.
    git-svn-id: http://talendforge.org/svn/tos/trunk@28472 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/repository/ERepositoryObjectType.java
+++ org.talend.core/src/main/java/org/talend/core/model/repository/ERepositoryObjectType.java
@@ -69,7 +69,7 @@ public enum ERepositoryObjectType {
     METADATA_CON_SYNONYM("repository.synonym", true), //$NON-NLS-1$
     METADATA_CON_QUERY("repository.query", true), //$NON-NLS-1$
     METADATA_CON_CDC("repository.CDC", true), //$NON-NLS-1$
-    METADATA_SAP_FUNCTION("repository.SAPFunction", false), //$NON-NLS-1$
+    METADATA_SAP_FUNCTION("repository.SAPFunction", true), //$NON-NLS-1$
 
     // feature 0006484 add
     METADATA_FILE_RULES("repository.metadataFileRules", "repository.metadataFileRules.alias"), //$NON-NLS-1$ //$NON-NLS-2$
