8868e4906cab47875e91a1d0665917c245a635e8
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-26 16:40:46 +0000

    feature 6590 : add wildcardPathFilter in commandline
    git-svn-id: http://talendforge.org/svn/tos/trunk@28815 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/repository/model/RepositoryConstants.java
+++ org.talend.core/src/main/java/org/talend/repository/model/RepositoryConstants.java
@@ -48,6 +48,8 @@ public class RepositoryConstants {
 
     public static final String FOLDER_PATTERN = "^[a-zA-Z]+[a-zA-Z0-9\\_]*$"; //$NON-NLS-1$
 
+    public static final String FOLDER_WILDCARD = "^[a-zA-Z\\*\\?]+[a-zA-Z0-9\\_\\*\\?]*$"; //$NON-NLS-1$
+
     public static final String REPOSITORY_ITEM_PATTERN_INTERN = "a-zA-Z0-9\\.\\-\\_\\ \\(\\)\\[\\]="; //$NON-NLS-1$
     
     public static final String REPOSITORY_ITEM_PATTERN = "^[" + REPOSITORY_ITEM_PATTERN_INTERN + "]+$"; //$NON-NLS-1$
