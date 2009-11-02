778d2b377c2d614bf9bb3b83069ad62ec0203993
zli <zli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-02 06:34:42 +0000

    bug 0009918 fixed:   NPE at line 1819 of TypeConvert in system routines.
    git-svn-id: http://talendforge.org/svn/tos/trunk@32020 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/TypeConvert.java
+++ org.talend.librariesmanager/resources/java/routines/system/TypeConvert.java
@@ -1816,7 +1816,7 @@ public class TypeConvert {
      */
     public static char Double2char(Double o) {
         if (o == null)
-            return (char) o.doubleValue();
+            return ' ';
         return (char) o.doubleValue();
     }
 
@@ -1826,7 +1826,7 @@ public class TypeConvert {
      */
     public static Character Double2Character(Double o) {
         if (o == null)
-            return null;
+            return 0;
         return Character.valueOf((char) o.doubleValue());
     }
 
