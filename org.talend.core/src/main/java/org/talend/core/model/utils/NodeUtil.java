185e4dec8fd2073f32e2051bda84ef6c444edebc
wyang <wyang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-12-03 10:23:11 +0000

    bug 9363 fixed: resolve a bug: reject link as lookup link
    git-svn-id: http://talendforge.org/svn/tos/trunk@33580 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/utils/NodeUtil.java
+++ org.talend.core/src/main/java/org/talend/core/model/utils/NodeUtil.java
@@ -35,8 +35,9 @@ public class NodeUtil {
      * DOC sort the outgoingconnections to make sure the first connection is EConnectionType.FLOW_MAIN or
      * EConnectionType.FLOW_REF<br/>
      * <p>
-     * bug:9363, if a component have 2 output links, one is EConnectionType.FLOW_MAIN, one is EConnectionType.FLOW_REF,
-     * make sure EConnectionType.FLOW_REF before EConnectionType.FLOW_MAIN(REJECT)
+     * bug:9363, if a component have 2 output links,
+     * <li>"EConnectionType.FLOW_MAIN(FLOW), EConnectionType.FLOW_REF(REJECT)"</li>
+     * <li>"EConnectionType.FLOW_MAIN(REJECT), EConnectionType.FLOW_REF(FLOW)"</li>, make FLOW before "REJECT"
      * </p>
      * 
      * @param node
@@ -55,32 +56,19 @@ public class NodeUtil {
 
                     EConnectionType lineStyle = connection1.getLineStyle();
                     EConnectionType lineStyle2 = connection2.getLineStyle();
-
-                    // 1. check EConnectionType.FLOW_REF
-                    if (lineStyle == EConnectionType.FLOW_REF) {
+                    // "FLOW" only for the default Main connection, if user define his connection like: "FILTER",
+                    // "REJECT", "FLOW", he should use this API in JET directly: List<? extends IConnection> connsFilter
+                    // = node.getOutgoingConnections("FILTER");
+                    if ("FLOW".equals(connection1.getConnectorName()) || lineStyle == EConnectionType.FLOW_MAIN
+                            || lineStyle == EConnectionType.FLOW_MERGE) {
                         return -1;
                     }
-                    if (lineStyle2 == EConnectionType.FLOW_REF) {
+                    if ("FLOW".equals(connection2.getConnectorName()) || lineStyle2 == EConnectionType.FLOW_MAIN
+                            || lineStyle2 == EConnectionType.FLOW_MERGE) {
                         return 1;
+                    } else {
+                        return 0;
                     }
-
-                    // 2. check EConnectionType.FLOW_MAIN, EConnectionType.FLOW_MERGE
-                    if (lineStyle == EConnectionType.FLOW_MAIN || lineStyle == EConnectionType.FLOW_MERGE) {
-                        return -1;
-                    }
-                    if (lineStyle2 == EConnectionType.FLOW_MAIN || lineStyle2 == EConnectionType.FLOW_MERGE) {
-                        return 1;
-                    }
-
-                    // 3. check others case
-                    if (!(lineStyle == EConnectionType.FLOW_MAIN || lineStyle == EConnectionType.FLOW_MERGE || lineStyle == EConnectionType.FLOW_REF)) {
-                        return -1;
-                    }
-                    if (!(lineStyle2 == EConnectionType.FLOW_MAIN || lineStyle2 == EConnectionType.FLOW_MERGE || lineStyle2 == EConnectionType.FLOW_REF)) {
-                        return 1;
-                    }
-
-                    return 0;
 
                 }
             });
