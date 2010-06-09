d8687a9edddcb69f9092f0461f2a3c284f2253a3
zli <zli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-06-09 09:52:26 +0000

    bug 0013620 fixed:  Under "internationalization ,click the button "import tanslation from babili" ,then appear error logs.
    git-svn-id: http://talendforge.org/svn/tos/trunk@43879 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/genhtml/FileCopyUtils.java
+++ org.talend.core/src/main/java/org/talend/core/model/genhtml/FileCopyUtils.java
@@ -12,6 +12,7 @@
 // ============================================================================
 package org.talend.core.model.genhtml;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
@@ -64,5 +65,41 @@ public class FileCopyUtils {
                 }
             }
         }
+    }
+
+    public static void copyFolder(String oldPath, String newPath) {
+
+        try {
+            (new File(newPath)).mkdirs();
+            File a = new File(oldPath);
+            String[] file = a.list();
+            File temp = null;
+            for (int i = 0; i < file.length; i++) {
+                if (oldPath.endsWith(File.separator)) {
+                    temp = new File(oldPath + file[i]);
+                } else {
+                    temp = new File(oldPath + File.separator + file[i]);
+                }
+
+                if (temp.isFile()) {
+                    FileInputStream input = new FileInputStream(temp);
+                    FileOutputStream output = new FileOutputStream(newPath + "/" + (temp.getName()).toString());
+                    byte[] b = new byte[1024 * 5];
+                    int len;
+                    while ((len = input.read(b)) != -1) {
+                        output.write(b, 0, len);
+                    }
+                    output.flush();
+                    output.close();
+                    input.close();
+                }
+                if (temp.isDirectory()) {
+                    copyFolder(oldPath + "/" + file[i], newPath + "/" + file[i]);
+                }
+            }
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+        }
+
     }
 }
