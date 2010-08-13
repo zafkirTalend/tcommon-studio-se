a1fb0326fbf0d8b4556a53bc543183b05a3f77d5
wzhang <wzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-13 03:30:24 +0000

    feature 14713 modified: change routines license as same as job and externalized strings.
    git-svn-id: http://talendforge.org/svn/tos/trunk@46714 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/ui/branding/AbstractBrandingService.java
+++ org.talend.core/src/main/java/org/talend/core/ui/branding/AbstractBrandingService.java
@@ -12,24 +12,22 @@
 // ============================================================================
 package org.talend.core.ui.branding;
 
+import org.talend.core.i18n.Messages;
+
 /**
  * wzhang class global comment. Detailled comment
  */
 public abstract class AbstractBrandingService implements IBrandingService {
 
     public String getJobLicenseHeader(String version) {
-        String contents = "\n//\n// " + BrandingConstants.GENERATEDCODE_COPYRIGHT_TITLE + "\n//\n// " //$NON-NLS-1$ //$NON-NLS-2$
-                + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY1 + "_" + this.getFullProductName() + "\n// " //$NON-NLS-1$ //$NON-NLS-2$
-                + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY2 + " " + version + ")\n// " //$NON-NLS-1$ //$NON-NLS-2$
-                + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY3 + "\n// " + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY4 //$NON-NLS-1$
-                + "\n// " + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY5 + "\n//\n"; //$NON-NLS-1$ //$NON-NLS-2$
-        return BrandingConstants.LICENSE_LINE + contents + BrandingConstants.LICENSE_LINE;
+        String contents = Messages.getString("AbstractBrandingService_job_license_header_content", //$NON-NLS-1$
+                this.getFullProductName(), version);
+        return contents;
     }
 
-    public String getRoutineLicenseHeader() {
-        String contents = BrandingConstants.GENERATEDCODE_COPYRIGHT_TITLE + "\n//\n// " //$NON-NLS-1$
-                + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY3 + "\n// " + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY4 //$NON-NLS-1$
-                + "\n// " + BrandingConstants.GENERATEDCODE_COPYRIGHT_BODY5; //$NON-NLS-1$
+    public String getRoutineLicenseHeader(String version) {
+        String contents = Messages.getString("AbstractBrandingService.routines_license_header_content", //$NON-NLS-1$
+                this.getFullProductName(), version);
         return contents;
     }
 }
