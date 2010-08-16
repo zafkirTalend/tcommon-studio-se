97dc874fa651278d1ef2784c15dedb1aed940a9c
hwang <hwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-16 10:26:00 +0000

    bug 14556: No response if select table from 'based on table' list then do 'Guess schema'
    git-svn-id: http://talendforge.org/svn/tos/trunk@46859 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/utils/ProcessStreamTrashReaderUtil.java
+++ org.talend.core/src/main/java/org/talend/core/model/utils/ProcessStreamTrashReaderUtil.java
@@ -16,6 +16,8 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.talend.commons.exception.WarningException;
+
 /**
  * cli class global comment. Detailled comment
  */
@@ -137,6 +139,73 @@ public final class ProcessStreamTrashReaderUtil {
                         String line = null;
                         while ((line = reader.readLine()) != null) {
                             System.out.println("getErrorStream " + line); //$NON-NLS-1$
+                            throw new WarningException("AAAAA");
+                        }
+                    } catch (Exception ex) {
+                        // nothing to do
+                    } finally {
+                        try {
+                            is.close();
+                        } catch (Exception ex) {
+                            // nothing to do
+                        }
+                    }
+                }
+            }.start();
+
+            boolean stopped = false;
+            while (!stopped) {
+                try {
+                    process.exitValue();
+                    stopped = true;
+                } catch (IllegalThreadStateException itse) {
+                    // nothing to do
+                }
+            }
+        } catch (Exception ioe) {
+            // nothing to do
+        }
+    }
+
+    public static void readAndForget(final Process process, final StringBuffer buffer) {
+        try {
+            // input stream thread
+            new Thread() {
+
+                public void run() {
+                    InputStream is = process.getInputStream();
+                    InputStreamReader din = new InputStreamReader(is);
+                    BufferedReader reader = new BufferedReader(din);
+                    try {
+                        String line = null;
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println("getInputStream " + line); //$NON-NLS-1$
+                        }
+                    } catch (Exception ex) {
+                        // nothing to do
+                    } finally {
+                        try {
+                            is.close();
+                        } catch (Exception ex) {
+                            // nothing to do
+                        }
+                    }
+                }
+            }.start();
+
+            // error stream thread
+            new Thread() {
+
+                public void run() {
+                    InputStream is = process.getErrorStream();
+                    InputStreamReader din = new InputStreamReader(is);
+                    BufferedReader reader = new BufferedReader(din);
+                    try {
+                        String line = null;
+                        while ((line = reader.readLine()) != null) {
+                            System.out.println("getErrorStream " + line); //$NON-NLS-1$
+                            buffer.append(line);
+                            buffer.append("\r\n");
                         }
                     } catch (Exception ex) {
                         // nothing to do
