a25ebd919e98290b4bb123ce52064010ff96d3ba
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-21 05:50:26 +0000

    bug 14924 fixed: Fail to logon project.
    git-svn-id: http://talendforge.org/svn/tos/trunk@48656 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/ui/TalendBrowserLaunchHelper.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/ui/TalendBrowserLaunchHelper.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -10,7 +10,7 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-package org.talend.core;
+package org.talend.core.ui;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -21,25 +21,30 @@ import org.talend.commons.exception.ExceptionHandler;
 /**
  * DOC Administrator class global comment. Detailled comment
  */
-public class BareBonesBrowserLaunch {
+public class TalendBrowserLaunchHelper {
 
-    static final String[] browsers = { "google-chrome", "firefox", "opera", "epiphany", "konqueror", "conkeror", "midori",
-            "kazehakase", "mozilla" };
-
-    static final String errMsg = "Error attempting to launch web browser";
+    private static final String[] browsers = { "google-chrome",//$NON-NLS-1$
+            "firefox", //$NON-NLS-1$
+            "opera", //$NON-NLS-1$
+            "epiphany",//$NON-NLS-1$
+            "konqueror", //$NON-NLS-1$
+            "conkeror", //$NON-NLS-1$
+            "midori", //$NON-NLS-1$ 
+            "kazehakase", //$NON-NLS-1$
+            "mozilla" }; //$NON-NLS-1$
 
     public static void openURL(String url) {
         Class<?> name;
         try {
-            name = Class.forName("java.awt.Desktop");
-            name.getDeclaredMethod("browse", new Class[] { java.net.URI.class }).invoke(
-                    name.getDeclaredMethod("getDesktop").invoke(null), new Object[] { java.net.URI.create(url) });
+            name = Class.forName("java.awt.Desktop"); //$NON-NLS-1$
+            name.getDeclaredMethod("browse", new Class[] { java.net.URI.class }).invoke( //$NON-NLS-1$
+                    name.getDeclaredMethod("getDesktop").invoke(null), new Object[] { java.net.URI.create(url) }); //$NON-NLS-1$
         } catch (Exception ignore) {
 
-            String osName = System.getProperty("os.name");
-            if (osName.startsWith("Mac OS")) {
+            String osName = System.getProperty("os.name"); //$NON-NLS-1$
+            if (osName.startsWith("Mac OS")) { //$NON-NLS-1$
                 try {
-                    Class.forName("com.apple.eio.FileManager").getDeclaredMethod("openURL", new Class[] { String.class }).invoke(
+                    Class.forName("com.apple.eio.FileManager").getDeclaredMethod("openURL", new Class[] { String.class }).invoke( //$NON-NLS-1$ //$NON-NLS-2$
                             null, new Object[] { url });
                 } catch (IllegalArgumentException e1) {
                     ExceptionHandler.process(e1);
@@ -54,9 +59,9 @@ public class BareBonesBrowserLaunch {
                 } catch (ClassNotFoundException e6) {
                     ExceptionHandler.process(e6);
                 }
-            } else if (osName.startsWith("Windows")) {
+            } else if (osName.startsWith("Windows")) { //$NON-NLS-1$
                 try {
-                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url); //$NON-NLS-1$
                 } catch (IOException e) {
                     ExceptionHandler.process(e);
                 }
@@ -65,7 +70,7 @@ public class BareBonesBrowserLaunch {
                 for (String b : browsers) {
                     try {
                         if (browser == null
-                                && Runtime.getRuntime().exec(new String[] { "which", b }).getInputStream().read() != -1) {
+                                && Runtime.getRuntime().exec(new String[] { "which", b }).getInputStream().read() != -1) { //$NON-NLS-1$
                             Runtime.getRuntime().exec(new String[] { browser = b, url });
                         }
                     } catch (IOException e) {
