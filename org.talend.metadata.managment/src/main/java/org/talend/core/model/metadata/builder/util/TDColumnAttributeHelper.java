87ba57eb9021d875d683fb4cec7a0388474ad0b6
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-28 05:32:13 +0000

    feature 14891: add some attribute for TOS on TdTable、Connection and TdColumn,and change litener from DeleteTableAction to DeleteAction for make dependency can be consider when connection be deleted with TOS.
    git-svn-id: http://talendforge.org/svn/tos/trunk@49043 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/util/TDColumnAttributeHelper.java
+++ org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/util/TDColumnAttributeHelper.java
@@ -22,9 +22,13 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.talend.commons.utils.database.DB2ForZosDataBaseMetadata;
+import org.talend.core.model.metadata.MappingTypeRetriever;
+import org.talend.core.model.metadata.MetadataTalendType;
 import org.talend.core.model.metadata.builder.connection.Connection;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.metadata.builder.database.ExtractMetaDataUtils;
 import org.talend.cwm.helper.ColumnHelper;
+import org.talend.cwm.helper.ConnectionHelper;
 import org.talend.cwm.helper.TableHelper;
 import org.talend.cwm.helper.TaggedValueHelper;
 import org.talend.cwm.relational.RelationalFactory;
@@ -50,6 +54,8 @@ public class TDColumnAttributeHelper {
 
     private static final String IBM_DB2_ZOS_PRODUCT_NAME = "DB2";
 
+    private static DatabaseConnection databaseconnection = null;
+
     private static Logger log = Logger.getLogger(TDColumnAttributeHelper.class);
 
     public static TdTable addTagVale(ResultSet resutSet, TdTable table) {
@@ -68,8 +74,127 @@ public class TDColumnAttributeHelper {
         return table;
     }
 
+    public static TdColumn addColumnAttribute(ResultSet resutSet, TdColumn column) throws SQLException {
+
+        // // --- add columns to table
+        // ResultSet columns = getConnectionMetadata(connection).getColumns(catalogName, schemaPattern, tablePattern,
+        // columnPattern);
+
+        // TODO scorreia other informations for columns can be retrieved here
+        // get the default value
+        // MOD mzhao 2009-04-09,Bug 6840: fetch LONG or LONG RAW column first , as these kind of columns are read as
+        // stream,if not read by select order, there will be "Stream has already been closed" error.
+        // Don't move the below block ,if you move it that emerge up bug klliu 2010-09-07
+        if (column == null) {
+            column = RelationalFactory.eINSTANCE.createTdColumn();
+        }
+        Object defaultvalue = null;
+        try {
+            defaultvalue = resutSet.getObject(GetColumn.COLUMN_DEF.name());
+        } catch (Exception e1) {
+            log.warn(e1, e1);
+        }
+        String defaultStr = (defaultvalue != null) ? String.valueOf(defaultvalue) : null;
+        TdExpression defExpression = createTdExpression(GetColumn.COLUMN_DEF.name(), defaultStr);
+
+        // MOD xqliu 2009-10-29 bug 9838
+        // MOD xqliu 2009-12-08 bug 9822, if use odbc connect to sqlserver the resultset is forward only!!!
+        String colName = null;
+        try {
+            colName = resutSet.getString(GetColumn.COLUMN_NAME.name());
+        } catch (Exception e1) {
+            log.warn(e1, e1);
+            if (colName == null) {
+                colName = e1.getMessage();
+            }
+        }
+        column.setName(colName);
+
+        int dataType = 0;
+        try {
+            dataType = resutSet.getInt(GetColumn.DATA_TYPE.name());
+            // MOD scorreia 2010-07-24 removed the call to column.getSQLDataType() here because obviously the sql
+            // data type it is null and results in a NPE
+        } catch (Exception e) {
+            log.warn(e, e);
+        }
+
+        String typeName = null;
+        try {
+            typeName = resutSet.getString(GetColumn.TYPE_NAME.name());
+            // MOD zshen when the database is mssql,the datatype for "date" and "time" is "-9" and "-2"
+            // ,respective.so change them to "91" and "92" for adapt to Java2SqlType.
+            Connection conn = ConnectionHelper.getConnection(column);
+            if (isMssql(createConnection((DatabaseConnection) conn).getObject())) {
+                if (typeName.toLowerCase().equals("date")) {
+                    dataType = 91;
+                    // MOD scorreia 2010-07-24 removed the call to column.getSQLDataType() here because obviously
+                    // the sql
+                    // data type it is null and results in a NPE
+                } else if (typeName.toLowerCase().equals("time")) {
+                    dataType = 92;
+                    // MOD scorreia 2010-07-24 removed the call to column.getSQLDataType() here because obviously
+                    // the sql
+                    // data type it is null and results in a NPE
+                }
+            }
+        } catch (Exception e1) {
+            log.warn(e1, e1);
+        }
+
+        try {
+            column.setLength(resutSet.getInt(GetColumn.COLUMN_SIZE.name()));
+        } catch (Exception e1) {
+            log.warn(e1, e1);
+        }
+
+        int decimalDigits = 0;
+        try {
+            decimalDigits = resutSet.getInt(GetColumn.DECIMAL_DIGITS.name());
+        } catch (Exception e) {
+            log.warn(e);
+        }
+
+        int numPrecRadix = 0;
+        try {
+            numPrecRadix = resutSet.getInt(GetColumn.NUM_PREC_RADIX.name());
+        } catch (Exception e) {
+            log.warn(e);
+        }
+
+        // get column description (comment)
+        try {
+            String colComment = resutSet.getString(GetColumn.REMARKS.name());
+            if (colComment == null) {
+                colComment = "";
+            }
+            ColumnHelper.setComment(colComment, column);
+        } catch (Exception e) {
+            log.warn(e, e);
+        }
+
+        // --- create and set type of column
+        // TODO scorreia get type of column on demand, not on creation of column
+        // TdSqlDataType sqlDataType = DatabaseContentRetriever.createDataType(columns);
+        TdSqlDataType sqlDataType = createDataType(dataType, typeName, decimalDigits, numPrecRadix);
+        column.setSqlDataType(sqlDataType);
+        // column.setType(sqlDataType); // it's only reference to previous sql data type
+
+        try {
+            column.getSqlDataType().setNullable(NullableType.get(resutSet.getInt(GetColumn.NULLABLE.name())));
+        } catch (Exception e1) {
+            log.warn(e1, e1);
+        }
+
+        column.setInitialValue(defExpression);
+
+        return column;
+    }
+
     public static TdColumn addColumnAttribute(ResultSet resutSet, TdColumn column, DatabaseConnection conn) throws SQLException {
+        databaseconnection = conn;
         return addColumnAttribute(resutSet, column, createConnection(conn).getObject());
+
     }
 
     public static TdColumn addColumnAttribute(ResultSet resutSet, TdColumn column, java.sql.Connection conn) throws SQLException {
@@ -187,6 +312,13 @@ public class TDColumnAttributeHelper {
 
         column.setInitialValue(defExpression);
 
+        String mapping = databaseconnection.getDbmsId();
+        MappingTypeRetriever mappingTypeRetriever = MetadataTalendType.getMappingTypeRetriever(mapping);
+        String talendType = mappingTypeRetriever.getDefaultSelectedTalendType(typeName, ExtractMetaDataUtils.getIntMetaDataInfo(
+                resutSet, "COLUMN_SIZE"), ExtractMetaDataUtils.getIntMetaDataInfo(resutSet, //$NON-NLS-1$
+                "DECIMAL_DIGITS")); //$NON-NLS-1$
+        column.setTalendType(talendType);
+        // mappingTypeRetriever = MetadataTalendType.getMappingTypeRetriever(metadataConnection.getMapping());
         return column;
     }
 
@@ -269,6 +401,7 @@ public class TDColumnAttributeHelper {
      * @param providerConnection the provider connection
      * @return a ReturnCode (never null)
      * 
+     * getby JavaSqlFactory
      */
     private static TypedReturnCode<java.sql.Connection> createConnection(DatabaseConnection providerConnection) {
         TypedReturnCode<java.sql.Connection> rc = new TypedReturnCode<java.sql.Connection>(false);
