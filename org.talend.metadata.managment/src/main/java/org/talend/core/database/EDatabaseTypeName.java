6e8bff4a890ab6fa406ee470332c3d3d59cb188f
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-29 06:23:20 +0000

    bug 15926 fixed: avoid problems with HSQL db
    git-svn-id: http://talendforge.org/svn/tos/trunk@49133 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment/src/main/java/org/talend/core/database/EDatabaseTypeName.java
+++ org.talend.metadata.managment/src/main/java/org/talend/core/database/EDatabaseTypeName.java
@@ -23,15 +23,18 @@ public enum EDatabaseTypeName {
           "MySQL", "MySQL", new Boolean(false), "MYSQL", "MYSQL", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     PSQL(
          "PostgreSQL", "PostgreSQL", new Boolean(true), "POSTGRESQL", "POSTGRE", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-    PLUSPSQL("PostgresPlus", "PostgresPlus", new Boolean(true), "POSTGRESPLUS", "POSTGREPLUS"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+    PLUSPSQL(
+             "PostgresPlus", "PostgresPlus", new Boolean(true), "POSTGRESPLUS", "POSTGREPLUS", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     ORACLEFORSID(
                  "ORACLE_SID", "Oracle with SID", new Boolean(true), "ORACLE", "DBORACLE", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     ORACLESN(
              "ORACLE_SERVICE_NAME", "Oracle with service name", new Boolean(true), "ORACLE", "DBORACLE", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     ORACLE_OCI(
                "ORACLE_OCI", "Oracle OCI", new Boolean(true), "ORACLE", "DBORACLE", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-    GODBC("Generic ODBC", "Generic ODBC", new Boolean(false), "ODBC"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    MSODBC("Microsoft SQL (Odbc driver)", "Microsoft SQL Server (Odbc driver)", new Boolean(false), "ODBC"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    GODBC(
+          "Generic ODBC", "Generic ODBC", new Boolean(false), "ODBC", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    MSODBC(
+           "Microsoft SQL (Odbc driver)", "Microsoft SQL Server (Odbc driver)", new Boolean(false), "ODBC", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     IBMDB2(
            "IBM DB2", "IBM DB2", new Boolean(true), "IBM_DB2", "DB2", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     IBMDB2ZOS(
@@ -45,42 +48,62 @@ public enum EDatabaseTypeName {
     MSSQL(
           "MSSQL", "Microsoft SQL Server", new Boolean(true), "SQL_SERVER", "MSSQL", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     // this don't use in Branch 2.0
-    HSQLDB("HSQLDB", "HSQLDB", new Boolean(false), "HSQLDB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    HSQLDB_SERVER("HSQLDB Server", "HSQLDB Server", new Boolean(false), "HSQLDB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    HSQLDB_WEBSERVER("HSQLDB WebServer", "HSQLDB WebServer", new Boolean(false), "HSQLDB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    HSQLDB_IN_PROGRESS("HSQLDB In-Process", "HSQLDB In-Process", new Boolean(false), "HSQLDB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    HSQLDB(
+           "HSQLDB", "HSQLDB", new Boolean(false), "HSQLDB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    HSQLDB_SERVER(
+                  "HSQLDB Server", "HSQLDB Server", new Boolean(false), "HSQLDB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    HSQLDB_WEBSERVER(
+                     "HSQLDB WebServer", "HSQLDB WebServer", new Boolean(false), "HSQLDB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    HSQLDB_IN_PROGRESS(
+                       "HSQLDB In-Process", "HSQLDB In-Process", new Boolean(false), "HSQLDB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-    JAVADB("JavaDB", "JavaDB", new Boolean(false), "JAVADB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    JAVADB(
+           "JavaDB", "JavaDB", new Boolean(false), "JAVADB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     INGRES(
            "Ingres", "Ingres", new Boolean(false), "INGRES", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), // "INGRES"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    INTERBASE("Interbase", "Interbase", new Boolean(false), "INTERBASE"), // "INTERBASE"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    SQLITE("SQLite", "SQLite", new Boolean(false), "SQLITE"), // "SQLITE"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    FIREBIRD("FireBird", "FireBird", new Boolean(false), "FIREBIRD"), // "FIREBIRD"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    INTERBASE(
+              "Interbase", "Interbase", new Boolean(false), "INTERBASE", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), // "INTERBASE"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    SQLITE(
+           "SQLite", "SQLite", new Boolean(false), "SQLITE", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), // "SQLITE"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    FIREBIRD(
+             "FireBird", "FireBird", new Boolean(false), "FIREBIRD", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), // "FIREBIRD"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     INFORMIX(
              "Informix", "Informix", new Boolean(true), "INFORMIX", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), // "INFORMIX"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    VECTORWISE("VectorWise", "VectorWise", new Boolean(false), "VECTORWISE"), //$NON-NLS-1$ //$NON-NLS-2$  //$NON-NLS-3$
+    VECTORWISE(
+               "VectorWise", "VectorWise", new Boolean(false), "VECTORWISE", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$  //$NON-NLS-3$
 
-    ACCESS("Access", "Access", new Boolean(false), "ACCESS"), // "ACCESS"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    ACCESS(
+           "Access", "Access", new Boolean(false), "ACCESS", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), // "ACCESS"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     TERADATA(
              "Teradata", "Teradata", new Boolean(true), "TERADATA", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), // "TERADATA"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     AS400(
           "AS400", "AS400", new Boolean(false), "AS400", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Login), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-    JAVADB_EMBEDED("JavaDB Embeded", "JavaDB Embeded", new Boolean(false), "JAVADB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    JAVADB_JCCJDBC("JavaDB JCCJDBC", "JavaDB JCCJDBC", new Boolean(false), "JAVADB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    JAVADB_DERBYCLIENT("JavaDB DerbyClient", "JavaDB DerbyClient", new Boolean(false), "JAVADB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    JAVADB_EMBEDED(
+                   "JavaDB Embeded", "JavaDB Embeded", new Boolean(false), "JAVADB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    JAVADB_JCCJDBC(
+                   "JavaDB JCCJDBC", "JavaDB JCCJDBC", new Boolean(false), "JAVADB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    JAVADB_DERBYCLIENT(
+                       "JavaDB DerbyClient", "JavaDB DerbyClient", new Boolean(false), "JAVADB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-    VERTICA("Vertica", "Vertica", new Boolean(true), "VERTICA"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    VERTICA(
+            "Vertica", "Vertica", new Boolean(true), "VERTICA", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-    MAXDB("MAXDB", "MaxDB", new Boolean(false), "MAXDB"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    MAXDB(
+          "MAXDB", "MaxDB", new Boolean(false), "MAXDB", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-    GREENPLUM("Greenplum", "Greenplum", new Boolean(true), "GREENPLUM", "GREENPLUM"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-    PARACCEL("ParAccel", "ParAccel", new Boolean(true), "PARACCEL", "PARACCEL"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-    NETEZZA("Netezza", "Netezza", new Boolean(false), "NETEZZA"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    SAS("SAS", "SAS", new Boolean(false), "SAS"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    GREENPLUM(
+              "Greenplum", "Greenplum", new Boolean(true), "GREENPLUM", "GREENPLUM", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+    PARACCEL(
+             "ParAccel", "ParAccel", new Boolean(true), "PARACCEL", "PARACCEL", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+    NETEZZA(
+            "Netezza", "Netezza", new Boolean(false), "NETEZZA", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    SAS("SAS", "SAS", new Boolean(false), "SAS", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
     // General JDBC not support schema defalut
-    GENERAL_JDBC("General JDBC", "General JDBC", new Boolean(false), "JDBC"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    EXASOL("Exasol", "Exasol", new Boolean(true), "Exasol");//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    GENERAL_JDBC(
+                 "General JDBC", "General JDBC", new Boolean(false), "JDBC", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    EXASOL(
+           "Exasol", "Exasol", new Boolean(true), "Exasol", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.None);//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
     // displayName is used in Java code.
     private String displayName;
@@ -128,14 +151,6 @@ public enum EDatabaseTypeName {
         return this.xmlType;
     }
 
-    EDatabaseTypeName(String dbType, String displayName, Boolean isNeedSchema, String product) {
-        this.displayName = displayName;
-        this.isNeedSchema = isNeedSchema;
-        this.dbType = dbType;
-        this.product = product;
-        this.xmlType = product;
-    }
-
     EDatabaseTypeName(String dbType, String displayName, Boolean isNeedSchema, String product,
             EDatabaseSchemaOrCatalogMapping catalogMappingField, EDatabaseSchemaOrCatalogMapping schemaMappingField) {
         this.displayName = displayName;
@@ -145,14 +160,6 @@ public enum EDatabaseTypeName {
         this.xmlType = product;
         this.catalogMappingField = catalogMappingField;
         this.schemaMappingField = schemaMappingField;
-    }
-
-    EDatabaseTypeName(String dbType, String displayName, Boolean isNeedSchema, String product, String xmlType) {
-        this.displayName = displayName;
-        this.isNeedSchema = isNeedSchema;
-        this.dbType = dbType;
-        this.product = product;
-        this.xmlType = xmlType;
     }
 
     EDatabaseTypeName(String dbType, String displayName, Boolean isNeedSchema, String product, String xmlType,
