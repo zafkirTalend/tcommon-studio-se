25ae58682d76792aa9b13a11cc8399f1dadfda51
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-11-04 10:44:48 +0000

    git-svn-id: http://talendforge.org/svn/tos/trunk@50931 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.model/src/main/java/org/talend/designer/core/model/utils/emf/talendfile/impl/MetadataTypeImpl.java
+++ org.talend.model/src/main/java/org/talend/designer/core/model/utils/emf/talendfile/impl/MetadataTypeImpl.java
@@ -25,23 +25,24 @@ import org.talend.designer.core.model.utils.emf.talendfile.TalendFilePackage;
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getColumn <em>Column</em>}</li>
- *   <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getComment <em>Comment</em>}</li>
- *   <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getConnector <em>Connector</em>}</li>
- *   <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getLabel <em>Label</em>}</li>
- *   <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getName <em>Name</em>}</li>
- *   <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getSource <em>Source</em>}</li>
+ * <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getColumn <em>Column</em>}</li>
+ * <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getComment <em>Comment</em>}</li>
+ * <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getConnector <em>Connector</em>}
+ * </li>
+ * <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getLabel <em>Label</em>}</li>
+ * <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getName <em>Name</em>}</li>
+ * <li>{@link org.talend.designer.core.model.utils.emf.talendfile.impl.MetadataTypeImpl#getSource <em>Source</em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @generated
  */
 public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
-     * The cached value of the '{@link #getColumn() <em>Column</em>}' containment reference list.
-     * <!-- begin-user-doc
+     * The cached value of the '{@link #getColumn() <em>Column</em>}' containment reference list. <!-- begin-user-doc
      * --> <!-- end-user-doc -->
+     * 
      * @see #getColumn()
      * @generated
      * @ordered
@@ -49,9 +50,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected EList column;
 
     /**
-     * The default value of the '{@link #getComment() <em>Comment</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The default value of the '{@link #getComment() <em>Comment</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getComment()
      * @generated
      * @ordered
@@ -59,9 +60,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected static final String COMMENT_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getComment() <em>Comment</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The cached value of the '{@link #getComment() <em>Comment</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getComment()
      * @generated
      * @ordered
@@ -69,9 +70,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected String comment = COMMENT_EDEFAULT;
 
     /**
-     * The default value of the '{@link #getConnector() <em>Connector</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The default value of the '{@link #getConnector() <em>Connector</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getConnector()
      * @generated
      * @ordered
@@ -79,9 +80,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected static final String CONNECTOR_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getConnector() <em>Connector</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The cached value of the '{@link #getConnector() <em>Connector</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getConnector()
      * @generated
      * @ordered
@@ -89,9 +90,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected String connector = CONNECTOR_EDEFAULT;
 
     /**
-     * The default value of the '{@link #getLabel() <em>Label</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The default value of the '{@link #getLabel() <em>Label</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getLabel()
      * @generated
      * @ordered
@@ -129,9 +130,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected String name = NAME_EDEFAULT;
 
     /**
-     * The default value of the '{@link #getSource() <em>Source</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The default value of the '{@link #getSource() <em>Source</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getSource()
      * @generated
      * @ordered
@@ -139,9 +140,9 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
     protected static final String SOURCE_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getSource() <em>Source</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The cached value of the '{@link #getSource() <em>Source</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getSource()
      * @generated
      * @ordered
@@ -150,14 +151,16 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
-    protected MetadataTypeImpl() {
+    public MetadataTypeImpl() {
         super();
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected EClass eStaticClass() {
@@ -166,6 +169,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public EList getColumn() {
@@ -177,6 +181,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getComment() {
@@ -185,6 +190,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setComment(String newComment) {
@@ -196,6 +202,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getConnector() {
@@ -204,17 +211,20 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setConnector(String newConnector) {
         String oldConnector = connector;
         connector = newConnector;
         if (eNotificationRequired())
-            eNotify(new ENotificationImpl(this, Notification.SET, TalendFilePackage.METADATA_TYPE__CONNECTOR, oldConnector, connector));
+            eNotify(new ENotificationImpl(this, Notification.SET, TalendFilePackage.METADATA_TYPE__CONNECTOR, oldConnector,
+                    connector));
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getLabel() {
@@ -223,6 +233,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setLabel(String newLabel) {
@@ -234,6 +245,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getName() {
@@ -242,6 +254,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setName(String newName) {
@@ -253,6 +266,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getSource() {
@@ -261,6 +275,7 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setSource(String newSource) {
@@ -272,123 +287,130 @@ public class MetadataTypeImpl extends EObjectImpl implements MetadataType {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
         switch (featureID) {
-            case TalendFilePackage.METADATA_TYPE__COLUMN:
-                return ((InternalEList)getColumn()).basicRemove(otherEnd, msgs);
+        case TalendFilePackage.METADATA_TYPE__COLUMN:
+            return ((InternalEList) getColumn()).basicRemove(otherEnd, msgs);
         }
         return super.eInverseRemove(otherEnd, featureID, msgs);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
         switch (featureID) {
-            case TalendFilePackage.METADATA_TYPE__COLUMN:
-                return getColumn();
-            case TalendFilePackage.METADATA_TYPE__COMMENT:
-                return getComment();
-            case TalendFilePackage.METADATA_TYPE__CONNECTOR:
-                return getConnector();
-            case TalendFilePackage.METADATA_TYPE__LABEL:
-                return getLabel();
-            case TalendFilePackage.METADATA_TYPE__NAME:
-                return getName();
-            case TalendFilePackage.METADATA_TYPE__SOURCE:
-                return getSource();
+        case TalendFilePackage.METADATA_TYPE__COLUMN:
+            return getColumn();
+        case TalendFilePackage.METADATA_TYPE__COMMENT:
+            return getComment();
+        case TalendFilePackage.METADATA_TYPE__CONNECTOR:
+            return getConnector();
+        case TalendFilePackage.METADATA_TYPE__LABEL:
+            return getLabel();
+        case TalendFilePackage.METADATA_TYPE__NAME:
+            return getName();
+        case TalendFilePackage.METADATA_TYPE__SOURCE:
+            return getSource();
         }
         return super.eGet(featureID, resolve, coreType);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void eSet(int featureID, Object newValue) {
         switch (featureID) {
-            case TalendFilePackage.METADATA_TYPE__COLUMN:
-                getColumn().clear();
-                getColumn().addAll((Collection)newValue);
-                return;
-            case TalendFilePackage.METADATA_TYPE__COMMENT:
-                setComment((String)newValue);
-                return;
-            case TalendFilePackage.METADATA_TYPE__CONNECTOR:
-                setConnector((String)newValue);
-                return;
-            case TalendFilePackage.METADATA_TYPE__LABEL:
-                setLabel((String)newValue);
-                return;
-            case TalendFilePackage.METADATA_TYPE__NAME:
-                setName((String)newValue);
-                return;
-            case TalendFilePackage.METADATA_TYPE__SOURCE:
-                setSource((String)newValue);
-                return;
+        case TalendFilePackage.METADATA_TYPE__COLUMN:
+            getColumn().clear();
+            getColumn().addAll((Collection) newValue);
+            return;
+        case TalendFilePackage.METADATA_TYPE__COMMENT:
+            setComment((String) newValue);
+            return;
+        case TalendFilePackage.METADATA_TYPE__CONNECTOR:
+            setConnector((String) newValue);
+            return;
+        case TalendFilePackage.METADATA_TYPE__LABEL:
+            setLabel((String) newValue);
+            return;
+        case TalendFilePackage.METADATA_TYPE__NAME:
+            setName((String) newValue);
+            return;
+        case TalendFilePackage.METADATA_TYPE__SOURCE:
+            setSource((String) newValue);
+            return;
         }
         super.eSet(featureID, newValue);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void eUnset(int featureID) {
         switch (featureID) {
-            case TalendFilePackage.METADATA_TYPE__COLUMN:
-                getColumn().clear();
-                return;
-            case TalendFilePackage.METADATA_TYPE__COMMENT:
-                setComment(COMMENT_EDEFAULT);
-                return;
-            case TalendFilePackage.METADATA_TYPE__CONNECTOR:
-                setConnector(CONNECTOR_EDEFAULT);
-                return;
-            case TalendFilePackage.METADATA_TYPE__LABEL:
-                setLabel(LABEL_EDEFAULT);
-                return;
-            case TalendFilePackage.METADATA_TYPE__NAME:
-                setName(NAME_EDEFAULT);
-                return;
-            case TalendFilePackage.METADATA_TYPE__SOURCE:
-                setSource(SOURCE_EDEFAULT);
-                return;
+        case TalendFilePackage.METADATA_TYPE__COLUMN:
+            getColumn().clear();
+            return;
+        case TalendFilePackage.METADATA_TYPE__COMMENT:
+            setComment(COMMENT_EDEFAULT);
+            return;
+        case TalendFilePackage.METADATA_TYPE__CONNECTOR:
+            setConnector(CONNECTOR_EDEFAULT);
+            return;
+        case TalendFilePackage.METADATA_TYPE__LABEL:
+            setLabel(LABEL_EDEFAULT);
+            return;
+        case TalendFilePackage.METADATA_TYPE__NAME:
+            setName(NAME_EDEFAULT);
+            return;
+        case TalendFilePackage.METADATA_TYPE__SOURCE:
+            setSource(SOURCE_EDEFAULT);
+            return;
         }
         super.eUnset(featureID);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public boolean eIsSet(int featureID) {
         switch (featureID) {
-            case TalendFilePackage.METADATA_TYPE__COLUMN:
-                return column != null && !column.isEmpty();
-            case TalendFilePackage.METADATA_TYPE__COMMENT:
-                return COMMENT_EDEFAULT == null ? comment != null : !COMMENT_EDEFAULT.equals(comment);
-            case TalendFilePackage.METADATA_TYPE__CONNECTOR:
-                return CONNECTOR_EDEFAULT == null ? connector != null : !CONNECTOR_EDEFAULT.equals(connector);
-            case TalendFilePackage.METADATA_TYPE__LABEL:
-                return LABEL_EDEFAULT == null ? label != null : !LABEL_EDEFAULT.equals(label);
-            case TalendFilePackage.METADATA_TYPE__NAME:
-                return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
-            case TalendFilePackage.METADATA_TYPE__SOURCE:
-                return SOURCE_EDEFAULT == null ? source != null : !SOURCE_EDEFAULT.equals(source);
+        case TalendFilePackage.METADATA_TYPE__COLUMN:
+            return column != null && !column.isEmpty();
+        case TalendFilePackage.METADATA_TYPE__COMMENT:
+            return COMMENT_EDEFAULT == null ? comment != null : !COMMENT_EDEFAULT.equals(comment);
+        case TalendFilePackage.METADATA_TYPE__CONNECTOR:
+            return CONNECTOR_EDEFAULT == null ? connector != null : !CONNECTOR_EDEFAULT.equals(connector);
+        case TalendFilePackage.METADATA_TYPE__LABEL:
+            return LABEL_EDEFAULT == null ? label != null : !LABEL_EDEFAULT.equals(label);
+        case TalendFilePackage.METADATA_TYPE__NAME:
+            return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+        case TalendFilePackage.METADATA_TYPE__SOURCE:
+            return SOURCE_EDEFAULT == null ? source != null : !SOURCE_EDEFAULT.equals(source);
         }
         return super.eIsSet(featureID);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String toString() {
-        if (eIsProxy()) return super.toString();
+        if (eIsProxy())
+            return super.toString();
 
         StringBuffer result = new StringBuffer(super.toString());
         result.append(" (comment: ");
