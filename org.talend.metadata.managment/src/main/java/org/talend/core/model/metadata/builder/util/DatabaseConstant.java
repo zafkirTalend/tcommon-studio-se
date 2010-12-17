cc1333740b5108b2326e8f5b66df3001c30e442a
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-17 01:46:27 +0000

    bug 17176:add the 'ODBC_TERADATA_PRODUCT_NAME' variable.
    git-svn-id: http://talendforge.org/svn/tos/trunk@52759 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/util/DatabaseConstant.java
+++ org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/util/DatabaseConstant.java
@@ -54,4 +54,6 @@ public final class DatabaseConstant {
     public static final String ODBC_INGRES_DRIVER_NAME = "JDBC-ODBC Bridge (CAIIOD";
 
     public static final String ODBC_PROGRESS_PRODUCT_NAME = "progress";
+
+    public static final String ODBC_TERADATA_PRODUCT_NAME = "teradata";
 }
