ad2ded7e1dfa0b3456b7fbd74f2efad19329f804
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-24 09:39:14 +0000

    bug 17890 fixed: Save the property to metadata with oracle component have NPE error
    git-svn-id: http://talendforge.org/svn/tos/trunk@53126 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/designerproperties/RepositoryToComponentProperty.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/designerproperties/RepositoryToComponentProperty.java
@@ -731,12 +731,12 @@ public class RepositoryToComponentProperty {
             if (databaseType.equals(EDatabaseTypeName.ORACLEFORSID.getDisplayName())) {
                 // see StatsAndLogConstants
                 // This connection is Oracle_SID
-                return EDatabaseTypeName.ORACLEFORSID.getXmlName();
+                return EDatabaseTypeName.ORACLEFORSID.getProduct();
             } else if (databaseType.equals(EDatabaseTypeName.ORACLESN.getDisplayName())) {
                 // This connection is Oracle_service_name
-                return EDatabaseTypeName.ORACLESN.getXmlName();
+                return EDatabaseTypeName.ORACLESN.getProduct();
             } else if (databaseType.equals(EDatabaseTypeName.ORACLE_OCI.getDisplayName())) {
-                return EDatabaseTypeName.ORACLE_OCI.getXmlName();
+                return EDatabaseTypeName.ORACLE_OCI.getProduct();
             } else if (databaseType.equals(EDatabaseTypeName.MSSQL.getDisplayName())) {
                 return EDatabaseTypeName.MSSQL.getXMLType(); // for component
             }
