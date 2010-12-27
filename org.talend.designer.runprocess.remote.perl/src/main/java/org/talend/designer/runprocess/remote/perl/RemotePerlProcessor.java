ecd921244797c590103e5a28ab58a04ff3f78ce2
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-27 05:31:01 +0000

    feature 17306 add: Separate plugins for the language generation.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@53149 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.runprocess.remote.perl/src/main/java/org/talend/designer/runprocess/remote/perl/RemotePerlProcessor.java
@@ -0,0 +1,76 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.runprocess.remote.perl;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.talend.core.GlobalServiceRegister;
+import org.talend.core.model.process.IProcess;
+import org.talend.core.model.properties.Property;
+import org.talend.core.ui.ICommandlineClientService;
+import org.talend.designer.core.perl.runprocess.PerlProcessor;
+import org.talend.designer.runprocess.IProcessMessageManager;
+import org.talend.designer.runprocess.ProcessorException;
+import org.talend.designer.runprocess.remote.RemoteProcessorHelper;
+import org.talend.designer.runprocess.remote.ui.RemoteManager;
+
+/**
+ * DOC amaumont class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend-code-templates.xml 1 2006-09-29 17:06:40Z nrousseau $
+ * 
+ */
+public class RemotePerlProcessor extends PerlProcessor {
+
+    RemoteManager manager = RemoteManager.getInstance();
+
+    /**
+     * DOC amaumont RemotePerlProcessor constructor comment.
+     * 
+     * @param process
+     * @param filenameFromLabel
+     */
+    public RemotePerlProcessor(IProcess process, Property property, boolean filenameFromLabel) {
+        super(process, property, filenameFromLabel);
+    }
+
+    @Override
+    public Process run(int statisticsPort, int tracePort, String watchParam, IProgressMonitor monitor,
+            IProcessMessageManager processMessageManager) throws ProcessorException {
+        if (manager.getTargetExecutionconfigureation().isRemote()) {
+            ICommandlineClientService cmdLineService = getCommandlineClientService();
+            if (cmdLineService != null && manager.getTargetExecutionconfigureation().getCommandlineServerConfig() != null) {
+                Process p = cmdLineService.deployAndRunByCommandline(manager.getTargetExecutionconfigureation(), this,
+                        statisticsPort, tracePort, watchParam);
+                if (p != null) {
+                    return p;
+                }
+            }
+
+            RemoteProcessorHelper remoteProcessorHelper = new RemoteProcessorHelper();
+            return remoteProcessorHelper.run(this, statisticsPort, tracePort, watchParam, monitor, processMessageManager);
+        } else {
+            return super.run(statisticsPort, tracePort, watchParam, monitor, processMessageManager);
+        }
+    }
+
+    private ICommandlineClientService getCommandlineClientService() {
+        ICommandlineClientService cmdLineServic = null;
+        try {
+            cmdLineServic = (ICommandlineClientService) GlobalServiceRegister.getDefault().getService(
+                    ICommandlineClientService.class);
+        } catch (RuntimeException e) {
+            // nothing to do
+        }
+        return cmdLineServic;
+    }
+}
---
+++ org.talend.designer.runprocess.remote.perl/src/main/java/org/talend/designer/runprocess/remote/perl/RunRemoteProcessPerlPlugin.java
@@ -0,0 +1,61 @@
+package org.talend.designer.runprocess.remote.perl;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class RunRemoteProcessPerlPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.talend.designer.runprocess.remote.perl"; //$NON-NLS-1$
+
+	// The shared instance
+	private static RunRemoteProcessPerlPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public RunRemoteProcessPerlPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static RunRemoteProcessPerlPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
