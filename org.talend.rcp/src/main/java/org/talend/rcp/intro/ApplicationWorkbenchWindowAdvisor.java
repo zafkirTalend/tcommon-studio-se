feffa1eaf2356b909d5b6df24d990792cd2fc8d7
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-02-22 05:53:31 +0000

    feature 18752 add:  Welcome tips
    git-svn-id: http://talendforge.org/svn/tos/trunk@55532 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp/src/main/java/org/talend/rcp/intro/ApplicationWorkbenchWindowAdvisor.java
+++ org.talend.rcp/src/main/java/org/talend/rcp/intro/ApplicationWorkbenchWindowAdvisor.java
@@ -23,12 +23,32 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PerspectiveAdapter;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
@@ -36,10 +56,12 @@ import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.handlers.IHandlerService;
+import org.eclipse.ui.internal.WorkbenchPage;
 import org.eclipse.ui.internal.ide.EditorAreaDropAdapter;
 import org.eclipse.ui.internal.ide.IDEInternalPreferences;
 import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
 import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.commons.utils.workbench.extensions.ExtensionImplementationProvider;
 import org.talend.commons.utils.workbench.extensions.ExtensionPointLimiterImpl;
 import org.talend.commons.utils.workbench.extensions.IExtensionPointLimiter;
@@ -72,6 +94,24 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     private List<IAction> actions = new ArrayList<IAction>();
 
     ApplicationActionBarAdvisor adviser = null;
+
+    private static String componentViewId = "org.talend.designer.core.ui.views.properties.ComponentSettingsView";
+
+    private static String gefViewId = "org.eclipse.gef.ui.palette_view";
+
+    private static String repositoryViewId = "org.talend.repository.views.repository";
+
+    private static String processViewId = "org.talend.designer.runprocess.ui.views.processview";
+
+    private Composite foreGroundComposite;
+
+    private Composite parentComposite;
+
+    private Composite backGroundComposite;
+
+    private boolean isTos = false;
+
+    private StackLayout stackLayout = new StackLayout();
 
     public static final IExtensionPointLimiter GLOBAL_ACTIONS = new ExtensionPointLimiterImpl("org.talend.core.global_actions", //$NON-NLS-1$
             "GlobalAction"); //$NON-NLS-1$
@@ -153,6 +193,233 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
             ApplicationDeletionUtil.removePreferencePages(this.getWindowConfigurer().getWindow(), Arrays.asList(prefsId));
         }
 
+        // feature 18752
+        setEditorAreaBG();
+        regisitPerspectiveListener();
+    }
+
+    private void regisitPerspectiveListener() {
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().addPerspectiveListener(new PerspectiveAdapter() {
+
+            @Override
+            public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
+                // MOD xqliu 2010-10-14 bug 15756
+                String pId = perspective.getId();
+                if (!pId.equals("org.talend.rcp.perspective")) {
+                    clearEditorAreaBG(false);
+                } else {
+                    if (isTos == true) {
+                        isTos = false;
+                        setEditorAreaBG();
+                    }
+                    clearEditorAreaBG(true);
+                }
+            }
+        });
+    }
+
+    private void clearEditorAreaBG(boolean flag) {
+        if (!parentComposite.isDisposed() && parentComposite != null)
+            if (flag) {
+                stackLayout.topControl = foreGroundComposite;
+                parentComposite.layout();
+            } else {
+                stackLayout.topControl = backGroundComposite;
+                parentComposite.layout();
+            }
+    }
+
+    private void setEditorAreaBG() {
+        // PlatformUI.getWorkbench().get
+        if (getWindowConfigurer().getWindow() == null) {
+            return;
+        }
+        if (getWindowConfigurer().getWindow().getActivePage() == null) {
+            return;
+        }
+        IBrandingService brandingService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
+                IBrandingService.class);
+        String title = brandingService.getShortProductName();
+        final WorkbenchPage page = (WorkbenchPage) getWindowConfigurer().getWindow().getActivePage();
+        if (!page.getPerspective().getId().equals("org.talend.rcp.perspective")) {
+            isTos = true;
+            return;
+        }
+        Composite client = page.getClientComposite();
+        Control[] children = client.getChildren();
+        for (int i = 0; i < children.length; i++) {
+            if (children[i] instanceof Composite) {
+                Composite child = (Composite) children[i];
+                Control[] controls = child.getChildren();
+                for (final Control control : controls) {
+                    if (control instanceof CTabFolder) {
+                        CTabFolder tabFolder = (CTabFolder) control;
+                        Control[] parents = tabFolder.getChildren();
+                        for (int j = 0; j < parents.length; j++) {
+                            if (parents[j] instanceof Composite) {
+                                Composite parent = (Composite) parents[j];
+                                parent.redraw();
+                                Image image = Activator.getImageDescriptor("icons/WelcomeScreenMockup.png").createImage();
+                                Image icon = Activator.getImageDescriptor("icons/createjob.png").createImage();
+                                GC gc = new GC(image);
+                                gc.setFont(new Font(parent.getDisplay(), "Arial", 10, SWT.NONE));
+                                gc.drawString(Messages.getString("tips.createJob"), 115, 145, true);
+                                gc.drawString(Messages.getString("tips.clickChoose"), 115, 160, true);
+                                gc.drawImage(icon, 265, 160);
+                                gc.drawText(Messages.getString("tips.optionMenu"), 357, 160, true);
+                                gc.drawText(Messages.getString("tips.addDataSource"), 485, 200, true);
+                                gc.drawText(Messages.getString("tips.components"), 485, 220, true);
+                                gc.setForeground(new Color(null, 0, 0, 0));
+                                gc.setFont(new Font(parent.getDisplay(), "Arial", 10, SWT.NONE));
+                                gc.drawText(Messages.getString("tips.viewAndEdit"), 125, 255, true);
+                                gc.drawText("in the", 125, 275, true);
+
+                                gc.drawText(Messages.getString("tips.finallyText"), 435, 320, true);
+                                gc.setForeground(new Color(null, 0, 0, 0));
+                                gc.setFont(new Font(parent.getDisplay(), "Arial", 10, SWT.NONE));
+                                gc.drawText(Messages.getString("tips.executeJob"), 590, 320, true);
+                                gc.drawText(Messages.getString("tips.viewResults"), 435, 340, true);
+                                gc.dispose();
+
+                                parentComposite = new Composite(parent, SWT.CENTER);
+                                parentComposite.setLayout(stackLayout);
+                                parentComposite.setSize(850, 450);
+                                backGroundComposite = new Composite(parentComposite, SWT.CENTER);
+                                backGroundComposite.setSize(850, 450);
+                                foreGroundComposite = new Composite(parentComposite, SWT.CENTER);
+
+                                foreGroundComposite.setLayout(new FormLayout());
+
+                                Label textTitle = new Label(foreGroundComposite, SWT.CENTER);
+                                FormData textTitleData = new FormData();
+                                textTitleData.left = new FormAttachment(0, 50);
+                                textTitleData.right = new FormAttachment(100, -65);
+                                textTitleData.top = new FormAttachment(0, 15);
+                                textTitleData.bottom = new FormAttachment(0, 49);
+                                textTitle.setLayoutData(textTitleData);
+                                textTitle.setFont(new Font(foreGroundComposite.getDisplay(), "Arial", 25, SWT.BOLD));
+                                textTitle.setBackground(new Color(null, 255, 255, 204));
+                                textTitle.setText(Messages.getString("tips.welcome") + " " + title);
+
+                                Label textSecondTitle = new Label(foreGroundComposite, SWT.CENTER);
+                                FormData textTitleData2 = new FormData();
+                                textTitleData2.left = new FormAttachment(0, 50);
+                                textTitleData2.right = new FormAttachment(100, -65);
+                                textTitleData2.top = new FormAttachment(0, 50);
+                                textTitleData2.bottom = new FormAttachment(0, 70);
+                                textSecondTitle.setLayoutData(textTitleData2);
+                                textSecondTitle.setFont(new Font(foreGroundComposite.getDisplay(), "Arial", 13, SWT.NONE));
+                                textSecondTitle.setBackground(new Color(null, 255, 255, 204));
+                                textSecondTitle.setText(title + " " + Messages.getString("tips.title"));
+
+                                Label textThirdTitle = new Label(foreGroundComposite, SWT.CENTER);
+                                FormData textTitleData3 = new FormData();
+                                textTitleData3.left = new FormAttachment(0, 50);
+                                textTitleData3.right = new FormAttachment(100, -65);
+                                textTitleData3.top = new FormAttachment(0, 70);
+                                textTitleData3.bottom = new FormAttachment(0, 90);
+                                textThirdTitle.setLayoutData(textTitleData3);
+                                textThirdTitle.setFont(new Font(foreGroundComposite.getDisplay(), "Arial", 13, SWT.NONE));
+                                textThirdTitle.setBackground(new Color(null, 255, 255, 204));
+                                textThirdTitle.setText(Messages.getString("tips.getStart"));
+
+                                Link jobDesign = new Link(foreGroundComposite, SWT.NONE);
+                                FormData layoutData = new FormData();
+                                layoutData.left = new FormAttachment(0, 381);
+                                layoutData.right = new FormAttachment(0, 485);
+                                layoutData.top = new FormAttachment(0, 145);
+                                layoutData.bottom = new FormAttachment(0, 165);
+                                jobDesign.setLayoutData(layoutData);
+                                jobDesign.setBackground(new Color(null, 255, 255, 204));
+                                jobDesign.setText("<a>Job Designer</a>.");
+                                jobDesign.addListener(SWT.Selection, new Listener() {
+
+                                    public void handleEvent(Event event) {
+                                        IViewPart view = page.findView(repositoryViewId);
+                                        try {
+                                            view = page.showView(repositoryViewId);
+                                        } catch (Exception e) {
+                                            ExceptionHandler.process(e);
+                                        }
+                                        page.findView(repositoryViewId).setFocus();
+                                    }
+                                });
+                                Link palette = new Link(foreGroundComposite, SWT.NONE);
+                                FormData paletteData = new FormData();
+                                paletteData.left = new FormAttachment(0, 680);
+                                paletteData.right = new FormAttachment(0, 720);
+                                paletteData.top = new FormAttachment(0, 220);
+                                paletteData.bottom = new FormAttachment(0, 240);
+                                palette.setLayoutData(paletteData);
+                                palette.setBackground(new Color(null, 255, 255, 204));
+                                palette.setText("<a>Palette </a>.");
+                                palette.addListener(SWT.Selection, new Listener() {
+
+                                    public void handleEvent(Event event) {
+                                        IViewPart view = page.findView(gefViewId);
+                                        try {
+                                            view = page.showView(gefViewId);
+                                        } catch (Exception e) {
+                                            ExceptionHandler.process(e);
+                                        }
+                                        page.findView(gefViewId).setFocus();
+                                    }
+                                });
+                                Link component = new Link(foreGroundComposite, SWT.NONE);
+                                FormData componentData = new FormData();
+                                componentData.left = new FormAttachment(0, 162);
+                                componentData.right = new FormAttachment(0, 260);
+                                componentData.top = new FormAttachment(0, 275);
+                                componentData.bottom = new FormAttachment(0, 295);
+                                component.setLayoutData(componentData);
+                                component.setBackground(new Color(null, 255, 255, 204));
+                                component.setText("<a>Component</a> tab.");
+                                component.addListener(SWT.Selection, new Listener() {
+
+                                    public void handleEvent(Event event) {
+                                        IViewPart view = page.findView(componentViewId);
+                                        try {
+                                            view = page.showView(componentViewId);
+                                        } catch (Exception e) {
+                                            ExceptionHandler.process(e);
+                                        }
+                                        page.findView(componentViewId).setFocus();
+                                    }
+                                });
+                                Link runJob = new Link(foreGroundComposite, SWT.NONE);
+                                FormData runJobData = new FormData();
+                                runJobData.left = new FormAttachment(0, 533);
+                                runJobData.right = new FormAttachment(0, 590);
+                                runJobData.top = new FormAttachment(0, 320);
+                                runJobData.bottom = new FormAttachment(0, 340);
+                                runJob.setLayoutData(runJobData);
+                                runJob.setBackground(new Color(null, 255, 255, 204));
+                                runJob.setText("<a> Run Job</a>");
+                                runJob.addListener(SWT.Selection, new Listener() {
+
+                                    public void handleEvent(Event event) {
+                                        IViewPart view = page.findView(processViewId);
+                                        try {
+                                            view = page.showView(processViewId);
+                                        } catch (Exception e) {
+                                            ExceptionHandler.process(e);
+                                        }
+                                        page.findView(processViewId).setFocus();
+                                    }
+                                });
+                                stackLayout.topControl = foreGroundComposite;
+                                foreGroundComposite.setBackgroundImage(image);
+                                foreGroundComposite.setSize(850, 450);
+                                parentComposite.layout();
+                                // lala.layout();
+
+                                // tabFolder.pack();
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
