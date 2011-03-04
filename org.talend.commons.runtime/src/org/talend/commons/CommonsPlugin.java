dd14cdd270d89f044715eeaedee317a19ddb80ad
sgandon <sgandon@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-04 14:00:50 +0000

    bug 19170 modifed : org.talend.commons split into org.talend.commons.runtime and org.talend.commons.
    git-svn-id: http://talendforge.org/svn/tos/trunk@56205 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.commons.runtime/src/org/talend/commons/CommonsPlugin.java
@@ -0,0 +1,62 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons;
+
+import org.eclipse.core.runtime.Plugin;
+
+/**
+ * Activator for Code Generator.
+ * 
+ * $Id$
+ * 
+ */
+public class CommonsPlugin extends Plugin {
+
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.talend.commons"; //$NON-NLS-1$
+
+    // The shared instance
+    private static CommonsPlugin plugin;
+
+    private static boolean headless = false;
+
+    private static boolean storeLibsInWorkspace = false;
+
+    
+    public static boolean isStoreLibsInWorkspace() {
+        return storeLibsInWorkspace;
+    }
+
+    public static void setStoreLibsInWorkspace(boolean storeLibsInWorkspace) {
+        CommonsPlugin.storeLibsInWorkspace = storeLibsInWorkspace;
+    }
+
+    /**
+     * Default Constructor.
+     */
+    public CommonsPlugin() {
+        plugin = this;
+    }
+
+    public static CommonsPlugin getDefault() {
+        return plugin;
+    }
+
+    public static boolean isHeadless() {
+        return headless;
+    }
+
+    public static void setHeadless(boolean headless) {
+        CommonsPlugin.headless = headless;
+    }
+}
---
+++ org.talend.commons.runtime/src/org/talend/commons/utils/platform/PluginChecker.java
@@ -0,0 +1,77 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons.utils.platform;
+
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public final class PluginChecker {
+
+    private static final String TDQ_ID = "org.talend.dataprofiler.core.tdq"; //$NON-NLS-1$
+
+    private static final String TDCP_ID = "org.talend.datacleansing.core"; //$NON-NLS-1$
+
+    private static final String TOP_BRANDING = "org.talend.rcp.branding.top"; //$NON-NLS-1$
+
+    private PluginChecker() {
+    }
+
+    /**
+     * Check if specific plug-in is loaded.
+     * 
+     * @return isLoaded
+     */
+    public static boolean isPluginLoaded(String pluginID) {
+        boolean isLoaded = true;
+        Bundle bundle = Platform.getBundle(pluginID);
+        if (bundle == null || (bundle != null && bundle.getState() == Bundle.UNINSTALLED)) {
+            isLoaded = false;
+        }
+        return isLoaded;
+    }
+
+    /**
+     * DOC bZhou Comment method "isTDQLoaded".
+     * 
+     * use it to test if the platform start by TDQ.
+     * 
+     * @return
+     */
+    public static boolean isTDQLoaded() {
+        return isPluginLoaded(TDQ_ID);
+    }
+
+    /**
+     * DOC bZhou Comment method "isTDCPLoaded".
+     * 
+     * use it to test if the platform start by TDCP.
+     * 
+     * @return
+     */
+    public static boolean isTDCPLoaded() {
+        return isPluginLoaded(TDCP_ID);
+    }
+
+    /**
+     * Method "isOnlyTopLoaded".
+     * 
+     * @return true when TOP is used standalone.
+     */
+    public static boolean isOnlyTopLoaded() {
+        return isPluginLoaded(TOP_BRANDING) && !isTDCPLoaded() && !isTDQLoaded();
+    }
+
+}
