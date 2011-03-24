338be5f4970940c25542dd660e4fd306f16bdfff
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-24 04:50:08 +0000

    selenium:replace this.waitforelementpresent with setspeed
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57263 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestBranchManageAddExistbranch.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestBranchManageAddExistbranch.java
@@ -12,11 +12,11 @@ public class TestBranchManageAddExistbranch extends Login {
 	public void addProjectbranchexist(String project, String branchname) {
 		// first add a branch to a project
 		selenium.setSpeed(MAX_SPEED);
-		this.waitForElementPresent("!!!menu.project.element!!!", 30);
+//		this.waitForElementPresent("!!!menu.project.element!!!", 30);
 		selenium.click("!!!menu.project.element!!!");//
-		this.waitForElementPresent(
-				"//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
-						+ project + "')]", 30);
+//		this.waitForElementPresent(
+//				"//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+//						+ project + "')]", 30);
 		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
 				+ project + "')]");// the selected project's id
 		selenium.click("idBranchManagementButton");
