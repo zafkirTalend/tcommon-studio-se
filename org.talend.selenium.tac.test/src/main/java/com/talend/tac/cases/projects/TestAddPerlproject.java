d871a4d7651f0eb04aec102852bab94d10efaed0
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-28 07:04:24 +0000

    selenium:update for TestAddPerlproject new feature type added
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57493 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestAddPerlproject.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestAddPerlproject.java
@@ -13,17 +13,22 @@ public class TestAddPerlproject extends Login{
 	TestDeletepro delete = new TestDeletepro();
   @Test
   @Parameters({ "SVNPerlProjecturl","ProjectType", "SVNuserName", "SVNuserPassword","Prolanguage" })
-  public void testAddPerlproject(String perlUrl,String protype,String username,String password,String language) {
+  public void testAddPerlproject(String perlUrl,String type,String username,String password,String language) throws InterruptedException {
 	  this.waitForElementPresent("!!!menu.project.element!!!", 30);
 	  String namecommon = "TestPro";
 	  String svnUrl =perlUrl;
 	  selenium.setSpeed(MID_SPEED);
 	  selenium.click("!!!menu.project.element!!!");
 	  selenium.click("idSubModuleAddButton");
-	  selenium.click("idLabelInput");
+	  Thread.sleep(5000);
 	  selenium.setSpeed(MIN_SPEED);
-	  selenium.type("idLabelInput", namecommon);
-	  selenium.fireEvent("idLabelInput", "blur");
+	  this.typeString("idLabelInput", namecommon);
+	// add the type select option selenium.setSpeed("2000");
+		if (selenium.isVisible("idProjectTypeComboBox")) {
+			selenium.click("idProjectTypeComboBox");
+			selenium.mouseDown("//div[text()='" + type + "']");
+			selenium.fireEvent("idProjectTypeComboBox", "blur");
+		}
 //	  if ("Java".equals(language) || "".equals(language)) {
 //
 //		} else {
@@ -34,35 +39,25 @@ public class TestAddPerlproject extends Login{
 		  selenium.click("idLanguageInput");
 		  selenium.mouseDownAt("//div[@role='listitem'][2]",""+KeyEvent.VK_ENTER);
 	  }
-	  selenium.type("idDescriptionInput", "adf");
-	  selenium.fireEvent("idDescriptionInput", "blur");
 	  selenium.click("idAdvanceInput");
-	  selenium.type("idUrlInput", svnUrl);// svn
-	  selenium.fireEvent("idUrlInput", "blur");
-	  selenium.type("idLoginInput", username);// svn account
-	  selenium.fireEvent("idLoginInput", "blur");
-	  selenium.type("idPasswordInput", password);// svn password
-	  selenium.fireEvent("idPasswordInput", "blur");
+	  this.typeString("idUrlInput", svnUrl);
+	  this.typeString("idLoginInput", username);
+	  this.typeString("idPasswordInput", password);
 	  selenium.click("idSvnCommitInput");
 	  selenium.mouseDownAt("//div[@role='listitem'][2]",""+KeyEvent.VK_ENTER);
 	  selenium.click("idSvnLockInput");
 	  selenium.mouseDownAt("//div[@role='listitem'][2]",""+KeyEvent.VK_ENTER);
 	  selenium.click("idSvnUserLogInput");
 	  selenium.setSpeed(MAX_SPEED);
-	  selenium.click("idDescriptionInput");
+	  this.typeString("idDescriptionInput", "perlproject");
 //	  selenium.focus("idFormSaveButton");
 //	  selenium.keyDownNative(""+KeyEvent.VK_ENTER);
 //	  selenium.keyUpNative(""+KeyEvent.VK_ENTER);
 	  selenium.click("idFormSaveButton");
-	  try {
-		Thread.sleep(5000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+	  Thread.sleep(5000);
 	  Assert.assertTrue(
 	    selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
-	      + namecommon + "')]")&&(selenium.isElementPresent("//img[@title='perl']")), "common project added failed");
+	      + namecommon + "')]")&&(selenium.isElementPresent("//img[@title='perl']")), "Perl project added failed");
 	 
 	  //delete the added perl project
 	  delete.okDelete(selenium,namecommon);
