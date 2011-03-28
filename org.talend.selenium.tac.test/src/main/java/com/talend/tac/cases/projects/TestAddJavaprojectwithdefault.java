a156077f032edbbf762e06d4ea82a9e4937475e0
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-28 07:07:30 +0000

    selenium:update for TestAddJavaprojectwithdefault new feature type added
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57494 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestAddJavaprojectwithdefault.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestAddJavaprojectwithdefault.java
@@ -8,42 +8,52 @@ import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 
-public class TestAddJavaprojectwithdefault extends Login{
+public class TestAddJavaprojectwithdefault extends Login {
 	TestDeletepro delete = new TestDeletepro();
-  @Test
-  @Parameters({"ProjectType","Prolanguage" })
-  public void testAddPerlproject(String protype,String language) {
-	  this.waitForElementPresent("!!!menu.project.element!!!", 30);
-	  String namecommon = "JavaDefaultPro";
-	 
-	  selenium.setSpeed(MID_SPEED);
-	  selenium.click("!!!menu.project.element!!!");
-	  selenium.click("idSubModuleAddButton");
-	  selenium.click("idLabelInput");
-	  selenium.setSpeed(MIN_SPEED);
-	  selenium.type("idLabelInput", namecommon);
-	  selenium.fireEvent("idLabelInput", "blur");
-	  if (!"Java".equals(language) && !"".equals(language)) {
-		  selenium.click("idLanguageInput");
-		  selenium.mouseDownAt("//div[@role='listitem'][2]",""+KeyEvent.VK_ENTER);
-	  }
-	  selenium.setSpeed(MAX_SPEED);
-	  selenium.type("idDescriptionInput", "adf");
-//	  selenium.focus("idFormSaveButton");
-//	  selenium.keyDownNative(""+KeyEvent.VK_ENTER);
-//	  selenium.keyUpNative(""+KeyEvent.VK_ENTER);
-	  selenium.click("idFormSaveButton");
-	  try {
+
+	@Test
+	@Parameters({ "ProjectType", "Prolanguage" })
+	public void testAddPerlproject(String type, String language)
+			throws InterruptedException {
+		this.waitForElementPresent("!!!menu.project.element!!!", 30);
+		String namecommon = "JavaDefaultPro";
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("!!!menu.project.element!!!");
+		selenium.click("idSubModuleAddButton");
 		Thread.sleep(5000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		selenium.setSpeed(MID_SPEED);
+		// add the type select option selenium.setSpeed("2000");
+		if (selenium.isVisible("idProjectTypeComboBox")) {
+			selenium.click("idProjectTypeComboBox");
+			selenium.mouseDownAt(("//div[text()='" + type + "']"),""+KeyEvent.VK_ENTER);
+//			selenium.fireEvent("idProjectTypeComboBox", "blur");
+			
+		}
+		// type project label
+		this.typeString("idLabelInput", namecommon);
+	
+		// select project language
+		if (!"Java".equals(language) && !"".equals(language)) {
+			selenium.click("idLanguageInput");
+			selenium.mouseDownAt("//div[@role='listitem'][2]", ""
+					+ KeyEvent.VK_ENTER);
+		}
+		
+		selenium.setSpeed(MAX_SPEED);
+		this.typeString("idDescriptionInput", "javadefault");
+		// selenium.focus("idFormSaveButton");
+		// selenium.keyDownNative(""+KeyEvent.VK_ENTER);
+		// selenium.keyUpNative(""+KeyEvent.VK_ENTER);
+		System.out.println("before click "+selenium.getValue("idProjectTypeComboBox"));
+		selenium.click("idFormSaveButton");
+		Thread.sleep(5000);
+		Assert.assertTrue(
+				selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+						+ namecommon + "')]")
+						&& (selenium.isElementPresent("//img[@title='java']")&&(selenium.isElementPresent("//img[@title='"+type+"']"))),
+				"javadefault project added failed");
+
+		delete.okDelete(selenium, namecommon);
+		selenium.setSpeed(MIN_SPEED);
 	}
-	  Assert.assertTrue(
-	    selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
-	      + namecommon + "')]")&&(selenium.isElementPresent("//img[@title='java']")), "javadefault project added failed");
-	 
-	  delete.okDelete(selenium,namecommon);
-	  selenium.setSpeed(MIN_SPEED);
-	 }
 }
