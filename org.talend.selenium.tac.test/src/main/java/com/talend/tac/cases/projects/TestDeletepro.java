6e5feda36ae75ff1b650ac23eb28d931560c077d
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-28 06:58:50 +0000

    selenium:update for TestDeletepro.okDelete(Selenium ,String ) replace setspeed(MID_SPEED) with setspeed(MAX_SPEED)
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57489 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestDeletepro.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestDeletepro.java
@@ -1,4 +1,5 @@
 package com.talend.tac.cases.projects;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Parameters;
@@ -7,53 +8,75 @@ import com.talend.tac.cases.Login;
 import com.thoughtworks.selenium.Selenium;
 
 public class TestDeletepro extends Login {
-	
+
 	public void cancelDelete(String name) {
 		selenium.click("!!!menu.project.element!!!");
-		this.waitForElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+name+"')]", 60);
-		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+name+"')]");
+		this.waitForElementPresent(
+				"//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+						+ name + "')]", 60);
+		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+				+ name + "')]");
 		selenium.chooseCancelOnNextConfirmation();
 		selenium.click("idSubModuleDeleteButton");
-		assert (selenium.getConfirmation()
-				.matches(other.getString("delete.project.warning")));
+		assert (selenium.getConfirmation().matches(other
+				.getString("delete.project.warning")));
 		// selenium.setSpeed("3000");
 	}
 
 	public void okDelete(String name) {
 		selenium.click("!!!menu.project.element!!!");
-		this.waitForElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+name+"')]", 60);
-		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+name+"')]");
+		this.waitForElementPresent(
+				"//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+						+ name + "')]", 60);
+		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+				+ name + "')]");
 		selenium.chooseOkOnNextConfirmation();
 		selenium.click("idSubModuleDeleteButton");
-		assert (selenium.getConfirmation()
-				.matches(other.getString("delete.project.warning")));
+		assert (selenium.getConfirmation().matches(other
+				.getString("delete.project.warning")));
 
 	}
-	public void okDelete(Selenium selenium,String name){
-//		s.click("!!!menu.project.element!!!");
-		if(selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+name+"')]"))
-		{ selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+name+"')]");
-		selenium.chooseOkOnNextConfirmation();
-		selenium.click("idSubModuleDeleteButton");
-		assert (selenium.getConfirmation()
-				.matches(other.getString("delete.project.warning")));
+
+	public void okDelete(Selenium selenium, String name)
+			throws InterruptedException {
+		// s.click("!!!menu.project.element!!!");
+		selenium.setSpeed(MAX_SPEED);
+		selenium.refresh();
+
+		if (selenium
+				.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+						+ name + "')]")) {
+			selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+					+ name + "')]");
+			selenium.chooseOkOnNextConfirmation();
+			selenium.click("idSubModuleDeleteButton");
+			selenium.getConfirmation();
+			Thread.sleep(5000);
+			Assert.assertFalse(
+					selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+							+ name + "')]"), "delete project " + name
+							+ " failed!");
 		}
-		
+		selenium.setSpeed(MIN_SPEED);
 	}
 
-	@Test(groups = { "Second" },dependsOnGroups={"Add"})
+	@Test(groups = { "Second" }, dependsOnGroups = { "Add" })
 	@Parameters({ "DeleteProjectname" })
 	public void testDeletepro(String deleteProname) throws Exception {
-		
+
 		this.waitForElementPresent("!!!menu.project.element!!!", 30);
 		cancelDelete(deleteProname);
 		okDelete(deleteProname);
-		Assert.assertFalse(!selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+deleteProname+"')]"), "Project delete failed!");
+		Assert.assertFalse(
+				!selenium
+						.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+								+ deleteProname + "')]"),
+				"Project delete failed!");
 	}
 
 	@AfterMethod
 	public void tearDown() throws Exception {
-//		selenium.stop();
+		// selenium.stop();
 	}
 
 }
