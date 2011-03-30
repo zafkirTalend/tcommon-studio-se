5a32914d7b1fa01568428410cc4c9d736b8b2eeb
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-30 05:26:12 +0000

    selenium case: add case for add a 'plan'        add a always exist 'plan'
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57614 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/AddPlan.java
@@ -0,0 +1,81 @@
+package com.talend.tac.cases.executePlan;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.cases.Login;
+
+public class AddPlan extends Login {
+     
+	//add a method(add Plan)
+	public void addPlan(String label,String description) {
+		
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
+	    selenium.setSpeed(MID_SPEED);
+		selenium.click("//button[text()='Add plan']");
+	    
+	    selenium.type("//input[@class=' x-form-field x-form-text x-form-invalid' and @name='label']", label);
+	    selenium.fireEvent("//input[@name='label']", "blur");
+		
+	    selenium.type("//input[@class=' x-form-field x-form-text' and @name='description']", description);
+	    selenium.fireEvent("//input[@name='description']", "blur");
+	    
+	}      
+	
+	
+	//clear all plan
+	@Test(groups={"addPlan"})
+	public void clearplan() {
+		
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
+		selenium.click("idSubModuleRefreshButton");
+		String xpathPlan = "//div[@class='x-grid3-cell-inner x-grid3-col-label']";
+			
+		for(int i =0;;i++) {
+			
+			if(selenium.isElementPresent(xpathPlan)) {
+				
+				selenium.mouseDown(xpathPlan);
+				selenium.click("idSubModuleDeleteButton");
+				Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the " +
+						"selected execution plan [\\s\\S]$"));
+			} else {
+				
+				System.out.println("plan is no exist");
+				break;
+				
+			}
+		} 
+				
+	}
+	
+	//add a plan 
+	@Test(dependsOnMethods={"clearplan"})
+	@Parameters({"label","description"})
+	public void testAddPlan(String label,String description) {
+		
+		addPlan(label, description);
+	    
+	    selenium.click("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
+	    selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.isElementPresent("//div[text()='testAddPlan']"));
+	    selenium.setSpeed(MIN_SPEED);
+	}   
+	
+	//add a exist plan 
+	@Test(dependsOnMethods={"testAddPlan"})
+	@Parameters({"label","description"})
+	public void testAddExistPlan(String label,String description) {
+		
+	    addPlan(label, description);
+	    
+	    selenium.click("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
+	    selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.isTextPresent(rb.getString("executionPlan.error.uniqueLabel")));
+	    selenium.setSpeed(MIN_SPEED);
+	}  
+	
+}
