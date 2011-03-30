c1b0a7c39743d80c882c199ab6133c1a4e5d6ae1
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-30 08:25:09 +0000

    selenium: add parameters
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57637 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/DeletePlan.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/DeletePlan.java
@@ -1,6 +1,7 @@
 package com.talend.tac.cases.executePlan;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
@@ -9,33 +10,35 @@ public class DeletePlan extends Login {
     
 	//delete a plan (choose 'Cancel')
 	@Test(dependsOnGroups={"modifyPlan"})
-	public void testCancelDeletePlan() {
+	@Parameters({"plan.delete.label"})
+	public void testCancelDeletePlan(String deletelabel) {
 		
 		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");//into 'executionplan'
 		Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
 		selenium.click("idSubModuleRefreshButton");
-		selenium.mouseDown("//div[text()='testModifyPlan']");//choose a exist plan
+		selenium.mouseDown("//div[text()='"+deletelabel+"']");//choose a exist plan
 		selenium.chooseCancelOnNextConfirmation();//choose 'Cancel'
 		selenium.click("idSubModuleDeleteButton");//click 'Delete' button
 		Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected execution plan [\\s\\S]$"));
 		selenium.setSpeed(MID_SPEED);
-		Assert.assertTrue(selenium.isElementPresent("//div[text()='testModifyPlan']"));
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='"+deletelabel+"']"));
 		selenium.setSpeed(MIN_SPEED);
 		
 	}
 	
 	//delete a plan (choose 'Ok')
 	@Test(dependsOnMethods={"testCancelDeletePlan"})
-	public void testDeletePlan() {
+	@Parameters({"plan.delete.label"})
+	public void testDeletePlan(String deletelabel) {
 		
 		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
 		Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
-		selenium.mouseDown("//div[text()='testModifyPlan']");//choose a exist plan
+		selenium.mouseDown("//div[text()='"+deletelabel+"']");//choose a exist plan
 		selenium.chooseOkOnNextConfirmation();//choose 'Ok'
 		selenium.click("idSubModuleDeleteButton");//click 'Delete' button
 		Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected execution plan [\\s\\S]$"));
 		selenium.setSpeed(MID_SPEED);
-		Assert.assertFalse(selenium.isElementPresent("//div[text()='testModifyPlan']"));
+		Assert.assertFalse(selenium.isElementPresent("//div[text()='"+deletelabel+"']"));
 		selenium.setSpeed(MIN_SPEED);
 		
 	}
--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/ModifyPlan.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/ModifyPlan.java
@@ -1,6 +1,7 @@
 package com.talend.tac.cases.executePlan;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
@@ -9,22 +10,23 @@ public class ModifyPlan extends Login {
     
 	//modify a plan
 	@Test(groups={"modifyPlan"},dependsOnGroups={"addPlan"})
-	public void testModifyPlan() {
+	@Parameters({"plan.label","plan.modifyLabel","plan.modifyDescription"})
+	public void testModifyPlan(String label,String newlabel,String newdescription) {
 		
 		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");//into 'executionplan'
 		Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
 		selenium.click("idSubModuleRefreshButton");
-		selenium.mouseDown("//div[text()='testAddPlan']");//select a exist plan
+		selenium.mouseDown("//div[text()='"+label+"']");//select a exist plan
 		
         selenium.setSpeed(MID_SPEED);
-        selenium.type("//input[@name='label']", "testModifyPlan");//label
+        selenium.type("//input[@name='label']", newlabel);//label
 		selenium.fireEvent("//input[@name='label']","blur");
-		selenium.type("//input[@class=' x-form-field x-form-text' and @name='description']", "test modify a plan");//description
+		selenium.type("//input[@class=' x-form-field x-form-text' and @name='description']", newdescription);//description
 		selenium.fireEvent("//input[@name='description']","blur");
 		
 	    selenium.click("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
 	    selenium.setSpeed(MID_SPEED);
-	    Assert.assertTrue(selenium.isElementPresent("//div[text()='testModifyPlan']"));
+	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+newlabel+"']"));
 	    selenium.setSpeed(MIN_SPEED);
 
 	}
