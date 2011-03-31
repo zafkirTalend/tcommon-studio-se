d5abff9eb6b0033cfe651b084621ae3a56f4fb7e
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-31 09:13:46 +0000

    selenium: cases optimize
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57725 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/users/TestDisplayUser.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/users/TestDisplayUser.java
@@ -19,6 +19,7 @@ public class TestDisplayUser extends Login {
 		Assert.assertTrue(selenium.isTextPresent(userName));
 		selenium.setSpeed(MID_SPEED);
 		selenium.click("//div[@class=' x-grid3-hd-inner x-grid3-hd-login x-component sort-asc ']/a[1] ");
+		selenium.setSpeed(MIN_SPEED);
 		selenium.setSpeed(MID_SPEED);
 		selenium.click("//a[text()='Show in Groups']");
 		Assert.assertFalse(selenium.isElementPresent("//div[@class='x-grid-group-div']"));
@@ -34,11 +35,11 @@ public class TestDisplayUser extends Login {
 	public void testModifyDisplayUsersColumns(String userName) throws Exception {
 		
 		this.clickWaitForElementPresent("idMenuUserElement");
-		selenium.setSpeed(MAX_SPEED);
+		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.isTextPresent(userName));
 		selenium.setSpeed(MIN_SPEED);
 		selenium.click("//div[@class=' x-grid3-hd-inner x-grid3-hd-login x-component sort-asc ']/a[1] ");
-        selenium.setSpeed(MAX_SPEED);
+        selenium.setSpeed(MID_SPEED);
 		selenium.mouseOver("//a[text()='Columns']");
 	    selenium.setSpeed(MIN_SPEED);
 		modifyDisplayUsersColumns("Login");
@@ -57,6 +58,7 @@ public class TestDisplayUser extends Login {
         
         modifyDisplayUsersColumns("Creation");
         
+        selenium.click("idSubModuleRefreshButton");
 	}
     
     //Modify the user display order  
@@ -66,7 +68,7 @@ public class TestDisplayUser extends Login {
 			String lastName,String _lastName,String firstName,String _firstName) throws Exception {
 		
 		this.clickWaitForElementPresent("idMenuUserElement");
-		selenium.setSpeed(MAX_SPEED);
+		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.isTextPresent(userName));
 	
 		selenium.click("//div[@class=' x-grid3-hd-inner x-grid3-hd-login x-component sort-asc ']/a[1] ");
@@ -86,7 +88,7 @@ public class TestDisplayUser extends Login {
 			String LoginNameNotChooseActive,String LoginNameChooseMulripleRoles) throws Exception {
 		
 		this.clickWaitForElementPresent("idMenuUserElement");
-		selenium.setSpeed(MAX_SPEED);
+		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.isTextPresent(userName));
 	    selenium.setSpeed(MIN_SPEED);
 	    
@@ -110,13 +112,13 @@ public class TestDisplayUser extends Login {
 			if(selenium.isVisible("//div[text()='"+userName+"']")) {
 			 
 				 selenium.mouseDown("//div[text()='"+title+"']");
-				 selenium.setSpeed(MAX_SPEED);
+				 selenium.setSpeed(MID_SPEED);
 				 Assert.assertFalse(selenium.isVisible("//div[text()='"+userName+"']"));
 				 selenium.setSpeed(MIN_SPEED);
 			} else {
 				 
 				 selenium.mouseDown("//div[text()='"+title+"']");
-				 selenium.setSpeed(MAX_SPEED);
+				 selenium.setSpeed(MID_SPEED);
 				 Assert.assertTrue(selenium.isVisible("//div[text()='"+userName+"']"));
 				 selenium.setSpeed(MIN_SPEED);
 			}
@@ -134,12 +136,12 @@ public class TestDisplayUser extends Login {
 
 		if(selenium.isElementPresent("//span[text()='"+xpathName+"']")) {
 			selenium.click("//a[text()='"+xpathName+"']/img[@class=' x-menu-item-icon']");
-			selenium.setSpeed(MAX_SPEED);
+			selenium.setSpeed(MID_SPEED);
 		    Assert.assertFalse(selenium.isElementPresent("//span[text()='"+xpathName+"']"));
 		    selenium.setSpeed(MIN_SPEED);
 		} else {
 			
-			selenium.setSpeed(MAX_SPEED);
+			selenium.setSpeed(MID_SPEED);
 			selenium.click("//a[text()='"+xpathName+"']/img[@class=' x-menu-item-icon']");
 		    Assert.assertTrue(selenium.isElementPresent("//span[text()='"+xpathName+"']"));
 		    selenium.setSpeed(MIN_SPEED);
@@ -150,14 +152,14 @@ public class TestDisplayUser extends Login {
 	//Modify the user display order 
 	public void modifyDisplayUsersOrder(String xpathName,String value,String value1) {
 		
-		selenium.setSpeed(MAX_SPEED);  
+		selenium.setSpeed(MID_SPEED);  
 		selenium.click("//div[@class=' x-grid3-hd-inner x-grid3-hd-"+xpathName+" x-component']/a[1]");
 		selenium.click("//a[text()='Sort Descending']");
 		System.out.println(selenium.getText("//div[@class='x-grid3-cell-inner x-grid3-col-"+xpathName+"']"));
 		Assert.assertEquals(selenium.getText("//div[@class='x-grid3-cell-inner x-grid3-col-"+xpathName+"']"), value);
         selenium.setSpeed(MIN_SPEED);
        
-        selenium.setSpeed(MAX_SPEED);
+        selenium.setSpeed(MID_SPEED);
         selenium.click("//div[@class=' x-grid3-hd-inner x-grid3-hd-"+xpathName+" x-component sort-desc ']/a[1]");
 		selenium.click("//a[text()='Sort Ascending']");
 		System.out.println(selenium.getText("//div[@class='x-grid3-cell-inner x-grid3-col-"+xpathName+"']"));
--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/users/TestDuplicateUser.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/users/TestDuplicateUser.java
@@ -15,22 +15,21 @@ public class TestDuplicateUser extends Login {
 	public void testDuplicateUser(String userName,String DulicateUser,String Password,String DulicatedUser) throws Exception {
 	    
 		this.clickWaitForElementPresent("idMenuUserElement");
+		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.isTextPresent(userName));
 		
 		if(selenium.isTextPresent("Copy_of_aaa@gmail.com")) {
 			System.out.println("Copy_of_aaa@gmail.com is exist");
-			
+			selenium.setSpeed(MIN_SPEED);
 		} else {
-
+            
 			selenium.mouseDown(DulicateUser);//Select an existing user
+			selenium.setSpeed(MIN_SPEED);
 			selenium.click("idSubModuleDuplicateButton");
 			selenium.type("idUserPasswordInput", Password);
 			selenium.fireEvent("idUserPasswordInput", "blur");
 			selenium.click("idUserPasswordInput");
-	///****/
-//			selenium.click("idTypeInput");
-//			selenium.mouseDownAt("//div[@role='listitem'][1]", ""+Event.ENTER);
-	///****/  
+
 			selenium.click("idFirstAdminConnInput");
 			selenium.setSpeed(MID_SPEED);
 			selenium.click("idFormSaveButton");
