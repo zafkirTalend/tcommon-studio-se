72e52a905116cdfeef25c3f325d75d103a0bf401
fzhong <fzhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-01 10:11:57 +0000

    SWTBot: solve the problem for can not close widget because of can not check radio button when creating advance web service
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@57826 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.tos.swtbot.test/src/org/talend/swtbot/Utilities.java
+++ org.talend.tos.swtbot.test/src/org/talend/swtbot/Utilities.java
@@ -686,99 +686,103 @@ public class Utilities {
         treeNode.contextMenu("Create WSDL schema").click();
         gefBot.waitUntil(Conditions.shellIsActive("Create new WSDL schema"));
         shell = gefBot.shell("Create new WSDL schema").activate();
+        try {
+            /* step 1 of 4 */
+            gefBot.textWithLabel("Name").setText(webServiceName);
+            boolean nextButtonIsEnabled = gefBot.button("Next >").isEnabled();
+            if (nextButtonIsEnabled) {
+                gefBot.button("Next >").click();
+            } else {
+                shell.close();
+                Assert.assertTrue("next button is not enabled, maybe the item name is exist,", nextButtonIsEnabled);
+            }
 
-        /* step 1 of 4 */
-        gefBot.textWithLabel("Name").setText(webServiceName);
-        boolean nextButtonIsEnabled = gefBot.button("Next >").isEnabled();
-        if (nextButtonIsEnabled) {
-            gefBot.button("Next >").click();
-        } else {
-            shell.close();
-            Assert.assertTrue("next button is not enabled, maybe the item name is exist,", nextButtonIsEnabled);
-        }
+            if ("simple".equals(type)) {
+                /* step 2 of 4 */
+                gefBot.button("Next >").click();
 
-        if ("simple".equals(type)) {
-            /* step 2 of 4 */
-            gefBot.button("Next >").click();
+                /* step 3 of 4 */
+                gefBot.textWithLabel("WSDL").setText(System.getProperty("webService.url"));
+                gefBot.textWithLabel("Method").setText(System.getProperty("webService.method"));
+                gefBot.button("Add ").click();
+                gefBot.button("Refresh Preview").click();
+                gefBot.waitUntil(new DefaultCondition() {
 
-            /* step 3 of 4 */
-            gefBot.textWithLabel("WSDL").setText(System.getProperty("webService.url"));
-            gefBot.textWithLabel("Method").setText(System.getProperty("webService.method"));
-            gefBot.button("Add ").click();
-            gefBot.button("Refresh Preview").click();
+                    public boolean test() throws Exception {
+
+                        return gefBot.button("Next >").isEnabled();
+                    }
+
+                    public String getFailureMessage() {
+                        shell.close();
+                        return "next button was never enabled";
+                    }
+                }, 60000);
+                gefBot.button("Next >").click();
+            } else if ("advanced".equals(type)) {
+                /* step 2 of 4 */
+                gefBot.radio("Advanced WebService").click();
+                gefBot.button("Next >").click();
+
+                /* step 3 of 4 */
+                gefBot.button(1).click();// click refresh button
+                gefBot.waitUntil(Conditions.shellCloses(gefBot.shell("Progress Information")), 30000);
+                gefBot.tableWithLabel("Operation:").click(0, 0);
+
+                // set input mapping
+                // left schema
+                gefBot.cTabItem(" Input mapping ").activate();
+                gefBot.button("Schema Management").click();
+                gefBot.shell("Schema").activate();
+                gefBot.buttonWithTooltip("Add").click();
+                gefBot.button("OK").click();
+                // right schema
+                gefBot.table(1).click(0, 1);
+                gefBot.buttonWithTooltip("Add list element").click();
+                gefBot.shell("ParameterTree").activate();
+                gefBot.tree().select("City");
+                gefBot.button("OK").click();
+
+                gefBot.table(1).click(1, 0);
+                gefBot.text().setText("input.newColumn");
+
+                // set output mapping
+                // left schema
+                gefBot.cTabItem(" Output mapping ").activate();
+                gefBot.table(0).click(0, 0);
+                gefBot.buttonWithTooltip("Add List element").click();
+                gefBot.shell("ParameterTree").activate();
+                gefBot.tree().select("GetWeatherResult");
+                gefBot.button("OK").click();
+                // right schema
+                gefBot.button("...").click();
+                gefBot.buttonWithTooltip("Add").click();
+                gefBot.button("OK").click();
+
+                gefBot.table(1).click(0, 0);
+                gefBot.text().setText("parameters.GetWeatherResult");
+
+                gefBot.button("Next >").click();
+            }
+
+            /* step 4 of 4 */
             gefBot.waitUntil(new DefaultCondition() {
 
                 public boolean test() throws Exception {
 
-                    return gefBot.button("Next >").isEnabled();
+                    return gefBot.button("Finish").isEnabled();
                 }
 
                 public String getFailureMessage() {
-                    gefBot.shell("Create new WSDL schema").close();
-                    return "next button was never enabled";
+                    shell.close();
+                    return "finish button was never enabled";
                 }
-            }, 60000);
-            gefBot.button("Next >").click();
-        } else if ("advanced".equals(type)) {
-            /* step 2 of 4 */
-            gefBot.radio("Advanced WebService").click();
-            gefBot.button("Next >").click();
-
-            /* step 3 of 4 */
-            gefBot.button(1).click();// click refresh button
-            gefBot.waitUntil(Conditions.shellCloses(gefBot.shell("Progress Information")), 30000);
-            gefBot.tableWithLabel("Operation:").click(0, 0);
-
-            // set input mapping
-            // left schema
-            gefBot.cTabItem(" Input mapping ").activate();
-            gefBot.button("Schema Management").click();
-            gefBot.shell("Schema").activate();
-            gefBot.buttonWithTooltip("Add").click();
-            gefBot.button("OK").click();
-            // right schema
-            gefBot.table(1).click(0, 1);
-            gefBot.buttonWithTooltip("Add list element").click();
-            gefBot.shell("ParameterTree").activate();
-            gefBot.tree().select("City");
-            gefBot.button("OK").click();
-
-            gefBot.table(1).click(1, 0);
-            gefBot.text().setText("input.newColumn");
-
-            // set output mapping
-            // left schema
-            gefBot.cTabItem(" Output mapping ").activate();
-            gefBot.table(0).click(0, 0);
-            gefBot.buttonWithTooltip("Add List element").click();
-            gefBot.shell("ParameterTree").activate();
-            gefBot.tree().select("GetWeatherResult");
-            gefBot.button("OK").click();
-            // right schema
-            gefBot.button("...").click();
-            gefBot.buttonWithTooltip("Add").click();
-            gefBot.button("OK").click();
-
-            gefBot.table(1).click(0, 0);
-            gefBot.text().setText("parameters.GetWeatherResult");
-
-            gefBot.button("Next >").click();
+            });
+            gefBot.button("Finish").click();
+        } catch (Exception e) {
+            shell.close();
+            Assert.fail(e.getCause().getMessage());
         }
-
-        /* step 4 of 4 */
-        gefBot.waitUntil(new DefaultCondition() {
-
-            public boolean test() throws Exception {
-
-                return gefBot.button("Finish").isEnabled();
-            }
-
-            public String getFailureMessage() {
-                gefBot.shell("Create new WSDL schema").close();
-                return "finish button was never enabled";
-            }
-        });
-        gefBot.button("Finish").click();
 
         SWTBotTreeItem newWebServiceItem = null;
         try {
