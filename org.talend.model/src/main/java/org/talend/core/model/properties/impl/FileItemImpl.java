dfddd060d3d5a716afd286f1fc88d3407c34c3eb
hcyi <hcyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-02 09:12:03 +0000

    bug 20095 fixed : when save routine will throw error .
    git-svn-id: http://talendforge.org/svn/tos/trunk@57878 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.model/src/main/java/org/talend/core/model/properties/impl/FileItemImpl.java
+++ org.talend.model/src/main/java/org/talend/core/model/properties/impl/FileItemImpl.java
@@ -27,20 +27,20 @@ import org.talend.core.model.properties.helper.ByteArrayResource;
  * <p>
  * The following features are implemented:
  * <ul>
- *   <li>{@link org.talend.core.model.properties.impl.FileItemImpl#getName <em>Name</em>}</li>
- *   <li>{@link org.talend.core.model.properties.impl.FileItemImpl#getExtension <em>Extension</em>}</li>
- *   <li>{@link org.talend.core.model.properties.impl.FileItemImpl#getContent <em>Content</em>}</li>
+ * <li>{@link org.talend.core.model.properties.impl.FileItemImpl#getName <em>Name</em>}</li>
+ * <li>{@link org.talend.core.model.properties.impl.FileItemImpl#getExtension <em>Extension</em>}</li>
+ * <li>{@link org.talend.core.model.properties.impl.FileItemImpl#getContent <em>Content</em>}</li>
  * </ul>
  * </p>
- *
+ * 
  * @generated
  */
 public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
-     * The default value of the '{@link #getName() <em>Name</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
-     * end-user-doc -->
+     * The default value of the '{@link #getName() <em>Name</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+     * -->
+     * 
      * @see #getName()
      * @generated
      * @ordered
@@ -58,9 +58,9 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
     protected String name = NAME_EDEFAULT;
 
     /**
-     * The default value of the '{@link #getExtension() <em>Extension</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The default value of the '{@link #getExtension() <em>Extension</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getExtension()
      * @generated
      * @ordered
@@ -68,9 +68,9 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
     protected static final String EXTENSION_EDEFAULT = null;
 
     /**
-     * The cached value of the '{@link #getExtension() <em>Extension</em>}' attribute.
-     * <!-- begin-user-doc --> <!--
+     * The cached value of the '{@link #getExtension() <em>Extension</em>}' attribute. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getExtension()
      * @generated
      * @ordered
@@ -78,9 +78,9 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
     protected String extension = EXTENSION_EDEFAULT;
 
     /**
-     * The cached value of the '{@link #getContent() <em>Content</em>}' reference.
-     * <!-- begin-user-doc --> <!--
+     * The cached value of the '{@link #getContent() <em>Content</em>}' reference. <!-- begin-user-doc --> <!--
      * end-user-doc -->
+     * 
      * @see #getContent()
      * @generated
      * @ordered
@@ -89,6 +89,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected FileItemImpl() {
@@ -97,6 +98,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected EClass eStaticClass() {
@@ -105,6 +107,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getName() {
@@ -113,6 +116,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setName(String newName) {
@@ -124,6 +128,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String getExtension() {
@@ -132,6 +137,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setExtension(String newExtension) {
@@ -143,15 +149,17 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public ByteArray getContent() {
         if (content != null && content.eIsProxy()) {
-            InternalEObject oldContent = (InternalEObject)content;
-            content = (ByteArray)eResolveProxy(oldContent);
+            InternalEObject oldContent = (InternalEObject) content;
+            content = (ByteArray) eResolveProxy(oldContent);
             if (content != oldContent) {
                 if (eNotificationRequired())
-                    eNotify(new ENotificationImpl(this, Notification.RESOLVE, PropertiesPackage.FILE_ITEM__CONTENT, oldContent, content));
+                    eNotify(new ENotificationImpl(this, Notification.RESOLVE, PropertiesPackage.FILE_ITEM__CONTENT, oldContent,
+                            content));
             }
         }
         return content;
@@ -159,6 +167,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public ByteArray basicGetContent() {
@@ -167,6 +176,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void setContent(ByteArray newContent) {
@@ -178,81 +188,88 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
         switch (featureID) {
-            case PropertiesPackage.FILE_ITEM__NAME:
-                return getName();
-            case PropertiesPackage.FILE_ITEM__EXTENSION:
-                return getExtension();
-            case PropertiesPackage.FILE_ITEM__CONTENT:
-                if (resolve) return getContent();
-                return basicGetContent();
+        case PropertiesPackage.FILE_ITEM__NAME:
+            return getName();
+        case PropertiesPackage.FILE_ITEM__EXTENSION:
+            return getExtension();
+        case PropertiesPackage.FILE_ITEM__CONTENT:
+            if (resolve)
+                return getContent();
+            return basicGetContent();
         }
         return super.eGet(featureID, resolve, coreType);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void eSet(int featureID, Object newValue) {
         switch (featureID) {
-            case PropertiesPackage.FILE_ITEM__NAME:
-                setName((String)newValue);
-                return;
-            case PropertiesPackage.FILE_ITEM__EXTENSION:
-                setExtension((String)newValue);
-                return;
-            case PropertiesPackage.FILE_ITEM__CONTENT:
-                setContent((ByteArray)newValue);
-                return;
+        case PropertiesPackage.FILE_ITEM__NAME:
+            setName((String) newValue);
+            return;
+        case PropertiesPackage.FILE_ITEM__EXTENSION:
+            setExtension((String) newValue);
+            return;
+        case PropertiesPackage.FILE_ITEM__CONTENT:
+            setContent((ByteArray) newValue);
+            return;
         }
         super.eSet(featureID, newValue);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public void eUnset(int featureID) {
         switch (featureID) {
-            case PropertiesPackage.FILE_ITEM__NAME:
-                setName(NAME_EDEFAULT);
-                return;
-            case PropertiesPackage.FILE_ITEM__EXTENSION:
-                setExtension(EXTENSION_EDEFAULT);
-                return;
-            case PropertiesPackage.FILE_ITEM__CONTENT:
-                setContent((ByteArray)null);
-                return;
+        case PropertiesPackage.FILE_ITEM__NAME:
+            setName(NAME_EDEFAULT);
+            return;
+        case PropertiesPackage.FILE_ITEM__EXTENSION:
+            setExtension(EXTENSION_EDEFAULT);
+            return;
+        case PropertiesPackage.FILE_ITEM__CONTENT:
+            setContent((ByteArray) null);
+            return;
         }
         super.eUnset(featureID);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public boolean eIsSet(int featureID) {
         switch (featureID) {
-            case PropertiesPackage.FILE_ITEM__NAME:
-                return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
-            case PropertiesPackage.FILE_ITEM__EXTENSION:
-                return EXTENSION_EDEFAULT == null ? extension != null : !EXTENSION_EDEFAULT.equals(extension);
-            case PropertiesPackage.FILE_ITEM__CONTENT:
-                return content != null;
+        case PropertiesPackage.FILE_ITEM__NAME:
+            return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+        case PropertiesPackage.FILE_ITEM__EXTENSION:
+            return EXTENSION_EDEFAULT == null ? extension != null : !EXTENSION_EDEFAULT.equals(extension);
+        case PropertiesPackage.FILE_ITEM__CONTENT:
+            return content != null;
         }
         return super.eIsSet(featureID);
     }
 
     /**
      * <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
      * @generated
      */
     public String toString() {
-        if (eIsProxy()) return super.toString();
+        if (eIsProxy())
+            return super.toString();
 
         StringBuffer result = new StringBuffer(super.toString());
         result.append(" (name: ");
@@ -267,9 +284,13 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
         if (!proxy.eClass().equals(PropertiesPackage.eINSTANCE.getByteArray())) {
             return super.eResolveProxy(proxy);
         }
-        
+
         URI proxyUri = proxy.eProxyURI();
         URI resourceUri = proxyUri.trimFragment();
+        // bug 0020095
+        if (eResource() == null) {
+            return super.eResolveProxy(proxy);
+        }
         ResourceSet resourceSet = eResource().getResourceSet();
         ByteArrayResource byteArrayResource = null;
 
@@ -308,7 +329,7 @@ public abstract class FileItemImpl extends ItemImpl implements FileItem {
                 return object;
             }
         }
-        
+
         throw new UnsupportedOperationException();
     }
 
