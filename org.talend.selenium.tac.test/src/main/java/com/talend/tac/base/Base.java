14d592bf9e82cce1b27aeb610d07e9fe3af9006f
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-06 09:15:47 +0000

    selenium case: modify  WAIT_TIME in the Base
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58020 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/base/Base.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/base/Base.java
@@ -24,7 +24,7 @@ public class Base {
 	public static String MID_SPEED = "3000";
 	public static String MIN_SPEED = "0";
 	public static String MAX_SPEED = "5000";
-	public static int WAIT_TIME = 20;
+	public static int WAIT_TIME = 40;
 	
 	
 	@BeforeClass
