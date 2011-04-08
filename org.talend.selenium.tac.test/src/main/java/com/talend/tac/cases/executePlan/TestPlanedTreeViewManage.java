aabbf47a0590d9aebb5ab64494392ccc43a236dd
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-08 09:16:15 +0000

    selenium:add TestPlanedTreeViewManage
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58210 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestPlanedTreeViewManage.java
@@ -0,0 +1,18 @@
+package com.talend.tac.cases.executePlan;
+
+import org.testng.annotations.Test;
+
+public class TestPlanedTreeViewManage {
+  @Test
+  public void testTreeManage() {
+  }
+  public void addOnOK(String okTask,String taskToadd){
+	  
+  }
+  public void addOnError(String errorTask,String taskToadd){
+	  
+  }
+  public void addAfter(String finishedTask,String followTask){
+	  
+  }
+}
