0e58777c7dd1f3451ab5f2da91d667e1da4ecd6a
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-08 09:14:47 +0000

    selenium:add TestAddExistPlan to add a plan with exist label
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58209 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestAddExistPlan.java
@@ -0,0 +1,67 @@
+package com.talend.tac.cases.executePlan;
+
+import java.awt.event.KeyEvent;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.cases.Login;
+
+public class TestAddExistPlan extends Login {
+     
+	
+	public void addPlan(String label,String description,String task) {
+
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		Assert.assertTrue(selenium
+				.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("//button[text()='Add plan']");
+		// type plan label
+//		this.typeString(
+//				"//input[@class=' x-form-field x-form-text x-form-invalid ' and @name='label']",
+//				label);
+		
+		  selenium.type(
+		  "//input[@class=' x-form-field x-form-text x-form-invalid' and @name='label']"
+		  , label); 
+		  selenium.fireEvent("//input[@name='label']", "blur");
+		 
+		// type plan description
+//		this.typeString(
+//				"//input[@class=' x-form-field x-form-text' and @name='description']",
+//				description);
+	
+		  selenium.type(
+		  "//input[@class=' x-form-field x-form-text' and @name='description']"
+		  , description);
+		  selenium.fireEvent("//input[@name='description']",
+		  "blur");
+		 
+		// select a task for the plan
+		selenium.click("String idExecutionPlanPlanFormTaskComboBox");
+		selenium.mouseDownAt("//div[@role='listitem'][1]", ""
+				+ KeyEvent.VK_ENTER);
+		// click save button
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
+
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='" + label
+				+ "']"));
+		selenium.setSpeed(MIN_SPEED);
+	}      
+
+	
+	//add a plan 
+	@Test(groups={"addExist"},dependsOnGroups={"addplan"})
+	@Parameters({"plan.label","plan.description","plan.task"})
+	public void testAddPlan(String label,String description,String task) {
+		
+		addPlan(label, description,task);
+	    
+	}   
+	
+	
+	
+}
