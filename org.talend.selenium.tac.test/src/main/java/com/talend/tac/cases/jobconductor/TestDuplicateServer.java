604124c88fed40938130fc8f409cffee7e56c8c9
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-13 08:25:00 +0000

    selenium:TestDuplicateServer added
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58432 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestDuplicateServer.java
@@ -0,0 +1,58 @@
+package com.talend.tac.cases.jobconductor;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+import com.talend.tac.cases.Login;
+
+public class TestDuplicateServer extends Login {
+	@Test(groups = { "Duplicate"},dependsOnGroups={"DeactiveReactive"} )
+	@Parameters({ "ServerLablename", "PortInvalidServer", "ServerDescription",
+			"ServerHost", "ServerCommondport", "ServerFiletransfortport",
+			"ServerMonitorport", "ServerTimeout", "ServerUsername",
+			"ServerPassword" })
+	public void testDuplicateServer(String lable, String invalidLable,
+			String description, String host, String commondport,
+			String transfortport, String monitorport, String time,
+			String username, String password) {
+		selenium.setSpeed(MAX_SPEED);
+		if (selenium.isVisible("!!!menu.executionServers.element!!!")) {
+			selenium.click("!!!menu.executionServers.element!!!");
+			waitForElementPresent("idSubModuleAddButton", 30000);
+
+		} else {
+			selenium.click("!!!menu.jobConductor.element!!!");
+			selenium.setSpeed(MID_SPEED);
+			selenium.click("!!!menu.executionServers.element!!!");
+			waitForElementPresent("idSubModuleAddButton", 30000);
+
+		}
+	
+		duplicateServer(lable);
+		// addServerwithInvalideports(invalidLable);
+		selenium.setSpeed(MIN_SPEED);
+
+	}
+
+	public void duplicateServer(String duplicatedServername) {
+		selenium.setSpeed(MAX_SPEED);
+		selenium.refresh();
+		selenium.mouseDown("//div[text()='" + duplicatedServername + "']");
+		selenium.click("idSubModuleDuplicateButton");
+		selenium.click("idFormSaveButton");
+		selenium.click("idSubModuleRefreshButton");
+		Assert.assertTrue(
+				selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='Copy_of_serverTest')]"),
+				"server  duplicated failed!");
+		selenium.setSpeed(MIN_SPEED);
+
+	}
+
+	public void typeString(String xpath, String value) {
+		selenium.click(xpath);
+		selenium.type(xpath, value);
+		selenium.fireEvent(xpath, "blur");
+	}
+}
