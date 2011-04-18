4d8ee29c08a5a7070e35e4bd0c5ba33dec1520ab
hcyi <hcyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-18 07:55:15 +0000

    bug 20365 fixed : on postgres, run sqlbuild will throw error.
    git-svn-id: http://talendforge.org/svn/tos/trunk@58690 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/metadata/query/AbstractQueryGenerator.java
+++ org.talend.core/src/main/java/org/talend/core/model/metadata/query/AbstractQueryGenerator.java
@@ -398,6 +398,10 @@ public abstract class AbstractQueryGenerator implements IQueryGenerator {
                 tableName = realTableName;
                 schemaName = schema;
             }
+            // bug 20365
+            if (getElement() != null && dbType.getDisplayName().equals(EDatabaseTypeName.PSQL.getDisplayName())) {
+                schemaName = schema;
+            }
             final String tableNameWithDBAndSchema = getTableNameWithDBAndSchema(dbName, schemaName, tableName);
 
             StringBuffer sql = new StringBuffer(100);
