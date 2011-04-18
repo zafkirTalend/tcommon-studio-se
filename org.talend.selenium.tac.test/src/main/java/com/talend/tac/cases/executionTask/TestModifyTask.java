5b0393d5c0b12a06640eb112891f464b199088d9
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-18 07:18:49 +0000

    selenium cases: update class "TestModifyTask"
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58680 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestModifyTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestModifyTask.java
@@ -3,40 +3,45 @@ package com.talend.tac.cases.executionTask;
 import java.awt.Event;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 public class TestModifyTask extends Login {
     
 	@Test(groups={"ModifyTask"},dependsOnGroups={"DeleteTask"})
-	public void testModifyTask() {
+	@Parameters({"label","modifyTask"})
+	public void testModifyTask(String label,String modifyLabel) {
 		
-		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-		selenium.setSpeed(MID_SPEED);
-	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-		selenium.click("idSubModuleRefreshButton");
-		selenium.mouseDown("//div[text()='testTaskNotChooseActive']");//select a exist plan
+		if(!selenium.isElementPresent("//span[text()='"+modifyLabel+"']")) {
+			
+			this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+			selenium.setSpeed(MID_SPEED);
+		    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+			selenium.click("idSubModuleRefreshButton");
+			selenium.mouseDown("//span[text()='"+label+"']");//select a exist task
 
-		
-		selenium.type("//input[@name='label']", "testModifyTask");//plan name /Label
-		selenium.fireEvent("//input[@name='label']", "blur");
-		selenium.click("idCommonProjectListBox");//select a project 
-		selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
-		selenium.click("idCommonBranchListBox()");  //select a Branch
-		selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
-		selenium.click("idCommonJobListBox()");   //select a job
-		selenium.setSpeed(MID_SPEED);
-		selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
-		selenium.click("idCommonContextListBox()");  
-		selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
- 
-		selenium.click("idJobConductorExecutionServerListBox()"); //select a server
-		selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
-		selenium.click("idJobConductorExecutionServerListBox()");
-		selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
-		selenium.click("idFormSaveButton");
-		Assert.assertTrue(selenium.isElementPresent("//div[text()='testModifyTask']"));
-		selenium.setSpeed(MIN_SPEED);
-		
+			
+			selenium.type("//input[@name='label']", modifyLabel);//task name /Label
+			selenium.fireEvent("//input[@name='label']", "blur");
+			selenium.click("idCommonProjectListBox");//select a project 
+			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
+			selenium.click("idCommonBranchListBox()");  //select a Branch
+			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
+			selenium.click("idCommonJobListBox()");   //select a job
+			selenium.setSpeed(MID_SPEED);
+			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
+			selenium.click("idCommonContextListBox()");  
+			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
+	 
+			selenium.click("idJobConductorExecutionServerListBox()"); //select a server
+			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
+			selenium.click("idJobConductorExecutionServerListBox()");
+			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
+			selenium.click("idFormSaveButton");
+			Assert.assertTrue(selenium.isElementPresent("//span[text()='"+modifyLabel+"']"));
+			selenium.setSpeed(MIN_SPEED);
+			
+		}	
 	}
 }
