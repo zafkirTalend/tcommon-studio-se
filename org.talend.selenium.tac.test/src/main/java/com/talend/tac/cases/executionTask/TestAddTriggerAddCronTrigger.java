b499fcb4da01c595e269b64197a0ada60a29139f
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-19 09:55:26 +0000

    selenium cases: add case testAddTriggerAddCronTrigger for test add a correct crontrigger                 add case testAddExistTriggerAddCronTrigger for test add a exist crontrigger                 add case testAddOverdueTriggerAddCronTrigger for test add a overdue crontrigger
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58805 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
@@ -1,37 +1,45 @@
 package com.talend.tac.cases.executionTask;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 
 public class TestAddTriggerAddCronTrigger extends Login{
-   
-	@Test
-    public void testAddTriggerAddCronTrigger() {
+    
+	public void addTriggerAddCronTrigger(String labelCronTrigger, String descriptionSronTrigger,String years,
+			String weeksStart, String weeksEnd, String monthsStart, String monthsEnd) {
     	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
     	selenium.setSpeed(MID_SPEED);
     	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.mouseDown("//div[text()='Copy_of_test_task']");//select a exist task
+    	selenium.setSpeed(MIN_SPEED);
+    	selenium.mouseDown("//span[text()='testModifyTask']");//select a exist task
 		selenium.click("//button[text()='Add trigger...']");//add a trigger
 		selenium.click("//a[text()='Add CronTrigger']");//add a  CronTrigger
 		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+rb.getString("trigger.action.addCronTrigger")+"']"));
-		selenium.setSpeed(MID_SPEED);
-		selenium.type("//div[@class='x-form-item ']/div/div/input[@class=' x-form-field x-form-text  x-form-invalid' and @name='label']", "TestCronTrigger");//label
-		selenium.fireEvent("//div[@class='x-form-item ']/div/div/input[@class=' x-form-field x-form-text  x-form-invalid' and @name='label']","blur");
+		selenium.setSpeed(MIN_SPEED);
+		selenium.type("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset" +
+				"//input[@name='label']", labelCronTrigger);//label
+		selenium.fireEvent("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset" +
+				"//input[@name='label']","blur");
 //			                                                                      " x-form-field x-form-text x-form-invalid"
-		selenium.type("//input[@name='description']", "test add a CronTrigger");//description
-		selenium.fireEvent("//input[@name='description']","blur");
+		selenium.type("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset" +
+				"//input[@name='description']", descriptionSronTrigger);//description
+		selenium.fireEvent("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset" +
+				"//input[@name='description']","blur");
 		
 		selenium.click("idSchedulingUiConfigButton");//choose data
 		
-		selenium.mouseDown("//div[text()='2011']");//choose years
-		selenium.mouseDown("//div[text()='Sunday']");//choose day of weeks
+		selenium.mouseDown("//div[text()='"+years+"']");//choose years
+		selenium.mouseDown("//div[text()='"+weeksStart+"']");//choose day of weeks
 		selenium.shiftKeyDown();
-		selenium.mouseDown("//div[text()='Saturday']");
+		selenium.mouseDown("//div[text()='"+weeksEnd+"']");
 		selenium.shiftKeyUp();
-		selenium.mouseDown("//div[text()='March']");//choose months
+		selenium.mouseDown("//div[text()='"+monthsStart+"']");//choose months
+		selenium.shiftKeyDown();
+		selenium.mouseDown("//div[text()='"+monthsEnd+"']");
 		selenium.mouseDown("//div[@class='x-column-inner']/div[2]/div[2]/div/div/div[text()='00']");//choose hours
 		selenium.shiftKeyDown();
 		selenium.mouseDown("//div[@class='x-column-inner']/div[2]/div[2]/div/div/div[text()='23']");
@@ -46,8 +54,55 @@ public class TestAddTriggerAddCronTrigger extends Login{
 		selenium.shiftKeyUp();
 		selenium.click("idSchedulingApplyButton");//save data
 		
-		selenium.click("idFormSaveButton");
-        Assert.assertTrue(selenium.isElementPresent("//div[text()='TestCronTrigger']"));
+	}
+	
+	//add a cron triiger
+	@Test
+	@Parameters({"addCronTriggerLabel","addCronTriggerDescription"})
+    public void testAddTriggerAddCronTrigger(String cronTriggerLabel,String description) {
+		
+		addTriggerAddCronTrigger(cronTriggerLabel, description, "2011", 
+				"Sunday", "Saturday", "January", "December");
+    			
+		selenium.click("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset/parent::form/" +
+    			"parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");
+		selenium.click("//span[text()='Triggers']/parent::span/parent::em/parent::a/parent::li/parent::ul/parent::div/" +
+		"parent::div/parent::div//button[text()='Refresh']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='TestCronTrigger']"));
 		selenium.setSpeed(MIN_SPEED);
     }
+	
+	//add a exist cron triiger
+	@Test(dependsOnMethods={"testAddTriggerAddCronTrigger"})
+	@Parameters({"addCronTriggerLabel","addCronTriggerExistTriggerDescription"})
+    public void testAddExistTriggerAddCronTrigger(String cronTriggerLabel, String description) {
+		
+		addTriggerAddCronTrigger(cronTriggerLabel, description, "2011", 
+				"Sunday", "Saturday", "January", "December");
+    			
+		selenium.click("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset/parent::form/" +
+    			"parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");
+        selenium.setSpeed("1000");
+		Assert.assertTrue(selenium.isTextPresent("Save failed: An execution trigger with this name already exists -- For more information see your log file"));
+		selenium.setSpeed(MIN_SPEED);
+		
+    }
+	
+	//add a cron triiger of data is overdue
+	@Test(dependsOnMethods={"testAddExistTriggerAddCronTrigger"})
+	@Parameters({"addCronTriggerOverdue","addCronTriggerOverdueDescription"})
+    public void testAddOverdueTriggerAddCronTrigger(String cronTriggerLabel, String description) {
+		
+		addTriggerAddCronTrigger(cronTriggerLabel, description, "2010", 
+				"Sunday", "Saturday", "January", "December");
+    			
+		selenium.click("//span[text()='Add Cron trigger']/parent::legend/parent::fieldset/parent::form/" +
+    			"parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");
+		selenium.setSpeed("1000");
+     	Assert.assertTrue(selenium.isTextPresent(rb.getString("trigger.error.trigger_will_never_fire")));
+		selenium.setSpeed(MIN_SPEED);	
+     	
+    }
+	
 }
