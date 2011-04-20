b2ec0f0b19d52fccb732918dc15cff92e17c307f
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-20 09:44:18 +0000

    selenium:update for use class TriggerDate
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58894 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestAddAddSimpleTriggerUseDefaulttime.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestAddAddSimpleTriggerUseDefaulttime.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
@@ -14,28 +15,32 @@ public class TestAddAddSimpleTriggerUseDefaulttime extends Login{
  
 	// add a simpleTrigger use default start time and end time
 	@Test(groups={"plan.addtrigger"})
-	public void testAddTriggerAddSimpleTrigger() {
-    	this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+	@Parameters({ "plan.toaddsimpletrigger.label", "plan.simpletrigger.defaulttime.label"})
+	public void testAddTriggerAddSimpleTrigger(String addsimpletriggerplan,String defaulttimeSimpletrigger) {
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
     	selenium.setSpeed(MID_SPEED);
     	Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
     	selenium.setSpeed(MIN_SPEED);
     	selenium.click("idSubModuleRefreshButton");
-    	selenium.mouseDown("//div[text()='testAddPlan']");//select a exist task
+    	selenium.mouseDown("//div[text()='"+addsimpletriggerplan+"']");//select a exist task
 		selenium.click("//button[text()='Add trigger...']");//add a trigger
 		selenium.click("//a[text()='Add SimpleTrigger']");//add a SimpleTrigger
-		
         Assert.assertTrue(selenium.isElementPresent("//span[text()='"+rb.getString("trigger.action.addSimpleTrigger")+"']"));
         selenium.setSpeed(MID_SPEED);
-        selenium.type("//input[@name='label']", "TestSimpleTrigger");//label
-		selenium.fireEvent("//input[@name='label']","blur");
-		selenium.type("//input[@class=' x-form-field x-form-text' and @name='description']", "test add a SimpleTrigger");//description
-		selenium.fireEvent("//input[@name='description']","blur");
-	
-        selenium.type("//input[@name='repeatInterval']", "20");//Time interval (s)
-	    selenium.fireEvent("//input[@name='repeatInterval']", "blur");
-	    selenium.click("//button[@id='idFormSaveButton' and  @class='x-btn-text ']");
+        //type simple trigger label
+        this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='label']",defaulttimeSimpletrigger );//label
+		//type simple trigger description
+        this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='description']", "SimpleTrigger");//description
+       
+        //type simple trigger repeat count
+        this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='repeatCount']", "40");//description
+	    //type simple trigger repeat interval
+        this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='repeatInterval']", "15");//Number of triggerings
+//	    selenium.click("//span[text()='Add simple trigger']/parent::legend/parent::fieldset/parent::form/parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");
+	    selenium.click("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-footer']/div[@class=' x-panel-btns']/div[@class=' x-small-editor x-panel-btns-center x-panel-fbar x-component x-toolbar-layout-ct']/table[@class='x-toolbar-ct']/tbody/tr/td[@class='x-toolbar-left']/table/tbody/tr[@class='x-toolbar-left-row']/td[@class='x-toolbar-cell']/table[@class=' x-btn x-component x-btn-text-icon ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[@class='x-btn-text ' and text()='Save']");
 	    selenium.setSpeed(MID_SPEED); 
-	    Assert.assertTrue(selenium.isElementPresent("//div[text()='TestSimpleTrigger']"));
+//	    Thread.sleep(5000);
+	    Assert.assertTrue(selenium.isElementPresent("//span[text()='"+defaulttimeSimpletrigger+"']"));
 	    selenium.setSpeed(MIN_SPEED);
 	}
 
