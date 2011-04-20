e3dfcc26d8768920e6c7d5ccac69d6e8c86e2d86
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-20 05:37:09 +0000

    selenium:add new case named TestAddTriggerAddCronTrigger to add crontrigger for a plan
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58846 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestAddTriggerAddCronTrigger.java
@@ -0,0 +1,84 @@
+package com.talend.tac.cases.executePlan;
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.cases.Login;
+public class TestAddTriggerAddCronTrigger extends Login{
+    	
+    
+	@Test
+	@Parameters({ "plan.toaddcrontrigger.label", "plan.crontrigger.label"})
+	public void testAddCronTrigger(String planlabel,String crongtriggerlabel) throws InterruptedException{
+	    TriggerDate date = new TriggerDate();
+		//open to execution plan add trigger page
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+    	selenium.setSpeed(MID_SPEED);
+    	Assert.assertTrue(selenium.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
+    	selenium.setSpeed(MIN_SPEED);
+    	selenium.click("idSubModuleRefreshButton");
+    	selenium.mouseDown("//div[text()='"+planlabel+"']");
+		selenium.click("//button[text()='Add trigger...']");
+		selenium.click("//a[text()='Add CronTrigger']");
+//		selenium.setSpeed(MID_SPEED);
+		//type  label
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='label']",crongtriggerlabel);
+		//type  description
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='description']", "testCrontrigger");
+    	//type minutes
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='minutes']", date.minutes);
+		//type hours
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='hours']", date.hours);
+		//type days
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='daysOfMonth']", date.days);
+		//type months
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='months']", date.months);
+		//type years
+		this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='years']", date.years);	
+		//click save button
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-footer']/div[@class=' x-panel-btns']/div[@class=' x-small-editor x-panel-btns-center x-panel-fbar x-component x-toolbar-layout-ct']/table[@class='x-toolbar-ct']/tbody/tr/td[@class='x-toolbar-left']/table/tbody/tr[@class='x-toolbar-left-row']/td[@class='x-toolbar-cell']/table[@class=' x-btn x-component x-btn-text-icon ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[@class='x-btn-text ' and text()='Save']");
+		Thread.sleep(5000);
+	    Assert.assertTrue(selenium.isElementPresent("//span[text()='"+crongtriggerlabel+"']"), "Crontrigger added failed!");
+	    selenium.setSpeed(MIN_SPEED);
+	}
+	private class TriggerDate{
+		public String minutes = "";
+		public String hours = "";
+		public String days = "";
+		public String months = "";
+		public String years = "";
+		public TriggerDate(){
+			Date date = new Date();
+			DateFormat   df   =   new   SimpleDateFormat( "yyyy-MM-dd hh:MM:ss"); 
+			date.setHours(date.getHours()+24);
+			String s = df.format(date);//system date 
+			s.replaceAll(" ", "-");
+			s.replaceAll(":", "-");
+//			System.out.println(s);
+			String after = (s.replaceAll(" ", "-").replaceAll(":", "-"));
+			System.out.println(after);
+			String dates[]=after.split("-");
+			
+			this.years +=dates[0] ;
+			System.out.println("years is: "+years);
+			this.months +=dates[1];
+			System.out.println("months is: "+months);
+			this.days += dates[2];
+			System.out.println("days is: "+days);
+			this.hours += dates[3];
+			System.out.println("hours is: "+hours);
+			this.minutes += dates[4];
+			System.out.println("minutes is: "+minutes);
+			
+		}
+		
+	}
+
+}
