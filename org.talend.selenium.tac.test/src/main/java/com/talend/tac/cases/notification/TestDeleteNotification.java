4358f7b4fe55230d9246b9683009a9816577bdb7
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-21 01:23:30 +0000

    selenium cases : add case(testCancelDeleteNotification) for test cancel delete notification                  add case(testDeleteNotification) for test delete notification
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58928 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDeleteNotification.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDeleteNotification.java
@@ -1,6 +1,7 @@
 package com.talend.tac.cases.notification;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
@@ -8,47 +9,43 @@ import com.talend.tac.cases.Login;
 public class TestDeleteNotification extends Login {
     
 	//delete a notification(cancel Delete Notification)
-	@Test(dependsOnGroups={"AddJobServersNotification"})
-	public void testCancelDeleteNotification() {
+	@Test(dependsOnGroups={"AddJobserverNotification"})
+	@Parameters({"eventNewUser"})
+	public void testCancelDeleteNotification(String eventNewUser) {
 		
 		this.clickWaitForElementPresent("!!!menu.notification.element!!!");
-		selenium.setSpeed(MAX_SPEED);
 		
-		if(selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-category']")) {
-	    	selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-category']");//click a exist notification
-			selenium.chooseCancelOnNextConfirmation();
-			selenium.click("idSubModuleDeleteButton");//click 'Delete' button
-			selenium.setSpeed(MAX_SPEED);
-		    Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected notification [\\s\\S]$"));
-	        selenium.setSpeed(MIN_SPEED);
-		 
-	    } else {
-	    	
-	    	System.out.println("element not exist");
-
-	    }
+	  	selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-category']");//click a exist notification
+		selenium.chooseCancelOnNextConfirmation();
+		selenium.click("idSubModuleDeleteButton");//click 'Delete' button
+		selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected notification [\\s\\S]$"));
+        selenium.setSpeed(MIN_SPEED);
+	    selenium.setSpeed(MID_SPEED);
+        Assert.assertTrue(selenium.isElementPresent("//div[text()='"+eventNewUser+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='true']"));		
+		selenium.setSpeed(MIN_SPEED);
 		
 	}
 	//delete a notification(Delete Notification)
 	@Test(dependsOnMethods={"testCancelDeleteNotification"})
-	public void testDeleteNotification() {
+	@Parameters({"eventNewUser"})
+	public void testDeleteNotification(String eventNewUser) {
 		
 		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
 		selenium.setSpeed(MAX_SPEED);
-		
-		if(selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-category']")) {
-	    	selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-category']");//choose a notification type
-			selenium.chooseOkOnNextConfirmation();
-			selenium.click("idSubModuleDeleteButton");//click 'Delete' button
-			selenium.setSpeed(MAX_SPEED);
-		    Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected notification [\\s\\S]$"));
-	        selenium.setSpeed(MIN_SPEED);
-		 
-	    } else {
-	    	
-	    	System.out.println("element not exist");
-
-	    }
+				
+    	selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-category']");//choose a notification type
+		selenium.chooseOkOnNextConfirmation();
+		selenium.click("idSubModuleDeleteButton");//click 'Delete' button
+		selenium.setSpeed(MAX_SPEED);
+	    Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected notification [\\s\\S]$"));
+        selenium.setSpeed(MIN_SPEED);
+	 
+        selenium.setSpeed(MID_SPEED);
+        Assert.assertTrue(!selenium.isElementPresent("//div[text()='"+eventNewUser+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='true']"));		
+		selenium.setSpeed(MIN_SPEED);
 		
 	}
 	
