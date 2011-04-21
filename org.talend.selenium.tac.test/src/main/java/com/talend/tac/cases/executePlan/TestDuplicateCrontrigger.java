bdbea44abcb5bb033fab61fc706ea51f3a7b4ec6
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-21 07:05:47 +0000

    selenium:add new cases : TestDuplicateSimpleTrigger:create a simpletrigger for a plan from duplicate button TestDuplicateCronTrigger:create a crontrigger for a plan from duplicate button
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@58962 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestDuplicateCrontrigger.java
@@ -0,0 +1,59 @@
+package com.talend.tac.cases.executePlan;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.cases.Login;
+
+public class TestDuplicateCrontrigger extends Login {
+
+	@Test
+	// (groups={"plan.trigger.duplicate"})
+	@Parameters({ "plan.toaddcrontrigger.label",
+			"plan.crontrigger.duplicate.label" })
+	public void testAddTriggerAddSimpleTrigger(String plan,
+			String triggertoduplicate) {
+		TriggerDate date = new TriggerDate().getFuture(24);
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium
+				.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
+		selenium.click("idSubModuleRefreshButton");
+		// select plan
+		selenium.mouseDown("//div[text()='" + plan + "']");// select a exist
+		// select trigger to duplicate
+		selenium.mouseDown("//span[text()='" + triggertoduplicate + "']");//
+		// click duplicate trigger button
+		selenium.click("//table[@class=' x-btn x-component x-btn-text-icon x-btn-focus ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[text()='Duplicate']");
+		// configure trigger information about time
+		// type minutes
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='minutes']",
+				date.minutes);
+		// type hours
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='hours']",
+				date.hours);
+		// type days
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='daysOfMonth']",
+				date.days);
+		// type months
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='months']",
+				date.months);
+		// type years
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='years']",
+				date.years);
+
+		// click save button to save trigger
+		selenium.click("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-footer']/div[@class=' x-panel-btns']/div[@class=' x-small-editor x-panel-btns-center x-panel-fbar x-component x-toolbar-layout-ct']/table[@class='x-toolbar-ct']/tbody/tr/td[@class='x-toolbar-left']/table/tbody/tr[@class='x-toolbar-left-row']/td[@class='x-toolbar-cell']/table[@class=' x-btn x-component x-btn-text-icon ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[@class='x-btn-text ' and text()='Save']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"
+				+ "Copy_of_" + triggertoduplicate + "']"));
+		selenium.setSpeed(MIN_SPEED);
+	}
+
+}
---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestDuplicateSimpletrigger.java
@@ -0,0 +1,68 @@
+package com.talend.tac.cases.executePlan;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.cases.Login;
+
+@SuppressWarnings("deprecation")
+public class TestDuplicateSimpletrigger extends Login {
+	TriggerDate date = new TriggerDate();
+	// add a simpleTrigger use default start time and end time
+	@Test
+	@Parameters({ "plan.toaddsimpletrigger.label",
+	"plan.simpletrigger.duplicate.label" })
+	
+	public void testAddTriggerAddSimpleTrigger(String plan,String simpleTriggerToduplicate) {
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium
+				.isElementPresent("//div[@class='header-title' and text()='Execution plan']"));
+		selenium.click("idSubModuleRefreshButton");
+		// select plan
+		selenium.mouseDown("//div[text()='" + plan + "']");
+															
+		// select trigger to duplicate
+		selenium.mouseDown("//div[text()='" + simpleTriggerToduplicate + "']");//
+		// click duplicate trigger button
+		selenium.click("//table[@class=' x-btn x-component x-btn-text-icon x-btn-focus ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[text()='Duplicate']");
+		// configure trigger informations:start time,end time and so on
+		// type simple trigger start time
+		selenium.click("//input[@name='startTime']/parent::div/div[@class='x-form-trigger x-form-date-trigger']");
+		selenium.setSpeed(MAX_SPEED);
+		// selenium.click("//div[@class=' x-icon-btn x-nodrag x-date-left-icon x-component ']");
+		selenium.click("//span[text()='" + date.getFuture(24).days + "']");
+		selenium.setSpeed(MIN_SPEED);
+		// this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='startTime']",
+		// date.getFuture("24"));//description
+		// type simple trigger end time
+		selenium.click("//input[@name='endTime']/parent::div/div[@class='x-form-trigger x-form-date-trigger']");
+		selenium.setSpeed(MAX_SPEED);
+		// selenium.click("//div[@class=' x-icon-btn x-nodrag x-date-left-icon x-component ']");
+		selenium.click("//span[text()='" + date.getFuture(72).days + "']");
+		selenium.setSpeed(MIN_SPEED);
+		// this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='endTime']",
+		// date.getFuture("48"));//description
+		// type simple trigger repeat count
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='repeatCount']",
+				"");// description
+		// type simple trigger repeat interval
+		this.typeString(
+				"//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='repeatInterval']",
+				"1500");// Number of triggerings
+		
+		// click save button to save trigger
+		selenium.click("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-footer']/div[@class=' x-panel-btns']/div[@class=' x-small-editor x-panel-btns-center x-panel-fbar x-component x-toolbar-layout-ct']/table[@class='x-toolbar-ct']/tbody/tr/td[@class='x-toolbar-left']/table/tbody/tr[@class='x-toolbar-left-row']/td[@class='x-toolbar-cell']/table[@class=' x-btn x-component x-btn-text-icon ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[@class='x-btn-text ' and text()='Save']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium
+				.isElementPresent("//span[text()='"+"Copy_of_"+simpleTriggerToduplicate+"']"));
+		selenium.setSpeed(MIN_SPEED);
+	}
+
+}
