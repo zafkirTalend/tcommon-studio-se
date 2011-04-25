47d20162b24b6b85565a8d1fd27cbb4865c3b2e4
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-25 09:57:54 +0000

    selenium: update testUploadLicense: add invalid license uploading check; lunch_tests.xml: add the path of invalid license,use the relative path where license file located.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59136 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/license/TestUploadLicense.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/license/TestUploadLicense.java
@@ -6,15 +6,26 @@ import static org.testng.Assert.*;
 import com.talend.tac.cases.Login;
 
 public class TestUploadLicense extends Login {
+	
 	@Test
-	@Parameters( { "license.file.path" })
-	public void testAddLicense(String license) {
-
-		
+	@Parameters( { "license.fileInvalid.path" })
+	public void testAddWrongLicense(String license) {
 		waitForElementPresent("idMenuLicenseElement", 30);
 		selenium.click("idMenuLicenseElement");
 		waitForElementPresent("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", WAIT_TIME);
 		selenium.type("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", "license");
+		selenium.click("//button[text()='Upload']");
+		//assertTrue(selenium.isTextPresent("License not set: License invalid"));
+		
+	}
+	
+	@Test
+	@Parameters( { "license.file.path" })
+	public void testAddLicense(String license) {
+		waitForElementPresent("idMenuLicenseElement", 30);
+		selenium.click("idMenuLicenseElement");
+		waitForElementPresent("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", WAIT_TIME);
+		selenium.type("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", license);
 		selenium.click("//button[text()='Upload']");
 		
 	}
