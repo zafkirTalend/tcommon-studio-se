b8e9a24ab5b862e6c813a73bd47bf78a910adcae
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-26 06:58:26 +0000

    selelenium: update TestUploadLicense, no license,invalid,valid license.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59173 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/license/TestUploadLicense.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/license/TestUploadLicense.java
@@ -8,12 +8,21 @@ import com.talend.tac.cases.Login;
 public class TestUploadLicense extends Login {
 	
 	@Test
+	public void testAddNoLicense() {
+		waitForElementPresent("idMenuLicenseElement", 30);
+		selenium.click("idMenuLicenseElement");
+		waitForElementPresent("//button[text()='Upload']", WAIT_TIME);
+		selenium.click("//button[text()='Upload']");
+		//assertTrue(selenium.isTextPresent("License not set: License invalid"));
+	}
+	
+	@Test
 	@Parameters( { "license.fileInvalid.path" })
-	public void testAddWrongLicense(String license) {
+	public void testAddInvalidLicense(String license) {
 		waitForElementPresent("idMenuLicenseElement", 30);
 		selenium.click("idMenuLicenseElement");
 		waitForElementPresent("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", WAIT_TIME);
-		selenium.type("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", "license");
+		selenium.type("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", license);
 		selenium.click("//button[text()='Upload']");
 		//assertTrue(selenium.isTextPresent("License not set: License invalid"));
 		
@@ -21,7 +30,7 @@ public class TestUploadLicense extends Login {
 	
 	@Test
 	@Parameters( { "license.file.path" })
-	public void testAddLicense(String license) {
+	public void testAddValidLicense(String license) {
 		waitForElementPresent("idMenuLicenseElement", 30);
 		selenium.click("idMenuLicenseElement");
 		waitForElementPresent("//button[contains(text(),'Browse')]/ancestor::table[1]/preceding-sibling::input[1]", WAIT_TIME);
