6de5987fd7af3c3d42586c0316b082a04473d5ba
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-27 06:51:18 +0000

    selenium case: add case for test delete a trigger
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59320 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDeleteTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDeleteTrigger.java
@@ -1,41 +1,30 @@
 package com.talend.tac.cases.executionTask;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 
 public class TestDeleteTrigger extends Login {
     
-	@Test(dependsOnMethods={"DuplicateTrigger"})
-	public void testCancleDeleteTrigger() {
+	//delete a trigger
+	@Test(groups={"DeleteTrigger"},dependsOnGroups={"DuplicateTrigger"})
+	@Parameters({"modifyTask","addSimpleTriggerLabel"})
+	public void testDeleteTrigger(String task, String trigger) {
 		
 		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
     	selenium.setSpeed(MID_SPEED);
     	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.mouseDown("//div[text()='Copy_of_test_task']");//select a exist task
-//    	selenium.mouseDown("//div[text()='a']");//select a exist task
-    	selenium.mouseDown("//div[text()='TestSimpleTrigger']");//select a exist task
-    	
-    	selenium.chooseCancelOnNextConfirmation();
-    	selenium.click("idSubModuleDeleteButton");
-    	Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected trigger [\\s\\S]$"));
-    	
-	}
-	
-	@Test(dependsOnMethods={"testCancleDeleteTrigger()"})
-	public void testDeleteTrigger() {
-		
-		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-    	selenium.setSpeed(MID_SPEED);
-    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.mouseDown("//div[text()='Copy_of_test_task']");//select a exist task
-//    	selenium.mouseDown("//div[text()='a']");//select a exist task
-    	selenium.mouseDown("//div[text()='TestSimpleTrigger']");//select a exist task
+    	selenium.mouseDown("//span[text()='"+task+"']");//select a exist task
+
+    	selenium.mouseDown("//span[text()='"+trigger+"']");//select a exist task
     	
     	selenium.chooseOkOnNextConfirmation();
-    	selenium.click("idSubModuleDeleteButton");
-    	Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected trigger [\\s\\S]$"));
+    	selenium.click("//span[text()='Triggers']/parent::span/parent::em/parent::a/parent::li/parent" +
+		"::ul/parent::div/parent::div/parent::div/parent::div//button[@id='idSubModuleDeleteButton']");
+		Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected trigger [\\s\\S]$"));
+		Assert.assertTrue(!selenium.isElementPresent("//span[text()='"+trigger+"']"));
     	
 	}
 }
