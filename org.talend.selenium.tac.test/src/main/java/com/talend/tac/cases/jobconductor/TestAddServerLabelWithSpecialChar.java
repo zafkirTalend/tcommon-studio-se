827fd0c805305a0b5c2aa601a731a1f6b9c09684
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-04 01:32:56 +0000

    selenium:add new case TestAddServerLabelWithSpecialChar
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59687 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestAddServerLabelWithSpecialChar.java
@@ -0,0 +1,37 @@
+package com.talend.tac.cases.jobconductor;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+import com.talend.tac.cases.Login;
+
+public class TestAddServerLabelWithSpecialChar extends Login {
+	@Test(groups = { "AddServer" })
+	@Parameters({ "NullLabelServerHost" })
+	public void testServerLabelSpecial(String host) throws InterruptedException {
+		Thread.sleep(5000);
+		this.waitForElementPresent("!!!menu.executionServers.element!!!",
+				WAIT_TIME);
+		selenium.click("!!!menu.executionServers.element!!!");
+		selenium.refresh();
+		this.waitForElementPresent("idSubModuleAddButton", WAIT_TIME);
+		selenium.click("idSubModuleAddButton");
+		Thread.sleep(3000);
+		// lable
+		this.typeString(other.getString("inputname.id.server.add.label"), "test_@server");
+		// host
+		this.typeString(other.getString("inputname.id.server.add.host"), host);
+		Assert.assertTrue(selenium
+				.isElementPresent("//img[@class='gwt-Image x-component ' and @role='alert']"));
+		selenium.click("idFormSaveButton");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertFalse(
+				selenium.isElementPresent("//div[text()='test_@server']"),
+				"Test server label special  failed!");
+		selenium.setSpeed(MIN_SPEED);
+
+	}
+
+}
