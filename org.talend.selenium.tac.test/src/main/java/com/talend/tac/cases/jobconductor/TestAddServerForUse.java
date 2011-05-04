6a6b0b6a72c3132fe9ec73c8e2df08e8a3a087a6
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-04 05:59:13 +0000

    selenium:update for TestAddServerForUse: add method to add an inactive server
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59710 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestAddServerForUse.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestAddServerForUse.java
@@ -9,36 +9,89 @@ import com.talend.tac.cases.Login;
 import com.talend.tac.cases.projects.TacCleaner;
 
 public class TestAddServerForUse extends Login {
-	@Test(groups = { "left" },dependsOnGroups={"CleanServer"})
-	@Parameters({ "ServerForUseAvailable", "ServerForUseUnavailable","ServeravailableHost"})
-	public void addServerForuse(String lableavailable,String labelunavailable,String availablehost) throws InterruptedException {
-	this.waitForElementPresent("!!!menu.executionServers.element!!!", WAIT_TIME);
-     new TacCleaner().cleanServersNotused(this.selenium);
-     addServer(lableavailable,availablehost);
-//     addServer(labelunavailable,"testUnavailable");
+	@Test(groups = { "left" }, dependsOnGroups = { "CleanServer" })
+	@Parameters({ "ServerForUseAvailable", "ServerForUseUnavailable","ServerForUseDownlabel",
+			"ServeravailableHost" })
+	public void addServerForuse(String lableavailable, String labelunactive,String downlabel,
+			String availablehost) throws InterruptedException {
+		this.waitForElementPresent("!!!menu.executionServers.element!!!",
+				WAIT_TIME);
+		new TacCleaner().cleanServersNotused(this.selenium);
+		addServerAvailable(lableavailable, availablehost);
+		addServerUnactive(labelunactive,availablehost);
+//		addServerDown(downlabel);
+		// addServer(labelunavailable,"testUnavailable");
 	}
-	public void addServer(String lable,String host) throws InterruptedException {
+
+	public void addServerAvailable(String lable, String host)
+			throws InterruptedException {
 		selenium.refresh();
 		this.waitForElementPresent("idSubModuleAddButton", WAIT_TIME);
 		selenium.click("idSubModuleAddButton");
 		Thread.sleep(5000);
-			// lable
-			
-			this.typeString(other.getString("inputname.id.server.add.label"),
-					lable);
-			
-			// host
-			this.typeString(other.getString("inputname.id.server.add.host"),
-					host);
-			
-			// save
-			selenium.setSpeed(MAX_SPEED);
-			selenium.setSpeed(MAX_SPEED);
-			selenium.click("idFormSaveButton");
-			Thread.sleep(3000);
-			Assert.assertTrue(selenium.isElementPresent("//div[text()='" + lable + "']"));
-		    selenium.setSpeed(MIN_SPEED);
+		// lable
+		this.typeString(other.getString("inputname.id.server.add.label"), lable);
+		// host
+		this.typeString(other.getString("inputname.id.server.add.host"), host);
+		// save
+		selenium.setSpeed(MAX_SPEED);
+		selenium.click("idFormSaveButton");
+		Thread.sleep(3000);
+		Assert.assertTrue((selenium.isElementPresent("//div[text()='" + lable
+				+ "']"))&&selenium.isElementPresent("//span[@class='serv-value' and (text()='UP')]"),"add server available failed!");
+		selenium.setSpeed(MIN_SPEED);
 
 	}
-	
+
+	public void addServerUnactive(String unactivelabel, String host)
+			throws InterruptedException {
+		selenium.refresh();
+		this.waitForElementPresent("idSubModuleAddButton", WAIT_TIME);
+		selenium.click("idSubModuleAddButton");
+		Thread.sleep(5000);
+		// lable
+		this.typeString(other.getString("inputname.id.server.add.label"),
+				unactivelabel);
+		// host
+		this.typeString(other.getString("inputname.id.server.add.host"), host);
+		selenium.click("//input[@class=' x-form-checkbox' and @type='checkbox' and @name='active']");
+		// save
+		selenium.setSpeed(MAX_SPEED);
+		selenium.click("idFormSaveButton");
+		Thread.sleep(8000);
+		selenium.refresh();
+		Thread.sleep(3000);
+		Assert.assertTrue((selenium.isElementPresent("//div[text()='"
+				+ unactivelabel + "']"))
+				&& (selenium
+						.isElementPresent("//span[@class='serv-value' and (text()='INACTIVE')]")),"add server vaule = inactive failed!");
+		selenium.setSpeed(MIN_SPEED);
+
+	}
+	public void addServerDown(String serverdown) throws InterruptedException{
+		selenium.refresh();
+		this.waitForElementPresent("idSubModuleAddButton", WAIT_TIME);
+		selenium.click("idSubModuleAddButton");
+		Thread.sleep(5000);
+		// lable
+		this.typeString(other.getString("inputname.id.server.add.label"),
+				serverdown);
+		// host
+		this.typeString(other.getString("inputname.id.server.add.host"), "unknownhost");
+		// save
+		selenium.setSpeed(MAX_SPEED);
+		selenium.click("idFormSaveButton");
+		Thread.sleep(10000);
+		selenium.refresh();
+		Thread.sleep(8000);
+		selenium.refresh();
+		Thread.sleep(8000);
+		Assert.assertTrue(((selenium.isElementPresent("//div[text()='"
+				+ serverdown + "']"))
+				&& (selenium
+						.isElementPresent("//span[@class='serv-value' and (text()='DOWN')]"))),"add server value = down failed!");
+		selenium.setSpeed(MIN_SPEED);
+		
+	}
+
 }
