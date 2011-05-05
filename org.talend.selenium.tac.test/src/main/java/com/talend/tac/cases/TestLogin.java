aac2666aeead542984dd6ae3b43310f731e6db6d
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-05 04:42:24 +0000

    selenium: add testSaveLoginOrNot , login and logout to see the corresponding result.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59797 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/TestLogin.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/TestLogin.java
@@ -54,6 +54,34 @@ this.waitForElementPresent("//div[text()='" + rb.getString("login.error.badPassw
 		selenium.click("idLoginButton");
 		this.waitForElementPresent("//div[text()=\"User '" + user +"' is disabled\"]", WAIT_TIME);
 }
+	//login and logout to check if the "save login" works.
+	@Test
+	@Parameters( { "userName", "userPassword" })
+	public void testSaveLoginOrNot(String user, String password) {
+	
+		waitForElementPresent("idLoginInput", WAIT_TIME);
+		waitForElementPresent("idLoginPasswordInput", WAIT_TIME);
+		waitForElementPresent("//input[@id='idLoginPasswordInput' and @checked]", WAIT_TIME);
+		selenium.windowMaximize();
+		
+		selenium.type("idLoginInput", user);
+		selenium.type("idLoginPasswordInput", password);
+		selenium.keyDown("idLoginPasswordInput", "\\13");
+		selenium.keyUp("idLoginPasswordInput", "\\13");
+		selenium.click("idLoginButton");
+		this.clickWaitForElementPresent("idLeftMenuTreeLogoutButton");
+		
+		waitForElementPresent("//input[@id='idLoginPasswordInput' and @checked]", WAIT_TIME);
+		assertEquals(selenium.getValue("idLoginInput"), user);
+//		assertEquals(selenium.getValue("idLoginPasswordInput"), password);
+		selenium.click("//input[@id='idLoginPasswordInput' and @checked]");
+		selenium.click("idLoginButton");
+		this.clickWaitForElementPresent("idLeftMenuTreeLogoutButton");
+		
+		waitForElementPresent("idLoginInput", WAIT_TIME);
+		assertEquals(selenium.getValue("idLoginInput"), "");
+		assertEquals(selenium.getValue("idLoginPasswordInput"), "");
+	}
 	
 	@Test
 	@Parameters( { "userName", "userPassword" })
