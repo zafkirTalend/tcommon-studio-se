759777100b43523590793ca8e3ad9c1b2a082a20
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-05 07:16:40 +0000

    selenium: update case testDuplicationTask
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59804 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDuplicateTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDuplicateTask.java
@@ -1,7 +1,5 @@
 package com.talend.tac.cases.executionTask;
 
-import java.awt.Event;
-
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -13,20 +11,13 @@ public class TestDuplicateTask  extends Login {
 	@Parameters({"labelNotChooseActive","duplicateTask"})
 	public void testDuplicateTask(String label, String duplicateLabel) {
 		
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+		selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+		selenium.click("idSubModuleRefreshButton");
 		if(!selenium.isElementPresent("//span[text()='"+duplicateLabel+"']")) {
 			
-			this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-			selenium.setSpeed(MID_SPEED);
-		    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-			selenium.click("idSubModuleRefreshButton");
-			selenium.mouseDown("//span[text()='"+label+"']");//select a exist task
-	        selenium.click("idSubModuleDuplicateButton");
-	        
-			selenium.click("idJobConductorExecutionServerListBox()"); //select a server
-			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER); 
 			
-			selenium.click("idJobConductorExecutionServerListBox()"); //select a server
-			selenium.mouseDownAt("//div[@role='listitem'][1]",""+Event.ENTER);
 			selenium.click("idFormSaveButton");
 			Assert.assertTrue(selenium.isElementPresent("//span[text()='"+duplicateLabel+"']"));
 			selenium.setSpeed(MIN_SPEED);
