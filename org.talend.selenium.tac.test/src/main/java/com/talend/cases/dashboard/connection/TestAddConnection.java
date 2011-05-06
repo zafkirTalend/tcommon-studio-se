d12832e9fbc14b9ca226b248c723edbae53c14c0
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-06 09:48:17 +0000

    selenium: update TestAddConnections, add check connections ,and simplify the long codes.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59956 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/connection/TestAddConnection.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/connection/TestAddConnection.java
@@ -1,6 +1,6 @@
 package com.talend.cases.dashboard.connection;
 
-import org.testng.Assert;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 import org.testng.annotations.Parameters;
 
@@ -10,126 +10,113 @@ import com.talend.tac.cases.Login;
 
 public class TestAddConnection extends Login {
 	@Test(groups = "AddConnection")
-	@Parameters({ "Mysql_Connectionlabel", "Mysql_Dbname","Mysql_Dbtype" ,"Mysql_Host",
-			"Mysql_Serverport", "Mysql_Username", "Mysql_Password",
-			"Mysql_Datasourse", "Mysql_Additional", "Mysql_Stattable",
-			"Mysql_Logstable" })
-	public void addConnectionMysql(String label, String dbname, String dbtype,String host,
-			String serverport, String username, String password,
+	@Parameters( { "Mysql_Connectionlabel", "Mysql_Dbname", "Mysql_Dbtype",
+			"Mysql_Host", "Mysql_Serverport", "Mysql_Username",
+			"Mysql_Password", "Mysql_Datasourse", "Mysql_Additional",
+			"Mysql_Stattable", "Mysql_Logstable" })
+	public void addConnectionMysql(String label, String dbname, String dbtype,
+			String host, String serverport, String username, String password,
 			String datasourse, String additional, String stat, String logs) {
-		addConnection(label, dbname, dbtype,host, serverport, username, password,
-				datasourse, additional, stat, logs);
+		addConnection(label, dbname, dbtype, host, serverport, username,
+				password, datasourse, additional, stat, logs);
 	}
 
 	@Test(groups = "AddConnection")
-	@Parameters({ "MSSQL_Connectionlabel", "MSSQL_Dbname","MSSQL_Dbtype", "MSSQL_Host",
-			"MSSQL_Serverport", "MSSQL_Username", "MSSQL_Password",
-			"MSSQL_Datasourse", "MSSQL_Additional", "MSSQL_Stattable",
-			"MSSQL_Logstable" })
-	public void addConnectionMSSQL(String label, String dbname,String dbtype, String host,
-			String serverport, String username, String password,
+	@Parameters( { "MSSQL_Connectionlabel", "MSSQL_Dbname", "MSSQL_Dbtype",
+			"MSSQL_Host", "MSSQL_Serverport", "MSSQL_Username",
+			"MSSQL_Password", "MSSQL_Datasourse", "MSSQL_Additional",
+			"MSSQL_Stattable", "MSSQL_Logstable" })
+	public void addConnectionMSSQL(String label, String dbname, String dbtype,
+			String host, String serverport, String username, String password,
 			String datasourse, String additional, String stat, String logs) {
-		addConnection(label, dbname,dbtype, host, serverport, username, password,
-				datasourse, additional, stat, logs);
+		addConnection(label, dbname, dbtype, host, serverport, username,
+				password, datasourse, additional, stat, logs);
 
 	}
+
 	@Test(groups = "AddConnection")
-	@Parameters({ "Oracle_Connectionlabel", "Oracle_Dbname", "Oracle_Dbtype","Oracle_Host",
-			"Oracle_Serverport", "Oracle_Username", "Oracle_Password",
-			"Oracle_Datasourse", "Oracle_Additional", "Oracle_Stattable",
-			"Oracle_Logstable" })
-	public void addConnectionOracle(String label, String dbname,String dbtype, String host,
-			String serverport, String username, String password,
+	@Parameters( { "Oracle_Connectionlabel", "Oracle_Dbname", "Oracle_Dbtype",
+			"Oracle_Host", "Oracle_Serverport", "Oracle_Username",
+			"Oracle_Password", "Oracle_Datasourse", "Oracle_Additional",
+			"Oracle_Stattable", "Oracle_Logstable" })
+	public void addConnectionOracle(String label, String dbname, String dbtype,
+			String host, String serverport, String username, String password,
 			String datasourse, String additional, String stat, String logs) {
-		addConnection(label, dbname,dbtype, host, serverport, username, password,
-				datasourse, additional, stat, logs);
+		addConnection(label, dbname, dbtype, host, serverport, username,
+				password, datasourse, additional, stat, logs);
 
 	}
 
-	public void addConnection(String label, String dbname,String dbtype, String host,
-			String serverport, String username, String password,
+	public void addConnection(String label, String dbname, String dbtype,
+			String host, String serverport, String username, String password,
 			String datasourse, String additional, String stat, String logs) {
 		this.waitForElementPresent("!!!menu.connections.element!!!", 30);
 		selenium.click("!!!menu.connections.element!!!");
 		this.waitForElementPresent("idSubModuleAddButton", 30);
 		selenium.click("idSubModuleAddButton");
 		this.waitForElementPresent("//span[text()='Database connection']", 30);
-		
+
 		// configure database parameters
 		// DB label
-		selenium.type(other.getString("inputname.id.connection.add.label"),
-				label);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.label"), "blur");
-		
+		type(other.getString("inputname.id.connection.add.label"), label);
 		// database server port
-		selenium.type(other.getString("inputname.id.connection.add.port"),
-				serverport);
-		selenium.fireEvent(other.getString("inputname.id.connection.add.port"),
-				"blur");
+		type(other.getString("inputname.id.connection.add.port"), serverport);
 		// database host
-		selenium.type(other.getString("inputname.id.connection.add.host"), host);
-		selenium.fireEvent(other.getString("inputname.id.connection.add.host"),
-				"blur");
+		type(other.getString("inputname.id.connection.add.host"), host);
 		// database name
-		selenium.type(other.getString("inputname.id.connection.add.database"),
-				dbname);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.database"), "blur");
+		type(other.getString("inputname.id.connection.add.database"), dbname);
 		// database user name
-		selenium.type(other.getString("inputname.id.connection.add.username"),
-				username);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.username"), "blur");
+		type(other.getString("inputname.id.connection.add.username"), username);
 		// database password
-		selenium.type(other.getString("inputname.id.connection.add.password"),
-				password);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.password"), "blur");
+		type(other.getString("inputname.id.connection.add.password"), password);
 		// data source
-		selenium.type(
-				other.getString("inputname.id.connection.add.dataSource"),
+		type(other.getString("inputname.id.connection.add.dataSource"),
 				datasourse);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.dataSource"),
-				"blur");
 		// additional parameters
-		selenium.type(other
-				.getString("inputname.id.connection.add.additionnalParams"),
+		type(other.getString("inputname.id.connection.add.additionnalParams"),
 				additional);
-		selenium.fireEvent(other
-				.getString("inputname.id.connection.add.additionnalParams"),
-				"blur");
 		// database state table
-		selenium.type(other.getString("inputname.id.connection.add.statTable"),
-				stat);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.statTable"),
-				"blur");
+		type(other.getString("inputname.id.connection.add.statTable"), stat);
 		// database logs table
-		selenium.type(other.getString("inputname.id.connection.add.logTable"),
-				logs);
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.logTable"), "blur");		
-		
+		type(other.getString("inputname.id.connection.add.logTable"), logs);
 		selenium.setSpeed(MID_SPEED);
-		// select db type 
+		// select db type
 		selenium.click(other.getString("inputname.id.connection.add.dbtype"));
-		
-		selenium.mouseDown("//div[@role='listitem']["+dbtype+"]");
-		
-		selenium.fireEvent(
-				other.getString("inputname.id.connection.add.dbtype"), "blur");
+		selenium.mouseDown("//div[@role='listitem'][" + dbtype + "]");
+		selenium.fireEvent(other.getString("inputname.id.connection.add.dbtype"), "blur");
 		selenium.click(other.getString("inputname.id.connection.add.logTable"));
-		
+		//check connections status
+		selenium.click("idCheckUrlButton");
+		waitForCheckConnectionStatus(4);
 		// save button
 		selenium.mouseDown("idFormSaveButton");
-		
-		selenium.keyPressNative(Event.ENTER+"");
-		selenium.keyUpNative(Event.ENTER+"");
-		
-		//check whether add success
-		Assert.assertTrue(selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+label+"')]"), "DBConnection type: "+dbtype+" added failed!");
+		selenium.keyPressNative(Event.ENTER + "");
+		selenium.keyUpNative(Event.ENTER + "");
+		// check whether add success
+		assertTrue(selenium.isElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"+ label + "')]"), "DBConnection type: "+ dbtype + " added failed!");
 	}
 
+	public void type(String locator, String value) {
+		selenium.type(locator, value);
+		selenium.fireEvent(locator, "blur");
+	}
+
+	public void waitForCheckConnectionStatus(int OK_Num) {
+		boolean flag = false;
+		int seconds_Counter = 0;
+		while (flag == false) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			flag = selenium.getXpathCount("//div[text()='OK']").intValue() >= OK_Num;
+			seconds_Counter++;
+			if (seconds_Counter >= WAIT_TIME)
+				assertTrue(selenium.getXpathCount("//div[text()='OK']")
+						.intValue() >= OK_Num);
+		}
+
+	}
 }
