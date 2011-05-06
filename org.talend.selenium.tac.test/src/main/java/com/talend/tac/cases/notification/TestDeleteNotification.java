ae700ffa4cf2e48b239f3ca399576ae2815310b9
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-06 10:19:26 +0000

    modiifed after click 'delete' assert
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@59965 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDeleteNotification.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDeleteNotification.java
@@ -9,8 +9,7 @@ import com.talend.tac.cases.Login;
 public class TestDeleteNotification extends Login {
     
 	//delete a notification(cancel Delete Notification)
-	@Test(groups={"DeleteNotification"})
-//	,dependsOnGroups={"AddJobserverNotification"})
+	@Test(groups={"DeleteNotification"},dependsOnGroups={"AddJobserverNotification"})
 	@Parameters({"eventNewUser"})
 	public void testCancelDeleteNotification(String eventNewUser) {
 		
@@ -34,17 +33,17 @@ public class TestDeleteNotification extends Login {
 	public void testDeleteNotification(String eventNewUser) {
 		
 		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
-		selenium.setSpeed(MAX_SPEED);
+		selenium.setSpeed(MID_SPEED);
 				
     	selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-category']");//choose a notification type
 		selenium.chooseOkOnNextConfirmation();
 		selenium.click("idSubModuleDeleteButton");//click 'Delete' button
-		selenium.setSpeed(MAX_SPEED);
+		selenium.setSpeed(MID_SPEED);
 	    Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected notification [\\s\\S]$"));
         selenium.setSpeed(MIN_SPEED);
 	 
         selenium.setSpeed(MID_SPEED);
-        Assert.assertFalse(selenium.isElementPresent("//div[text()='"+eventNewUser+"']/" +
+        Assert.assertTrue(!selenium.isElementPresent("//div[text()='"+eventNewUser+"']/" +
 		"parent::td/parent::tr//img[@class='gwt-Image' and @title='true']"));		
 		selenium.setSpeed(MIN_SPEED);
 		
