02bf0b037aca8e4d7a27ccb2f741bef38c05b9d1
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-10 10:14:48 +0000

    selenium: Modified xpath
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@60175 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddJVMParametersForTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddJVMParametersForTask.java
@@ -1,6 +1,5 @@
 package com.talend.tac.cases.executionTask;
 
-import java.awt.Event;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -8,25 +7,37 @@ import org.testng.annotations.Test;
 import com.talend.tac.cases.Login;
 
 public class TestAddJVMParametersForTask  extends Login {
-    @Test
-    @Parameters({"modifyTask"})
+    
+	@Test
+    @Parameters({"labelAddJVMParametersForTask"})
 	public void testAddJVMParametersForTask(String label) {
+    	
     	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
     	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
     	//select a exist task
+    	selenium.setSpeed(MID_SPEED);
     	selenium.mouseDown("//span[text()='"+label+"']");
 		this.clickWaitForElementPresent("//span[text()='JVM parameters']");
 		this.clickWaitForElementPresent("idJobConductorCmdPrmAddButton()");
 		//jvm parameters
 		this.clickWaitForElementPresent("//td[@role='gridcell'][2]//img");
-		this.waitForElementPresent("//input[@class=' x-form-field x-form-text x-form-focus']", WAIT_TIME);//jvm
-		selenium.click("//input[@class=' x-form-field x-form-text x-form-focus']");
-		selenium.type("//input[@class=' x-form-field x-form-text x-form-focus']","-Xms356M -Xmx1024M");
+		this.waitForElementPresent("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']//input", WAIT_TIME);//jvm
+		selenium.click("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']//input");
+		selenium.type("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']//input","-Xms356M -Xmx1024M");
 		//description
-		this.clickWaitForElementPresent("//td[@role='gridcell'][3]//img");
-		this.waitForElementPresent("//input[@class=' x-form-field x-form-text x-form-focus']", WAIT_TIME);//jvm
-		selenium.click("//input[@class=' x-form-field x-form-text x-form-focus']");
-		selenium.type("//input[@class=' x-form-field x-form-text x-form-focus']","jvm set in task");
-		selenium.keyDown("//input[@class=' x-form-field x-form-text x-form-focus']", "\\13");
-	}
+		this.clickWaitForElementPresent("//span[text()='Description']//ancestor::div[@class='x-grid3-viewport']//td[@class='x-grid3-col x-grid3" +
+				"-cell x-grid3-td-description x-grid3-cell-last ']//img[@title='Click to edit']");
+		this.waitForElementPresent("//span[text()='Description']//ancestor::div[@class='x-grid3-viewport']", WAIT_TIME);//jvm
+		selenium.click("//span[text()='Description']//ancestor::div[@class='x-grid3-viewport']//input");
+		selenium.type("//span[text()='Description']//ancestor::div[@class='x-grid3-viewport']//input","jvm set in task");
+		selenium.keyDown("//span[text()='Description']//ancestor::div[@class='x-grid3-viewport']//input", "\\13");
+	    selenium.setSpeed(MIN_SPEED);
+	    
+	    selenium.click("idJobConductorTaskRunButton()");
+	    this.waitForElementPresent("//label[text()='Ok']", WAIT_TIME);
+	    Assert.assertTrue(selenium.isElementPresent("//label[text()='Ok']"));
+	    selenium.click("//div[@class=' x-nodrag x-tool-close x-tool x-component']");
+	    
+		
+    }
 }
