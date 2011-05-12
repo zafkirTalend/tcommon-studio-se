ed2e98ab0a0d375529156fe00ca8a574365e483d
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-12 02:15:22 +0000

    selenium: update TestTaskDisplay, add testSearchTask.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@60268 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestTaskDisplay.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestTaskDisplay.java
@@ -8,25 +8,48 @@ import static org.testng.Assert.*;
 import com.talend.tac.cases.Login;
 
 public class TestTaskDisplay extends Login {
-	@Test
+	@Test(description="")
 	@Parameters({"labelReferenceproTjava"})
-	public void testTaskDisplay(String TaskLable){
+	public void testTaskDisplayGroupedByProject(String TaskLable){
 		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
 		
-		this.waitForElementPresent("//div[contains(text(),'Project: "+ TaskLable + "')]", WAIT_TIME);
+		this.waitForElementPresent("//div[contains(text(),'Project: referencepro')]", WAIT_TIME);
 		//click the group of task
-		this.waitForElementPresent("//span[text()='testTask2']", WAIT_TIME);
+		this.waitForElementPresent("//span[text()='"+TaskLable+"']", WAIT_TIME);
 		//make a task selected
-		selenium.mouseDown("//span[text()='testTask2']");
-		//assert the display(by attribute)
-		assertFalse(selenium.isElementPresent("//div[@class='x-grid3-row  x-unselectable-single ']//span[text()='testTask2']"));
-		selenium.mouseDown("//div[contains(text(),'Project: "+ TaskLable + "')]");
+		selenium.mouseDown("//span[text()='"+TaskLable+"']");
+		//collapsed the group and assert the display(by attribute) 
+		assertFalse(selenium.isElementPresent("//div[@class='x-grid3-row  x-unselectable-single ']//span[text()='"+TaskLable+"']"));
+		selenium.mouseDown("//div[contains(text(),'Project: referencepro')]");
 		selenium.refresh();
-		this.waitForElementPresent("//div[contains(text(),'Project: "+ TaskLable + "')]", WAIT_TIME);
-		//the task of this group is extended
-		selenium.mouseDown("//div[contains(text(),'Project: "+ TaskLable + "')]");
-		assertTrue(selenium.isElementPresent("//div[@class='x-grid3-row  x-unselectable-single ']//span[text()='testTask2']"));
+		this.waitForElementPresent("//div[contains(text(),'Project: referencepro')]", WAIT_TIME);
+		//expand the group
+		selenium.mouseDown("//div[contains(text(),'Project: referencepro')]");
+		assertTrue(selenium.isElementPresent("//div[@class='x-grid3-row  x-unselectable-single ']//span[text()='"+TaskLable+"']"));
 		
 	}
 
+	@Test(description="input a key work and search task")
+	@Parameters({"labelReferenceproTjava"})
+	public void testSearchTask (String TaskLable){
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+		
+		this.waitForElementPresent("//div[contains(text(),'Project: referencepro')]", WAIT_TIME);
+		//click the group of task
+		this.waitForElementPresent("//span[text()='"+TaskLable+"']", WAIT_TIME);
+		//wait and input a keyWord
+		this.waitForElementPresent("//button[text()='Filter tasks']/ancestor::td/following-sibling::td//input", WAIT_TIME);
+		selenium.typeKeys("//button[text()='Filter tasks']/ancestor::td/following-sibling::td//input","input a keyword");
+		//setSpeed,wait the tasks disappears and make sure the assertFalse works well.
+		selenium.setSpeed(MID_SPEED);
+		assertFalse(selenium.isElementPresent("//span[text()='"+TaskLable+"']"));
+		selenium.setSpeed(MIN_SPEED);
+		//clear the keyWrods
+		selenium.click("//button[text()='Filter tasks']/ancestor::td/following-sibling::td//input/following-sibling::div");
+		assertTrue(selenium.isElementPresent("//span[text()='"+TaskLable+"']"));
+		//type a the name of task as a keyword
+		selenium.typeKeys("//button[text()='Filter tasks']/ancestor::td/following-sibling::td//input",TaskLable);
+		assertTrue(selenium.isElementPresent("//span[text()='"+TaskLable+"']"));
+	}
+	
 }
