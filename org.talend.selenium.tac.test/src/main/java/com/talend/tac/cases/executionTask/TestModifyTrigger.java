e6dee5d4775f1413fb42f2701118c5089548a1f6
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-12 09:25:03 +0000

    selenium:    add case ---testModifySimpleTriggerTimeInterval()                     //test change time interval of simple trigger
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@60344 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestModifyTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestModifyTrigger.java
@@ -1,13 +1,18 @@
 package com.talend.tac.cases.executionTask;
 
+import java.awt.Event;
+
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
+import com.talend.tac.cases.executePlan.TriggerDate;
 
 public class TestModifyTrigger extends Login {
     
+	TriggerDate date = new TriggerDate();
+	
     //add method for test change trigger name	
 	public void renameTrigger(String taskLabel, String labelBeforeModify, String labelAfterModified, 
 			String description ,String triggerType) {
@@ -64,6 +69,79 @@ public class TestModifyTrigger extends Login {
 			, String description, String triggerType) {
 		
 		renameTrigger(taskLabel, labelBeforeModify, labelAfterModified, description, triggerType);
+		
+	}
+	
+	
+	//test change time interval of simple trigger
+	@Test(dependsOnMethods={"testRanameCronTrigger"})
+	@Parameters({"labelTRunJobByTaskRun", "addSimpleTriggerLabelNotChooseDate","simpleTriggerType"})
+	public void testModifySimpleTriggerTimeInterval(String taskLabel, String triggerLabel, String triggerType) {
+		
+
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");//into JobConductor page
+    	selenium.setSpeed(MID_SPEED);
+    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+    	Assert.assertTrue(selenium.isElementPresent("//span[text()='"+taskLabel+"']"));  
+    	selenium.setSpeed(MIN_SPEED);
+    	selenium.mouseDown("//span[text()='"+taskLabel+"']");//select a exist task    	  	
+    	
+    	selenium.setSpeed(MID_SPEED);
+       	selenium.mouseDown("//span[text()='"+triggerLabel+"']");//select a exist trigger
+        this.waitForElementPresent("//input[@name='startTime']", WAIT_TIME);    
+//        selenium.keyDown("//input[@name='startTime']", ""+Event.BACK_SPACE);
+    	selenium.click("//label[text()='Start time:']/parent::div//div/div/div");//start date
+		if(date.isClickFutureMonthButton(date.getFuture(24))) {//type in start time
+			
+        	selenium.setSpeed(MID_SPEED);
+        	selenium.click("//div[@class=' x-icon-btn x-nodrag x-date-right-icon x-component']");
+        	selenium.click("//td[@class='x-date-active']/a/span[text()='"+date.getFuture(24).days+"']");
+        	selenium.setSpeed(MIN_SPEED);
+        	
+        }
+
+    	else{//click tianshu}  
+        	selenium.setSpeed(MID_SPEED);
+        	selenium.click("//td[@class='x-date-active']/a/span[text()='"+date.getFuture(24).days+"']");
+        	selenium.setSpeed(MIN_SPEED);
+        	
+        }
+		
+		selenium.click("//label[text()='End time:']/parent::div//div/div/div");//start date
+		if(date.isClickFutureMonthButton(date.getFuture(48))) {//type in start time
+			
+        	selenium.setSpeed(MID_SPEED);
+        	selenium.click("//div[@class=' x-icon-btn x-nodrag x-date-right-icon x-component']");
+        	selenium.click("//td[@class='x-date-active']/a/span[text()='"+date.getFuture(48).days+"']");
+        	selenium.setSpeed(MIN_SPEED);
+        	
+        }
+        
+    	else{//click tianshu}  
+        	selenium.setSpeed(MID_SPEED);
+        	selenium.click("//td[@class='x-date-active']/a/span[text()='"+date.getFuture(48).days+"']");
+        	selenium.setSpeed(MIN_SPEED);
+        	
+        }  
+		
+	
+    
+        String timeIntervalXpath = "//span[text()='Add simple trigger']/parent::legend/parent::fieldset" +
+        		"//input[@name='repeatInterval']";
+       	
+       	Assert.assertEquals(selenium.getValue(timeIntervalXpath), "20");
+    	this.typeString(timeIntervalXpath, "40");//Time interval (s)
+    	
+        selenium.setSpeed(MID_SPEED);
+	    selenium.click("//span[text()='"+triggerType+"']/parent::legend/parent::fieldset/parent::form/" +
+		"parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");//click save button
+		selenium.setSpeed(MIN_SPEED);
+		selenium.click("//span[text()='Triggers']/parent::span/parent::em/parent::a/parent::li/parent::ul/parent::div/" +
+		"parent::div/parent::div//button[text()='Refresh']");//click refresh button
+        selenium.setSpeed(MID_SPEED);
+	  	selenium.mouseDown("//span[text()='"+triggerLabel+"']");//select a exist trigger
+       	Assert.assertEquals(selenium.getValue(timeIntervalXpath), "40");
+       	selenium.setSpeed(MIN_SPEED);
 		
 	}
 	
