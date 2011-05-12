76478415b0d6ed65621521a8bba0428773f7661f
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-12 09:23:33 +0000

    Modified cases: TestAddTriggerAddFileTrigger---testAddTriggerAddFileTriggerAddExist()                    //modified some xpath
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@60343 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddFileTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddFileTrigger.java
@@ -1,78 +1,61 @@
 package com.talend.tac.cases.executionTask;
 
-import java.awt.Event;
-
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 
 public class TestAddTriggerAddFileTrigger extends Login {
-  
-	@Test
-    public void testAddTriggerAddFileTrigger() {
-    	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-    	selenium.setSpeed(MID_SPEED);
-    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.mouseDown("//div[text()='Copy_of_test_task']");//select a exist task
-		selenium.click("//button[text()='Add trigger...']");//add a trigger
-		selenium.click("//a[text()='Add FileTrigger']");//add a FileTrigger
-        Assert.assertTrue(selenium.isElementPresent("//span[text()='"+rb.getString("trigger.action.addFileTrigger")+"']"));
-		selenium.type("//input[@class=' x-form-field x-form-text x-form-invalid']", "TestFileTrigger");//label
-		selenium.fireEvent("//input[@class=' x-form-field x-form-text x-form-invalid']","blur");
-		selenium.getValue("//input[@class=' x-form-field x-form-text x-form-invalid']");
-		selenium.type("//input[@name='description']", "test add a CronTrigger");//description
-		selenium.fireEvent("//input[@name='description']","blur");
-		
-		selenium.type("//input[@name='checkTimeInterval']", "5");//checkTimeInterval
-		selenium.type("//input[@name='folderPath']", "//D:");//folderPath
-		selenium.type("fileMask", "*.txt");//fileMask
-		
-		selenium.click("//input[@class=' x-form-checkbox' and @name='exist']");//*.txt is exist
-		Assert.assertTrue(selenium.isChecked("//input[@class=' x-form-checkbox' and @name='exist']"));
-		selenium.click("//input[@class=' x-form-checkbox' and @name='created']");//*.txt is created
-		Assert.assertTrue(selenium.isChecked("//input[@class=' x-form-checkbox' and @name='created']"));
-		selenium.click("//input[@class=' x-form-checkbox' and @name='modified']");//*.txt is modified
-		Assert.assertTrue(selenium.isChecked("//input[@class=' x-form-checkbox' and @name='modified']"));
-		selenium.click("//div[@class='x-form-trigger x-form-trigger-arrow ']");//choose server
-		selenium.mouseDownAt("//div[@role='listitem'][1]", ""+Event.ENTER);
-		Assert.assertEquals(selenium.getValue("//input[@name='executionServerId']"), "aaaa");
-		selenium.click("idFormSaveButton");
-		Assert.assertTrue(selenium.isElementPresent("//div[text()='TestFileTrigger']"));
-		selenium.setSpeed(MIN_SPEED);
-	 }
+ 
 	 
-	@Test(dependsOnMethods={"testAdd_trigger_AddFileTrigger()"})
-    public void testAddTriggerAddFileTriggerAddExist() {
+	@Test(dependsOnGroups={"AddCronTrigger"})
+	@Parameters({"TaskBaseBranch","addFileTriggerOfExist","addFileTriggerOfExistDescription","FolderPath",
+		"FileMask","triggerExistCondition","ServerForUseAvailable"})
+    public void testAddTriggerAddFileTriggerAddExist(String taskLabel,String triggerLabel,String triggerDescription,
+    		String folderPath,String fileMask,String triggerExistCondition,String serverName) {
     	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
     	selenium.setSpeed(MID_SPEED);
     	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.mouseDown("//div[text()='Copy_of_test_task']");//select a exist task
+    	selenium.setSpeed(MIN_SPEED);
+    	selenium.mouseDown("//span[text()='"+taskLabel+"']");//select a exist task
 		selenium.click("//button[text()='Add trigger...']");//add a trigger
-		selenium.click("//a[text()='Add FileTrigger']");//add a FileTrigger
+		selenium.click("//a[text()='Add file trigger']");//add a FileTrigger
         Assert.assertTrue(selenium.isElementPresent("//span[text()='"+rb.getString("trigger.action.addFileTrigger")+"']"));
-		selenium.type("//input[@class=' x-form-field x-form-text x-form-invalid']", "TestFileTrigger");//label
-		selenium.fireEvent("//input[@class=' x-form-field x-form-text x-form-invalid']","blur");
-		selenium.getValue("//input[@class=' x-form-field x-form-text x-form-invalid']");
-		selenium.type("//input[@name='description']", "test add a CronTrigger");//description
-		selenium.fireEvent("//input[@name='description']","blur");
 		
-		selenium.type("//input[@name='checkTimeInterval']", "5");//checkTimeInterval
-		selenium.type("//input[@name='folderPath']", "//D:");//folderPath
-		selenium.type("fileMask", "*.txt");//fileMask
+    	this.typeString("//span[text()='Add file trigger']/parent::legend/parent::fieldset" +
+				"//input[@name='label']", triggerLabel);//label
+				                                                                     
+		this.typeString("//span[text()='Add file trigger']/parent::legend/parent::fieldset" +
+				"//input[@name='description']", triggerDescription);//description
+		
+		this.typeString("//input[@name='checkTimeInterval']", "20");//checkTimeInterval
+		
+		this.typeString("//input[@name='folderPath']", folderPath);//folderPath
+		
+		this.typeString("//input[@name='fileMask']", "*.txt");//fileMask
 		
 		selenium.click("//input[@class=' x-form-checkbox' and @name='exist']");//*.txt is exist
 		Assert.assertTrue(selenium.isChecked("//input[@class=' x-form-checkbox' and @name='exist']"));
-		selenium.click("//input[@class=' x-form-checkbox' and @name='created']");//*.txt is created
-		Assert.assertTrue(selenium.isChecked("//input[@class=' x-form-checkbox' and @name='created']"));
-		selenium.click("//input[@class=' x-form-checkbox' and @name='modified']");//*.txt is modified
-		Assert.assertTrue(selenium.isChecked("//input[@class=' x-form-checkbox' and @name='modified']"));
-		selenium.click("//div[@class='x-form-trigger x-form-trigger-arrow ']");//choose server
-		selenium.mouseDownAt("//div[@role='listitem'][1]", ""+Event.ENTER);
-		Assert.assertEquals(selenium.getValue("//input[@name='executionServerId']"), "aaaa");
-		selenium.click("idFormSaveButton");
-		Assert.assertTrue(selenium.isTextPresent(rb.getString("trigger.error.uniqueLabel")));
+		selenium.setSpeed(MID_SPEED);
+	
+		selenium.click("//label[text()='Check file server:']/parent::div//input[@name='executionServerId']");
+		selenium.mouseDown("//div[text()='" + serverName + "']");
+		
+		selenium.click("//span[text()='Add file trigger']/parent::legend/parent::fieldset/parent::form/" +
+		"parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");
 		selenium.setSpeed(MIN_SPEED);
-	   
+		if(!selenium.isElementPresent("//span[text()='"+triggerLabel+"']")) {
+			selenium.click("//span[text()='Triggers']/parent::span/parent::em/parent::a/parent::li/parent::ul/parent::div/" +
+			"parent::div/parent::div//button[text()='Refresh']");
+    	}
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+triggerLabel+"']"));
+		selenium.setSpeed(MIN_SPEED);
+		
+		this.waitForElementPresent("//span[text()='Generating...']", WAIT_TIME);
+	   	Assert.assertTrue(selenium.isElementPresent("//span[text()='Generating...']"));
+	    
 	 }
+	
 }
