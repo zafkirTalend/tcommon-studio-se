7723ef302fd628016e2a5cf1dfc8c442148e6ac9
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-27 08:29:16 +0000

    selenium: Add new cases: TestAddTriggerAddFileTrigger---addFileTrigger()                    --creat method(addFileTrigger) for test add file trigger                             ---testAddTriggerAddFileTriggerAddExistOfLabelExist()                    --test add a file trigger of label is exist                             ---testAddTriggerAddFileTriggerWrongFormValue()                    --test add a cronTrigger use wrong form value
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61259 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddFileTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddFileTrigger.java
@@ -8,13 +8,11 @@ import com.talend.tac.cases.Login;
 
 public class TestAddTriggerAddFileTrigger extends Login {
  
-	 
-	@Test(dependsOnGroups={"AddCronTrigger"})
-	@Parameters({"TaskBaseBranch","addFileTriggerOfExist","addFileTriggerOfExistDescription","FolderPath",
-		"FileMask","triggerExistCondition","ServerForUseAvailable"})
-    public void testAddTriggerAddFileTriggerAddExist(String taskLabel,String triggerLabel,String triggerDescription,
-    		String folderPath,String fileMask,String triggerExistCondition,String serverName) {
-    	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+	//creat method(addFileTrigger) for test add file trigger
+	public void addFileTrigger(String taskLabel, String triggerLabel, String triggerDescription,
+			 String intervalTime, String folderPath, String fileMask, String serverName) {
+		
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
     	selenium.setSpeed(MID_SPEED);
     	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
     	selenium.setSpeed(MIN_SPEED);
@@ -38,31 +36,85 @@ public class TestAddTriggerAddFileTrigger extends Login {
 				                                                                     
 		this.typeString("idJobConductorFileTriggerDescInput", triggerDescription);//description
 		
-		this.typeString("idJobConductorFileTriggerCheckTimeIntervalInput", "20");//checkTimeInterval
+		this.typeString("idJobConductorFileTriggerCheckTimeIntervalInput", intervalTime);//checkTimeInterval
 		
 		this.typeString("idJobConductorFileTriggerFolderPathInput", folderPath);//folderPath
 		
-		this.typeString("idJobConductorFileTriggerFileMaskInput", "*.txt");//fileMask
+		this.typeString("idJobConductorFileTriggerFileMaskInput", fileMask);//fileMask
 		
 		selenium.click("idJobConductorFileTriggerFtExitCheckBox");//*.txt is exist
 		Assert.assertTrue(selenium.isChecked("idJobConductorFileTriggerFtExitCheckBox"));
-		selenium.setSpeed(MID_SPEED);
-		
-		selenium.click("idJobConductorFileTriggerFileServerListBox");
+	
+		selenium.click("idJobConductorFileTriggerFileServerListBox");//select an server	
+		this.waitForElementPresent("//div[text()='" + serverName + "']", WAIT_TIME);
 		selenium.mouseDown("//div[text()='" + serverName + "']");
 		
+	}
+	
+	//test add a file trigger
+	@Test(dependsOnGroups={"AddCronTrigger"})
+	@Parameters({"TaskBaseBranch","addFileTriggerOfExist","addFileTriggerOfExistDescription","FolderPath",
+		"FileMask","ServerForUseAvailable"})
+    public void testAddTriggerAddFileTriggerAddExist(String taskLabel,String triggerLabel,String triggerDescription,
+    		String folderPath,String fileMask,String serverName) {
+    	
+		addFileTrigger(taskLabel, triggerLabel, triggerDescription, "60", folderPath, fileMask, serverName);
+		
 		selenium.click("idFileTriggerSave");
-		selenium.setSpeed(MIN_SPEED);
+	
 		if(!selenium.isElementPresent("//span[text()='"+triggerLabel+"']")) {
 			selenium.click("idTriggerRefresh");
     	}
-		selenium.setSpeed(MID_SPEED);
+		this.waitForElementPresent("//span[text()='"+triggerLabel+"']", WAIT_TIME);
 		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+triggerLabel+"']"));
 		selenium.setSpeed(MIN_SPEED);
 		this.waitForElementPresent("//span[text()='Running...']", WAIT_TIME);
 		Assert.assertTrue(selenium.isElementPresent("//span[text()='Running...']"));
 		
 		
+	}
+	
+	//test add a	 file trigger of label is exist
+	@Test(dependsOnMethods={"testAddTriggerAddFileTriggerAddExist"})
+	@Parameters({"TaskBaseBranch","addFileTriggerOfExist","addFileTriggerOfExistDescription","FolderPath",
+		"FileMask","ServerForUseAvailable"})
+    public void testAddTriggerAddFileTriggerAddExistOfLabelExist(String taskLabel,String triggerLabel,String triggerDescription,
+    		String folderPath,String fileMask,String serverName) {
+    	
+		addFileTrigger(taskLabel, triggerLabel, triggerDescription, "60", folderPath, fileMask, serverName);
+		
+		selenium.click("idFileTriggerSave");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isTextPresent(rb.getString("trigger.error.uniqueLabel")));
+		selenium.setSpeed(MIN_SPEED);
+		
+	}
+	
+	
+	//test add a cronTrigger use wrong form value
+	@Test(dependsOnMethods={"testAddTriggerAddFileTriggerAddExist"})
+	@Parameters({"TaskBaseBranch","addFileTriggerOfExistWrongForm","addFileTriggerOfExistDescriptionWrongForm"
+		,"ServerForUseAvailable"})
+    public void testAddTriggerAddFileTriggerWrongFormValue(String taskLabel,String triggerLabel,String triggerDescription,
+    		String serverName) {
+    	
+		addFileTrigger(taskLabel, triggerLabel, triggerDescription, "ss", "", "", serverName);
+		
+		Assert.assertTrue(selenium.isElementPresent("//input[@id='idJobConductorFileTriggerLabelInput']//ancestor::div" +
+				"[@class='x-form-item ']//img[@class='gwt-Image x-component ']"));//add assert for "label"  
+		Assert.assertTrue(selenium.isElementPresent("//input[@id='idJobConductorFileTriggerCheckTimeIntervalInput']" +
+				"//ancestor::div[@class='x-form-item ']//img[@class='gwt-Image x-component ']"));//add assert for "Pollint (s):"  
+		Assert.assertTrue(selenium.isElementPresent("//input[@id='idJobConductorFileTriggerFolderPathInput']" +
+				"//ancestor::div[@class='x-form-item ']//img[@class='gwt-Image x-component ']"));//add assert for "Folder path:" 
+		Assert.assertTrue(selenium.isElementPresent("//input[@id='idJobConductorFileTriggerFileMaskInput']//ancestor::div" +
+				"[@class='x-form-item ']//img[@class='gwt-Image x-component ']"));//add assert for "File mask:" 
+        
+		selenium.click("idFileTriggerSave");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isTextPresent("Fix errors in form before save"));
+		selenium.setSpeed(MIN_SPEED);
+		
 	 }
+	
 	
 }
