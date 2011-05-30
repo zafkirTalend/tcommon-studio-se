e7ecd58fb5e62358433dd1642c01fc62f94ab90e
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-30 09:02:51 +0000

    selenium:added a class named TestPlanExecution include three cases : 	testGeneratePlan, 	testDeployPlan, 	testRunPlan,
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61353 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestPlanExecution.java
@@ -0,0 +1,122 @@
+package com.talend.tac.cases.executePlan;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.base.Base;
+import com.talend.tac.cases.Login;
+
+public class TestPlanExecution extends Login {
+
+	public void addTask(String label, String description, String projectName,
+			String branchName, String jobName, String version, String context,
+			String serverName, String statisticName) {
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='"
+				+ rb.getString("menu.jobConductor") + "']"));
+		selenium.setSpeed(MIN_SPEED);
+		selenium.click("idSubModuleAddButton");
+		this.typeString("idJobConductorTaskLabelInput", label);// plan name
+		this.typeString("idJobConductorTaskDescInput", description);// plan name
+		if (!selenium.isChecked("idJobConductorTaskActiveListBox")) {
+			selenium.click("idJobConductorTaskActiveListBox");// check active
+			Assert.assertTrue(selenium
+					.isChecked("idJobConductorTaskActiveListBox"));
+		}
+		this.selectDropDownList("idTaskProjectListBox", projectName);
+		this.selectDropDownList("idTaskBranchListBox", branchName);
+		this.selectDropDownList("idTaskJobListBox", jobName);
+		this.selectDropDownList("idTaskVersionListBox", version);
+		this.selectDropDownList("idTaskContextListBox", context);
+		this.selectDropDownList("idJobConductorExecutionServerListBox",
+				serverName);
+		this.selectDropDownList("idJobConductorTaskStatisticsListBox",
+				statisticName);
+		this.selectDropDownList("idJobConductorOnUnavailableJobServerListBox",
+				"Wait");
+		if (!selenium.isElementPresent("//span[text()='" + label + "']")) {
+			selenium.click("idFormSaveButton");
+			this.waitForElementPresent("//span[text()='" + label + "']",
+					Base.WAIT_TIME);
+		}
+	}
+
+	public void addPlan(String label, String description, String task) {
+
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		this.waitForElementPresent(
+				"//div[@class='header-title' and text()='Execution Plan']",
+				WAIT_TIME);
+		Assert.assertTrue(selenium
+				.isElementPresent("//div[@class='header-title' and text()='Execution Plan']"));
+		this.clickWaitForElementPresent("//button[text()='Add plan']");
+		this.typeString("idExecutionPlanPlanFormLabelInput", label);
+		this.typeString("idExecutionPlanPlanFormDescInput", description);
+		this.selectDropDownList("String idExecutionPlanPlanFormTaskComboBox",
+				task);
+		// click save button
+		selenium.mouseDown("//span[@class='x-fieldset-header-text' and text()='Execution Plan']//ancestor::div[@class='x-tab-panel-body x-tab-panel-body-top']//button[@id='idFormSaveButton']");
+		selenium.click("//span[@class='x-fieldset-header-text' and text()='Execution Plan']//ancestor::div[@class='x-tab-panel-body x-tab-panel-body-top']//button[@id='idFormSaveButton']");
+		selenium.mouseUp("//span[@class='x-fieldset-header-text' and text()='Execution Plan']//ancestor::div[@class='x-tab-panel-body x-tab-panel-body-top']//button[@id='idFormSaveButton']");
+		this.waitForElementPresent("//span[text()='" + label + "']", WAIT_TIME);
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='" + label
+				+ "']"));
+		selenium.setSpeed(MIN_SPEED);
+	}
+
+	@Test
+	@Parameters({"plan.unexecuted.label","plan.unexecuted.tasklabel","labelDescription","AddcommonProjectname","branchNameTrunk","jobNameTJava","version0.1",
+		"context","ServerForUseAvailable","statisticEnabled"})
+	public void testGeneratePlan(String planlabel,String tasklabel, String labelDescription,String commonpro,String branch,String jobName,
+			String version,String context,String jobServer,String statistic) throws InterruptedException {
+		
+		addTask(tasklabel, labelDescription,commonpro,
+				branch, jobName,version, context,
+				jobServer,statistic);
+		addPlan(planlabel, "planunexcuted", tasklabel) ;
+		selenium.refresh();
+		this.waitForElementPresent("//span[text()='" + planlabel + "']", WAIT_TIME);
+		Thread.sleep(2000);
+		selenium.mouseDown("//span[text()='" + planlabel + "']");
+		selenium.click("idJobConductorTaskGenerateButton");
+		this.waitForElementPresent("//span[text()='Generating...']", WAIT_TIME);
+		this.waitForElementPresent("//span[text()='Ready to deploy']", WAIT_TIME);
+	}
+	
+	@Test(dependsOnMethods={"testGeneratePlan"})
+	@Parameters({"plan.unexecuted.label"})
+	public void testDeployPlan(String planLabel){
+		selenium.refresh();
+		this.waitForElementPresent("//span[text()='" + planLabel + "']", WAIT_TIME);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		selenium.mouseDown("//span[text()='" + planLabel + "']");
+		selenium.click("idJobConductorTaskDeployButton");
+		this.waitForElementPresent("//span[text()='Ready to run']", WAIT_TIME);
+		
+	}
+	
+	@Test(dependsOnMethods={"testDeployPlan"})
+	@Parameters({"plan.unexecuted.label"})
+	public void testRunPlan(String planLabel){
+		selenium.refresh();
+		this.waitForElementPresent("//span[text()='" + planLabel + "']", WAIT_TIME);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		selenium.mouseDown("//span[text()='" + planLabel + "']");
+		selenium.click("idJobConductorTaskRunButton");
+		this.waitForElementPresent("//span[text()='Running...']", WAIT_TIME);
+		this.waitForElementPresent("//span[text()='Ready to run']", WAIT_TIME);
+	}
+
+}
