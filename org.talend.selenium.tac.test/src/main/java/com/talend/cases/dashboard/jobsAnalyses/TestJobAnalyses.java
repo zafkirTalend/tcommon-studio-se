38f998fae18f2a3068b5341cffb8dd5a1007becb
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-05-31 07:50:24 +0000

    selenium: update TestJobAnalyses. changes, using the name of conenctions as a xpath instead of the index.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61459 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/jobsAnalyses/TestJobAnalyses.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/jobsAnalyses/TestJobAnalyses.java
@@ -10,12 +10,12 @@ import com.talend.tac.cases.Login;
 
 public class TestJobAnalyses extends Login {
 	@Test
-	public void testJobAnalyses() {
+	@Parameters({"Mysql_Connectionlabel"})
+	public void testJobAnalyses(String connection) {
 		this.clickWaitForElementPresent("!!!menu.dashjobs.element!!!");
 		//wait and select the first "connection"
 		this.clickWaitForElementPresent("//label[text()='Active connection:']/following-sibling::div//input");
-		this.waitForElementPresent("//div[contains(@class,'x-combo-list-item ')][1]", WAIT_TIME);
-		selenium.mouseDown("//div[contains(@class,'x-combo-list-item ')][1]");
+		this.MouseDownWaitForElementPresent("//div[contains(@class,'x-combo-list-item ') and text()=' "+ connection +"']");
 		this.waitForElementPresent("//img[@title='Ok']", WAIT_TIME);
 		//select a connection and simulate a click
 		selenium.mouseDown("//img[@title='Ok']");
