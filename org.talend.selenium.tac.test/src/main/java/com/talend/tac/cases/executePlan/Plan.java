634003438377d9a21a74876ef6ea91be6ae70dde
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-01 07:58:34 +0000

    selenium:add a method to class Plan named addPlan(String planLabel, String rootTask, String description) would be
    used by other cases.
    
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61576 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/Plan.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/Plan.java
@@ -1,36 +1,65 @@
 package com.talend.tac.cases.executePlan;
 
-import java.awt.event.KeyEvent;
-
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
 import com.talend.tac.cases.Login;
 
 public class Plan extends Login {
 
-public void deleteTrigger(String fileTriggerLabel){
-	this.waitForElementPresent("//span[text()='" + fileTriggerLabel + "']", WAIT_TIME);
-	selenium.mouseDown("//span[text()='" + fileTriggerLabel + "']");
-	selenium.chooseOkOnNextConfirmation();
-	try {
-		Thread.sleep(2000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public void addPlan(String planLabel, String rootTask, String description) {
+
+		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
+		this.waitForElementPresent(
+				"//div[@class='header-title' and text()='Execution Plan']",
+				WAIT_TIME);
+		Assert.assertTrue(selenium
+				.isElementPresent("//div[@class='header-title' and text()='Execution Plan']"));
+		this.clickWaitForElementPresent("//button[text()='Add plan']");
+		this.typeString("idExecutionPlanPlanFormLabelInput", planLabel);
+		this.typeString("idExecutionPlanPlanFormDescInput", description);
+		this.selectDropDownList("String idExecutionPlanPlanFormTaskComboBox",
+				rootTask);
+		selenium.mouseDown("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
+		selenium.click("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
+		selenium.mouseUp("//button[@class='x-btn-text ' and @id='idFormSaveButton']");
+		this.waitForElementPresent("//span[text()='" + planLabel + "']",
+				WAIT_TIME);
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"
+				+ planLabel + "']"));
+		selenium.setSpeed(MIN_SPEED);
 	}
-	selenium.click("idTriggerDelete");
-	selenium.getConfirmation();
-	try {
-		Thread.sleep(3000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+
+	public void deleteTrigger(String fileTriggerLabel) {
+		this.waitForElementPresent("//span[text()='" + fileTriggerLabel + "']",
+				WAIT_TIME);
+		selenium.mouseDown("//span[text()='" + fileTriggerLabel + "']");
+		selenium.chooseOkOnNextConfirmation();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		selenium.click("idTriggerDelete");
+		selenium.getConfirmation();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(
+				selenium.isElementPresent("//span[text()='" + fileTriggerLabel
+						+ "']"), "trigger delete failed!");
 	}
-	Assert.assertFalse(
-			selenium.isElementPresent("//span[text()='" + fileTriggerLabel
-					+ "']"), "trigger delete failed!");
-}
+	
+	public void sleep(int seconds){
+		try {
+			Thread.sleep(seconds);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
