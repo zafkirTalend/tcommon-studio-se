b926724a43b9169b952abffa412f63a485ec73b6
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-01 02:38:53 +0000

    selenium:  TestDeteleTask---testCancleDeleteTask()               ---testDeleteTask()         modified case of 'setSpeed'
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61522 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDeteleTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDeteleTask.java
@@ -12,13 +12,13 @@ public class TestDeteleTask  extends Login {
 		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
 		selenium.setSpeed(MID_SPEED);
 	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-		selenium.click("idSubModuleRefreshButton");//click "Refresh"
+		selenium.setSpeed(MIN_SPEED);
+	    selenium.click("idSubModuleRefreshButton");//click "Refresh"
 		selenium.mouseDown("//span[text()='testTaskNotChooseActive']");//select a exist task
 		selenium.chooseCancelOnNextConfirmation();
 		selenium.click("idSubModuleDeleteButton");//clcik "Delete"
 		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.getConfirmation().matches(other.getString("delete.plan.warning")));
-		selenium.click("idFormSaveButton");
 		Assert.assertTrue(selenium.isElementPresent("//span[text()='testTaskNotChooseActive']"));
 		selenium.setSpeed(MIN_SPEED);
 		
@@ -29,13 +29,12 @@ public class TestDeteleTask  extends Login {
 		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
 		selenium.setSpeed(MID_SPEED);
 	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-		selenium.click("idSubModuleRefreshButton"); //click "Refresh"
+		selenium.setSpeed(MIN_SPEED);
+	    selenium.click("idSubModuleRefreshButton"); //click "Refresh"
 		selenium.mouseDown("//span[text()='testTaskNotChooseActive']");//select a exist task
 		selenium.chooseOkOnNextConfirmation();
 		selenium.click("idSubModuleDeleteButton");//clcik "Delete"
-		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.getConfirmation().matches(other.getString("delete.plan.warning")));
-		selenium.click("idFormSaveButton");
 		selenium.setSpeed(MID_SPEED);
 		Assert.assertFalse(selenium.isElementPresent("//span[text()='testTaskNotChooseActive']"));//the plan cannot appear
 		selenium.setSpeed(MIN_SPEED);
