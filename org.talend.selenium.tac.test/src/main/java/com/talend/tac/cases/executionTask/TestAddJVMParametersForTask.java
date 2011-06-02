00453eed7932e3eb18434f06afa21839d6e55aff
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-02 10:14:13 +0000

    **--**Add case TestAddJVMParametersForTask---addJVMParametersForTask()        -add method for add jvm parameter                            ---checkJvmValueInGridRecoverLastExecution()        -check jvm value in grid and RecoverLastExecution                            ---testAddActiveCorrectJVMParametersForTask()        -test add a correct jvm parameter and active to task, then run it and check its value display in grid and RecoverLastExecution
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61675 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddJVMParametersForTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddJVMParametersForTask.java
@@ -8,10 +8,13 @@ import com.talend.tac.cases.Login;
 
 public class TestAddJVMParametersForTask  extends Login {
     
-//	@Test
-//	(dependsOnGroups={"AddTask"})
-    @Parameters({"labelAddJVMParametersForTask"})
-	public void testAddJVMParametersForTask(String label) {
+	String jvmParameterCorrect = "-Xms512M -Xmx512M";
+	String jvmParameterDescription = "jvm set in task";
+	
+	String expectationValue = jvmParameterCorrect+" "+"("+jvmParameterDescription+")";
+    
+	//add method for add jvm parameter 
+	public void addJVMParametersForTask(String label, String jvmParameter) {
     	
     	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
     	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
@@ -19,8 +22,8 @@ public class TestAddJVMParametersForTask  extends Login {
     	selenium.setSpeed(MID_SPEED);
     	selenium.mouseDown("//span[text()='"+label+"']");
     	selenium.setSpeed(MIN_SPEED);
-		this.clickWaitForElementPresent("//span[text()='JVM parameters']");
-		this.clickWaitForElementPresent("idJobConductorCmdPrmAddButton");
+		this.clickWaitForElementPresent("//span[text()='JVM parameters']");//into JVMParameter page
+		this.clickWaitForElementPresent("idJobConductorCmdPrmAddButton");//click add button, add jvm parameter
 		//jvm parameters
 		this.clickWaitForElementPresent("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']" +
 				"//div[@class='x-grid3-cell-inner x-grid3-col-parameter']");
@@ -29,7 +32,7 @@ public class TestAddJVMParametersForTask  extends Login {
 		selenium.click("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']" +
 				"//input[contains(@class,'x-form-field x-form-text x-form-focus')]");
 		selenium.type("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']" +
-				"//input[contains(@class,'x-form-field x-form-text x-form-focus')]","-version");
+				"//input[contains(@class,'x-form-field x-form-text x-form-focus')]",jvmParameter);//input jvm parameter
 		//description
 		this.clickWaitForElementPresent("//span[text()='Description']//ancestor::div[@class='x-grid3-viewport']" +
 				"//div[@class='x-grid3-cell-inner x-grid3-col-description']");
@@ -40,8 +43,8 @@ public class TestAddJVMParametersForTask  extends Login {
 				"//div[@class='x-grid3-cell-inner x-grid3-col-description']//ancestor::div[@class='x-grid3-" +
 				"viewport']//input");
 		selenium.type("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']" +
-				"//div[@class='x-grid3-cell-inner x-grid3-col-description']//ancestor::div[@class='x-grid3-" +
-				"viewport']//input","jvm set in task");
+				"//div[@class='x-grid3-cell-inner x-grid3-col-description']//ancestor::div[@	class='x-grid3-" +
+				"viewport']//input","jvm set in task");//input jvm parameter description
 		selenium.keyDown("//span[text()='JVM parameter']//ancestor::div[@class='x-grid3-viewport']" +
 				"//div[@class='x-grid3-cell-inner x-grid3-col-description']//ancestor::div[@class='x-grid3-" +
 				"viewport']//input", "\\13");
@@ -51,19 +54,62 @@ public class TestAddJVMParametersForTask  extends Login {
 	    this.waitForElementPresent("//label[text()='Ok']", WAIT_TIME);
 	    selenium.click("//div[@class=' x-nodrag x-tool-close x-tool x-component']");
 	    
-	    if(selenium.isElementPresent("//table[contains(@class,'x-btn x-toolbar-more x-component x-btn-icon')]")) {
+	    
+    }
+	
+	//check jvm value in grid and RecoverLastExecution
+	public void checkJvmValueInGridRecoverLastExecution() {
+	    
+		if(selenium.isElementPresent("//table[contains(@class,'x-btn x-toolbar-more x-component x-btn-icon')]")) {
 	    	
 	    	selenium.click("//table[contains(@class,'x-btn x-toolbar-more x-component x-btn-icon')]");
 	    	
 	    }	
 	    
-	    selenium.click("idJobConductorTaskRecoverButton");
+	    selenium.click("idJobConductorTaskRecoverButton");//into 
 	    
-	    String jvmValue = selenium.getText("//label[text()='JVM Values:']//ancestor::div[contains" +
+	    this.waitForElementPresent("//label[text()='JVM Values:']//ancestor::div[contains" +
+	    		"(@class,'x-form-item')]//textarea[contains(@class,'x-form-field x-form-textarea')]", WAIT_TIME);
+	    String lastRunEndTime = selenium.getValue("//label[text()='Task:']//ancestor::div" +
+	    		"[@class=' x-form-label-left']/div[3]//input[@class=' x-form-field x-form-text']");
+	    String jvmValue = selenium.getValue("//label[text()='JVM Values:']//ancestor::div[contains" +
 	    		"(@class,'x-form-item')]//textarea[contains(@class,'x-form-field x-form-textarea')]");
 	    System.out.println(jvmValue);
-	    
-	    Assert.assertTrue(selenium.isTextPresent(jvmValue));
+	    System.out.println(lastRunEndTime);
+	    Assert.assertEquals(jvmValue, expectationValue);
 		
+	    this.clickWaitForElementPresent("!!!menu.grid_task_executions_history.element!!!");
+	    
+	    this.waitForElementPresent("//div[text()='Task execution monitoring']//ancestor::div[@class='x-" +
+	    		"panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']" +
+	    		"//span[text()='Task start date']//ancestor::div[@class='x-grid3-viewport']" +
+	    		"//div[@class='x-grid3-cell-inner x-grid3-col-taskEndDate']//" +
+	    		"span[@title='"+lastRunEndTime+"']", WAIT_TIME);
+	    selenium.mouseDown("//div[text()='Task execution monitoring']//ancestor::div[@class='x-" +
+	    		"panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']" +
+	    		"//span[text()='Task start date']//ancestor::div[@class='x-grid3-viewport']" +
+	    		"//div[@class='x-grid3-cell-inner x-grid3-col-taskEndDate']//" +
+	    		"span[@title='"+lastRunEndTime+"']");
+	    
+	    String jvmValueInGrid = selenium.getValue("//div[text()='Task execution monitoring']" +
+	    		"//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder" +
+	    		" x-border-layout-ct']//textarea[@name='jvmValues']");
+	    System.out.println("*-*-*-*-*-*"+jvmValueInGrid);
+	    selenium.setSpeed(MAX_SPEED);
+	    Assert.assertEquals(jvmValueInGrid, expectationValue);
+	    selenium.setSpeed(MIN_SPEED);
+		
+	}
+	
+	//test add a correct jvm parameter and active to task, then run it and check its value display in grid and RecoverLastExecution
+	@Test	
+//	(dependsOnGroups={"AddTask"})
+    @Parameters({"labelAddJVMParametersForTask"})
+	public void testAddActiveCorrectJVMParametersForTask(String label) {
+    	
+    	addJVMParametersForTask(label, jvmParameterCorrect);
+    	
+    	checkJvmValueInGridRecoverLastExecution();
+	    
     }
 }
