d83e051d1dbd16bb2039c8b02e6d1d51b37dc534
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-09 08:38:43 +0000

    selenium: TestDeleteNotification---testDeleteUserNotifitionByDeleteUser()           -add a  user notificaton, select an user 'jackzhang@gmail.com', then into user page and 	delete 'jackzhang@gmail.com', return notification page and check corresponding 	 notification is deleted                       ---testDeleteTaskByDeleteTask()           -add a  task notificaton, select an task 'testModifyTask', then into Task page and 	delete 'testModifyTask', return notification page and check corresponding 	 notification is deleted                       ---testDeleteJobServerByDeleteJobServer()           -add a  JobServer notificaton, select an JobServer 'use_server_unactive', then into JobServer page and 	delete 'use_server_unactive', return notification page and check corresponding 	 notification is deleted
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61988 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDeleteNotification.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDeleteNotification.java
@@ -4,14 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.talend.tac.cases.Login;
-
-public class TestDeleteNotification extends Login {
+public class TestDeleteNotification extends AddNotification {
     
-	//delete a notification(cancel Delete Notification)
+	//delete a notification(cancel Delete user Notification)
 	@Test(groups={"DeleteNotification"},dependsOnGroups={"AddJobserverNotification"})
 	@Parameters({"eventNewUser"})
-	public void testCancelDeleteNotification(String eventNewUser) {
+	public void testCancelDeleteUserNotification(String eventNewUser) {
 		
 		this.clickWaitForElementPresent("!!!menu.notification.element!!!");
 		
@@ -27,10 +25,10 @@ public class TestDeleteNotification extends Login {
 		selenium.setSpeed(MIN_SPEED);
 		
 	}
-	//delete a notification(Delete Notification)
-	@Test(dependsOnMethods={"testCancelDeleteNotification"})
+	//delete a notification(Delete User Notification)
+	@Test(dependsOnMethods={"testCancelDeleteUserNotification"})
 	@Parameters({"eventNewUser"})
-	public void testDeleteNotification(String eventNewUser) {
+	public void testDeleteUserNotification(String eventNewUser) {
 		
 		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
 		selenium.setSpeed(MID_SPEED);
@@ -49,4 +47,115 @@ public class TestDeleteNotification extends Login {
 		
 	}
 	
+	/**add a  user notificaton, select an user 'jackzhang@gmail.com', then into user page and 
+	delete 'jackzhang@gmail.com', return notification page and check corresponding
+	 notification is deleted**/
+	@Test(dependsOnMethods={"testDeleteUserNotification"})
+	@Parameters({"categoryUser","eventUserCreation","descriptionUserCreation","LoginName"})
+	public void testDeleteUserNotifitionByDeleteUser(String categoryUser, String eventUserCreation, 
+			String descriptionUserCreation, String loginName) {
+		
+		this.addUserNotification(2, categoryUser, 2, eventUserCreation, descriptionUserCreation, loginName);
+		selenium.click("idActiveInput");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertFalse(selenium.isChecked("idActiveInput"));
+		selenium.setSpeed(MID_SPEED);
+		
+		selenium.click("idFormSaveButton");
+		selenium.setSpeed(MID_SPEED);
+        Assert.assertTrue(selenium.isElementPresent("//div[text()='"+eventUserCreation+"']/" +
+        		"parent::td/parent::tr//img[@class='gwt-Image' and @title='false']"));		
+ 		selenium.setSpeed(MIN_SPEED);
+ 		this.clickWaitForElementPresent("idMenuUserElement");
+ 		this.waitForElementPresent("//div[text()='"+loginName+"']", WAIT_TIME);
+		selenium.mouseDown("//div[text()='"+loginName+"']");//Select an existing user
+		selenium.chooseOkOnNextConfirmation();	
+		selenium.click("//div[text()='Users' and @class='header-title']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//button[@id='idSubModuleDeleteButton']");//delete a user 
+
+		selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected user [\\s\\S]$"));
+	    selenium.setSpeed(MIN_SPEED);
+	    this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
+        selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(!selenium.isElementPresent("//div[text()='"+eventUserCreation+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='false']"));
+	    selenium.setSpeed(MIN_SPEED);
+	    
+	}
+	
+	/**add a  task notificaton, select an task 'BranchTask', then into Task page and 
+	delete 'BranchTask', return notification page and check corresponding
+	 notification is deleted**/
+	@Test(dependsOnMethods={"testDeleteUserNotification"})
+	@Parameters({"eventTaskFailed","duplicateTask"})
+	public void testDeleteTaskByDeleteTask(String eventTaskFailed, String Copy_of_testTaskNotChooseActive) {
+		
+		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
+		
+		this.waitForElementPresent("//div[text()='"+eventTaskFailed+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='false']", WAIT_TIME);
+        Assert.assertTrue(selenium.isElementPresent("//div[text()='"+eventTaskFailed+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='false']"));
+        
+        this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+		selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+		selenium.setSpeed(MIN_SPEED);
+	    selenium.click("idSubModuleRefreshButton"); //click "Refresh"
+		selenium.mouseDown("//span[text()='"+Copy_of_testTaskNotChooseActive+"']");//select a exist task
+		selenium.chooseOkOnNextConfirmation();
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("//div[text()='Job Conductor' and @class='header-title']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//button[@id='idSubModuleDeleteButton']");//clcik "Delete"
+		selenium.setSpeed(MIN_SPEED);//clcik "Delete"
+		Assert.assertTrue(selenium.getConfirmation().matches(other.getString("delete.plan.warning")));
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertFalse(selenium.isElementPresent("//span[text()='"+Copy_of_testTaskNotChooseActive+"']"));//the task cannot appear
+		selenium.setSpeed(MIN_SPEED);
+		
+        this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
+		
+        selenium.setSpeed(MAX_SPEED);
+        Assert.assertFalse(selenium.isElementPresent("//div[text()='"+eventTaskFailed+"']/" +
+	            "parent::td/parent::tr//img[@class='gwt-Image' and @title='false']"));
+        selenium.setSpeed(MIN_SPEED);
+	}
+    
+	
+	/**add a  jobServer notificaton, select an jobServer 'use_server_unactive', then into jobServer page and 
+	delete 'use_server_unactive', return notification page and check corresponding
+	 notification is deleted**/
+	@Test(dependsOnMethods={"testDeleteUserNotification"})
+	@Parameters({"eventJobServerAlert","ServerForUseUnavailable"})
+	public void testDeleteJobServerByDeleteJobServer(String eventJobServerAlert, String jobServer) {
+		
+		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
+		
+		this.waitForElementPresent("//div[text()='"+eventJobServerAlert+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='true']", WAIT_TIME);
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='"+eventJobServerAlert+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='true']"));
+				 
+        this.clickWaitForElementPresent("!!!menu.executionServers.element!!!");
+	    selenium.click("idSubModuleRefreshButton"); //click "Refresh"
+	    this.waitForElementPresent("//div[text()='"+jobServer+"']", WAIT_TIME);
+		selenium.mouseDown("//div[text()='"+jobServer+"']");//select a exist task
+		selenium.chooseOkOnNextConfirmation();
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("//div[text()='Servers' and @class='header-title']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//button[@id='idSubModuleDeleteButton']");//clcik "Delete"
+		selenium.setSpeed(MIN_SPEED);
+		Assert.assertTrue(selenium.getConfirmation().matches("^Are you sure you want to remove the selected execution server [\\s\\S]$"));
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertFalse(selenium.isElementPresent("//div[text()='"+jobServer+"']"));//the task cannot appear
+		selenium.setSpeed(MIN_SPEED);
+		
+        this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
+		
+		selenium.setSpeed(MAX_SPEED);
+		Assert.assertFalse(selenium.isElementPresent("//div[text()='"+eventJobServerAlert+"']/" +
+		"parent::td/parent::tr//img[@class='gwt-Image' and @title='true']"));
+        selenium.setSpeed(MIN_SPEED);
+        
+		
+	}
+
 }
