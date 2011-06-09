1e07691f314e3d11b7bec3c064360ec904c5ed38
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-09 08:38:16 +0000

    selenium: TestAddTasksNotification---testAddTaskFailedNotification()                         ---testAddTaskNotificationUncheckActive()           -modified calss extend 'AddNotification'           -add parameter in methods, modified parameter of taskname
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@61987 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestAddTasksNotification.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestAddTasksNotification.java
@@ -4,16 +4,15 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.talend.tac.cases.Login;
-
-public class TestAddTasksNotification extends Login {
+public class TestAddTasksNotification extends AddNotification {
     
     //add a task'notification(TaskFailedNotification)
 	@Test(groups={"AddTaskNotification"}, dependsOnGroups={"AddUserNotification"})
-	@Parameters({"categoryTask","eventTaskFailed","descriptionTaskFailed"})
-	public void testAddTaskFailedNotification(String categoryTask, String eventTaskFailed, String descriptionTaskFailed) {
+	@Parameters({"categoryTask","eventTaskFailed","descriptionTaskFailed","TaskBaseBranch"})
+	public void testAddTaskFailedNotification(String categoryTask, String eventTaskFailed, 
+			String descriptionTaskFailed,String taskBaseBranch) {
 		
-		addNotification(1, categoryTask, 1, eventTaskFailed, descriptionTaskFailed);
+		this.addTaskNotification(1, categoryTask, 1, eventTaskFailed, descriptionTaskFailed,taskBaseBranch);
 		
 		selenium.click("idFormSaveButton");
 		selenium.setSpeed(MID_SPEED);
@@ -25,10 +24,11 @@ public class TestAddTasksNotification extends Login {
 
 	//add a task'notification(Uncheck Active)
 	@Test(dependsOnMethods={"testAddTaskFailedNotification"})
-	@Parameters({"categoryTask","eventTaskFailed","descriptionTaskFailed"})
-	public void testAddTaskNotificationUncheckActive(String categoryTask, String eventTaskFailed, String descriptionTaskFailed) {
+	@Parameters({"categoryTask","eventTaskFailed","descriptionTaskFailed","duplicateTask"})
+	public void testAddTaskNotificationUncheckActive(String categoryTask, String eventTaskFailed,
+			String descriptionTaskFailed, String Copy_of_testTaskNotChooseActive) {
 		
-		addNotification(1, categoryTask, 1, eventTaskFailed, descriptionTaskFailed);
+		this.addTaskNotification(1, categoryTask, 1, eventTaskFailed, descriptionTaskFailed,Copy_of_testTaskNotChooseActive);
 		selenium.click("idActiveInput");
 		selenium.setSpeed(MID_SPEED);
 		Assert.assertFalse(selenium.isChecked("idActiveInput"));
@@ -40,30 +40,5 @@ public class TestAddTasksNotification extends Login {
 		"parent::td/parent::tr//img[@class='gwt-Image' and @title='false']"));
 		selenium.setSpeed(MIN_SPEED);
 	}
-	
-	public void addNotification(int LabelInput,String LabelInputValue,int DescriptionInput,
-			String DescriptionInputValue,String notificationInformation) {
-		
-		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
-		selenium.click("idSubModuleAddButton");
-		selenium.setSpeed(MID_SPEED);
-		Assert.assertTrue(selenium.isElementPresent("//img[@class='gwt-Image x-component ']"));
-		selenium.setSpeed(MIN_SPEED);
-		this.selectDropDownList("//input[@id='idLabelInput']", LabelInput);//choose a notification type
-		Assert.assertEquals(selenium.getValue("idLabelInput"), LabelInputValue);
-		this.selectDropDownList("//input[@id='idDescriptionInput']", DescriptionInput);//choose a event
-		String description = selenium.getValue("idDescriptionInput");
-		System.out.println(description);
-		selenium.setSpeed(MID_SPEED);
-		Assert.assertEquals(description , DescriptionInputValue);
-		Assert.assertTrue(selenium.isTextPresent(notificationInformation));
-	
-		selenium.click("//i[text()='"+notificationInformation+"']//ancestor::tbody//" +
-				"button[@id='idNotificationRepUserButton']");
-		selenium.mouseDown("//div[text()='testMulripleRoles@company.com']/parent::td/preceding-sibling::td");//choose event trigger users
-		selenium.click("//button[text()='Apply']");
-		selenium.click("idNotificationTaskButton");
-		selenium.mouseDown("//div[text()='testModifyTask']/parent::td/preceding-sibling::td");//choose event trigger task
-		selenium.click("//button[text()='Apply']");	
-	}
+
 }
