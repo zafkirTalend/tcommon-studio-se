7a8c170e0f308809c64980f0ee7468423489eae4
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-10 09:05:53 +0000

    selenium:update for TestAddAddSimpleTriggerUseDefaulttime:add a new plan used for simpletrigger.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@62117 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestAddAddSimpleTriggerUseDefaulttime.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestAddAddSimpleTriggerUseDefaulttime.java
@@ -14,12 +14,9 @@ public class TestAddAddSimpleTriggerUseDefaulttime extends Plan{
  
 	// add a simpleTrigger use default start time and end time
 	@Test(groups={"plan.addtrigger"})
-	@Parameters({ "plan.toaddsimpletrigger.label", "plan.simpletrigger.defaulttime.label"})
-	public void testAddTriggerAddSimpleTriggerDefaulttime(String addsimpletriggerplan,String defaulttimeSimpletrigger) {
-		this.clickWaitForElementPresent("!!!menu.executionPlan.element!!!");
-		this.waitForElementPresent("//div[@class='header-title' and text()='Execution Plan']", WAIT_TIME);
-		Assert.assertTrue(selenium
-				.isElementPresent("//div[@class='header-title' and text()='Execution Plan']"));
+	@Parameters({ "plan.toaddsimpletrigger.label","plan.toaddsimpletrigger.roottask", "plan.simpletrigger.defaulttime.label"})
+	public void testAddTriggerAddSimpleTriggerDefaulttime(String addsimpletriggerplan,String task,String defaulttimeSimpletrigger) {
+		this.addPlan(addsimpletriggerplan, task, "planforsimpletrigger");
     	selenium.mouseDown("//span[text()='"+addsimpletriggerplan+"']");//select a exist task
 		selenium.click("idTriggerAdd trigger...");//add a trigger
 		selenium.click("//a[text()='Add simple trigger']");//add a SimpleTrigger
@@ -36,8 +33,7 @@ public class TestAddAddSimpleTriggerUseDefaulttime extends Plan{
         this.typeString("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader']/div[@class=' x-panel-noborder x-panel x-component']/div[@class='x-panel-bwrap']/div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder']/form[@class=' x-form-label-left']/fieldset[@class=' x-fieldset x-component']/div[@class=' x-form-label-left']/div[@class='x-form-item ']/div/div[@class=' x-form-field-wrap  x-component ']/input[@name='repeatInterval']", "15");//Number of triggerings
 //	    selenium.click("//span[text()='Add simple trigger']/parent::legend/parent::fieldset/parent::form/parent::div/parent::div/parent::div/parent::div/parent::div//button[@id='idFormSaveButton']");
 	    selenium.click("//div[@class=' x-panel x-component ']/div[@class='x-panel-bwrap']/div[@class='x-panel-footer']/div[@class=' x-panel-btns']/div[@class=' x-small-editor x-panel-btns-center x-panel-fbar x-component x-toolbar-layout-ct']/table[@class='x-toolbar-ct']/tbody/tr/td[@class='x-toolbar-left']/table/tbody/tr[@class='x-toolbar-left-row']/td[@class='x-toolbar-cell']/table[@class=' x-btn x-component x-btn-text-icon ']/tbody[@class='x-btn-small x-btn-icon-small-left']/tr/td[@class='x-btn-mc']/em/button[@class='x-btn-text ' and text()='Save']");
-	    selenium.setSpeed(MID_SPEED); 
-//	    Thread.sleep(5000);
+	    this.waitForElementPresent("//span[text()='"+defaulttimeSimpletrigger+"']", WAIT_TIME);
 	    Assert.assertTrue(selenium.isElementPresent("//span[text()='"+defaulttimeSimpletrigger+"']"));
 	    selenium.setSpeed(MIN_SPEED);
 	    this.deleteTrigger(defaulttimeSimpletrigger);
