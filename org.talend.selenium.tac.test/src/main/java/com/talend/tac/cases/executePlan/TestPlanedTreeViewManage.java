ae1b645b662cc98f8a43bde9c6ef194472257a2b
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-22 10:44:56 +0000

    selenium:add two method named   testTreeManageRepeateAddError:add error node is forbidden if a node has on-error node already.   testTreeManageRepeateAddOK:add ok node is forbidden if a node has on-ok node already.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63131 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestPlanedTreeViewManage.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestPlanedTreeViewManage.java
@@ -2,6 +2,7 @@ package com.talend.tac.cases.executePlan;
 
 import java.awt.event.KeyEvent;
 
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -13,23 +14,90 @@ public class TestPlanedTreeViewManage extends Plan{
 	  selenium.mouseDown("//span[text()='"
 				+ planLabel + "']");
 	  this.waitForElementPresent("//span[@class='x-tree3-node-text' and text()='"+rootTask+"']", WAIT_TIME);
-	  this.addOnOK(rootTask, "ok");
-	  this.addOnError(rootTask, "error");
+	  this.addOnOK(rootTask, taskOk);
+	  this.addOnError(rootTask, taskError);
 	  
 	  
   }
+  
+  @Test(dependsOnMethods={"testTreeManage"})
+  @Parameters({ "treeviewmanage.plan.label", "treeviewmanage.plan.roottask", "treeviewmanage.plan.taskok", "treeviewmanage.plan.taskerror","treeviewmanage.plan.taskafter" })
+  public void testTreeManageRepeateAddOK(String planLabel,String rootTask,String taskOk,String taskError,String taskAfter) {
+	 this.waitForElementPresent("//span[text()='"
+				+ planLabel + "']", WAIT_TIME);
+	 this.sleep(2000);
+	  selenium.mouseDown("//span[text()='"
+				+ planLabel + "']");
+	  this.waitForElementPresent("//span[@class='x-tree3-node-text' and text()='"+rootTask+"']", WAIT_TIME);
+	  this.sleep(2000);
+	  selenium.mouseDown("//span[@class='x-tree3-node-text' and text()='"+rootTask+"']");
+	  this.sleep(2000);
+//	  selenium.chooseOkOnNextConfirmation();
+	  selenium.click("idExecutionPlanTreeViewOnOkButton");
+	  this.sleep(2000);
+	  if(selenium.isAlertPresent()){
+		  System.out.println("Alert present:");
+		  Assert.assertTrue(selenium.getAlert().equals("ON_OK node has been existed")); 
+	  }
+	  if(selenium.isPromptPresent()){
+		  System.out.println("Prompt present:");
+		  Assert.assertTrue(selenium.getAlert().equals("ON_OK node has been existed")); 
+	  }
+	  if(selenium.isConfirmationPresent()){
+		  System.out.println("Confirmation present:");
+		  Assert.assertTrue(selenium.getAlert().equals("ON_OK node has been existed")); 
+	  }
+
+	  
+	  
+  }
+  
+  @Test(dependsOnMethods={"testTreeManage"})
+  @Parameters({ "treeviewmanage.plan.label", "treeviewmanage.plan.roottask", "treeviewmanage.plan.taskok", "treeviewmanage.plan.taskerror","treeviewmanage.plan.taskafter" })
+  public void testTreeManageRepeateAddError(String planLabel,String rootTask,String taskOk,String taskError,String taskAfter) {
+	 this.waitForElementPresent("//span[text()='"
+				+ planLabel + "']", WAIT_TIME);
+	 this.sleep(2000);
+	  selenium.mouseDown("//span[text()='"
+				+ planLabel + "']");
+	  this.waitForElementPresent("//span[@class='x-tree3-node-text' and text()='"+rootTask+"']", WAIT_TIME);
+	  this.sleep(2000);
+	  selenium.mouseDown("//span[@class='x-tree3-node-text' and text()='"+rootTask+"']");
+	  this.sleep(2000);
+//	  selenium.chooseOkOnNextConfirmation();
+	  selenium.click("idExecutionPlanTreeViewOnErrorButton");
+	  this.sleep(2000);
+	  if(selenium.isAlertPresent()){
+		  System.out.println("Alert present:");
+		  Assert.assertTrue(selenium.getAlert().equals("ON_ERROR node has been existed")); 
+	  }
+	  if(selenium.isPromptPresent()){
+		  System.out.println("Prompt present:");
+		  Assert.assertTrue(selenium.getAlert().equals("ON_ERROR node has been existed")); 
+	  }
+	  if(selenium.isConfirmationPresent()){
+		  System.out.println("Confirmation present:");
+		  Assert.assertTrue(selenium.getAlert().equals("ON_ERROR node has been existed")); 
+	  }
+
+	  
+	  
+  }
+  
   public void addOnOK(String okTask,String taskToadd){
 	  this.waitForElementPresent("//span[@class='x-tree3-node-text' and text()='"+okTask+"']", WAIT_TIME);
 	  this.sleep(2000);
 	  selenium.mouseDown("//span[@class='x-tree3-node-text' and text()='"+okTask+"']");
 	  this.sleep(2000);
-//	  selenium.setSpeed(MID_SPEED);
 	  selenium.click("idExecutionPlanTreeViewOnOkButton");
 	  this.waitForElementPresent("//span[@class='x-fieldset-header-text' and text()='Add OnOk for planned task']", WAIT_TIME);
 	  this.selectDropDownList("idExecutionPlanTreeFormTaskComboBox", taskToadd);
+	  
+	  String saveButton = "//div[@class='header-title' and text()='Execution Plan']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//span[@class='x-fieldset-header-text' and text()='Add OnOk for planned task']/ancestor::form[@class=' x-form-label-left']//button[text()='Save']";
 //      selenium.keyPressNative(""+KeyEvent.VK_TAB);
 //      selenium.keyDownNative(""+KeyEvent.VK_ENTER);
-	  selenium.click("//div[@class='header-title' and text()='Execution Plan']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//span[@class='x-fieldset-header-text' and text()='Add OnOk for planned task']/ancestor::form[@class=' x-form-label-left']//button[text()='Save']");
+//	  selenium.keyDownNative(""+KeyEvent.VK_TAB);
+	  selenium.click(saveButton);
 
 	  this.waitForElementPresent("//span[@class='x-tree3-node-text' and text()='OnOk -> "+taskToadd+"']", WAIT_TIME);
 	  selenium.setSpeed(MIN_SPEED);
