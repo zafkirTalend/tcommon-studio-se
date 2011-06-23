4e83d5ad20baee531c9bef219c83aeb702eae248
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-23 06:02:24 +0000

    selenium:update for TestPlanExecutionLogsDisplay:    change xpath of 'number of items per page' input.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63159 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestPlanExecutionLogsDisplay.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/TestPlanExecutionLogsDisplay.java
@@ -1,6 +1,5 @@
 package com.talend.tac.cases.executePlan;
 
-import java.awt.event.KeyEvent;
 
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
@@ -13,12 +12,15 @@ public class TestPlanExecutionLogsDisplay extends Plan {
 	public void testCheckPlanLogsPerpage(String planLabel,String taskLabel) {
 		// this.addPlan(planLabel, rootTask, "treeManagePlan");
 		this.addPlan(planLabel, taskLabel, "testPlanlogsdisplay");
-		this.runPlanAndCheck(planLabel, taskLabel, 15);
+		this.runPlanAndCheck(planLabel, taskLabel, 11);
 		this.clickWaitForElementPresent("//span[@class='x-tab-strip-text  ' and text()='History']");
 		this.clickWaitForElementPresent("//div[contains(@class,'x-small-editor x-toolbar x-component x-toolbar-layout-ct')]//table[contains(@class,'x-btn x-component x-btn-icon')]");
-		int defaultCounts = Integer.parseInt(selenium.getValue("//div[@class=' x-form-field-wrap  x-component ' and @title='Enter the number of items per page']//input"));
+		this.sleep(3000);
+		int defaultCounts = Integer.parseInt(selenium.getValue("//div[@class='header-title' and text()='Execution Plan']//ancestor::div[@class=' x-panel-noborder x-panel x-component']//div[@title='Enter the number of items per page']//input"));
+		System.out.println("defaultCounts: " +defaultCounts);
+		System.out.println("logs get is: "+selenium.getXpathCount("//img[@class='gwt-Image' and @title='Ok']").intValue());
 		Assert.assertTrue(selenium.getXpathCount("//img[@class='gwt-Image' and @title='Ok']").intValue()==defaultCounts, "test plan execution logs per page failed!");
-	}
+	}////div[@class='header-title' and text()='Execution Plan']//ancestor::div[@class=' x-panel-noborder x-panel x-component']
 	
 	@Test(dependsOnMethods="testCheckPlanLogsPerpage")
 	 @Parameters({ "planexecutionlogsdisplay.plan.label",
@@ -35,10 +37,12 @@ public class TestPlanExecutionLogsDisplay extends Plan {
 				WAIT_TIME);
 		this.clickWaitForElementPresent("//span[@class='x-tab-strip-text  ' and text()='History']");
 		this.clickWaitForElementPresent("//div[contains(@class,'x-small-editor x-toolbar x-component x-toolbar-layout-ct')]//table[contains(@class,'x-btn x-component x-btn-icon')]");
+		this.sleep(3000);
 		int newNum = 5;
 		typeAndPressEnter("//div[@class='my-paging-text x-component ' and text()='Page']//ancestor::td[@class='x-toolbar-cell']/following-sibling::td//input[@class='gwt-TextBox x-component ']", "1");
 		typeAndPressEnter("//div[@class=' x-form-field-wrap  x-component ' and @title='Enter the number of items per page']//input", ""+newNum);
 		this.sleep(3000);
+		System.out.println("after modified,logs get is: "+selenium.getXpathCount("//img[@class='gwt-Image' and @title='Ok']").intValue());
 		Assert.assertTrue(selenium.getXpathCount("//img[@class='gwt-Image' and @title='Ok']").intValue()==newNum, "test plan execution logs per page failed!");
 	}
 	
