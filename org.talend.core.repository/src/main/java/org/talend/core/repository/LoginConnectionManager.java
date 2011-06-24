2548649a07c2de71c77e37e140e8897f2259340d
ldong <ldong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-24 12:05:13 +0000

    feature 22550: Add changes needed for uniserv branding
    git-svn-id: http://talendforge.org/svn/tos/trunk@63288 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.repository/src/main/java/org/talend/core/repository/LoginConnectionManager.java
+++ org.talend.core.repository/src/main/java/org/talend/core/repository/LoginConnectionManager.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.core.repository;
 
 import java.util.ArrayList;
@@ -5,28 +17,32 @@ import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
-import org.talend.commons.utils.workbench.extensions.ExtensionImplementationProvider;
-import org.talend.commons.utils.workbench.extensions.ExtensionPointLimiterImpl;
-import org.talend.commons.utils.workbench.extensions.IExtensionPointLimiter;
 
 public class LoginConnectionManager {
 
-    private static final IExtensionPointLimiter CONNECT_PROVIDER = new ExtensionPointLimiterImpl(
-            "org.talend.core.repository.connection_provider", "connectionValidate", 1, -1);
-
     public static List<ILoginConnectionService> getRemoteConnectionService() {
         List<ILoginConnectionService> uniservConnectionServices = new ArrayList<ILoginConnectionService>();
-        List<IConfigurationElement> extension = ExtensionImplementationProvider.getInstanceV2(CONNECT_PROVIDER);
-
-        for (IConfigurationElement current : extension) {
-            try {
-                Object object = current.createExecutableExtension("class");
-                if (object instanceof ILoginConnectionService) {
-                    uniservConnectionServices.add((ILoginConnectionService) object);
+        IExtensionRegistry extensionRegistry = Platform.getExtensionRegistry();
+        IExtensionPoint extensionPoint = extensionRegistry.getExtensionPoint("org.talend.core.repository.connection_provider"); //$NON-NLS-1$
+        if (extensionPoint != null) {
+            IExtension[] extensions = extensionPoint.getExtensions();
+            for (IExtension extension : extensions) {
+                IConfigurationElement[] configurationElements = extension.getConfigurationElements();
+                for (IConfigurationElement configurationElement : configurationElements) {
+                    try {
+                        Object service = configurationElement.createExecutableExtension("class"); //$NON-NLS-1$
+                        if (service instanceof ILoginConnectionService) {
+                            uniservConnectionServices.add((ILoginConnectionService) service);
+                        }
+                    } catch (CoreException e) {
+                        ExceptionHandler.process(e);
+                    }
                 }
-            } catch (CoreException e) {
-                ExceptionHandler.process(e);
             }
         }
         return uniservConnectionServices;
