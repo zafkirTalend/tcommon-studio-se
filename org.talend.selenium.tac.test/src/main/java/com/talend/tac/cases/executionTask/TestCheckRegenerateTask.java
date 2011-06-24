1a38feabac661e68983569803d4f9744f27077ba
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-24 07:41:53 +0000

    selenium:**--**Add new cases: TestCheckRegenerateTask---testCheckRegenerateTask()       -add case for check regenerate task
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63247 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestCheckRegenerateTask.java
@@ -0,0 +1,56 @@
+package com.talend.tac.cases.executionTask;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.cases.Login;
+
+public class TestCheckRegenerateTask extends Login {
+    
+	//add case for check regenerate task
+	@Test
+	@Parameters({"jobNameTRunJob","version0.1","context"})
+	public void testCheckRegenerateTask(String jobName, String version, String context) {
+		
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");//into task page 
+        selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+	    selenium.setSpeed(MIN_SPEED);
+	    
+	    this.waitForElementPresent("//span[text()='testAddsimpleTask']", WAIT_TIME);
+	    selenium.mouseDown("//span[text()='testAddsimpleTask']");//select an exist task
+	    
+	    if(!selenium.isElementPresent("//span[text()='testAddsimpleTask']//ancestor::tr//span[text()=" +
+	    		"'Ready to deploy']")) {
+	    	
+	    	 selenium.setSpeed(MID_SPEED);
+	 	    selenium.click("idJobConductorTaskGenerateButton");//generate task
+	 	    selenium.setSpeed(MIN_SPEED);
+	    	
+	    }
+	   	    
+	    this.waitForElementPresent("//span[text()='testAddsimpleTask']//ancestor::tr//span[text()=" +
+	    		"'Ready to deploy']", WAIT_TIME);
+	    Assert.assertTrue(selenium.isElementPresent("//span[text()='testAddsimpleTask']//ancestor::tr" +
+	    		"//span[text()='Ready to deploy']"));
+	    
+	    this.selectDropDownList("idTaskJobListBox", jobName);
+    	this.selectDropDownList("idTaskVersionListBox", version);
+    	this.selectDropDownList("idTaskContextListBox", context);
+    	if(!selenium.isChecked("idJobConductorRegenerateJobOnChangeCheckBox")) {
+    		
+    		selenium.click("idJobConductorRegenerateJobOnChangeCheckBox");//check regenerate
+        	Assert.assertTrue(selenium.isChecked("idJobConductorRegenerateJobOnChangeCheckBox"));	
+    		
+    	} 
+    	selenium.click("idFormSaveButton");
+    	
+    	this.waitForElementPresent("//span[text()='testAddsimpleTask']//ancestor::tr//span[text()" +
+	    		"='Ready to generate']", WAIT_TIME);
+	    Assert.assertTrue(selenium.isElementPresent("//span[text()='testAddsimpleTask']//ancestor::tr" +
+	    		"//span[text()='Ready to generate']"));
+	    		
+	}
+	
+}
