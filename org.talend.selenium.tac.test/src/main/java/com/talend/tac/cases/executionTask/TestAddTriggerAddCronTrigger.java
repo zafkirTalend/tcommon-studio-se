a24c160ebae377bb8d72e2f06847e72e8aa25ee2
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-24 07:49:15 +0000

    selenium:TestAddTriggerAddCronTrigger---testAddOverdueTriggerAddCronTrigger()          -modified xpath of save button           modified wait time before last assert
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63249 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
@@ -117,10 +117,9 @@ public class TestAddTriggerAddCronTrigger extends Login{
 		addTriggerAddCronTrigger(taskLabel,cronTriggerLabel, description, "2010", 
 				"Sunday", "Saturday", "January", "December");
     	selenium.setSpeed(MID_SPEED);		
-		selenium.click("//div[text()='Job Conductor' and @class='header-title']//ancestor::div//span[text()='Add Cron trigger']" +
-				"//ancestor::div[@class='x-panel-bwrap']//button[@id='idCrontTriggerSave']");
+		selenium.click("//button[@id='idCrontTriggerSave']");
 		selenium.setSpeed(MIN_SPEED);
-		selenium.setSpeed(MID_SPEED);
+		selenium.setSpeed("5000");
 	   	Assert.assertTrue(selenium.isTextPresent(rb.getString("trigger.error.trigger_will_never_fire")));
 	    selenium.setSpeed(MIN_SPEED); 	                        
     }
