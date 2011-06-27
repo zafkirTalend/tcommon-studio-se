8a0106a60af6f5f2b6503d36cc3657c54bf659cb
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-27 07:51:24 +0000

    selenium:add a new case as a method of class AddPlan name testAddPlanLabelWithSpecialChar:to try add a plan label with special char .
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63356 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/AddPlan.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executePlan/AddPlan.java
@@ -10,9 +10,30 @@ public class AddPlan extends Plan {
 	// , dependsOnGroups = { "cleanplan" }
 	@Parameters({ "plan.label", "plan.description", "plan.task" })
 	public void testAddPlan(String label, String description, String task) {
-		this.addPlan(label, task, description);
+		this.addPlan(label, task, description);	
 		// addPlan(label, description, task);
 
 	}
+	
+	@Test(groups = { "addplan" },dependsOnGroups={"Menu"})
+	// , dependsOnGroups = { "cleanplan" }
+	@Parameters({ "plan.label", "plan.description", "plan.task" })
+	public void testAddPlanLabelWithSpecialChar(String label, String description, String task) {
+		label = "sdaf;test";
+		this.openExecutionPlanMenu();
+		this.clickWaitForElementPresent("//button[text()='Add plan']");
+		this.typeString("idExecutionPlanPlanFormLabelInput", label);
+		this.waitForElementPresent("//img[@class='gwt-Image x-component ' and @role='alert']", WAIT_TIME);
+		this.typeString("idExecutionPlanPlanFormDescInput", description);
+		this.selectDropDownList("String idExecutionPlanPlanFormTaskComboBox",
+				task);
+		selenium.mouseDown("//span[@class='x-fieldset-header-text' and text()='Execution Plan']/ancestor::div[@class=' x-panel x-component']//button[@id='idFormSaveButton']");
+		selenium.click("//span[@class='x-fieldset-header-text' and text()='Execution Plan']/ancestor::div[@class=' x-panel x-component']//button[@id='idFormSaveButton']");
+		selenium.mouseUp("//span[@class='x-fieldset-header-text' and text()='Execution Plan']/ancestor::div[@class=' x-panel x-component']//button[@id='idFormSaveButton']");
+		this.waitForTextPresent("Fix errors in form before save", WAIT_TIME);
+		selenium.setSpeed(MIN_SPEED);
+
+	}
+	
 
 }
