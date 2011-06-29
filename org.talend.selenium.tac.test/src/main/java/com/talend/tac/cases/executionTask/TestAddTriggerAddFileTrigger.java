ce48437db06e5bfc6d4f62aa15ea5e4a4190d210
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-29 06:27:23 +0000

    selenium:TestAddTriggerAddFileTrigger   -add file trigger, use method(addFileTrigger)
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63504 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddFileTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddFileTrigger.java
@@ -6,50 +6,8 @@ import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 
-public class TestAddTriggerAddFileTrigger extends Login {
+public class TestAddTriggerAddFileTrigger extends AddTrigger {
  
-	//creat method(addFileTrigger) for test add file trigger
-	public void addFileTrigger(String taskLabel, String triggerLabel, String triggerDescription,
-			 String intervalTime, String folderPath, String fileMask, String serverName) {
-		
-		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-    	selenium.setSpeed(MID_SPEED);
-    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.setSpeed(MIN_SPEED);
-    	selenium.mouseDown("//span[text()='"+taskLabel+"']");//select a exist task
-    	
-    	if(!selenium.isChecked("idJobConductorTaskActiveListBox")) {
-    		
-    		selenium.click("idJobConductorTaskActiveListBox");//check active
-        	Assert.assertTrue(selenium.isChecked("idJobConductorTaskActiveListBox"));
-        	selenium.click("idFormSaveButton");
-    		
-    	}
-    
-    	
-    	selenium.mouseDown("//span[text()='"+taskLabel+"']");//select a exist task again
-		selenium.click("idTriggerAdd trigger...");//add a trigger
-		selenium.click("idTriggerAdd file trigger");//add a FileTrigger
-        Assert.assertTrue(selenium.isElementPresent("//span[text()='"+rb.getString("trigger.action.addFileTrigger")+"']"));
-		
-    	this.typeString("idJobConductorFileTriggerLabelInput", triggerLabel);//label
-				                                                                     
-		this.typeString("idJobConductorFileTriggerDescInput", triggerDescription);//description
-		
-		this.typeString("idJobConductorFileTriggerCheckTimeIntervalInput", intervalTime);//checkTimeInterval
-		
-		this.typeString("idJobConductorFileTriggerFolderPathInput", folderPath);//folderPath
-		
-		this.typeString("idJobConductorFileTriggerFileMaskInput", fileMask);//fileMask
-		
-		selenium.click("idJobConductorFileTriggerFtExitCheckBox");//*.txt is exist
-		Assert.assertTrue(selenium.isChecked("idJobConductorFileTriggerFtExitCheckBox"));
-	
-		selenium.click("idJobConductorFileTriggerFileServerListBox");//select an server	
-		this.waitForElementPresent("//div[text()='" + serverName + "']", WAIT_TIME);
-		selenium.mouseDown("//div[text()='" + serverName + "']");
-		
-	}
 	
 	//test add a file trigger
 	@Test
