6b483b35cf20a02efb8d3cd058231c6842724c23
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-30 08:46:50 +0000

    selenium:new cases added:   TestGridExecutionsPageDisplay      testGridGoToCurrentTime      testGridGoToSpecialDate      testGridNoFutureExecutionsAfterAFutureDate
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63597 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/grid/TestGridExecutionsPageDisplay.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/grid/TestGridExecutionsPageDisplay.java
@@ -1,6 +1,10 @@
 package com.talend.tac.cases.grid;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
@@ -34,6 +38,99 @@ public class TestGridExecutionsPageDisplay extends Grid {
         	
 	}
 	
+	@Test(dependsOnMethods={"testGridGoToFirstPage"})
+	@Parameters({"grid.task.label"})
+	public void testGridGoToCurrentTime(String label) {
+		String label2 = label+"second";
+        this.openGridMenu();
+        this.sleep(5000);
+        selenium.type("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']", "1");
+	    selenium.keyDown("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']","\\13");
+	    this.sleep(5000);
+	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("1"), "go to first page failed!");
+	    Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==10,"Grid past execution display failed!");
+       selenium.setSpeed(MID_SPEED);
+	    selenium.click("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//table[@title='Go to current time']");
+        this.sleep(5000);
+	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("3"), "go to current time without future execution failed!");
+	    Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==2,"Grid go to current time without future execution failed!");
+        this.addSimpleTriggerForTask(label, "testSimpleTrigger", "150", "10");
+        this.sleep(1000);
+        this.openGridMenu();
+        this.sleep(5000);
+        selenium.click("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//table[@title='Go to current time']");
+	    this.sleep(5000);
+	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("3"), "go to current time without future execution failed!");
+	    Assert.assertTrue(selenium.getXpathCount("//img[@class='gwt-Image' and @title='Ok']").intValue()>=2,"Grid go to current time without future execution failed!");
+	    Assert.assertTrue(selenium.getXpathCount("//span[@title='Waiting for triggering...' and text()='Waiting for triggering...']").intValue()==7,"Grid go to current time without future execution failed!");
+	    this.deleteSimpleTriggerOfTask(label, "testSimpleTrigger");
+	}
+	
+	@Test(dependsOnMethods={"testGridGoToFirstPage"})
+	@Parameters({"grid.task.label"})
+	public void testGridNoFutureExecutionsAfterAFutureDate(String label) {
+		String label2 = label+"second";
+		this.addSimpleTriggerForTask(label, "triggerRun100", "150", "100");
+		this.sleep(2000);
+        this.openGridMenu();
+        this.sleep(5000);
+        selenium.type("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']", "1");
+	    selenium.keyDown("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']","\\13");
+	    this.sleep(5000);
+	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("1"), "go to first page failed!");
+	    Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==10,"Grid past execution display failed!");
+        //
+	    selenium.type("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//div[@title='Go to date...']//input", "2100-06-08 13:54:01");
+		selenium.keyDown("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//div[@title='Go to date...']//input","\\13");
+		this.sleep(5000);
+		Assert.assertTrue(selenium.getXpathCount("//span[@title='Waiting for triggering...' and text()='Waiting for triggering...']").intValue()==0,"Grid Future 2100 execution display failed!");
+		 Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==0,"Grid past execution display failed!");
+		this.deleteSimpleTriggerOfTask(label, "triggerRun100");
+		 //
+      
+	}
+	
+	@Test(dependsOnMethods={"testGridGoToFirstPage"})
+	@Parameters({"grid.task.label"})
+	public void testGridGoToSpecialDate(String label) {
+		String label2 = label+"second";
+		this.addSimpleTriggerForTask(label, "triggerUnfinished", "150", "");
+		this.sleep(2000);
+        this.openGridMenu();
+        this.sleep(5000);
+        selenium.type("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']", "1");
+	    selenium.keyDown("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']","\\13");
+	    this.sleep(5000);
+	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("1"), "go to first page failed!");
+	    Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==10,"Grid past execution display failed!");
+        //
+	    String definedTime = "2100-06-08 13:54:01";
+	    selenium.type("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//div[@title='Go to date...']//input", definedTime);
+		selenium.keyDown("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//div[@title='Go to date...']//input","\\13");
+		this.sleep(5000);
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("//div[@qtip='Expected triggering date']//a[@class='x-grid3-hd-btn']");
+		selenium.click("//a[@class=' x-menu-item x-component' and text()='Sort Descending']");
+		selenium.setSpeed(MIN_SPEED);
+		this.sleep(2000);
+		String dateFuture = selenium.getText("//div[@class='x-grid3-cell-inner x-grid3-col-expectedTriggeringDate']");
+		Date date1 = null;
+		Date date2 = null;
+		try {
+			date1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(definedTime);
+	        date2= 	new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(dateFuture);
+	        Assert.assertTrue(date1.compareTo(date2)<=0);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==10,"Grid past execution display failed!");
+		this.deleteSimpleTriggerOfTask(label, "triggerUnfinished");
+		 //
+      
+	}
+	
+	
 //	@Test//(dependsOnMethods={"testGridGoToFirstPage"})
 	@Parameters({"grid.task.label","labelDescription","AddcommonProjectname","branchNameTrunk","jobNameTJava","version0.1",
 		"context","ServerForUseAvailable","statisticEnabled"})
@@ -47,8 +144,8 @@ public class TestGridExecutionsPageDisplay extends Grid {
 	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("1"), "go to first page failed!");
 	    Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==10,"Grid past execution display failed!");
        selenium.setSpeed(MID_SPEED);
-       selenium.mouseDown("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//td[9]");
-	    selenium.click("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//td[9]");
+       selenium.mouseDown("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//td[9]");
+	    selenium.click("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//td[9]");
         this.sleep(5000);
 	    Assert.assertTrue(selenium.getValue("//div[@class=' x-small-editor x-toolbar x-component x-toolbar-layout-ct ']//input[@class='gwt-TextBox x-component ']").equals("3"), "go to last page failed!");
 	    Assert.assertTrue(selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label+"']").intValue()+selenium.getXpathCount("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "+label2+"']").intValue()==2,"Grid past execution display failed!");
