daa4f3018393062d864ef7caf0a31963a2dfcd92
lwang <lwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-30 10:00:59 +0000

    selenium: update, complete and enable the case TestBusinessModeler with the new added IDs.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63607 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/bussinessModeler/TestBusinessModeler.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/bussinessModeler/TestBusinessModeler.java
@@ -6,19 +6,42 @@ import static org.testng.Assert.*;
 import com.talend.tac.cases.Login;
 
 public class TestBusinessModeler extends Login {
-	
-	@Test(enabled=false,description="disable this case now, there are same ids")
-	@Parameters( { "AddcommonProjectname","branches" ,"bussinesModel.name","Mysqlconnection"})
+	 
+	@Test
+	@Parameters( { "AddcommonProjectname","branches" ,"bussinesModel.name","Mysql_Connectionlabel"})
 	public void testBusinessModeler(String project,String branches,String modelName,String connectionName) {
 		this.clickWaitForElementPresent("!!!menu.businessModeler.element!!!");
+		//select
 		this.selectDropDownList("idProjectListBox", project);
 		this.selectDropDownList("idBranchListBox", branches);
-//		this.selectDropDownList("idBusinessListBox", modelName); id same
-//		this.selectDropDownList("idBusinessListBox", connectionName); id same
-		selenium.click("//button[text()='load");
-		selenium.click("//button[text()='save");
+		this.selectDropDownList("idBusinessListBox", modelName); 
+		this.selectDropDownList("idBusinessConnectionsListBox", connectionName);
+		//load 
+		selenium.click("//button[text()='load']");
+		assertEquals(selenium.getValue("idProjectListBox"),project);
+		assertEquals(selenium.getValue("idBranchListBox"),branches);
+//		assertEquals(selenium.getValue("idBusinessListBox"),modelName);
+		assertEquals(selenium.getValue("idBusinessConnectionsListBox"),connectionName);
+		//edit ?
+		//save
+		selenium.click("//button[text()='save']");
 		selenium.getAlert();
-		selenium.click("//button[text()='close'");
+		//close
+		selenium.click("//button[text()='close']");
+		assertEquals(selenium.getValue("idProjectListBox"),"Select a Project");
+		assertEquals(selenium.getValue("idBranchListBox"),"Select a Branch");
+		assertEquals(selenium.getValue("idBusinessListBox"),"Select a Business model");
+		assertEquals(selenium.getValue("idBusinessConnectionsListBox"),"");
+	}
+
+	@Override
+	public void selectDropDownList(String id, String itemName) {
+		// TODO Auto-generated method stub
+		selenium.click("//input[@id='"+id+"']"
+				+ "/following-sibling::div");
+		this.waitForElementPresent("//div[contains(text(),'" + itemName + "')]", WAIT_TIME);
+		selenium.mouseDown("//div[contains(text(),'" + itemName + "')]");
+		selenium.fireEvent("//input[@id='"+id+"']", "blur");
 	}
 	
 }
