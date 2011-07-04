0e534de7fe3fd522509d41d28872ac0a87ac69f6
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-04 07:03:43 +0000

    selenium:**--**Modified automation cases TestAddTriggerAddCronTrigger---testAddExistTriggerAddCronTrigger()   -modified its execution order
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63717 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
@@ -30,61 +30,8 @@ public class TestAddTriggerAddCronTrigger extends AddTrigger {
 	    selenium.setSpeed(MIN_SPEED); 	                        
     }
 	
-    @Test(dependsOnMethods={"testAddOverdueTriggerAddCronTrigger"})
-//	(groups={"AddCronTrigger"},dependsOnGroups={"DeleteTrigger"})
-	@Parameters({"labelRefProJobByMainProTRunJobRun","addCronTriggerLabel","addCronTriggerDescription"})
-    public void testAddTriggerAddCronTrigger(String taskLabel,String cronTriggerLabel,String description) {
-		
-		addTriggerAddCronTrigger(taskLabel,cronTriggerLabel, description, "2011", 
-				"Sunday", "Saturday", "January", "December");
-    			
-		selenium.click("idCrontTriggerSave");
-				
-		selenium.setSpeed(MID_SPEED);
-		if(!selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']")) {
-			selenium.click("idTriggerRefresh");
-    	}
-		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']"));
-		selenium.setSpeed(MIN_SPEED);
-    }
-	
-	//add a exist cron triiger, set date is by UI
-	@Test(dependsOnMethods={"testAddTriggerAddCronTrigger"})
-	@Parameters({"labelRefProJobByMainProTRunJobRun","addCronTriggerLabel","addCronTriggerExistTriggerDescription"})
-    public void testAddExistTriggerAddCronTrigger(String taskLabel,String cronTriggerLabel, String description) {
-		
-		//open to execution task add trigger page
-		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-    	selenium.setSpeed(MID_SPEED);
-    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.setSpeed(MIN_SPEED);
-    	this.waitForElementPresent("//span[text()='"+taskLabel+"']", WAIT_TIME);
-       	selenium.mouseDown("//span[text()='"+taskLabel+"']");
-		selenium.click("idTriggerAdd trigger...");
-		selenium.click("idTriggerAdd CRON trigger");
-		this.typeString("idJobConductorCronTriggerLabelInput",cronTriggerLabel);
-		//type  description
-		this.typeString("idJobConductorCronTriggerDescInput", description);
-    	//type minutes
-		this.typeString("idJobConductorCronTriggerMintInput", date.minutes);
-		//type hours
-		this.typeString("idJobConductorCronTriggerHourInput", date.hours);
-		//type days
-		this.typeString("idJobConductorCronTriggerDayOfMonthInput", date.days);
-		//type months
-		this.typeString("idJobConductorCronTriggerMonthInput", date.months);
-		//type years
-		this.typeString("idJobConductorCronTriggerYearInput", date.years);	
-		//click save button
-		selenium.click("idCrontTriggerSave");
-		Assert.assertTrue(selenium.isTextPresent("Save failed: An execution trigger with this name already exists -- For more information see your log file"));
-		selenium.setSpeed(MIN_SPEED);
-		
-    }
-	
-	
 	//add a CronTrigger,selected job is referencetjava(tjava(from referecepro))**/
-	@Test(dependsOnMethods={"testAddExistTriggerAddCronTrigger"})
+	@Test(dependsOnMethods={"testAddOverdueTriggerAddCronTrigger"})
 	@Parameters({ "labelReferenceproTjava","addCronTriggerByHandInputDateLabel", "addCronTriggerByHandInputDateDescription"})
 	public void testAddCronByHandInputDateTrigger(String taskLabel,String addCronTrigger,String addCronTriggerDescription) throws InterruptedException{
 	
@@ -124,5 +71,56 @@ public class TestAddTriggerAddCronTrigger extends AddTrigger {
 	    Assert.assertTrue(selenium.isElementPresent("//span[text()='"+addCronTrigger+"']"));
 	    selenium.setSpeed(MIN_SPEED);
 	}
+	
+	//add a exist cron triiger, set date is by UI
+	@Test(dependsOnMethods={"testAddCronByHandInputDateTrigger"})
+	@Parameters({"labelReferenceproTjava","addCronTriggerByHandInputDateLabel","addCronTriggerExistTriggerDescription"})
+    public void testAddExistTriggerAddCronTrigger(String taskLabel,String cronTriggerLabel, String description) {
+		
+		//open to execution task add trigger page
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+    	selenium.setSpeed(MID_SPEED);
+    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+    	selenium.setSpeed(MIN_SPEED);
+    	this.waitForElementPresent("//span[text()='"+taskLabel+"']", WAIT_TIME);
+       	selenium.mouseDown("//span[text()='"+taskLabel+"']");
+		selenium.click("idTriggerAdd trigger...");
+		selenium.click("idTriggerAdd CRON trigger");
+		this.typeString("idJobConductorCronTriggerLabelInput",cronTriggerLabel);
+		//type  description
+		this.typeString("idJobConductorCronTriggerDescInput", description);
+    	//type minutes
+		this.typeString("idJobConductorCronTriggerMintInput", date.minutes);
+		//type hours
+		this.typeString("idJobConductorCronTriggerHourInput", date.hours);
+		//type days
+		this.typeString("idJobConductorCronTriggerDayOfMonthInput", date.days);
+		//type months
+		this.typeString("idJobConductorCronTriggerMonthInput", date.months);
+		//type years
+		this.typeString("idJobConductorCronTriggerYearInput", date.years);	
+		//click save button
+		selenium.click("idCrontTriggerSave");
+		Assert.assertTrue(selenium.isTextPresent("Save failed: An execution trigger with this name already exists -- For more information see your log file"));
+		selenium.setSpeed(MIN_SPEED);
+		
+    }
+	
+    @Test(dependsOnMethods={"testAddExistTriggerAddCronTrigger"})
+	@Parameters({"labelRefProJobByMainProTRunJobRun","addCronTriggerLabel","addCronTriggerDescription"})
+    public void testAddTriggerAddCronTrigger(String taskLabel,String cronTriggerLabel,String description) {
+		
+		addTriggerAddCronTrigger(taskLabel,cronTriggerLabel, description, "2011", 
+				"Sunday", "Saturday", "January", "December");
+    			
+		selenium.click("idCrontTriggerSave");
+				
+		selenium.setSpeed(MID_SPEED);
+		if(!selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']")) {
+			selenium.click("idTriggerRefresh");
+    	}
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']"));
+		selenium.setSpeed(MIN_SPEED);
+    }
 	
 }
