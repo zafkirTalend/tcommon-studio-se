722d74ce97bcffe46f9332733a7f9e306b9eab09
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-04 07:05:18 +0000

    selenium:**--**Add new automation cases TestModifyTask---modifiedTask()   -creation method for modifiy task               ---testModifyLabelOfGeneratingTaskAndCheckWarnInfo()   -test modify label of generating task and check warn info               ---testModifyJobOfGeneratingTaskAndCheckWarnInfo()   -test modify job of generating task and check warn info
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@63718 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestModifyTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestModifyTask.java
@@ -7,23 +7,30 @@ import org.testng.annotations.Test;
 import com.talend.tac.cases.Login;
 public class TestModifyTask extends Login {
     
+	//creation method for modify task
+	public void modifiedTask() {
+		
+		this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
+		selenium.setSpeed(MID_SPEED);
+	    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
+	    
+	}
+	
 	@Test(groups={"ModifyTask"},dependsOnGroups={"DeleteTask"})
 	@Parameters({"label","modifyTask","AddreferenceProjectname","branchNameTrunk","jobNameReferencetjava","version0.1",
 		"context","ServerForUseAvailable","statisticEnabled"})
 	public void testModifyTask(String label,String modifyLabel,String projectName,String branchName,String jobName,
 			String version,String context,String jobServer,String statistic) {
 		
+		modifiedTask();
+		
+		selenium.setSpeed(MID_SPEED);
 		if(!selenium.isElementPresent("//span[text()='"+modifyLabel+"']")) {
-			
-			this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");
-			selenium.setSpeed(MID_SPEED);
-		    Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-			
-		    selenium.click("idSubModuleRefreshButton");
-			selenium.mouseDown("//span[text()='"+label+"']");//select a exist task
-            selenium.setSpeed(MIN_SPEED);
-            this.typeString("idJobConductorTaskLabelInput", modifyLabel);//task name /Label
-            
+		    selenium.setSpeed(MIN_SPEED);
+			this.waitForElementPresent("//span[text()='"+label+"']	", WAIT_TIME);
+		    selenium.mouseDown("//span[text()='"+label+"']");//select a exist task
+
+            this.typeString("idJobConductorTaskLabelInput", modifyLabel);//task name /Label            
             this.selectDropDownList("idTaskProjectListBox", projectName);
         	this.selectDropDownList("idTaskBranchListBox", branchName);
         	this.selectDropDownList("idTaskJobListBox", jobName);
@@ -33,8 +40,59 @@ public class TestModifyTask extends Login {
 			selenium.click("idFormSaveButton");
 			this.waitForElementPresent("//span[text()='"+modifyLabel+"']", WAIT_TIME);
 			Assert.assertTrue(selenium.isElementPresent("//span[text()='"+modifyLabel+"']"));
-			selenium.setSpeed(MIN_SPEED);
 			
 		}	
+		selenium.setSpeed(MIN_SPEED);
 	}
+	
+	//test modify label of generating task and check warn info
+	@Test(dependsOnMethods={"testModifyTask"})
+	@Parameters({"TaskBaseBranch"})
+	public void testModifyLabelOfGeneratingTaskAndCheckWarnInfo(String label) {
+		
+		modifiedTask();		
+		
+		this.waitForElementPresent("//span[text()='"+label+"']", WAIT_TIME);
+		selenium.mouseDown("//span[text()='"+label+"']");//select a exist task
+        selenium.setSpeed(MID_SPEED);
+		selenium.click("idJobConductorTaskGenerateButton");//generate task
+		
+		this.typeString("idJobConductorTaskLabelInput", "modifyGeneratingTask");//task name /Label   
+		selenium.setSpeed(MIN_SPEED);
+		selenium.click("idFormSaveButton");
+		selenium.setSpeed(MAX_SPEED);
+		Assert.assertTrue(selenium.isTextPresent("Another operation is locking the" +
+				" task 'modifyGeneratingTask', please retry later "));
+		selenium.setSpeed(MIN_SPEED);	
+		
+	}
+	
+	//test modify job of generating task and check warn info
+	@Test(dependsOnMethods={"testModifyTask"})
+	@Parameters({"TaskBaseBranch","modifyTask","AddreferenceProjectname","branchNameTrunk","jobNameReferencetjava","version0.1",
+		"context"})
+	public void testModifyJobOfGeneratingTaskAndCheckWarnInfo(String label,String modifyLabel,String projectName,String branchName,String jobName,
+			String version,String context) throws InterruptedException {
+		
+		modifiedTask();		
+		
+		this.waitForElementPresent("//span[text()='"+label+"']", WAIT_TIME);
+		selenium.mouseDown("//span[text()='"+label+"']");//select a exist task
+        selenium.setSpeed(MID_SPEED);
+		selenium.click("idJobConductorTaskGenerateButton");//generate task
+		selenium.setSpeed(MIN_SPEED);
+		Thread.sleep(3000);       
+        this.selectDropDownList("idTaskProjectListBox", projectName);
+    	this.selectDropDownList("idTaskBranchListBox", branchName);
+    	this.selectDropDownList("idTaskJobListBox", jobName);
+    	this.selectDropDownList("idTaskVersionListBox", version);
+    	this.selectDropDownList("idTaskContextListBox", context);
+		selenium.setSpeed(MIN_SPEED);
+		selenium.click("idFormSaveButton");
+		selenium.setSpeed(MAX_SPEED);
+		Assert.assertTrue(selenium.isTextPresent(rb.getString("executionTask.locked")));
+		selenium.setSpeed(MIN_SPEED);	
+		
+	}
+	
 }
