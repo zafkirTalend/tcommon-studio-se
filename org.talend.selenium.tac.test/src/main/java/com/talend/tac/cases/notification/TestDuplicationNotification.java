129e790a16f535d2da37a1096fdc8232bcbd51ea
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-12 08:05:43 +0000

    selenium:TestDuplicationNotification---testDuplicationNotification()   --modified parameter of 'eventUserDeletion'
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@64194 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDuplicationNotification.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/TestDuplicationNotification.java
@@ -1,6 +1,7 @@
 package com.talend.tac.cases.notification;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
@@ -8,11 +9,12 @@ import com.talend.tac.cases.Login;
 
 public class TestDuplicationNotification extends Login {
 	@Test(dependsOnGroups={"AddTaskNotification"})
-	public void testDuplicationNotification() {
+	@Parameters({"eventUserDeletion"})
+	public void testDuplicationNotification(String eventUserDeletion) {
 		
 		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
 		selenium.setSpeed(MID_SPEED);
-		selenium.mouseDown("//div[text()='UserDeletionNotification']/parent::td/" +
+		selenium.mouseDown("//div[text()='"+eventUserDeletion+"']/parent::td/" +
 				"parent::tr//img[@class='gwt-Image' and @title='true']");//click a exist notification
 		selenium.click("idActiveInput");
 		selenium.setSpeed(MID_SPEED);
@@ -22,7 +24,7 @@ public class TestDuplicationNotification extends Login {
 		
 		selenium.click("idFormSaveButton");
 		selenium.setSpeed(MID_SPEED);
-		Assert.assertTrue(selenium.isElementPresent("//div[text()='UserDeletionNotification']/parent::td/parent::tr" +
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='"+eventUserDeletion+"']/parent::td/parent::tr" +
 				"//img[@class='gwt-Image' and @title='false']"));
 	    selenium.setSpeed(MIN_SPEED); 
 		
