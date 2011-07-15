cbe6387a88cc074e7a48ebfb3c52d26aad71d4d0
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-15 09:52:55 +0000

    selenium:AddNotification---addLicenseNotification()   -creation method for add license notification AddNotification---addUserNotification()   -add assert of select user after click apply AddNotification---addTaskNotification()   -add assert of select user and task after click apply AddNotification---addJobServerNotification()   -add assert of select user and server after click apply
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@64438 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/AddNotification.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/notification/AddNotification.java
@@ -66,10 +66,17 @@ public class AddNotification extends Login {
 		this.waitForElementPresent("//div[text()='testMulripleRoles@company.com']/parent::td/preceding-sibling::td", WAIT_TIME);
 		selenium.mouseDown("//div[text()='testMulripleRoles@company.com']/parent::td/preceding-sibling::td");//choose event trigger users
 		selenium.click("//button[text()='Apply']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//i[text()=' - testMulripleRoles@company.com']"));
+		selenium.setSpeed(MIN_SPEED);
 		selenium.click("idNotificationTaskButton");
 		this.waitForElementPresent("//div[text()='"+taskName+"']/parent::td/preceding-sibling::td", WAIT_TIME);
 		selenium.mouseDown("//div[text()='"+taskName+"']/parent::td/preceding-sibling::td");//choose event trigger task
-		selenium.click("//button[text()='Apply']");	
+		selenium.click("//button[text()='Apply']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//i[text()=' - "+taskName+"']"));
+		selenium.setSpeed(MIN_SPEED);
+		
 	}
 
 	//add method for add task notification
@@ -98,12 +105,52 @@ public class AddNotification extends Login {
 		this.waitForElementPresent("//div[text()='admin@company.com']/parent::td/preceding-sibling::td", WAIT_TIME);
 		selenium.mouseDown("//div[text()='admin@company.com']/parent::td/preceding-sibling::td");//choose event trigger users
 		selenium.click("//button[text()='Apply']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//i[text()=' - admin@company.com']"));
+		selenium.setSpeed(MIN_SPEED);
 		selenium.click("idNotificationJobserverButton");
 		this.waitForElementPresent("//div[text()='"+jobServer+"']/parent::td/preceding-sibling::td", WAIT_TIME);
 		selenium.mouseDown("//div[text()='"+jobServer+"']/parent::td/preceding-sibling::td");//choose event trigger jobServer
 		selenium.click("//button[text()='Apply']");	
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//i[text()=' - "+jobServer+"']"));
+		selenium.setSpeed(MIN_SPEED);
 
 
 	}
+	
+
+	//add method for add license notification
+	public void addLicenseNotification(int LabelInput,String LabelInputValue,int DescriptionInput,
+			String DescriptionInputValue,String notificationInformation) {
+		
+		this.clickWaitForElementPresent("!!!menu.notification.element!!!");//into notification
+		selenium.click("idSubModuleAddButton");//click add button
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//img[@class='gwt-Image x-component ']"));
+		selenium.setSpeed(MIN_SPEED);	
+		this.selectDropDownList("//input[@id='idLabelInput']", LabelInput);//choose a notification type
+		Assert.assertEquals(selenium.getValue("idLabelInput"), LabelInputValue);
+		this.selectDropDownList("//input[@id='idDescriptionInput']", DescriptionInput);//choose a event
+		String description = selenium.getValue("idDescriptionInput");
+		System.out.println(description);
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertEquals(description , DescriptionInputValue);
+		Assert.assertTrue(selenium.isTextPresent(notificationInformation));
+		selenium.setSpeed(MIN_SPEED);
+		selenium.click("idLabelInput");
+		selenium.mouseDownAt("//div[@role='listitem']["+LabelInput+"]", ""+KeyEvent.VK_ENTER);
+		
+		selenium.click("//i[text()='"+notificationInformation+"']//ancestor::tbody//" +
+		"button[@id='idNotificationRepUserButton']");//choose event trigger users
+		this.waitForElementPresent("//div[text()='testMulripleRoles@company.com']/parent::td/preceding-sibling::td", WAIT_TIME);
+		selenium.mouseDown("//div[text()='testMulripleRoles@company.com']/parent::td/preceding-sibling::td");//choose event trigger users
+		selenium.click("//button[text()='Apply']");
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isElementPresent("//i[text()=' - testMulripleRoles@company.com']"));
+		selenium.setSpeed(MIN_SPEED);
+			
+	}
+	
 	
 }
