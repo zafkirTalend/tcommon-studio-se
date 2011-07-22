73d2baa617f3617d3873aa9321fd54dfdd16f681
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-22 01:49:33 +0000

    selenium:add new case TestGridRecoveryManagement.testGridExecutionInfoRunByTrigger
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@64798 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/grid/TestGridRecoveryManagement.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/grid/TestGridRecoveryManagement.java
@@ -95,5 +95,82 @@ public class TestGridRecoveryManagement extends Grid {
 		Assert.assertTrue(selenium.getValue("//span[@class='x-fieldset-header-text' and text()='Job Execution']//ancestor::fieldset[@class=' x-fieldset x-component']//label[text()='Job Server:']//ancestor::div[@class='x-form-item ']//input").contains(serverName), "Task server information shows wrong!");
 		Assert.assertTrue(selenium.getValue("//span[@class='x-fieldset-header-text' and text()='Job Execution']//ancestor::fieldset[@class=' x-fieldset x-component']//label[text()='Job:']//ancestor::div[@class='x-form-item ']//input").contains(jobName), "Task job information shows wrong!");
 	}
+	
+	@Test
+	@Parameters({ "grid.task.label", "labelDescription",
+			"AddcommonProjectname", "branchNameTrunk", "jobNameTJava",
+			"version0.1", "context", "ServerForUseAvailable",
+			"statisticEnabled" })
+	public void testGridExecutionInfo(String label, String description,
+			String projectName, String branchName, String jobName,
+			String version, String context, String serverName,
+			String statisticName) {
+		this.cleanTask();
+		this.addTask(label, description, projectName, branchName, jobName,
+				version, context, serverName, statisticName);
+		this.openGridMenu();
+		this.sleep(5000);
+		Assert.assertTrue(
+				selenium.getXpathCount(
+						"//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "
+								+ label + "']").intValue() == 0,
+				"Grid past execution display failed!");
+		this.openTaskMenu();
+		this.runTask(label, 1);
+		this.openGridMenu();
+		this.sleep(5000);
+		this.waitForElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "
+				+ label + "']", WAIT_TIME);
+		Assert.assertTrue(
+				selenium.getXpathCount(
+						"//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "
+								+ label + "']").intValue() == 1,
+				"Grid Future execution display failed!");
+		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "
+								+ label + "']");
+	    this.sleep(4000);
+	    this.waitForTextPresent(serverName, WAIT_TIME);
+	    Assert.assertTrue(selenium.getValue("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//textarea[@name='contextValues']").contains("birth: yyyy-MM-dd HH:mm:ss;1988-08-13 00:00:00"), "test execution infor shows in grid failed!");
+	    Assert.assertTrue(selenium.getValue("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//textarea[@name='contextValues']").contains("name: JackZhang"), "test execution infor shows in grid failed!");
+	    Assert.assertTrue(selenium.getValue("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//textarea[@name='contextValues']").contains("age: 23"), "test execution infor shows in grid failed!");
+	    Assert.assertTrue(selenium.getText("//label[text()='Triggered by:']//ancestor::div[@class='x-form-item ']//div[contains(@class,'x-form-label x-component')]").contains("Manual run by user 'admin@company.com'"),"test run task directly execution infor trigger by show wrong!");
+	    //+" (192.168.0.94:cmd=8000/file=8001/monitor=8888)"
+	    
+	}
+	
+	@Test
+	@Parameters({ "grid.task.label", "labelDescription",
+			"AddcommonProjectname", "branchNameTrunk", "jobNameTJava",
+			"version0.1", "context", "ServerForUseAvailable",
+			"statisticEnabled" })
+	public void testGridExecutionInfoRunByTrigger(String label, String description,
+			String projectName, String branchName, String jobName,
+			String version, String context, String serverName,
+			String statisticName) {
+		this.cleanTask();
+		this.addTask(label, description, projectName, branchName, jobName,
+				version, context, serverName, statisticName);
+		this.addSimpleTriggerForTask(label, "test", "10", "2");
+		this.sleep(60000);
+		this.waitForElementPresent("//img[@title='Completed']", WAIT_TIME);
+		this.openGridMenu();
+		this.sleep(5000);
+		Assert.assertTrue(
+				selenium.getXpathCount(
+						"//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "
+								+ label + "']").intValue() == 3,
+				"Grid past execution display failed!");
+		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-taskLabel' and text()='Task: "
+								+ label + "']");
+	    this.sleep(4000);
+	    this.waitForTextPresent(serverName, WAIT_TIME);
+	    Assert.assertTrue(selenium.getValue("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//textarea[@name='contextValues']").contains("birth: yyyy-MM-dd HH:mm:ss;1988-08-13 00:00:00"), "test execution infor shows in grid failed!");
+	    Assert.assertTrue(selenium.getValue("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//textarea[@name='contextValues']").contains("name: JackZhang"), "test execution infor shows in grid failed!");
+	    Assert.assertTrue(selenium.getValue("//div[@class='header-title' and text()='Task execution monitoring']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//textarea[@name='contextValues']").contains("age: 23"), "test execution infor shows in grid failed!");
+	    Assert.assertTrue(selenium.getText("//label[text()='Triggered by:']//ancestor::div[@class='x-form-item ']//div[contains(@class,'x-form-label x-component')]").contains("SimpleTrigger[talendTriggerName='test'"),"test run task by trigger execution infor trigger by show wrong!");
+	    //+" (192.168.0.94:cmd=8000/file=8001/monitor=8888)"
+	    
+	}
+	
 
 }
