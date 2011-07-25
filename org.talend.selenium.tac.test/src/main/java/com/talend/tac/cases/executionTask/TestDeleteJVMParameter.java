f4fc0feb17dbcb5eccc46f6c6d697b8af97bed81
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-25 07:05:17 +0000

    selenium:TestDeleteJVMParameter---testDelteJVMParameter()    -add wait time(Base.WAIT_TIME)
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@64883 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDeleteJVMParameter.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestDeleteJVMParameter.java
@@ -3,6 +3,8 @@ package com.talend.tac.cases.executionTask;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.talend.tac.base.Base;
+
 public class TestDeleteJVMParameter extends JVMParameter {
     
 	  
@@ -16,7 +18,7 @@ public class TestDeleteJVMParameter extends JVMParameter {
 	@Parameters({"labelAddJVMParametersForTask"})
 	public void testDelteJVMParameter(String label) {
 
-    	this.addJVMParametersForTask("Active", label, jvmParameterCorrect, jvmParameterDescription, "Ok");
+    	this.addJVMParametersForTask("Active", label, jvmParameterCorrect, jvmParameterDescription, "Ok",Base.WAIT_TIME);
 	    
     	this.deleteJVMParameter(label);
     	
