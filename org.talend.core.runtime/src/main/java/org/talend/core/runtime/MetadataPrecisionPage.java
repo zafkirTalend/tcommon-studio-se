b2e8e7e09b4ea64dd4f2f703a9eef7d248c5fca9
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-25 06:57:54 +0000

    bug 21427 fixed:Integrate database structure in Talend referential
    git-svn-id: http://talendforge.org/svn/tos/trunk@64878 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core.runtime/src/main/java/org/talend/core/runtime/MetadataPrecisionPage.java
@@ -0,0 +1,28 @@
+package org.talend.core.runtime;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.talend.core.model.repository.RepositoryManager;
+import org.talend.core.prefs.ITalendCorePrefConstants;
+
+public class MetadataPrecisionPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+    public MetadataPrecisionPage() {
+        setPreferenceStore(RepositoryManager.getPreferenceStore());
+    }
+
+    @Override
+    protected void createFieldEditors() {
+        addField(new BooleanFieldEditor(ITalendCorePrefConstants.FORBIDDEN_MAPPING_LENGTH_PREC_LOGIC,
+                "Forbidden mappingfile length and precision logic", getFieldEditorParent()));
+
+    }
+
+    public void init(IWorkbench workbench) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
