70a0ec02a5320ce9e0c5bf6e3bcd40728a8f0496
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-25 07:05:34 +0000

    seleniumTestAddTriggerAddCronTrigger---testAddTriggerAddCronTrigger()    -add wait time(Base.MAX_WAIT_TIME)
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@64884 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestAddTriggerAddCronTrigger.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.talend.tac.base.Base;
 import com.talend.tac.cases.executePlan.TriggerDate;
 
 public class TestAddTriggerAddCronTrigger extends AddTrigger {
@@ -124,7 +125,7 @@ public class TestAddTriggerAddCronTrigger extends AddTrigger {
 		selenium.setSpeed(MIN_SPEED);
 		
 		this.waitForElementPresent("//span[text()='"+taskLabel+"']//ancestor::tr" +
-					"//span[text()='Running...']", WAIT_TIME);
+					"//span[text()='Running...']", Base.MAX_WAIT_TIME);
 		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+taskLabel+"']//ancestor::tr" +
 					"//span[text()='Running...']"));
 		
