f0c356dde9a1af0e13d68bc272fac23646ee8be7
wwang <wwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-28 04:02:01 +0000

    feature 23162:Support Document type in XMLMap lookup,this is a basic version,only inner join,unique match.
    git-svn-id: http://talendforge.org/svn/tos/trunk@65123 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/system/Document.java
+++ org.talend.librariesmanager/resources/java/routines/system/Document.java
@@ -12,6 +12,13 @@
 // ============================================================================
 package routines.system;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Node;
+
 /**
  * DOC Administrator class global comment. Detailled comment
  */
@@ -33,4 +40,66 @@ public class Document {
 			
 		return this.doc.asXML();
 	}
+	
+	/**
+	 * lookup document action
+	 * @param doc
+	 * @param loopPath
+	 * @param lookupInfo
+	 * @param xpathOfResults
+	 * @return
+	 */
+	public List<Map<String,Object>> LookupDocument(String loopPath, Map<String,Object> lookupInfo,List<String> xpathOfResults) {
+		if(doc == null || lookupInfo == null) {
+			return null;
+		}
+		List<Map<String,Object>> result = new ArrayList<Map<String,Object>>();
+		String loopXpath = loopPath.substring(loopPath.indexOf(":") + 1);
+		org.dom4j.Document document = doc.getDocument();
+		org.dom4j.XPath xpathObjectForDoc = document.createXPath(loopXpath);
+		java.util.List<org.dom4j.tree.AbstractNode> nodes = xpathObjectForDoc.selectNodes(document);
+		for(org.dom4j.tree.AbstractNode node : nodes) {
+			boolean reject = false;
+			//lookup action
+			for(String path : lookupInfo.keySet()) {
+				Object lookupValue = lookupInfo.get(path);
+				String xpath = path.substring(path.indexOf(":") + 1);
+				org.dom4j.XPath xpathObjectForLookup = node.createXPath(getXPathByLoopXPath(loopXpath,xpath));
+				Node nodeOfLookup = xpathObjectForLookup.selectSingleNode(node);
+				if(nodeOfLookup==null || lookupValue==null || !lookupValue.equals(nodeOfLookup.getText())) {
+					reject = true;
+					break;
+				}
+			}
+			//generate result action
+			if(reject) {
+				//do nothing
+			} else {
+				Map<String,Object> row = new HashMap<String,Object>();
+				for(String path : xpathOfResults) {
+					String xpath = path.substring(path.indexOf(":") + 1);
+					org.dom4j.XPath xpathObjectForResult = node.createXPath(getXPathByLoopXPath(loopXpath,xpath));
+					Node nodeOfResult = xpathObjectForResult.selectSingleNode(node);
+					row.put(path, nodeOfResult.getText());
+					result.add(row);
+				}
+			}
+			
+		}
+		return result;
+	}
+	
+	private String getXPathByLoopXPath(String loopXpath,String xpath) {
+		String result = "";
+		loopXpath = loopXpath + "/";
+		result = xpath.replace(loopXpath, "");
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Document doc = new Document();
+		System.out.println(doc.getXPathByLoopXPath("/root/students/student","/root/students/student/name"));
+		System.out.println(doc.getXPathByLoopXPath("/root/students/student","/root/info/school"));
+	}
+	
 }
