73cd6f3ac21c6513350427ed77e1b81afedbc31d
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-29 09:29:55 +0000

    selenium:**--**Modified cases TestGenerateDeployRunStopPauseTaskResumeTask  -modified class extends clas 'TaskUtil'
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@65241 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestGenerateDeployRunStopPauseTaskResumeTask.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/executionTask/TestGenerateDeployRunStopPauseTaskResumeTask.java
@@ -5,36 +5,9 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.base.Base;
-import com.talend.tac.cases.Login;
 
-public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
-	
-	public void generateDeployRunTask(String taskLabel, String buttonXpath) {
-		
-    	this.clickWaitForElementPresent("!!!menu.executionTasks.element!!!");//into executiontask page
-    	selenium.setSpeed(MID_SPEED);
-    	Assert.assertTrue(selenium.isElementPresent("//div[text()='"+rb.getString("menu.jobConductor")+"']"));
-    	selenium.setSpeed(MIN_SPEED);
-    	this.waitForElementPresent("//span[text()='"+taskLabel+"']", WAIT_TIME);
-    	selenium.mouseDown("//span[text()='"+taskLabel+"']");//select an simple task
-    	selenium.click(buttonXpath);//click function button
-          	
-	}
-	
-	public void changeCommandLineConfig(String hostAddress) {
-		
-		selenium.refresh();
-		this.clickWaitForElementPresent("idMenuConfigElement");//into Configuration page
-		this.waitForElementPresent("//div[contains(text(),'Command line/primary')]", WAIT_TIME);
-		selenium.mouseDown("//div[contains(text(),'Command line/primary')]");
-		this.clickWaitForElementPresent("//div[contains(text(),'Command line/primary')]//ancestor::div[@class='x-grid-group ']" +
-				"//div[text()='Host']//ancestor::tr[@role='presentation']//img[@title='Click to edit']");
-		this.waitForElementPresent("//div[@class=' x-form-field-wrap  x-component']//input", WAIT_TIME); 
-		System.out.println("*--------------*");
-		this.typeString("//div[@class=' x-form-field-wrap  x-component']//input",hostAddress);
-			
-	}
-	
+public class TestGenerateDeployRunStopPauseTaskResumeTask extends TaskUtils {
+
 	//test deploy a simple task
 	@Test
 	@Parameters({"modifyTask"})
@@ -82,7 +55,7 @@ public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
 	}
 	
 	//Run a task with a job containing a subjob
-	@Test(dependsOnMethods={"testRunSimpleTask"})
+	@Test(dependsOnMethods={"testGenerateTask"})
 	@Parameters({"labelTRunJobByTaskRun"})
 	public void testRunTaskWithJobContainingSubjob(String taskLabel) {
 		
@@ -96,7 +69,7 @@ public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
 	}
 
 	//test stop a running task
-	@Test(dependsOnMethods={"testRunTaskWithJobContainingSubjob"})
+	@Test(dependsOnMethods={"testGenerateTask"})
 	@Parameters({"modifyTask", "statisticRemoved(regeneration needed, fastest)", "statisticEnabled(regeneration needed)"})
 	public void testStopARunningTask(String taskLabel, String statisticRemovedRegeneration, String statisticEnabledReGeneration) {
 		
@@ -126,7 +99,7 @@ public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
 	
 	//test generating a task using remote CommandLine
 	/*needed start a remote commandLine*/
-	@Test(dependsOnMethods={"testStopARunningTask"})
+	@Test(dependsOnMethods={"testGenerateTask"})
 	@Parameters({"remotehostAddress", "modifyTask", "localhostAddress"})
 	public void testGenerateTaskUsingRemoteCommandLine(String remotehostAddress, String taskLabel,String localhostAddress) {
 		
@@ -147,7 +120,7 @@ public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
     
 
 	//test generating a task using remote wrong CommandLine
-	@Test(dependsOnMethods={"testGenerateTaskUsingRemoteCommandLine"})
+	@Test(dependsOnMethods={"testGenerateTask"})
 	@Parameters({"remotehostAddressWithWrong", "modifyTask", "localhostAddress"})
 	public void testGenerateTaskUsingRemoteWrongCommandLine(String remotehostAddressWithWrong, String taskLabel,String localhostAddress) {
 		
@@ -164,7 +137,7 @@ public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
     
 	
 	//Run a task with a Inactive server
-	@Test(dependsOnMethods={"testGenerateTaskUsingRemoteWrongCommandLine"})
+	@Test(dependsOnMethods={"testGenerateTask"})
 	@Parameters({"TaskWithInactiveServer","labelDescription","AddcommonProjectname","branchNameTrunk",
 		"jobNameTJava","version0.1","context","ServerForUseUnavailable","statisticEnabled"})
 	public void testRunTaskWithInactiveServer(String label,String description,String projectName,String branchName,
@@ -180,4 +153,68 @@ public class TestGenerateDeployRunStopPauseTaskResumeTask extends Login {
 		
 	}
 	
+	//generate a generating task and check warn info
+	@Test(dependsOnMethods={"testRunSimpleTask"})
+	@Parameters({"modifyTask"})
+	public void testGenerateAGeneratingTask(String taskLabel) {
+		
+		generateDeployRunTask(taskLabel, "idJobConductorTaskGenerateButton");//click generate button
+		this.waitForElementPresent("//span[text()='"+taskLabel+"']//ancestor::tr" +
+				"//span[text()='Generating...']", WAIT_TIME);
+		
+		selenium.setSpeed(MAX_SPEED);
+		selenium.click("//button[@id='idJobConductorTaskGenerateButton']");
+		selenium.setSpeed(MIN_SPEED);
+		
+		selenium.setSpeed(MIN_SPEED);
+		Assert.assertTrue(selenium.isTextPresent("The requested action cannot be carried out because the execution task"), "The requested action cannot be carried out because the execution " +
+				"task is without appear");
+		selenium.setSpeed(MIN_SPEED);
+		
+	}
+	
+	//test pauserTask button and resumeTask button
+	@Test(dependsOnMethods={"testRunSimpleTask"})
+	@Parameters({"modifyTask","addCronTriggerLabel","addCronTriggerDescription"})
+	public void testPauseTaskResumeTask(String taskLabel, String cronTriggerLabel,String description) {
+		
+		intoJobConductor(taskLabel);
+		
+		addTriggerAddCronTrigger(taskLabel,cronTriggerLabel, description, "2011", 
+				"Sunday", "Saturday", "January", "December");
+    			
+		selenium.click("idCrontTriggerSave");
+				
+		selenium.setSpeed(MID_SPEED);
+		
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']"));
+		selenium.setSpeed(MIN_SPEED);
+		
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("idJobConductorTaskPauseButton");
+		selenium.setSpeed(MIN_SPEED);
+		
+		this.waitForElementPresent("//span[text()='"+taskLabel+"']//ancestor::table[@class='x-grid3-row-table']//td[contains(@class,'x-grid3-col x-grid3-cell x-grid3-td-triggersStatus')]" +
+				"//img[@title='All triggers paused']", WAIT_TIME+35);
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+taskLabel+"']//ancestor::table[@class='x-grid3-row-table']//td[contains(@class,'x-grid3-col x-grid3-cell x-grid3-td-triggersStatus')]" +
+				"//img[@title='All triggers paused']"), "//span[text()='"+taskLabel+"']//ancestor::table[@class='x-grid3-row-table']//td[contains(@class,'x-grid3-col x-grid3-cell x-grid3-td-triggersStatus')]" +
+						"//img[@title='All triggers paused'] is without appear");
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']//ancestor::table[@class='x-grid3-row-table']" +
+				"//img[@alt='Paused']"), "//span[text()='"+cronTriggerLabel+"']//ancestor::table[@class='x-grid3-row-table']//img[@alt='Paused'] is without appear");
+		
+		selenium.setSpeed(MID_SPEED);
+		selenium.click("idJobConductorTaskPauseButton");
+		selenium.setSpeed(MIN_SPEED);
+		
+		Assert.assertTrue(selenium.isElementPresent("//span[text()='"+taskLabel+"']//ancestor::table[@class='x-grid3-row-table']//td[contains(@class,'x-grid3-col x-grid3-cell x-grid3-td-triggersStatus')]" +
+				"//img[@title='At least one Trigger is running' ]"), "//span[text()='"+taskLabel+"']//ancestor::table[@class='x-grid3-row-table']//td[contains(@class,'x-grid3-col x-grid3-cell x-grid3-td-triggersStatus')]" +
+						"//img[@title='At least one Trigger is running' ] is without appear");
+        Assert.assertTrue(selenium.isElementPresent("//span[text()='"+cronTriggerLabel+"']//ancestor::table[@class='x-grid3-row-table']" +
+		"//img[@alt='Normal']"), "//span[text()='"+cronTriggerLabel+"']//ancestor::table[@class='x-grid3-row-table']//img[@alt='Normal'] is without appear");
+		
+	    clearTriggers(taskLabel);
+	    
+	}
+	
 }
+  
