1793d77e14880dfb4a3dc2cd4e250e2d8c8413bc
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-01 09:38:05 +0000

    selenium:update for TestJobAnalyses: add this.sleep(5000) after a jobanalyses to waite load data from database
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@65301 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/jobsAnalyses/TestJobAnalyses.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/jobsAnalyses/TestJobAnalyses.java
@@ -15,11 +15,12 @@ public class TestJobAnalyses extends Login {
 		this.clickWaitForElementPresent("!!!menu.dashjobs.element!!!");
 		//wait and select the first "connection"
 		this.clickWaitForElementPresent("//label[text()='Active connection:']/following-sibling::div//input");
-		this.MouseDownWaitForElementPresent("//div[contains(@class,'x-combo-list-item ') and text()=' "+ connection +"']");
+		this.MouseDownWaitForElementPresent("//div[contains(@class,'x-combo-list-item') and text()=' "+connection+"']");
 		this.waitForElementPresent("//img[@title='Ok']", WAIT_TIME);
 		//select a connection and simulate a click
 		selenium.mouseDown("//img[@title='Ok']/ancestor::table");
 		//not less than one "Ok"s are displayed
+		this.sleep(5000);
 		assertTrue(selenium.getXpathCount("//img[@title='Ok']").intValue() > 1);
 		//System.out.println(selenium.getXpathCount("//img[@title='Ok']"));
 	}
