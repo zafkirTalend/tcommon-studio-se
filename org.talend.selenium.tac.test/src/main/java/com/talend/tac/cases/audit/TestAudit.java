918b24ceb9d0cd76e7434cfae7ea7aa8b53ada48
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-03 01:09:57 +0000

    selenium:update for TestAudit:add verify for audit success.
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@65383 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAudit.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAudit.java
@@ -1,5 +1,7 @@
 package com.talend.tac.cases.audit;
 
+
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -16,12 +18,14 @@ public class TestAudit extends Login {
 	@Parameters({"AddcommonProjectname"})
 	public void testAuditTrunk(String projectName){
 		testAuditProcess(projectName, "trunk");
+		Assert.assertTrue(checkAudit(projectName),"TestAudit audit trunk failed!");
 	}
 	
 	@Test
 	@Parameters({"AddcommonProjectname"})
 	public void testAuditBranch(String projectName){
 		testAuditProcess(projectName, "branch");
+		Assert.assertTrue(checkAudit(projectName),"TestAudit audit branch failed!");
 	}
 	/**
 	 * test audit process, projectName is the name of project,branches: we can define it as "trunk" or "branch"
@@ -32,7 +36,7 @@ public class TestAudit extends Login {
 		this.clickWaitForElementPresent("!!!menu.audit.element!!!");
 		//project
 		this.clickWaitForElementPresent("//label[text()='Project:']/following-sibling::div//table//div//div");
-		this.waitForElementPresent("//div[@role='listitem' and text()='"+projectName+"']",WAIT_TIME);
+		this.waitForElementPresent("//div[@role='listitem' and text()='"+projectName+"']",WAIT_TIME+70);
 		selenium.mouseDown("//div[@role='listitem' and text()='"+projectName+"']");
 		//branches
 		this.clickWaitForElementPresent("//label[text()='Branch:']/following-sibling::div//div/div");
@@ -43,6 +47,14 @@ public class TestAudit extends Login {
 		selenium.click("//button[text()='Start audit']");
 		//audit is started,but it need a very long time to be finished. we can think and check the result.
 		
+	
+	}
+	
+	public boolean checkAudit(String projectName){
+		
+	boolean ok = false;
+	ok = this.waitForTextPresent("The Audit process has terminated successfully", MAX_WAIT_TIME);
+	return ok;
 	
 	}
 }
