2b3bf9b14a4fb6340c4b73c826bc274b3b232c38
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-03 08:08:08 +0000

    selenium:update for TestAudit:   add a method to check links appear bellow auditlist   add a method to change commondline to class TestAudit.   add a test named testAuditTrunkStop:   add a test named testAuditWithOutStartCommondline
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@65420 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAudit.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAudit.java
@@ -13,26 +13,61 @@ import com.talend.tac.cases.Login;
  * @author lwang
  *
  */
+
 public class TestAudit extends Login {
 	@Test
 	@Parameters({"AddcommonProjectname"})
 	public void testAuditTrunk(String projectName){
-		testAuditProcess(projectName, "trunk");
-		Assert.assertTrue(checkAudit(projectName),"TestAudit audit trunk failed!");
+		auditProcess(projectName, "trunk");
+		int linksbefore = checkAuditListLink(projectName);
+		Assert.assertTrue(checkAuditInfo(projectName),"TestAudit audit trunk failed!");
+		this.sleep(5000);
+		Assert.assertTrue((checkAuditListLink(projectName)==linksbefore +1),"TestAudit audit trunk failed,not create links!");
+	}
+	
+	@Test
+	@Parameters({"AddcommonProjectname"})
+	public void testAuditTrunkStop(String projectName){
+		auditProcess(projectName, "trunk");
+		Assert.assertTrue(this.waitForTextPresent("Running...", WAIT_TIME));
+		this.sleep(2000);
+		selenium.click("//button[text()='Stop']");
+		Assert.assertTrue(this.waitForTextPresent("Stopping...", WAIT_TIME));
+		Assert.assertFalse(this.waitForTextPresent("Saving reports... !", WAIT_TIME),"TestAudit stop audit trunk failed!");
 	}
 	
 	@Test
 	@Parameters({"AddcommonProjectname"})
 	public void testAuditBranch(String projectName){
-		testAuditProcess(projectName, "branch");
-		Assert.assertTrue(checkAudit(projectName),"TestAudit audit branch failed!");
+		auditProcess(projectName, "branch");
+		int linksbefore = checkAuditListLink(projectName);
+		Assert.assertTrue(checkAuditInfo(projectName),"TestAudit audit branch failed!");
+		this.sleep(5000);
+		Assert.assertTrue((checkAuditListLink(projectName)==linksbefore +1),"TestAudit audit branch failed,not create links!");
 	}
+	
+	@Test
+	@Parameters({"AddcommonProjectname","remotehostAddress","remotehostAddressWithWrong"})
+	public void testAuditWithOutStartCommondline(String projectName,String normalCommondline,String wrongCommondline){
+		System.err.println(wrongCommondline);	
+		this.changeCommandLineConfig(wrongCommondline);
+		selenium.refresh();
+		this.sleep(5000);
+		auditProcess(projectName, "branch");
+		Assert.assertTrue(this.waitForTextPresent("Failed to connect to CommandLine", MAX_WAIT_TIME));
+		Assert.assertTrue(this.waitForTextPresent("org.talend.administrator.common.exception.SystemException: Failed to connect to CommandLine", MAX_WAIT_TIME));
+		this.sleep(5000);
+		this.changeCommandLineConfig(normalCommondline);
+		selenium.refresh();
+		this.sleep(5000);
+	}
+	
 	/**
 	 * test audit process, projectName is the name of project,branches: we can define it as "trunk" or "branch"
 	 * @param projectName
 	 * @param branches
 	 */
-	public void testAuditProcess(String projectName,String branches){
+	public void auditProcess(String projectName,String branches){
 		this.clickWaitForElementPresent("!!!menu.audit.element!!!");
 		//project
 		this.clickWaitForElementPresent("//label[text()='Project:']/following-sibling::div//table//div//div");
@@ -50,11 +85,33 @@ public class TestAudit extends Login {
 	
 	}
 	
-	public boolean checkAudit(String projectName){
+	public boolean checkAuditInfo(String projectName){
 		
 	boolean ok = false;
 	ok = this.waitForTextPresent("The Audit process has terminated successfully", MAX_WAIT_TIME);
+	if(this.waitForTextPresent("The Audit process has terminated with error(s)", WAIT_TIME)){
+		 ok = false;
+	 }
 	return ok;
 	
+	}
+	public int checkAuditListLink(String projectName){
+		
+		int linksNum = this.selenium.getXpathCount("//a[contains(text(),'Audit for project "+'"'+projectName.toUpperCase()+'"'+" created at')]").intValue();
+		return linksNum;
+	}
+	public void changeCommandLineConfig(String hostAddress) {
+		System.err.println(hostAddress);
+		selenium.refresh();
+		selenium.setSpeed("1000");
+		this.clickWaitForElementPresent("idMenuConfigElement");//into Configuration page
+		this.waitForElementPresent("//div[contains(text(),'Command line/primary')]", WAIT_TIME);
+		selenium.mouseDown("//div[contains(text(),'Command line/primary')]");
+		this.clickWaitForElementPresent("//div[contains(text(),'Command line/primary')]//ancestor::div[@class='x-grid-group ']" +
+				"//div[text()='Host']//ancestor::tr[@role='presentation']//img[@title='Click to edit']");
+		this.waitForElementPresent("//div[@class=' x-form-field-wrap  x-component']//input", WAIT_TIME); 
+		System.out.println("*--------------*");
+		this.typeString("//div[contains(text(),' Command line/primary (')]/ancestor::div[@class='x-grid3-body']/following-sibling::div/div/input",hostAddress);
+		selenium.setSpeed(MIN_SPEED);
 	}
 }
