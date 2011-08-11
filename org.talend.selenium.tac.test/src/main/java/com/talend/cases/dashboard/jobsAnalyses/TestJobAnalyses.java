76b7d65474f561f5d76fb830a2b7b00f1486d51d
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-11 08:43:28 +0000

    selenium:update for TestJobAnalyses to resolve problems of active connection dropdownlist can not open
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@65855 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/jobsAnalyses/TestJobAnalyses.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/cases/dashboard/jobsAnalyses/TestJobAnalyses.java
@@ -7,17 +7,26 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Parameters;
 
 import java.awt.Event;
+import java.awt.event.KeyEvent;
 
 import com.talend.tac.base.Base;
 import com.talend.tac.cases.Login;
 
 public class TestJobAnalyses extends Login {
+
 	@Test
 	@Parameters({"Mysql_Connectionlabel"})
 	public void testJobAnalyses(String connection) {
 		this.clickWaitForElementPresent("!!!menu.dashjobs.element!!!");
 		//wait and select the first "connection"
+		this.waitForElementPresent("//label[text()='Active connection:']/following-sibling::div//input",WAIT_TIME);
 		this.clickWaitForElementPresent("//label[text()='Active connection:']/following-sibling::div//input");
+		System.out.println("clicked!");
+		this.sleep(5000);
+		selenium.keyDownNative(""+KeyEvent.VK_DOWN);
+		selenium.keyUpNative(""+KeyEvent.VK_DOWN);
+		this.sleep(5000);
+//		this.openDropDownList("//label[text()='Active connection:']/following-sibling::div//input");
 		this.MouseDownWaitForElementPresent("//div[contains(@class,'x-combo-list-item') and text()=' "+connection+"']");
 		this.waitForElementPresent("//img[@title='Ok']", WAIT_TIME);
 		//select a connection and simulate a click
@@ -36,12 +45,16 @@ public class TestJobAnalyses extends Login {
 		this.clickWaitForElementPresent("!!!menu.dashjobs.element!!!");
 		//wait and select the first "connection"
 		this.clickWaitForElementPresent("//label[text()='Active connection:']/following-sibling::div//input");
+		this.sleep(5000);
+		selenium.keyDownNative(""+KeyEvent.VK_DOWN);
+		selenium.keyUpNative(""+KeyEvent.VK_DOWN);
+		this.sleep(5000);
 		this.MouseDownWaitForElementPresent("//div[contains(@class,'x-combo-list-item') and text()=' "+connection+"']");
 		this.waitForElementPresent("//img[@title='Ok']", WAIT_TIME);
 		//select a connection and simulate a click
 		selenium.mouseDown("//img[@title='Ok']/ancestor::table");
 		//not less than one "Ok"s are displayed
-		this.sleep(5000);
+		this.sleep(8000);
 		assertTrue(selenium.getXpathCount("//img[@title='Ok']").intValue() > 1);
 		//System.out.println(selenium.getXpathCount("//img[@title='Ok']"));
 		this.clickWaitForElementPresent("!!!menu.connections.element!!!");
