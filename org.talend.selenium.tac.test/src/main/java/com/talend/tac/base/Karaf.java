7857638b8682484ad818f1a778144b65b4a3fb2e
zpbai <zpbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-12 07:58:08 +0000

    selenium: fixed bug for karaf action           add the feature of ant action
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@65943 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/base/Karaf.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/base/Karaf.java
@@ -3,6 +3,7 @@ package com.talend.tac.base;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Hashtable;
 
 import org.apache.sshd.ClientChannel;
 import org.apache.sshd.ClientSession;
@@ -11,27 +12,35 @@ import org.apache.sshd.client.future.ConnectFuture;
 import org.apache.sshd.common.RuntimeSshException;
 
 public class Karaf {
-    private static String host = "localhost";
-    private static int port = 8101;
-    private static String user = "karaf";
-    private static String password = "karaf";
-
-    private static SshClient client = null;
-    private static ClientSession session = null;
-    private static ClientChannel channel;
+	private String libPath;
 	
-	private static final InputStream string2Input(String str) {
+    private String host = "localhost";
+    private int port = 8101;
+    private String user = "karaf";
+    private String password = "karaf";
+    
+
+    public Karaf(String host, String libPath){
+    	this.host = host;
+    	this.libPath = libPath;
+    }
+    
+    private SshClient client = null;
+    private ClientSession session = null;
+    private ClientChannel channel;
+	
+	private InputStream string2Input(String str) {
 		return new ByteArrayInputStream(str.getBytes());
 	}
 	
-	private static final String[] arrayAppend(String array[], String append) {
+	private String[] arrayAppend(String array[], String append) {
 		for(int i=0; i<array.length; i++) {
 			array[i] = array[i]+append;
 		}
 		return array;
 	}
 	
-	public static final String array2String(String array[]){
+	private String array2String(String array[]){
 		String str = "";
 		for(String element:array) {
 			str = str + element;
@@ -39,7 +48,7 @@ public class Karaf {
 		return str;
 	}
 	
-	static final ClientSession connectKaraf(){
+	private ClientSession connectKaraf(){
         client = SshClient.setUpDefaultClient();
         client.start();
         
@@ -64,7 +73,7 @@ public class Karaf {
 		return session;
 	}
 	
-	private static final void action(InputStream input, int timeout){
+	private void action(InputStream input, int timeout){
 		try {
 			channel = session.createChannel("shell");
 //      channel.setIn(System.in);
@@ -84,21 +93,53 @@ public class Karaf {
 		System.exit(0);
 	}
 	
-	private static final void doAction(String action, int timeout){		
+	private void doAction(String action, int timeout){		
 		action(string2Input(action+"\n"),timeout);
 	}
 	
-	private static final void doActions(String actions[], int timeout){
+	private void doActions(String actions[], int timeout){
 		action(string2Input(array2String(arrayAppend(actions,"\n"))),timeout);
 	}
 	
-	public static final void doKarafAction(String action, int timeout){
+	private void doKarafAction(String action, int timeout){
 		connectKaraf();
 		doAction(action, timeout);
 	}
 	
-	public static final void doKarafActions(String actions[], int timeout){
+	private void doKarafActions(String actions[], int timeout){
 		connectKaraf();
 		doActions(actions, timeout);
+	}
+	
+	public void karafAction(String action, int timeout){
+		AntAction antAction = new AntAction();
+		
+		Hashtable properties = new Hashtable();
+		properties.put("hudson.selenium.libs", libPath);
+		properties.put("karaf.path", new Base().getAbsolutePath(""));
+		
+		Base base = new Base();
+		System.out.println(base.getAbsolutePath(""));
+		properties.put("host", host);
+		properties.put("action", action);
+		properties.put("timeout", timeout);
+		
+		antAction.runTarget("Karaf.xml", properties);
+		
+		try {
+			Thread.sleep(timeout + 3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * host, action, timeout
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Karaf karaf = new Karaf(args[0], null);
+		karaf.doKarafAction(args[1], Integer.parseInt(args[2]));
 	}
 }
