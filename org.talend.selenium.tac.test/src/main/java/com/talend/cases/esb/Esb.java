d2d8dd05fb24914bcdc4d1304bc921f778bf377f
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-08-19 05:43:55 +0000

    selenium:update for Esb: add method sendServiceRequest
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@66273 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/cases/esb/Esb.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/cases/esb/Esb.java
@@ -5,10 +5,14 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.talend.tac.base.Karaf;
 import com.talend.tac.cases.Login;
 
 
 public class Esb extends Login {
+	
+	Karaf karaf = null;
+//		new Karaf("192.168.0.94", "D:/jar");
 	
 	
 	/**
@@ -18,4 +22,37 @@ public class Esb extends Login {
 		this.clickWaitForElementPresent("!!!menu.serviceactivity.element!!!");
 		this.waitForElementPresent("//div[@class='header-title' and text()='Service Activity Monitoring']", WAIT_TIME);
 	}
+	
+	public void sendServiceRequest(String karafURL,String jarPath,String consumerName){
+		karaf = new Karaf(karafURL, jarPath);
+		System.out.println("service request send-->  start!");
+		karaf.karafAction("job:start "+consumerName+"", 10000);
+		System.out.println("service request send-->  ended!");
+		
+	}
+	
+	/**
+	 * this method is used to wait something disappear
+	 * @param element: element that should disappear
+	 * @param timeout: max wait time
+	 */
+	public void waitForElementDispear(String element, int timeout) {
+		if (selenium.isElementPresent(element)) {
+			for (int second = 0;; second++) {
+				if (second >= timeout)
+					Assert.assertFalse(selenium.isElementPresent(element));
+				try {
+					if ((!selenium.isElementPresent(element))) {
+						break;
+					} else {
+						this.sleep(1000);
+					}
+				} catch (Exception e) {
+				}
+
+			}
+		}
+
+	}
+	
 }
