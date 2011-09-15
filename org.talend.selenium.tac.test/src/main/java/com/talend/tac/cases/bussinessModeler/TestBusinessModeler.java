e6d5fea3ea694bbdcc5f4c074deea91f3281f2e7
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-15 10:16:10 +0000

    selenium:**--**business modeler change id of element 'BM designer'
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@67984 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/bussinessModeler/TestBusinessModeler.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/bussinessModeler/TestBusinessModeler.java
@@ -1,8 +1,9 @@
 package com.talend.tac.cases.bussinessModeler;
 
+import junit.framework.Assert;
+
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 import com.talend.tac.cases.Login;
 
 public class TestBusinessModeler extends Login {
@@ -10,7 +11,12 @@ public class TestBusinessModeler extends Login {
 	@Test
 	@Parameters( { "AddcommonProjectname","branches" ,"bussinesModel.name","Mysql_Connectionlabel"})
 	public void testBusinessModeler(String project,String branches,String modelName,String connectionName) {
-		this.clickWaitForElementPresent("!!!menu.businessModeler.element!!!");
+		this.clickWaitForElementPresent("!!!menu.businessModeler.designer.element!!!");
+		
+		this.waitForElementPresent("//div[text()='Business modeler designer'" +
+				" and @class='header-title']", WAIT_TIME);
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='Business modeler designer' " +
+				"and @class='header-title']"));
 		//select
 		this.selectDropDownList("idProjectListBox", project);
 		this.selectDropDownList("idBranchListBox", branches);
@@ -18,20 +24,20 @@ public class TestBusinessModeler extends Login {
 		this.selectDropDownList("idBusinessConnectionsListBox", connectionName);
 		//load 
 		selenium.click("//button[text()='load']");
-		assertEquals(selenium.getValue("idProjectListBox"),project);
-		assertEquals(selenium.getValue("idBranchListBox"),branches);
+		Assert.assertEquals(selenium.getValue("idProjectListBox"),project);
+		Assert.assertEquals(selenium.getValue("idBranchListBox"),branches);
 //		assertEquals(selenium.getValue("idBusinessListBox"),modelName);
-		assertEquals(selenium.getValue("idBusinessConnectionsListBox"),connectionName);
+		Assert.assertEquals(selenium.getValue("idBusinessConnectionsListBox"),connectionName);
 		//edit ?
 		//save
 		selenium.click("//button[text()='save']");
 		selenium.getAlert();
 		//close
 		selenium.click("//button[text()='close']");
-		assertEquals(selenium.getValue("idProjectListBox"),"Select a Project");
-		assertEquals(selenium.getValue("idBranchListBox"),"Select a Branch");
-		assertEquals(selenium.getValue("idBusinessListBox"),"Select a Business model");
-		assertEquals(selenium.getValue("idBusinessConnectionsListBox"),"");
+		Assert.assertEquals(selenium.getValue("idProjectListBox"),"Select a Project");
+		Assert.assertEquals(selenium.getValue("idBranchListBox"),"Select a Branch");
+		Assert.assertEquals(selenium.getValue("idBusinessListBox"),"Select a Business model");
+		Assert.assertEquals(selenium.getValue("idBusinessConnectionsListBox"),"");
 	}
 
 	@Override
