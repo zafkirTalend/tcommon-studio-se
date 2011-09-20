9a97f72cbfd5e22d8c57754a3c4228a846458bc5
dlin <dlin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-20 01:12:41 +0000

    bug TDI-17654 resolved:Remove Perl Preferences
    git-svn-id: http://talendforge.org/svn/tos/trunk@68250 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/prefs/LibrariesMainPreferencesPage.java
+++ org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/prefs/LibrariesMainPreferencesPage.java
@@ -2,32 +2,15 @@ package org.talend.librariesmanager.prefs;
 
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.RadioGroupFieldEditor;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.CorePlugin;
-import org.talend.core.GlobalServiceRegister;
-import org.talend.core.service.ICorePerlService;
 import org.talend.librariesmanager.i18n.Messages;
 
 public class LibrariesMainPreferencesPage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
-    private static final String SINGLE_MODE = "true"; //$NON-NLS-1$
-
-    private static final String SEPARATE_MODE = "false"; //$NON-NLS-1$
-
-    // private String oldPath;
-
     private DirectoryFieldEditor externalLibrariesPath;
-
-    private DirectoryFieldEditor externalLibrariesPathJava;
-
-    private DirectoryFieldEditor externalLibrariesPathPerl;
-
-    private RadioGroupFieldEditor choice;
 
     public LibrariesMainPreferencesPage() {
         super(GRID);
@@ -35,50 +18,21 @@ public class LibrariesMainPreferencesPage extends FieldEditorPreferencePage impl
 
     @Override
     protected void createFieldEditors() {
-        choice = new RadioGroupFieldEditor(PreferencesUtilities.EXTERNAL_LIB_PATH_MODE_SINGLE,
-                Messages.getString("MainPreferencesPage.folderSelectionMode"), 1, new String[][] { //$NON-NLS-1$
-                { Messages.getString("MainPreferencesPage.sameRootFolder"), SINGLE_MODE }, //$NON-NLS-1$
-                        { Messages.getString("MainPreferencesPage.separateRootFolder"), SEPARATE_MODE } }, //$NON-NLS-1$
-                getFieldEditorParent());
 
         externalLibrariesPath = new DirectoryFieldEditor(PreferencesUtilities.EXTERNAL_LIB_PATH,
                 Messages.getString("MainPreferencesPage.externalLibPath"), getFieldEditorParent()); //$NON-NLS-1$
         addField(externalLibrariesPath);
-        externalLibrariesPathJava = new DirectoryFieldEditor(PreferencesUtilities.EXTERNAL_LIB_PATH_JAVA,
-                Messages.getString("MainPreferencesPage.externalLibPathJava"), getFieldEditorParent()); //$NON-NLS-1$
-        addField(externalLibrariesPath);
-        if (GlobalServiceRegister.getDefault().isServiceRegistered(ICorePerlService.class)) {
-            externalLibrariesPathPerl = new DirectoryFieldEditor(PreferencesUtilities.EXTERNAL_LIB_PATH_PERL,
-                    Messages.getString("MainPreferencesPage.externalLibPathPerl"), getFieldEditorParent()); //$NON-NLS-1$
-        }
-        addField(choice);
-        addField(externalLibrariesPath);
-        addField(externalLibrariesPathJava);
-        if (GlobalServiceRegister.getDefault().isServiceRegistered(ICorePerlService.class)) {
-            addField(externalLibrariesPathPerl);
-        }
         activateFields(getPreferenceStore().getBoolean(PreferencesUtilities.EXTERNAL_LIB_PATH_MODE_SINGLE));
     }
 
     @Override
     protected void initialize() {
         super.initialize();
-        choice.setPropertyChangeListener(new MyListener());
-    }
-
-    private class MyListener implements IPropertyChangeListener {
-
-        public void propertyChange(PropertyChangeEvent event) {
-            activateFields(event.getNewValue().equals(SINGLE_MODE));
-        }
+        externalLibrariesPath.setEnabled(true, getFieldEditorParent());
     }
 
     private void activateFields(boolean singleMode) {
         externalLibrariesPath.setEnabled(singleMode, getFieldEditorParent());
-        externalLibrariesPathJava.setEnabled(!singleMode, getFieldEditorParent());
-        if (GlobalServiceRegister.getDefault().isServiceRegistered(ICorePerlService.class)) {
-            externalLibrariesPathPerl.setEnabled(!singleMode, getFieldEditorParent());
-        }
     }
 
     public void init(IWorkbench workbench) {
