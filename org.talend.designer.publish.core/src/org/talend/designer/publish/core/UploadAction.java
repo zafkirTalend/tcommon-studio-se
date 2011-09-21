ff0316fb0547713bf39166415a7724c0cb0f62d7
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-21 07:07:01 +0000

    fix missing bundle for features when publish route
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@68409 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.publish.core/src/org/talend/designer/publish/core/UploadAction.java
+++ org.talend.designer.publish.core/src/org/talend/designer/publish/core/UploadAction.java
@@ -27,6 +27,7 @@ public class UploadAction {
 
 		FeaturesModel featuresModel = new FeaturesModel(groupId, artifactId,
 				version, repositoryUrl, userName, password);
+		featuresModel.addSubBundle(bundleModel);
 		deployFeatures(featuresModel);
 		return true;
 	}
@@ -47,31 +48,36 @@ public class UploadAction {
 	public static void main(String[] args) throws Exception {
 		UploadAction uploadAction = new UploadAction();
 
-		BundleModel b1 = new BundleModel(new File("userRoutines.jar"),
-				"org.talend.liugang", "userRoutines1", "1.0",
-				"http://localhost:8080/archiva/repository/snapshots/", "gliu",
-				"liugang123");
-		BundleModel b2 = new BundleModel(new File("userRoutines.jar"),
-				"trg.talend.liugang", "userRoutines2", "1.0",
-				"http://localhost:8080/archiva/repository/snapshots/", "gliu",
-				"liugang123");
+		// BundleModel b1 = new BundleModel(new File("userRoutines.jar"),
+		// "org.talend.liugang", "userRoutines1", "1.0",
+		// "http://localhost:8080/archiva/repository/snapshots/", "gliu",
+		// "liugang123");
+		// BundleModel b2 = new BundleModel(new File("userRoutines.jar"),
+		// "trg.talend.liugang", "userRoutines2", "1.0",
+		// "http://localhost:8080/archiva/repository/snapshots/", "gliu",
+		// "liugang123");
+		//
+		// FeaturesModel f1 = new FeaturesModel("org.talend.liugang", "user1",
+		// "1.0", "http://localhost:8080/archiva/repository/snapshots/",
+		// "gliu", "liugang123");
+		// FeaturesModel f2 = new FeaturesModel("trg.talend.liugang", "user2",
+		// "1.0", "http://localhost:8080/archiva/repository/snapshots/",
+		// "gliu", "liugang123");
+		//
+		// f2.addSubBundle(b1);
+		// f2.addSubBundle(b2);
+		// f1.addSubBundle(b1);
+		// f1.addSubBundle(b2);
+		// f1.addSubFeature(f2);
+		//
+		// uploadAction.deployBundle(b1);
+		// uploadAction.deployBundle(b2);
+		// uploadAction.deployFeatures(f1);
+		// uploadAction.deployFeatures(f2);
 
-		FeaturesModel f1 = new FeaturesModel("org.talend.liugang", "user1",
-				"1.0", "http://localhost:8080/archiva/repository/snapshots/",
-				"gliu", "liugang123");
-		FeaturesModel f2 = new FeaturesModel("trg.talend.liugang", "user2",
-				"1.0", "http://localhost:8080/archiva/repository/snapshots/",
-				"gliu", "liugang123");
-
-		f2.addSubBundle(b1);
-		f2.addSubBundle(b2);
-		f1.addSubBundle(b1);
-		f1.addSubBundle(b2);
-		f1.addSubFeature(f2);
-
-		uploadAction.deployBundle(b1);
-		uploadAction.deployBundle(b2);
-		uploadAction.deployFeatures(f1);
-		uploadAction.deployFeatures(f2);
+		uploadAction.deployRoute("TestEERoute_0.1.jar", "org.talend.liugang",
+				"TestEERoute2", "2.0.22-SNAPSHOT", null,
+				"http://192.168.0.10:18080/archiva/repository/snapshots/",
+				"gliu", "123liu");
 	}
 }
