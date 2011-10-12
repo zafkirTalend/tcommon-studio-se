a4f15044979016447af957909c20c946480c8726
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-12 07:23:01 +0000

    selenium:add case(testAddTalendRuntimeServerWithWrongFormatPort) in class(TestAddServerWithTalendRuntime)   -add a runtime server with wrong format port add case(testAddTalendRuntimeServerAndChangeDefaultPort) in class(TestAddServerWithTalendRuntime)   -add a runtime server and change default port
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@69855 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestAddServerWithTalendRuntime.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/jobconductor/TestAddServerWithTalendRuntime.java
@@ -27,5 +27,50 @@ public class TestAddServerWithTalendRuntime extends Server {
 		Assert.assertTrue(selenium.isElementPresent("//div[text()='testRuntimeServer']//ancestor::table[contains(@class,'x-grid3-row-table')]//button[text()='Admin server']"));
 		
 	}
+
+	@Test
+	@Parameters({ "ServerLablename","ServerDescription",
+			"ServerHost", "ServerCommondport", "ServerFiletransfortport",
+			"ServerMonitorport", "ServerTimeout", "ServerUsername",
+			"ServerPassword" })
+	public void testAddTalendRuntimeServerWithWrongFormatPort(String lable,
+			String description, String host, String commondport,
+			String transfortport, String monitorport, String time,
+			String username, String password) {
+		
+		this.addRuntimeServer(lable, description, host, commondport, transfortport, monitorport,
+				time, username, password, "wrongFormatPort", "wrongFormatPort", "wrongFormatPort",
+				"YES");
+		Assert.assertTrue(selenium.isElementPresent("//label[text()='Mgmt-Server port:']" +
+				"//parent::div//img[@class='gwt-Image x-component ']"));
+		Assert.assertTrue(selenium.isElementPresent("//label[text()='Mgmt-Reg port:']" +
+		"//parent::div//img[@class='gwt-Image x-component ']"));
+		Assert.assertTrue(selenium.isElementPresent("//label[text()='Mgmt-Server port:']" +
+		"//parent::div//img[@class='gwt-Image x-component ']"));
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='Fix errors in form before save']"));
+		
+	}
+	
+
+	@Test
+	@Parameters({ "RuntimeServerChangeDefaultPortLablename","ServerDescription",
+			"ServerHost", "ServerCommondport", "ServerFiletransfortport",
+			"ServerMonitorport", "ServerTimeout", "ServerUsername",
+			"ServerPassword" })
+	public void testAddTalendRuntimeServerAndChangeDefaultPort(String lable,
+			String description, String host, String commondport,
+			String transfortport, String monitorport, String time,
+			String username, String password) {
+		
+		this.addRuntimeServer(lable, description, host, commondport, transfortport, monitorport,
+				time, username, password, "6565", "6566", "6567",
+				"YES");
+        this.checkServerStatus(lable, server_status_up);
+		
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='testRuntimeServer']//ancestor::table[contains(@class,'x-grid3-row-table')]//div[contains(@class,'x-grid3-cell-inner" +
+				" x-grid3-col-talendRuntime') and text()='Talend Runtime']"));
+		Assert.assertTrue(selenium.isElementPresent("//div[text()='testRuntimeServer']//ancestor::table[contains(@class,'x-grid3-row-table')]//button[text()='Admin server']"));
+		
+	}
 	
 }
