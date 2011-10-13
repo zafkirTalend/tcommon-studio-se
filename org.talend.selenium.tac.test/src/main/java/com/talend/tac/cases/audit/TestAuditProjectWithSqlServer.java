9124e0983f55ce8c096d5198c941723f40db87e9
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-13 02:32:28 +0000

    selenium:add case(testAuditProjectWithSqlServer) in class(TestAuditProjectWithSqlServer)   -audit a project with sqlserver
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@69949 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAuditProjectWithSqlServer.java
@@ -0,0 +1,42 @@
+package com.talend.tac.cases.audit;
+
+import java.awt.Event;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestAuditProjectWithSqlServer extends Audit {
+	
+	//audit a project with sqlserver
+	@Test
+	@Parameters({"sqlserverURL", "sqlserverUserName", "sqlserverPassWord", "sqlserverDriver", "ProjectWithSpaceChar"})
+	public void testAuditProjectWithSqlServer(String url, String userName, String userPassWd, String driver,
+			   String projectName) {
+		
+	   this.openAudit();
+		   
+	   /*change db info*/
+	   this.configAuditDB(url, userName, driver, userPassWd);
+	   selenium.setSpeed(MID_SPEED);
+	   selenium.keyPressNative(Event.TAB +"");
+	   selenium.keyPressNative(Event.TAB +"");
+	   selenium.click("idDbConfigSaveButton");
+	   selenium.setSpeed(MIN_SPEED);
+	   
+	   this.waitForCheckConnectionStatus("//div[text()='OK']", 3);
+	   selenium.click("//div[contains(@class,'x-nodrag x-tool-close x-tool x-component')]");
+	   /*change db info*/
+	   
+	   this.auditProcess(projectName, "trunk");
+	   
+	   /*check audit tesult*/
+	   this.waitForElementPresent("//div//font[1][text()='The Audit process has terminated successfully']", WAIT_TIME*4);
+	   this.waitForElementPresent("//div//font[3][text()='The Audit process has terminated successfully']", WAIT_TIME*4);	   
+	   
+	   int auditListLinkCount = this.checkAuditListLink(projectName);
+	   Assert.assertEquals(auditListLinkCount, 3);
+		
+	}
+	
+}
