77c7b9d61ec4f8ef7bcd548418ef25825a1fa6d1
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-13 08:47:41 +0000

    selenium:add case(testChangeAuditReportStoredPathInConfiguration) in class(TestChangeAuditReportStoredPathInConfiguration)   -change audit report stored path in configuration, go to audit page.select a  project and branch ,start audit, go to the directory to check the reports generated add case(testChangeReportStoredPathWithNotExistPath) in class(TestChangeAuditReportStoredPathInConfiguration)   -change 'reports stored path' with not exist path
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@70000 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestChangeAuditReportStoredPathInConfiguration.java
@@ -0,0 +1,161 @@
+package com.talend.tac.cases.audit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.awt.Event;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.talend.tac.base.Base;
+
+public class TestChangeAuditReportStoredPathInConfiguration extends Audit {
+	
+	public String locatorOfAllInputTags = other.getString("commandline.conf.all.input");
+		
+/*	//change audit report stored path in configuration, go to audit page.select a  project
+   and branch ,start audit, go to the directory to check the reports generated */
+	@Test
+	@Parameters({"auditStoredReportsPath","mysqlURL", "mysqlUserName", "mysqlPassWord", "mysqlDriver", "ProjectWithSpaceChar"})
+	public void testChangeAuditReportStoredPathInConfiguration(String auditStoredReportsPath, String url, String userName, String userPassWd, String driver,
+			   String projectName) {
+		
+		//go to configuration page
+		this.clickWaitForElementPresent("idMenuConfigElement");
+		
+		//change value of 'reports stored path' to a directory exist
+		this.waitForElementPresent("//div[contains(text(),'Audit (')]", WAIT_TIME);
+		selenium.mouseDown("//div[contains(text(),'Audit (')]");
+		
+		this.clickWaitForElementPresent(other.getString("audit.conf.reportsStoredPath.editButton"));//click the edit button to make the input tag shown.
+		String defaultPath = selenium.getValue(locatorOfAllInputTags);
+		selenium.keyDownNative(""+KeyEvent.VK_ENTER);
+		System.out.println(">>>>>>>>"+defaultPath);
+		
+	    this.typeWordsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, this.getAbsolutePath(auditStoredReportsPath));
+	    this.AssertEqualsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, this.getAbsolutePath(auditStoredReportsPath),other.getString("audit.conf.reportsStoredPath.statusIcon"));
+		
+	    this.openAudit();
+		   
+	    /*change db info*/
+	    this.configAuditDB(url, userName, driver, userPassWd);
+	    selenium.setSpeed(MID_SPEED);
+	    selenium.keyPressNative(Event.TAB +"");
+	    selenium.keyPressNative(Event.TAB +"");
+	    selenium.click("idDbConfigSaveButton");
+	    selenium.setSpeed(MIN_SPEED);
+	   
+	    this.waitForCheckConnectionStatus("//div[text()='OK']", 3);
+	    selenium.click("//div[contains(@class,'x-nodrag x-tool-close x-tool x-component')]");
+	    /*change db info*/
+	   
+	    this.auditProcess(projectName, "trunk");
+	   
+	    /*check audit tesult*/
+	    this.waitForElementPresent("//div//font[1][text()='The Audit process has terminated successfully']", WAIT_TIME*4);
+	    this.waitForElementPresent("//div//font[3][text()='The Audit process has terminated successfully']", WAIT_TIME*4);	   
+//	    this.waitForElementPresent("//a[contains(text(),'Audit for project \"PROJECT_SPACE\" created at')]", WAIT_TIME);
+		
+	    //check report file whether exist in local directory
+	    File   directory   =   new   File(this.getAbsolutePath(auditStoredReportsPath));
+		File[]   file   =   directory.listFiles();
+		
+		String fileName = file[0].getName();		
+		String[] reportFilenames = fileName.split("_",3);
+		
+		String reportFileName = reportFilenames[2];
+		System.out.println(reportFileName);	
+		
+	    File auditReportFile = new File(this.getAbsolutePath(auditStoredReportsPath)+"/"+"PROJECT_SPACE"+"_"+reportFileName);
+		System.out.println(auditReportFile);
+	    
+	    for (int seconds = 0;; seconds++) {
+			if (seconds >= WAIT_TIME) {
+				assertTrue(auditReportFile.exists());
+			}
+			if (auditReportFile.exists()) {
+				System.out.println(seconds + "' used to download");
+				break;
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	    
+	    auditReportFile.delete();
+	    
+	  //go to configuration page
+		this.clickWaitForElementPresent("idMenuConfigElement");
+		
+		//change value of 'reports stored path' to a directory exist
+		this.waitForElementPresent("//div[contains(text(),'Audit (')]", WAIT_TIME);
+		selenium.mouseDown("//div[contains(text(),'Audit (')]");
+		
+	    this.typeWordsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, defaultPath);
+	    this.AssertEqualsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, defaultPath,other.getString("audit.conf.reportsStoredPath.statusIcon"));
+	    
+	}
+	
+	//change 'reports stored path' with not exist path
+	@Test
+	@Parameters({"auditStoredReportsPathWithNotExistPath"})
+	public void testChangeReportStoredPathWithNotExistPath(String notExistPath) {
+		
+		//go to configuration page
+		this.clickWaitForElementPresent("idMenuConfigElement");
+		
+		//change value of 'reports stored path' to a directory not exist
+		this.waitForElementPresent("//div[contains(text(),'Audit (')]", WAIT_TIME);
+		selenium.mouseDown("//div[contains(text(),'Audit (')]");
+		
+		this.clickWaitForElementPresent(other.getString("audit.conf.reportsStoredPath.editButton"));//click the edit button to make the input tag shown.
+		String defaultPath = selenium.getValue(locatorOfAllInputTags);
+		selenium.keyDownNative(""+KeyEvent.VK_ENTER);
+		System.out.println(">>>>>>>>"+defaultPath);
+		
+		/*change 'reports stored path' to a not exist path*/
+	    this.typeWordsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, notExistPath);
+	    this.AssertEqualsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, notExistPath,other.getString("audit.conf.reportsStoredPath.notExistPath.statusIcon"));
+	    
+	    this.waitForElementPresent("//div[contains(text(),'Audit (')]/parent::div/following-sibling::div//table//div[text()='Reports stored path']/parent::td/following-sibling::" +
+	    		"td//div[text()='Path does not exist']", WAIT_TIME);
+	    
+	    /*change 'reports stored path' to incipient path*/
+	    this.typeWordsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, defaultPath);
+	    this.AssertEqualsInConfigurationMenu(other.getString("audit.conf.reportsStoredPath.editButton"),locatorOfAllInputTags, defaultPath,other.getString("audit.conf.reportsStoredPath.statusIcon"));
+	}
+	
+	/**
+	 * type a value in configuration menu.click the edit button firstly to wait for the input to appear.
+	 * @param locatorOfEditButton
+	 * @param locatorOfInput
+	 * @param value
+	 */
+	public void typeWordsInConfigurationMenu(String locatorOfEditButton,String locatorOfInput,String value){
+		 this.clickWaitForElementPresent(locatorOfEditButton);//click the edit button to make the input tag shown.
+		 this.typeWaitForElementPresent(locatorOfInput, value);
+		
+	}
+	/**
+	 * assertions,check the value in input tag is as expected,and check the status icon.
+	 * @param locatorOfEditButton
+	 * @param locatorOfInput	
+	 * @param value
+	 */
+		public void AssertEqualsInConfigurationMenu(String locatorOfEditButton,String locatorOfInput,String value,String statusIconLocator){
+		this.AssertEqualsInConfigurationMenu(locatorOfEditButton, locatorOfInput, value);
+			this.waitForElementPresent(statusIconLocator, WAIT_TIME);//wait and check the icon status.
+	}
+	public void AssertEqualsInConfigurationMenu(String locatorOfEditButton,String locatorOfInput,String value){
+		this.clickWaitForElementPresent(locatorOfEditButton);//click the edit button to make the input tag shown.
+		this.waitForElementPresent(locatorOfInput, Base.WAIT_TIME);
+		assertEquals(selenium.getValue(locatorOfInput), value);
+		selenium.fireEvent(locatorOfInput, "blur");
+	}
+	
+}
