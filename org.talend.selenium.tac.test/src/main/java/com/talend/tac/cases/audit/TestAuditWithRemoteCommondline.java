a7fb3a0ac7320cbd1d050004d203dfc87297ca34
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-25 08:10:26 +0000

    selenium:>>modified cases(testAuditTrunkWithRemoteCommondline) in class(TestAuditWithRemoteCommondline)   --invoking function(getDefaultReportPath), get get incipient report path   --invoking function(checkReportPdf), check report pdf after audit
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@70831 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAuditWithRemoteCommondline.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestAuditWithRemoteCommondline.java
@@ -1,6 +1,8 @@
 package com.talend.tac.cases.audit;
 
 
+import java.io.File;
+
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -9,13 +11,25 @@ import org.testng.annotations.Test;
 
 public class TestAuditWithRemoteCommondline extends Audit {
 	@Test
-	@Parameters({"AddcommonProjectname"})
-	public void testAuditTrunkWithRemoteCommondline(String projectName){
+	@Parameters({"AddcommonProjectname","trunjobWithCheckpoint", "tjavaWithMulripleCheckpoint", 
+		"jobNameTJava"})
+	public void testAuditTrunkWithRemoteCommondline(String projectName, String tRunJobCheckPoint, String tjavaCheckpoint,
+			String tjava){
+
+		//get get incipient report path
+		String defaultPath = this.getDefaultReportPath();
+		
 		auditProcess(projectName, "trunk");
 		int linksbefore = checkAuditListLink(projectName);
 		Assert.assertTrue(checkAuditInfo(projectName),"TestAudit audit trunk failed!");
 		this.sleep(5000);
 		Assert.assertTrue((checkAuditListLink(projectName)==linksbefore +1),"TestAudit audit trunk failed,not create links!");
+		
+		File auditReportFile = this.checkReportPdf(defaultPath, projectName, tjava);
+        Assert.assertTrue(this.isExistedInfoInPdf(this.getDefaultReportPath()+"/"+this.getReportFileName(), tRunJobCheckPoint));
+	    Assert.assertTrue(this.isExistedInfoInPdf(this.getDefaultReportPath()+"/"+this.getReportFileName(), tjavaCheckpoint));
+	    auditReportFile.delete();
+		
 	}
 	
 }
