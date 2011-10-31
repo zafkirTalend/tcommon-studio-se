11efff94038d160f038ba9f2f20930cd56e17d3a
hwang <hwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-31 06:10:22 +0000

    TDI-18193:Deleting a Service fails although no related objects are open anymore in Studio
    git-svn-id: http://talendforge.org/svn/tos/trunk@71214 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/model/repository/ERepositoryObjectType.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/model/repository/ERepositoryObjectType.java
@@ -378,10 +378,10 @@ public class ERepositoryObjectType extends DynaEnum<ERepositoryObjectType> {
             "repository.systemIndicators.phoneNumberStatistics", "TDQ_Libraries/Indicators/System Indicators/Phone Number Statistics", "SYSTEM_INDICATORS_PHONENUMBER_STATISTICS", 100, true, "repository.systemIndicators.phoneNumberStatistics.alias", new String[] { "DQ" }, new String[] {}, false); //$NON-NLS-1$ //$NON-NLS-2$
 
     public final static ERepositoryObjectType SERVICESOPERATION = new ERepositoryObjectType(
-			"repository.servicesOperation", "services", "SERVICESOPERATION", 101, true, "servicesOperation.alias", new String[] { "DI", "CAMEL" }, new String[] {}, false); //$NON-NLS-1$ //$NON-NLS-2$
+            "repository.servicesOperation", "services", "SERVICESOPERATION", 101, true, "servicesOperation.alias", new String[] { "DI", "CAMEL" }, new String[] {}, false); //$NON-NLS-1$ //$NON-NLS-2$
 
     public final static ERepositoryObjectType SERVICESPORT = new ERepositoryObjectType(
-			"repository.servicesPort", "services", "SERVICESPORT", 102, true, "servicesPort.alias", new String[] { "DI", "CAMEL" }, new String[] {}, false); //$NON-NLS-1$ //$NON-NLS-2$
+            "repository.servicesPort", "services", "SERVICESPORT", 102, true, "servicesPort.alias", new String[] { "DI", "CAMEL" }, new String[] {}, false); //$NON-NLS-1$ //$NON-NLS-2$
 
     private String alias;
 
@@ -643,6 +643,8 @@ public class ERepositoryObjectType extends DynaEnum<ERepositoryObjectType> {
             return "";//$NON-NLS-1$
         } else if (type == COMPONENTS) {
             return "components";
+        } else if (type.getType().equals("SERVICES")) {
+            return "Services";
         } else {
             return "job";
         }
