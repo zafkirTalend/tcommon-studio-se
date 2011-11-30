c2e68dd2315069cb1f9e2be63a352af1c009e3ca
fzhong <fzhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-11-30 09:52:53 +0000

    SWTBot: add test about Copy/Paste business model elements and modify test about assignment for business model
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@73493 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.tos.swtbot.test/src/tosstudio/businessmodels/CopyPasteBusinessModelElementTest.java
+++ org.talend.tos.swtbot.test/src/tosstudio/businessmodels/CopyPasteBusinessModelElementTest.java
@@ -12,63 +12,57 @@
 // ============================================================================
 package tosstudio.businessmodels;
 
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
-import org.eclipse.swtbot.swt.finder.waits.Conditions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.talend.swtbot.TalendSwtBotForTos;
 import org.talend.swtbot.Utilities;
+import org.talend.swtbot.helpers.BusinessModelHelper;
 import org.talend.swtbot.items.TalendBusinessModelItem;
-import org.talend.swtbot.items.TalendJobItem;
 
 /**
  * DOC fzhong class global comment. Detailled comment
  */
 @RunWith(SWTBotJunit4ClassRunner.class)
-public class AssignmentForBusinessModelTest extends TalendSwtBotForTos {
+public class CopyPasteBusinessModelElementTest extends TalendSwtBotForTos {
 
     private TalendBusinessModelItem businessModelItem;
 
-    private TalendJobItem jobItem;
-
     private static final String BUSINESSMODEL_NAME = "businessModel1"; //$NON-NLS-1$
-
-    private static final String JOB_NAME = "job1"; //$NON-NLS-1$
 
     @Before
     public void initialisePrivateFields() {
         businessModelItem = new TalendBusinessModelItem(BUSINESSMODEL_NAME);
         businessModelItem.create();
-        jobItem = new TalendJobItem(JOB_NAME);
-        jobItem.create();
-        jobItem.getEditor().saveAndClose();
     }
 
     @Test
-    public void assignmentForBusinessModelTest() {
+    public void copyPasteBusinessModelElementTest() {
         SWTBotGefEditor editor = businessModelItem.getEditor();
         String componentName = "test";
         Utilities.dndPaletteToolOntoBusinessModel(editor, "Data", new Point(100, 100), componentName);
-        Utilities.dndMetadataOntoBusinessModel(editor, jobItem.getItem(), new Point(100, 100));
         editor.save();
-        gefBot.viewByTitle("Business Models(" + businessModelItem.getItemFullName() + ")").setFocus();
-        selecteAllTalendTabbedPropertyListIndex(1);
-        gefBot.waitUntil(Conditions.tableHasRows(gefBot.table(0), 1));
-        Assert.assertEquals("items did not assign to business model", "Job", gefBot.table(0).cell(0, "Type"));
-        Assert.assertEquals("items did not assign to business model", jobItem.getItemName(), gefBot.table(0).cell(0, "Name"));
+        SWTBotGefEditPart editPart = editor.getEditPart(componentName);
+        editor.click(editPart);
+        editor.clickContextMenu("Copy");
+        editor.clickContextMenu("Paste");
+        List<SWTBotGefEditPart> editParts = BusinessModelHelper.editPartsWithLabel(editor, componentName);
+        Assert.assertEquals("business model elements did not copy", 2, editParts.size());
     }
 
     @After
     public void removePreviouslyCreateItems() {
         businessModelItem.getEditor().saveAndClose();
         Utilities.cleanUpRepository(businessModelItem.getParentNode());
-        Utilities.cleanUpRepository(jobItem.getParentNode());
         Utilities.emptyRecycleBin();
     }
 }
