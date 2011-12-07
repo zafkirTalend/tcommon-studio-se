511cf95d30c36546c4e9dfc0580534fa699d3969
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-07 07:25:06 +0000

    bug  TDI-18802 fixed:Add the possibility to set the root element as loop in the output xml wizard
    git-svn-id: http://talendforge.org/svn/tos/trunk@73948 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment.ui/src/main/java/org/talend/repository/ui/wizards/metadata/connection/files/xml/action/CreateElementAction.java
+++ org.talend.metadata.managment.ui/src/main/java/org/talend/repository/ui/wizards/metadata/connection/files/xml/action/CreateElementAction.java
@@ -51,6 +51,7 @@ public class CreateElementAction extends SelectionProviderAction {
             form.redrawLinkers();
         }
         form.updateConnection();
+        form.updateStatus();
     }
 
     private boolean createChildNode(FOXTreeNode node) {
