49a5fdf1425cbb8cfa2349c00c48339b884d8896
hcyi <hcyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-07 02:13:06 +0000

    bug TDI-18445 fixed : when create new job version -> SVN commit dialog shouldn't appear .
    git-svn-id: http://talendforge.org/svn/tos/trunk@73912 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.repository/src/main/java/org/talend/core/repository/model/ProxyRepositoryFactory.java
+++ org.talend.core.repository/src/main/java/org/talend/core/repository/model/ProxyRepositoryFactory.java
@@ -798,6 +798,18 @@ public final class ProxyRepositoryFactory implements IProxyRepositoryFactory {
         // getStatus(item)
         if (getStatus(item).isPotentiallyEditable()) {
             this.repositoryFactoryFromProvider.lock(item);
+            //
+            if ((item instanceof JobletProcessItem || item instanceof ProcessItem)
+                    && getStatus(item) == ERepositoryStatus.LOCK_BY_USER) {
+                String docId = item.getProperty().getId() + "doc";
+                IRepositoryViewObject repositoryViewObject = this.repositoryFactoryFromProvider.getLastVersion(
+                        projectManager.getCurrentProject(), docId);
+                if (repositoryViewObject != null) {
+                    Property property = repositoryViewObject.getProperty();
+                    Item documentationItem = property.getItem();
+                    this.repositoryFactoryFromProvider.lock(documentationItem);
+                }
+            }
             // i18n
             // log.debug("Lock [" + item + "] by \"" + getRepositoryContext().getUser() + "\".");
             String str[] = new String[] { item.toString(), getRepositoryContext().getUser().toString() };
@@ -1235,7 +1247,17 @@ public final class ProxyRepositoryFactory implements IProxyRepositoryFactory {
             Date modificationDate = obj.getProperty().getModificationDate();
             if (modificationDate == null || commitDate == null || modificationDate.before(commitDate)) {
                 this.repositoryFactoryFromProvider.unlock(obj);
-
+                //
+                if (obj instanceof JobletProcessItem || obj instanceof ProcessItem) {
+                    String docId = obj.getProperty().getId() + "doc";
+                    IRepositoryViewObject repositoryViewObject = this.repositoryFactoryFromProvider.getLastVersion(
+                            projectManager.getCurrentProject(), docId);
+                    if (repositoryViewObject != null) {
+                        Property property = repositoryViewObject.getProperty();
+                        Item documentationItem = property.getItem();
+                        this.repositoryFactoryFromProvider.unlock(documentationItem);
+                    }
+                }
                 // i18n
                 // log.debug("Unlock [" + obj + "] by \"" + getRepositoryContext().getUser() + "\".");
                 String str[] = new String[] { obj.toString(), getRepositoryContext().getUser().toString() };
