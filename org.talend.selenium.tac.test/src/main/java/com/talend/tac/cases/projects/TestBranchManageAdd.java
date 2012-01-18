e43dd55e6fcb4350836f69d638fd9377be5cd57d
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-01-18 08:34:55 +0000

    selenium: add cases to project :add branch named "trunk" to trunk and branches
    git-svn-id: http://talendforge.org/svn/tis_shared/branches/branch-5_0@76982 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestBranchManageAdd.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestBranchManageAdd.java
@@ -86,4 +86,35 @@ public class TestBranchManageAdd extends Login {
 		selenium.setSpeed(MIN_SPEED);
 
 	}
+	
+	@Test
+	@Parameters({ "AddcommonProjectname", "BranchName" })
+	public void testAddProjectbranchNamedTrunkToTrunk(String project, String branchname) throws InterruptedException {
+		// first add a branch to a project
+		branchname = "trunk";
+		selenium.setSpeed(MIN_SPEED);
+		this.waitForElementPresent("!!!menu.project.element!!!", Base.WAIT_TIME);
+
+		selenium.click("!!!menu.project.element!!!");//
+		this.waitForElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+				+ project + "')]", Base.WAIT_TIME);
+		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+				+ project + "')]");// the selected project's id
+		selenium.click("idBranchManagementButton");
+		selenium.setSpeed(MID_SPEED);
+		this.clickWaitForElementPresent("idBranchManagementSourceInput");
+		this.waitForElementPresent("//div[text()='trunk']", WAIT_TIME);
+		selenium.mouseDown("//div[text()='trunk']");
+		selenium.fireEvent("idBranchManagementSourceInput", "blur");
+		selenium.setSpeed(MIN_SPEED);
+		this.typeString("idBranchManagementTargetInput", branchname);
+		this.waitForElementPresent("//input[@id='idBranchManagementTargetInput']//ancestor::div[contains(@class,'x-form-element x-form-el-x')]//img", WAIT_TIME);
+//		selenium.setSpeed(MID_SPEED);
+		selenium.click("idBranchManagementCreateButton");
+		Assert.assertFalse(this.waitElement("//span[text()='"
+				+ other.getString("project.branchmanage.add.conform") + "']", WAIT_TIME));
+		selenium.click("//div[@class=' x-nodrag x-tool-close x-tool x-component']");
+
+	}
+	
 }
--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestBranchManageAddExistbranch.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/projects/TestBranchManageAddExistbranch.java
@@ -63,6 +63,36 @@ public class TestBranchManageAddExistbranch extends Login {
 		closeButton(project, branchname);
 
 	}
+	
+	@Test
+	@Parameters({ "AddcommonProjectname", "BranchName" })
+	public void testAddProjectbranchNamedTrunkToBranch(String project, String branchname) throws InterruptedException {
+		// first add a branch to a project
+		
+		selenium.setSpeed(MIN_SPEED);
+		this.waitForElementPresent("!!!menu.project.element!!!", Base.WAIT_TIME);
+
+		selenium.click("!!!menu.project.element!!!");//
+		this.waitForElementPresent("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+				+ project + "')]", Base.WAIT_TIME);
+		selenium.mouseDown("//div[@class='x-grid3-cell-inner x-grid3-col-label' and (text()='"
+				+ project + "')]");// the selected project's id
+		selenium.click("idBranchManagementButton");
+		selenium.setSpeed(MID_SPEED);
+		this.clickWaitForElementPresent("idBranchManagementSourceInput");
+		this.waitForElementPresent("//div[text()='branches/"+branchname+"']", WAIT_TIME);
+		selenium.mouseDown("//div[text()='branches/"+branchname+"']");
+		selenium.fireEvent("idBranchManagementSourceInput", "blur");
+		selenium.setSpeed(MIN_SPEED);
+		this.typeString("idBranchManagementTargetInput", "trunk");
+		this.waitForElementPresent("//input[@id='idBranchManagementTargetInput']//ancestor::div[contains(@class,'x-form-element x-form-el-x')]//img", WAIT_TIME);
+//		selenium.setSpeed(MID_SPEED);
+		selenium.click("idBranchManagementCreateButton");
+		Assert.assertFalse(this.waitElement("//span[text()='"
+				+ other.getString("project.branchmanage.add.conform") + "']", WAIT_TIME));
+		selenium.click("//div[@class=' x-nodrag x-tool-close x-tool x-component']");
+
+	}
 
 	public void closeButton(String project, String branchname) {
 //		selenium.setSpeed(MID_SPEED);
