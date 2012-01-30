3386f760abc91c1072f0363d0d9b42da2c798270
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-01-30 03:24:05 +0000

    selenium:add cases about designer and viewer's read right to server and virtual server
    git-svn-id: http://talendforge.org/svn/tis_shared/branches/branch-5_0@77318 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/rolesRights/TestCheckRoleDesignerPrivilege.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/rolesRights/TestCheckRoleDesignerPrivilege.java
@@ -1,14 +1,15 @@
 package com.talend.tac.cases.rolesRights;
-
+import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.Assert.assertTrue;
-
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.talend.tac.cases.Login;
 
 public class TestCheckRoleDesignerPrivilege extends Login {
 
-	@Test(description="can be improved ")
+	@Test
 	public void testCheckRoleDesignerPrivilege() {
 		waitForElementPresent("idLeftMenuTreeLogoutButton",WAIT_TIME );
 		assertTrue(selenium.isElementPresent("!!!menu.project.element!!!"));//projects
@@ -23,11 +24,58 @@ public class TestCheckRoleDesignerPrivilege extends Login {
 //		assertTrue(selenium.isElementPresent("!!!menu.soamanager.element!!!"));
 		assertTrue(selenium.isElementPresent("!!!menu.businessModeler.browser.element!!!"));
 		assertTrue(selenium.isElementPresent("!!!menu.dashjobs.element!!!"));
-//		assertTrue(selenium.isElementPresent("!!!menu.soamanager.dashboard.element!!!"));
+		assertTrue(selenium.isElementPresent("!!!menu.dashamc.element!!!"));
 		assertTrue(selenium.isElementPresent("!!!menu.commandline.dashboard.element!!!"));
 		assertTrue(selenium.isElementPresent("!!!menu.grid_task_executions_history.element!!!"));
 		assertTrue(selenium.isElementPresent("!!!menu.graphic_task_executions_history.element!!!"));
 		assertTrue(selenium.isElementPresent("!!!menu.audit.element!!!"));
 
 	}
+	
+	
+	
+	@Test
+	@Parameters({ "ServerForUseAvailable", "ServerForUseUnavailable","ServerForUseDownlabel",
+			"ServeravailableHost" })
+	public void testDesignerReadForServer(String lableavailable, String labelunactive,String downlabel,
+			String availablehost) {
+		waitForElementPresent("idLeftMenuTreeLogoutButton",WAIT_TIME );
+		assertTrue(selenium.isElementPresent("!!!menu.executionServers.element!!!"));
+		this.waitForElementPresent("!!!menu.executionServers.element!!!",
+				WAIT_TIME);
+		selenium.click("!!!menu.executionServers.element!!!");
+		this.waitForElementPresent(
+				"//div[@class='header-title' and text()='Servers']//ancestor::div[@class=' x-viewport x-component x-border-layout-ct']//span[@class='x-fieldset-header-text' and text()='Execution server']",
+				WAIT_TIME);
+		this.waitForElementPresent(
+				"//div[@class='header-title' and text()='Servers']", WAIT_TIME);
+		this.waitForElementPresent("//div[@class='header-title' and text()='Servers']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//button[@id='idSubModuleRefreshButton']", WAIT_TIME);
+		
+		assertFalse(selenium.isVisible("//div[@class='header-title' and text()='Servers']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//button[@id='idSubModuleAddButton']"));
+		this.waitForElementPresent("//div[text()='" + lableavailable + "']",
+				WAIT_TIME);
+		selenium.mouseDown("//div[text()='" + lableavailable + "']");
+		this.sleep(3000);
+		Assert.assertTrue(selenium.getValue((other.getString("inputname.id.server.add.host"))).equals(availablehost)) ;
+		String testlabel=lableavailable+"testModify";
+		this.typeString(other.getString("inputname.id.server.add.label"),
+				testlabel);
+		selenium.click("//div[@class='header-title' and text()='Servers']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-border-layout-ct']//button[@id='idFormSaveButton']");
+	    assertFalse(this.waitElement("//div[text()='" + testlabel + "']", WAIT_TIME));
+		
+	}
+	
+	@Test
+	public void testDesignerReadForVirtualServers() {
+		waitForElementPresent("idLeftMenuTreeLogoutButton",WAIT_TIME );
+		assertTrue(selenium.isElementPresent("!!!menu.virtual_server.element!!!"));
+		
+		selenium.click("!!!menu.virtual_server.element!!!");
+		this.waitForElementPresent(
+				"//div[@class='header-title' and text()='Virtual servers']", WAIT_TIME);
+		this.waitForElementPresent("//div[@class='header-title' and text()='Virtual servers']//ancestor::div[@class='x-panel-body x-panel-body-noheader x-panel-body-noborder x-border-layout-ct']//span[text()='use_server_available']",WAIT_TIME);
+		assertFalse(this.waitElement("//button[@class='x-btn-text ' and text()='Add a virtual server']", WAIT_TIME));
+		
+	}
+	
 }
