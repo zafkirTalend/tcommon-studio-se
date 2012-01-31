96143d4687da5301f52680c1aa6c4fb48c1a89b9
dfcao <dfcao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-01-31 02:49:25 +0000

    SWTBot:update ValuesAsTreeTabOfContextTest add test if order by context and if edit the value of built-in type
    git-svn-id: http://talendforge.org/svn/tis_shared/branches/branch-5_0@77373 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.tos.swtbot.test/src/tosstudio/context/ValuesAsTreeTabOfContextTest.java
+++ org.talend.tos.swtbot.test/src/tosstudio/context/ValuesAsTreeTabOfContextTest.java
@@ -17,11 +17,13 @@ import junit.framework.Assert;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.talend.swtbot.SWTBotTreeExt;
+import org.talend.swtbot.SWTBotTreeItemExt;
 import org.talend.swtbot.TalendSwtBotForTos;
 import org.talend.swtbot.Utilities;
 import org.talend.swtbot.items.TalendContextItem;
@@ -41,6 +43,10 @@ public class ValuesAsTreeTabOfContextTest extends TalendSwtBotForTos {
 
     private final static String contextName = "context1"; //$NON-NLS-1$
 
+    private final static String newContextName = "context2"; //$NON-NLS-1$
+
+    private final static String newValue = "value4"; //$NON-NLS-1$
+
     @Before
     public void initialisePrivateFields() {
         jobItem = new TalendJobItem(jobName);
@@ -55,10 +61,43 @@ public class ValuesAsTreeTabOfContextTest extends TalendSwtBotForTos {
         Utilities.dndMetadataOntoJob(jobEditor, contextItem.getItem(), null, new Point(20, 20));
         gefBot.viewByTitle("Contexts(Job " + jobName + " 0.1)").setFocus();
         gefBot.cTabItem("Values as tree").activate();
+
+        // test if order by variable
         SWTBotTreeExt treeExt = new SWTBotTreeExt(gefBot.tree(0));
         treeExt.header("Variable").click();
         String actual = gefBot.tree(0).cell(0, "Variable");
-        Assert.assertEquals("the order is not the expected order ", "var2", actual);
+        Assert.assertEquals("cann't order by variable ", "var2", actual);
+
+        // test if order by context
+        gefBot.toolbarButtonWithTooltip("Configure Contexts...").click();
+        gefBot.shell("Configure Contexts");
+        gefBot.button("New...").click();
+        SWTBotShell contextShell = gefBot.shell("New Context");
+        gefBot.text().setText(newContextName);
+        boolean okButtonIsEnable = gefBot.button("OK").isEnabled();
+        if (okButtonIsEnable) {
+            gefBot.button("OK").click();
+        } else {
+            contextShell.close();
+            Assert.assertTrue("the ok button is uneable,maybe becasue the context name is exist,", okButtonIsEnable);
+        }
+        gefBot.button("OK").click();
+
+        treeExt.header("Context").click();
+        treeExt.header("Context").click();
+        String result = gefBot.tree(0).getTreeItem(System.getProperty("context.variable0")).getNode(0).cell(1);
+        Assert.assertEquals("can't order by context", newContextName, result);
+
+        // test if edit the value of built-in type
+        gefBot.cTabItem("Variables").activate();
+        gefBot.button(0).click();
+        gefBot.cTabItem("Values as tree").activate();
+        SWTBotTreeItemExt treeItemExt = new SWTBotTreeItemExt(gefBot.tree(0).expandNode("new1").getNode(0).select().click());
+        treeItemExt.click(4);
+        gefBot.text().setText(newValue);
+        treeItemExt.click(3);
+        String value = gefBot.tree(0).getTreeItem("new1").getNode(0).cell(4);
+        Assert.assertEquals("cann't edit the value of built-in type", newValue, value);
 
     }
 
