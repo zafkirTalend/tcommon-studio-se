f8ef3883df6f8386375f3c3da91295075e2558c5
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-02 06:58:33 +0000

    selenium:add case for test add a esbconductor with already exist label
    git-svn-id: http://talendforge.org/svn/tis_shared/branches/branch-5_0@77605 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/esbconductor/TestAddESBConductor.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/esbconductor/TestAddESBConductor.java
@@ -17,8 +17,28 @@ public class TestAddESBConductor extends ESBConductorUtils {
 		this.addESBConductor(label, des, repository, group, artifact, version,
 				name, type, context, server);
 		
+		selenium.setSpeed(MID_SPEED);
 		Assert.assertTrue(selenium.isElementPresent("//div[text()='"+label+"']" +
 				"//ancestor::table[@class='x-grid3-row-table']//span[text()='Ready to deploy']"));
+		selenium.setSpeed(MIN_SPEED);
+		
+	}
+	
+	/*add esbconductor with already label*/
+	@Test
+	@Parameters({"labelOfService", "desOfService", "repository", "group", "artifact",
+		"version", "name", "type", "context", "serverOfRuntime"})
+	public void testAddESBConductorWithAlreadyLabel(String label, String des, String repository,
+			String group, String artifact, String version, String name, String type, 
+			String context, String server) {
+		
+		this.addESBConductor(label, des, repository, group, artifact, version,
+				name, type, context, server);
+		
+		selenium.setSpeed(MID_SPEED);
+		Assert.assertTrue(selenium.isTextPresent("An execution task with" +
+				" this name already exists"));
+		selenium.setSpeed(MIN_SPEED);
 		
 	}
 	
