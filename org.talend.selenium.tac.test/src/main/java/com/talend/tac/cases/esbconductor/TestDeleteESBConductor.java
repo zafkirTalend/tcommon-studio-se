3606acd6161af6b4c9aab4726b2c8d8ae305d002
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-16 08:45:43 +0000

    selenium:TestDeleteESBConductor---testDeleteUndeployESBConductor()  --add case for test delete a undeployed'conductor(this's a bug)
    git-svn-id: http://talendforge.org/svn/tis_shared/branches/branch-5_0@78331 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/esbconductor/TestDeleteESBConductor.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/esbconductor/TestDeleteESBConductor.java
@@ -25,4 +25,30 @@ public class TestDeleteESBConductor extends ESBConductorUtils {
 			String context, String server) {
 	this.deleteESBConductorOK(label);
 	}
+	
+	@Test
+	@Parameters({"deleteWithUndeployConductor", "desOfService", "repository", "group", "artifact",
+		"version", "name", "type", "context", "serverOfRuntime"})
+	public void testDeleteUndeployESBConductor(String label, String des, String repository,
+			String group, String artifact, String version, String name, String type, 
+			String context, String server) {
+		
+		String deployId = "idESBConductorTaskGridDeployButton";
+		String status = "Deployed and started";
+		String promptInfo = "Feature '"+name+"' deployed.";
+
+		String undeployId = "idESBConductorTaskGridUndeployButton";
+		String undeployStatus = "Undeployed";
+		String undeployPopupInfo = "Are you sure you want to undeploy the feature '"+name+"'";
+		String undeployPromptInfo = "Feature '"+name+"' undeployed.";
+		
+		this.addESBConductor(label, des, repository, group, artifact,
+				version, name, type, context, server);
+		this.deployStartConductor(label, name, promptInfo, deployId, status);
+		this.undeployStopConductor(label, name, undeployId, undeployStatus,
+				undeployPopupInfo, undeployPromptInfo);
+		this.deleteESBConductorOK(label);
+		
+	}
+	
 }
