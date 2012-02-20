e8103466035f02ebc8039ed8c378c9b4cb5472ba
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-20 09:49:31 +0000

    selenium:update cases of audit
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@78543 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/audit/TestChangeCommondlineToLocalHost.java
@@ -0,0 +1,17 @@
+package com.talend.tac.cases.audit;
+
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class TestChangeCommondlineToLocalHost extends Audit {
+	
+
+	
+	@Test
+	@Parameters({"localhostAddress"})
+	public void testCommondlineLocalhost(String ip){
+		this.changeCommandLineConfig(ip,  other.getString("commandLine.conf.primary.host.statusIcon"));
+	}
+}
