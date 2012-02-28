b1040d5d0dca11cf899705cc60bfe1cbcfb5a560
hwang <hwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-28 03:03:45 +0000

    TDI-19731 Prepare OEM for Hortonworks
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@78940 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.rcp.branding.tos.bigdata/src/main/java/org/talend/rcp/branding/tos/bigdata/HortonActionBarHelper.java
@@ -0,0 +1,109 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.rcp.branding.tos.bigdata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.eclipse.jface.preference.IPreferenceNode;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.internal.registry.PerspectiveDescriptor;
+import org.eclipse.ui.internal.registry.PerspectiveRegistry;
+import org.eclipse.ui.internal.registry.ViewDescriptor;
+import org.eclipse.ui.internal.registry.ViewRegistry;
+import org.eclipse.ui.views.IViewDescriptor;
+import org.talend.rcp.intro.ActionBarBuildHelper;
+
+/**
+ * DOC hwang class global comment. Detailled comment
+ */
+public class HortonActionBarHelper extends ActionBarBuildHelper {
+
+    @Override
+    public void postWindowOpen() {
+        super.postWindowOpen();
+        String[] removeIds = { "perspective" /* , "org.talend.rcp.intro.ShowViewAction" */};
+        for (String id : removeIds) {
+            windowMenu.remove(id);
+        }
+
+        String[] removeHelpIds = { "org.eclipse.core.internal.registry.ConfigurationElementHandle@1a8a", "org.talend.help.perl",
+                "org.eclipse.ui.actionSet.keyBindings", "org.talend.help.perl.OpenPerlHelpAction" };
+        for (String id : removeHelpIds) {
+            helpMenu.remove(id);
+        }
+
+        String[] perspectivesId = { "org.eclipse.jdt.ui.JavaPerspective", "org.eclipse.team.ui.TeamSynchronizingPerspective",
+                "org.eclipse.ui.resourcePerspective", "org.epic.core.Perspective", "org.eclipse.pde.ui.PDEPerspective",
+                "org.talend.rcp.perspective" };
+
+        List<IPerspectiveDescriptor> perspectivesToDelete = new ArrayList<IPerspectiveDescriptor>();
+
+        for (IPerspectiveDescriptor desc : window.getWorkbench().getPerspectiveRegistry().getPerspectives()) {
+            if (ArrayUtils.contains(perspectivesId, desc.getId())) {
+                perspectivesToDelete.add(desc);
+            }
+        }
+
+        for (IPerspectiveDescriptor desc : perspectivesToDelete) {
+            PerspectiveDescriptor perspDesc = (PerspectiveDescriptor) desc;
+            PerspectiveRegistry registry = (PerspectiveRegistry) window.getWorkbench().getPerspectiveRegistry();
+            PerspectiveDescriptor[] descriptors = { perspDesc };
+            registry.removeExtension(perspDesc.getConfigElement().getDeclaringExtension(), descriptors);
+        }
+
+        String[] viewsId = { "org.eclipse.ant.ui.views.AntView", "org.eclipse.pde.runtime.RegistryBrowser",
+                "org.eclipse.pde.ui.DependenciesView", "org.eclipse.pde.ui.PluginsView", "org.eclipse.search.SearchResultView",
+                "org.eclipse.search.ui.views.SearchView", "org.eclipse.team.sync.views.SynchronizeView",
+                "org.eclipse.team.ui.GenericHistoryView", "org.eclipse.ui.browser.view",
+                "org.eclipse.ui.cheatsheets.views.CheatSheetView", "org.eclipse.ui.texteditor.TemplatesView",
+                "org.eclipse.ui.views.AllMarkersView", "org.eclipse.ui.views.BookmarkView",
+                "org.eclipse.team.sync.views.SynchronizeView", "org.eclipse.ui.views.ProblemView",
+                "org.eclipse.ui.views.PropertySheet", "org.eclipse.ui.views.TaskList", "org.epic.core.views.browser.BrowserView",
+                "org.epic.perleditor.views.ExplainErrorsView", "org.epic.perleditor.views.PerlDocView",
+                "org.epic.regexp.views.RegExpView", "org.talend.designer.core.codeView", "org.talend.scheduler.views.Scheduler",
+                "org.eclipse.ui.navigator.ProjectExplorer", "org.talend.designer.components.ecosystem.ui.views.EcosystemView",
+                "org.talend.designer.codegen.perlmodule.ModulesView", "org.eclipse.wst.common.snippets.internal.ui.SnippetsView" };
+
+        List<IViewDescriptor> viewsToDelete = new ArrayList<IViewDescriptor>();
+
+        for (IViewDescriptor desc : window.getWorkbench().getViewRegistry().getViews()) {
+            if (ArrayUtils.contains(viewsId, desc.getId())) {
+                viewsToDelete.add(desc);
+            }
+        }
+
+        for (IViewDescriptor desc : viewsToDelete) {
+            ViewDescriptor viewDesc = (ViewDescriptor) desc;
+            ViewRegistry registry = (ViewRegistry) window.getWorkbench().getViewRegistry();
+            ViewDescriptor[] descriptors = { viewDesc };
+            registry.removeExtension(viewDesc.getConfigurationElement().getDeclaringExtension(), descriptors);
+        }
+
+        String[] prefsId = { "org.eclipse.ant.ui.AntPreferencePage",
+                "org.eclipse.datatools.connectivity.internal.ui.preferences.DataToolsMainPage",
+                "org.eclipse.debug.ui.DebugPreferencePage", "org.eclipse.team.ui.TeamPreferences",
+                "org.epic.core.preferences.PerlMainPreferencePage", "org.eclipse.pde.ui.MainPreferencePage" };
+        List<IPreferenceNode> prefsToDelete = new ArrayList<IPreferenceNode>();
+        for (IPreferenceNode node : window.getWorkbench().getPreferenceManager().getRootSubNodes()) {
+            if (ArrayUtils.contains(prefsId, node.getId())) {
+                prefsToDelete.add(node);
+            }
+        }
+        for (IPreferenceNode node : prefsToDelete) {
+            window.getWorkbench().getPreferenceManager().remove(node);
+        }
+    }
+
+}
---
+++ org.talend.rcp.branding.tos.bigdata/src/main/java/org/talend/rcp/branding/tos/bigdata/HortonBrandingConfiguration.java
@@ -0,0 +1,189 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.rcp.branding.tos.bigdata;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jface.action.ICoolBarManager;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.ui.PlatformUI;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.ui.branding.DefaultBrandingConfiguration;
+import org.talend.core.ui.branding.IBrandingConfiguration;
+import org.talend.repository.model.IRepositoryNode;
+import org.talend.repository.model.IRepositoryNode.ENodeType;
+import org.talend.repository.model.IRepositoryNode.EProperties;
+import org.talend.repository.model.RepositoryNode;
+
+/**
+ * DOC hwang class global comment. Detailled comment
+ */
+public class HortonBrandingConfiguration extends DefaultBrandingConfiguration {
+
+    public List<IRepositoryNode> getHiddenRepositoryCategory(RepositoryNode parent, String type) {
+        List<IRepositoryNode> nodes = super.getHiddenRepositoryCategory(parent, type);
+
+        RepositoryNode businessProcessNode = new RepositoryNode(null, parent, ENodeType.SYSTEM_FOLDER);
+        businessProcessNode.setProperties(EProperties.LABEL, ERepositoryObjectType.BUSINESS_PROCESS);
+        businessProcessNode.setProperties(EProperties.CONTENT_TYPE, ERepositoryObjectType.BUSINESS_PROCESS);
+        nodes.add(businessProcessNode);
+
+        RepositoryNode docNode = new RepositoryNode(null, parent, ENodeType.SYSTEM_FOLDER);
+        docNode.setProperties(EProperties.LABEL, ERepositoryObjectType.DOCUMENTATION);
+        docNode.setProperties(EProperties.CONTENT_TYPE, ERepositoryObjectType.DOCUMENTATION);
+        nodes.add(docNode);
+
+        RepositoryNode metadataNode = new RepositoryNode(null, parent, ENodeType.SYSTEM_FOLDER);
+        metadataNode.setProperties(EProperties.LABEL, ERepositoryObjectType.METADATA);
+        metadataNode.setProperties(EProperties.CONTENT_TYPE, ERepositoryObjectType.METADATA);
+        nodes.add(metadataNode);
+
+        return nodes;
+
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IActionBarHelper#fillMenuBar(org.eclipse.jface.action.IMenuManager)
+     */
+    public void fillMenuBar(IMenuManager menuBar) {
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IActionBarHelper#fillCoolBar(org.eclipse.jface.action.ICoolBarManager)
+     */
+    public void fillCoolBar(ICoolBarManager coolBar) {
+        coolBar.removeAll();
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#getAvailableComponents()
+     */
+    public String[] getAvailableComponents() {
+        return null;
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#getAvailableLanguages()
+     */
+    public String[] getAvailableLanguages() {
+        return new String[] { ECodeLanguage.JAVA.getName() };
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#getJobEditorSettings()
+     */
+    public Map<String, Object> getJobEditorSettings() {
+        return new HashMap<String, Object>();
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#isUseMailLoginCheck()
+     */
+    public boolean isUseMailLoginCheck() {
+        return true;
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#isUseProductRegistration()
+     */
+    public boolean isUseProductRegistration() {
+        return true;
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#isAllowDebugMode()
+     */
+    public boolean isAllowDebugMode() {
+        return true;
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#isUseDemoProjects()
+     */
+    public boolean isUseDemoProjects() {
+        return false;
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#getAdditionalTitle()
+     */
+    public String getAdditionalTitle() {
+        return "";
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#setAdditionalTitle(java.lang.String)
+     */
+    public void setAdditionalTitle(String title) {
+
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#getInitialWindowPerspectiveId()
+     */
+    public String getInitialWindowPerspectiveId() {
+        PreferenceManager pm = PlatformUI.getWorkbench().getPreferenceManager();
+        pm.find("org.talend.core.prefs").remove("org.talend.repository.prefs.documentation");
+        pm.find("org.talend.core.prefs").findSubNode("org.talend.designer.core.ui.preferences.SpecificSettingPreferencePage")
+                .remove("org.talend.sqlbuilder.ui.prefs.sqlbuilder");
+        pm.find("org.talend.core.prefs").remove("org.talend.designer.core.ui.preferences.PerformancePreferencePage");
+        return IBrandingConfiguration.PERSPECTIVE_CAMEL_ID;
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#setUseMailLoginCheck(boolean)
+     */
+    public void setUseMailLoginCheck(boolean useMainLoginCheck) {
+
+    }
+
+    /*
+     * (non-Jsdoc)
+     * 
+     * @see org.talend.core.ui.branding.IBrandingConfiguration#setUseProductRegistration(boolean)
+     */
+    public void setUseProductRegistration(boolean useProductRegistration) {
+
+    }
+
+}
