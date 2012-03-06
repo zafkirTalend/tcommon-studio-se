a2829e7ceed76f2051251e7a4b2151759c3f922d
fwang <fwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-06 05:20:41 +0000

    bug TDI-19859 fixed:Copyright header in generated code and system routines is still set to 2010
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@79265 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/ui/branding/AbstractBrandingService.java
+++ org.talend.core/src/main/java/org/talend/core/ui/branding/AbstractBrandingService.java
@@ -26,7 +26,7 @@ public abstract class AbstractBrandingService implements IBrandingService {
     }
 
     public String getRoutineLicenseHeader(String version) {
-        String contents = Messages.getString("AbstractBrandingService.routines_license_header_content", //$NON-NLS-1$
+        String contents = Messages.getString("AbstractBrandingService.routines_license_header_content1", //$NON-NLS-1$
                 this.getFullProductName(), version);
         return contents;
     }
