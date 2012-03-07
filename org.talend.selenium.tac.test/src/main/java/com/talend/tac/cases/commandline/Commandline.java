0fb433805f72d4343cfbf60c9422cf6ba28db30d
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-07 07:40:14 +0000

    selenium： add case for test commandline command help
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@79404 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/commandline/Commandline.java
+++ org.talend.selenium.tac.test/src/main/java/com/talend/tac/cases/commandline/Commandline.java
@@ -9,8 +9,8 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Commandline {
-	private String server = "localhost";
-	private int port = 8002;
+	protected String server = "localhost";
+	protected int port = 8002;
 
 	public Commandline() {
 		if(System.getProperty("commandline.server")!=null)
@@ -20,28 +20,38 @@ public class Commandline {
 			this.port = Integer.parseInt(System.getProperty("commandline.port"));
 	}
 	
-	public StringBuffer runCommand(String server, int port, String command) throws UnknownHostException, IOException{
+	public String runCommand(String server, int port, String command) {
 		StringBuffer sb = new StringBuffer();
-		Socket s = new Socket(server, port);
-		InputStream is = s.getInputStream();
-		OutputStream os = s.getOutputStream();
-		
-		command = command+"\n";
-		os.write(command.getBytes());
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		
-		String r = br.readLine();
-		sb.append(r);
-		while(!"".equals(r) && null!=r && !"\n".equals(r)){
-			r = br.readLine();
-			sb.append(r + "\n");
-			if(r.contains("Copyright (C)") && r.contains("www.talend.com")) {
+		Socket s;
+		try {
+			s = new Socket(server, port);
+			InputStream is = s.getInputStream();
+			OutputStream os = s.getOutputStream();
+			
+			command = command+"\n";
+			os.write(command.getBytes());
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			String r = br.readLine();
+			sb.append(r);
+			while(!"".equals(r) && null!=r && !"\n".equals(r)){
 				r = br.readLine();
-				sb.append(r);
-				break;
+				sb.append(r + "\n");
+				if(r.contains("Copyright (C)") && r.contains("www.talend.com")) {
+					r = br.readLine();
+					sb.append(r);
+					break;
+				}
 			}
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		System.out.println(sb);
-		return sb;
+		
+		System.out.println(sb.toString().trim().replaceAll(" |\\\\|\\+|\\-|[\n\r]|\\'|;|\"|\\|", ""));
+		return sb.toString().trim().replaceAll(" |\\\\|\\+|\\-", "").replaceAll("[\n\r]", "").replaceAll("\\'|;|\"|\\|", "" );
 	}
 }
