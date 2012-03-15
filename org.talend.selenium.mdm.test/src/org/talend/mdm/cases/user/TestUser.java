c11ddef89b0d1f61f7f6c711652f462a8c09f5b1
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-15 09:18:13 +0000

    selenium:add cases testAdduserAllowedAdmin,testAddUserAllowedWeb,testAddUserWebOverAllowed
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@79873 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/org/talend/mdm/cases/user/TestUser.java
+++ org.talend.selenium.mdm.test/src/org/talend/mdm/cases/user/TestUser.java
@@ -1,6 +1,7 @@
 package org.talend.mdm.cases.user;
 
 import org.talend.mdm.Login;
+import org.talend.mdm.impl.LicenseImpl;
 import org.talend.mdm.impl.UserImpl;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -9,16 +10,34 @@ import org.testng.annotations.Test;
 
 public class TestUser extends Login {
 	UserImpl userImpl;
+	LicenseImpl license;
 	@BeforeMethod
 	public void beforeMethod(){
 		userImpl = new UserImpl(driver);
+		license = new LicenseImpl(driver);
 		logger.info("Set Before Info");
 	}
 		
+	
+	@Test
+	@Parameters( { "roles" })
+	public void testAddUserAllowedAdmin(String roles) {
+		license.openLicense();
+		userImpl.addUserAllowed(roles, license.getAvailableAdmin());
+	}
+	
+	@Test
+	@Parameters( { "roles" })
+	public void testAddUserAllowedWeb(String roles) {
+		license.openLicense();
+		userImpl.addUserAllowed(roles, license.getAvailableWeb());
+	}
+	
+	
 	@Test
 	@Parameters( { "identifier", "first.name", "last.name", "password", "confirm.password", "email", "company", "default.version", "active", "roles" })
-	public void testAddUser(String identifier,String firstName,String lastName,String password, String confirmPassword,
-			String email, String company, String defaultVersion, String active, String roles) {
-		userImpl.addUserImpl(identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, Boolean.parseBoolean(active.trim()), this.splitParameter(roles));
+	public void testAddUserWebOverAllowed(String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion,boolean active,String roles) {
+		license.openLicense();
+		userImpl.addUserOverAllowedWeb(identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, active, roles,license.getAvailableWeb());
 	}
 }
