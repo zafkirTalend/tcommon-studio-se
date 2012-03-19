8888cf41c68327a03ae9b27d75d38596744285ac
hjbai2 <hjbai2@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-19 08:31:59 +0000

    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@80015 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.mdm.test/src/org/talend/mdm/cases/record/TestRecord.java
@@ -0,0 +1,34 @@
+package org.talend.mdm.cases.record;
+
+import org.talend.mdm.Login;
+import org.talend.mdm.impl.RecordImpl;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class TestRecord extends Login {
+	RecordImpl recordImpl;
+	@BeforeMethod
+	public void beforeMethod(){
+		recordImpl = new RecordImpl(driver);
+		logger.info("Set Before Info");
+	}
+		
+	@Test
+	@Parameters( { "container","modle","entity","subelement" })
+	public void testDeleteRecord(String container,String modle,String entity,String subelement) {
+		recordImpl.deleteRecordImpl(container,modle,entity,subelement);
+	}
+	@Test
+	@Parameters( { "container","modle","entity","subelement", "name", "age" })
+	public void testCreateRecord(String container,String modle,String entity,String subelement,String name,String age) {
+		recordImpl.createRecordImpl(container, modle, entity, subelement, name, age);
+	}
+	@Test
+	@Parameters( { "container","modle","entity","subelement", "name", "age" })
+	public void testUpdateRecord(String container,String modle,String entity,String subelement,String name,String age) {
+		recordImpl.updateRecordImpl(container, modle, entity, subelement, name, age);
+	}
+	
+}
