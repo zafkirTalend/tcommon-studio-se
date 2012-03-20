1489d6cc322e0faf2960ce98823508c9f5347d99
fyan2 <fyan2@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-20 08:28:04 +0000

    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@80121 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/org/talend/tac/modules/SamInformation.java
@@ -0,0 +1,56 @@
+package org.talend.tac.modules;
+
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasInputDevices;
+import org.openqa.selenium.Mouse;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.internal.Locatable;
+import org.talend.tac.base.WebDriverBase;
+
+import com.talend.tac.base.Karaf;
+
+public class SamInformation extends WebDriverBase{
+     public  SamInformation(WebDriver driver) {
+    	 super.setDriver(driver);
+         this.driver = driver;
+     }
+     
+     public void mouseDown(String xpathExpression) {
+  	   Locatable hoverItem = (Locatable) driver.findElement(By.xpath(xpathExpression));
+  	   Mouse mouse = ((HasInputDevices) driver).getMouse();
+  	   mouse.mouseDown(hoverItem.getCoordinates());
+  	   try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+     }
+     
+     public void installServiceAndConsumer(String karafURL,String providerName,String consumerName) { 
+    	 Karaf karaf=new Karaf(karafURL);
+    	 karaf.karafAction("start"+providerName+"control-bundle", WAIT_TIME_MAX);
+    	 karaf.karafAction("install -s file://"+getAbsolutePath("org/talend/tac/folder/esb/"+consumerName+"-0.1.jar")+"", WAIT_TIME_MAX);    	 
+    	 logger.info("install Service and Consumer");
+     }
+     
+     public void intoSamPage() {	 
+    	 this.waitforElementDisplayed(By.id("!!!menu.serviceactivity.element!!!"), WAIT_TIME_MAX);
+    	 this.clickElementById("!!!menu.serviceactivity.element!!!");
+    	 this.waitforTextDisplayed("Service Activity Monitoring",WAIT_TIME_MAX);
+    	 logger.info("intoSamPage");    	 
+     }
+     
+     public void locatorEvents() { 
+    	 this.waitforElementDisplayed(By.xpath("//div[@title='one-way']"), WAIT_TIME_MAX);
+    	 this.mouseDown("//div[@title='one-way']");
+    	 Assert.assertTrue(this.isElementPresent(By.xpath("//legend[text()='Consumer']//parent::fieldset[@class='samui-participant-details']"), WAIT_TIME_MAX));
+    	 Assert.assertTrue(this.isElementPresent(By.xpath("//legend[text()='Provider']//parent::fieldset[@class='samui-participant-details']"), WAIT_TIME_MAX));
+    	 Assert.assertTrue(this.isElementPresent(By.xpath("//fieldset[@class='samui-event-details-REQ_OUT']"), WAIT_TIME_MAX));
+    	 Assert.assertTrue(this.isElementPresent(By.xpath("//fieldset[@class='samui-event-details-REQ_IN']"), WAIT_TIME_MAX));
+    	 logger.info("locator and check events");
+     }
+     
+}
