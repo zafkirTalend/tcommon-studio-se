a02aa68484694ada53c8edbfe00143f358088000
fgzhang <fgzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-21 06:44:00 +0000

    selenium:TestProjectReference---testDeleteRefProDisplay()   --TDIQA-5344:projects display(auto) TestProjectReference---testRetrievePeoject()   --TDIQA-378:Retrieve(auto) TestProjectReference---testRemoveReference()   --TDIQA-382:Remove reference(auto)
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@80198 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.tac.test/src/main/java/org/talend/tac/cases/reference/TestProjectReference.java
@@ -0,0 +1,72 @@
+package org.talend.tac.cases.reference;
+
+import org.talend.tac.base.WebdriverLogin;
+import org.talend.tac.modules.impl.ReferenceImpl;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestProjectReference extends WebdriverLogin {
+   
+	
+	ReferenceImpl refernceImpl;
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		refernceImpl = new ReferenceImpl(driver);
+		
+	}
+	
+	@Test
+	@Parameters({"AddreferenceProjectname", "comm.pro.trunk", "ref.pro.info"})
+	public void testReference(String source, String target, String refInfo) {
+		
+		refernceImpl.referenceImpl(source, target, refInfo);
+		
+	}
+	
+	@Test
+	@Parameters({"type.DI", "cycle.ref.pro1", "cycle.ref.pro2", "cycle.ref.pro3"
+		, "cycle.ref.pro1.info", "cycle.ref.pro2.info", "cycle.ref.pro3.info"})
+	public void testCycleReferenceproRestriction(String typeDI, String cycleRefPro1, String cycleRefPro2
+			, String cycleRefPro3, String cycleRefPro1Info, String cycleRefPro2Info,
+			String cycleRefPro3Info) {
+		
+		refernceImpl.cycleRestrictionReferenceproImpl(typeDI, 1, cycleRefPro1, cycleRefPro2, cycleRefPro3, cycleRefPro1Info, cycleRefPro2Info, cycleRefPro3Info);
+		
+	}
+	
+	@Test
+	@Parameters({"remove.ref.pro1", "type.DI", "remove.ref.pro1.info", "spacepro.info"})
+	public void testRemoveReference(String removeRefPro1, String typeDI, String
+			removeRefPro1Info, String spaceproInfo) {
+		
+        refernceImpl.removeReferenceImpl(removeRefPro1, typeDI, 1,
+        		removeRefPro1, spaceproInfo, removeRefPro1Info);
+		
+	}
+	
+	@Test
+	@Parameters({"retrieve.ref.pro1", "AddreferenceProjectname", "spacepro.info"
+		, "ref.pro.info", "trunk", "retrieve.ref.pro1.info", "type.DI"})
+	public void testRetrievePeoject(String projectLabel, String refpro, 
+			String spacePro, String refProInfo, String trunk,
+			 String retrieveRefPro1Info, String typeDI) {
+		
+		refernceImpl.retrieveDisplayReferenceImpl(projectLabel, typeDI, 1,
+				trunk, typeDI, refpro, trunk, typeDI,
+				spacePro, retrieveRefPro1Info, refProInfo);
+		
+	}		
+	
+	@Test
+	@Parameters({"delete.ref.pro1", "type.DI", "spacepro.info", "del.ref.pro1.info"})
+	public void testDeleteRefProDisplay(String projectLabel, String typeDI, 
+			String spaceProInfo, String delRefPro1Info) {
+		
+		refernceImpl.deleteRefproDispalyReferenceImpl(projectLabel, typeDI, 1,
+				projectLabel, spaceProInfo, delRefPro1Info, 1);
+		
+	}
+	
+}
