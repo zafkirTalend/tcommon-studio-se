ce501c44df4a76c5775fa236dda19102e4af4145
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-28 02:51:02 +0000

    selenium:add cases of license and login for mdm
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@80472 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/cases/user/TestUser.java
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/cases/user/TestUser.java
@@ -1,8 +1,13 @@
 package org.talend.mdm.cases.user;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.talend.mdm.Login;
 import org.talend.mdm.impl.LicenseImpl;
+import org.talend.mdm.impl.LogonImpl;
 import org.talend.mdm.impl.UserImpl;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -11,8 +16,10 @@ import org.testng.annotations.Test;
 public class TestUser extends Login {
 	UserImpl userImpl;
 	LicenseImpl license;
+	 LogonImpl loger;
 	@BeforeMethod
 	public void beforeMethod(){
+		 loger = new LogonImpl(driver);
 		userImpl = new UserImpl(driver);
 		license = new LicenseImpl(driver);
 		logger.info("Set Before Info");
@@ -24,6 +31,14 @@ public class TestUser extends Login {
 	public void testAddUserAllowedAdmin(String roles) {
 		license.openLicense();
 		userImpl.addUserAllowed(roles, license.getAvailableAdmin());
+	}
+	
+	
+	@Test
+	@Parameters( { "identifier", "first.name", "last.name", "password", "confirm.password", "email", "company", "default.version", "active", "roles" })
+	public void testAddUserAdminOverAllowed(String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion,boolean active,String roles) {
+		license.openLicense();
+		userImpl.addUserOverAllowedAdmin(identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, active, roles,license.getAvailableWeb());
 	}
 	
 	@Test
@@ -58,6 +73,13 @@ public class TestUser extends Login {
 	
 	@Test
 	@Parameters( {"user.name","identifier", "first.name", "last.name", "password", "confirm.password", "email", "company", "default.version", "active", "roles" })
+	public void testUserSelectedAfterSave(String userNameAdministrator,String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion, boolean active,String roles) {
+		userImpl.userSelectedAfterSave(userNameAdministrator,identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, active, splitParameter(roles));
+	}
+	
+	
+	@Test
+	@Parameters( {"user.name","identifier", "first.name", "last.name", "password", "confirm.password", "email", "company", "default.version", "active", "roles" })
 	public void testaddUserWithMultipleRolesOneAllowedAnotherNot(String userNameAdministrator,String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion, boolean active,String roles) {
 		userImpl.addUserWithMultipleRolesOneAllowedAnotherNot(userNameAdministrator,identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, active, splitParameter(roles));
 	}
@@ -68,4 +90,37 @@ public class TestUser extends Login {
 	public void testAddUserWithPasswordNotEqualsConfirmPassword(String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion, boolean active,String roles) {
 		userImpl.addUserWithPasswordNotEqualsConfirmPassword(identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, active, splitParameter(roles));
 	}
+	
+	@Test
+	@Parameters( {"user.name","user.password" ,"identifier", "first.name", "last.name", "password", "confirm.password", "email", "company", "default.version", "active", "roles" })
+	public void testAddUserInactiveCheckLoginThenActive(String administrator,String adminPass,String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion, boolean active,String roles) {
+		userImpl.addUserInactive(administrator, adminPass, identifier,
+				firstName, lastName, password, confirmPassword, email, company,
+				defaultVersion, active, splitParameter(roles));
+		loger.logout();
+		loger.loginWithExistInactiveUserImpl(identifier, password, locator
+				.getString("login.exist.user.inactive.allert.message"));
+		loger.loginAdministrator(administrator, adminPass, locator
+				.getString("login.administrator.forcelogin.message"));
+		userImpl.activeAnUserInactive(identifier, firstName, lastName,
+				password, confirmPassword, email, company, defaultVersion,
+				active, splitParameter(roles));
+		loger.logout();
+		loger.loginAdministrator(identifier, password, locator
+				.getString("login.administrator.forcelogin.message"));
+ 
+	}
+	
+	@Test
+	@Parameters( {"identifier", "first.name", "last.name", "password", "confirm.password", "email", "company", "default.version", "active"})
+	public void testAddUserWithoutSelectRoles(String identifier,String firstName,String lastName,String password,String confirmPassword,String email,String company,String defaultVersion, boolean active) {
+		userImpl.addUserWithoutSelectRoles(identifier, firstName, lastName, password, confirmPassword, email, company, defaultVersion, active);
+	}
+	
+	@Test
+	@Parameters( {"url","user.name"})
+	public void testFlushCache(String url,String userName){
+		userImpl.flushCache(userName,url);
+	}
+	
 }
