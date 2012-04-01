8753769d18cf1fadf2b3da9f9d0eaba6f196fb74
zpbai <zpbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-01 06:39:28 +0000

    Selenium: restore org.talend.selenium.mdm.test
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@80827 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/cases/record/TestRecord.java
@@ -0,0 +1,77 @@
+package org.talend.mdm.cases.record;
+
+import org.talend.mdm.Login;
+import org.talend.mdm.impl.RecordImpl;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class TestRecord extends Login {
+	RecordImpl recordImpl;
+	@BeforeMethod
+	public void beforeMethod(){
+		recordImpl = new RecordImpl(driver);	
+		logger.info("Set Before Info");
+	}		
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value","feild1Name" })
+	public void testDeleteRecord(String container,String modle,String entity,String feild1Value,String feild1Name ) {
+		recordImpl.deleteRecordImpl(container,modle,entity,feild1Value,feild1Name);
+	}
+	
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value","feild1Name" })
+	public void testJournalOpenRecord(String container,String modle,String entity,String feild1Value,String feild1Name ) {
+		recordImpl.JournalOpenRecordImpl(container,modle,entity,feild1Value,feild1Name);
+	}
+	
+	
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value","feild1Name" })
+	public void testDeleteRecordToRecycle(String container,String modle,String entity,String feild1Value,String feild1Name ) {
+		recordImpl.deleteRecordToRecycleImpl(container,modle,entity,feild1Value,feild1Name);
+	}
+	
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value","feild1Name" })
+	public void testRestoreFromRecycle(String container,String modle,String entity,String feild1Value,String feild1Name ) {
+		recordImpl.restoreFromRecycle(container,modle,entity,feild1Value,feild1Name);
+	}		
+	
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value", "feild2Value", "feild3Value","feild1Name","feild2Name","feild3Name" })
+	public void testCreateRecord(String container,String modle,String entity,String feild1Value,String feild2Value,String feild3Value,String feild1Name,String feild2Name,String feild3Name) {
+		recordImpl.createRecordImpl(container, modle, entity, feild1Value, feild2Value, feild3Value,feild1Name,feild2Name,feild3Name);
+	}
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value", "feild2Value", "feild3Value","feild1Name","feild2Name","feild3Name","feild1UpdateValue" })
+	public void testDuplicateRecord(String container,String modle,String entity,String feild1Value,String feild2Value,String feild3Value,String feild1Name,String feild2Name,String feild3Name,String feild1UpdateValue) {
+		recordImpl.duplicateRecordImpl(container, modle, entity, feild1Value, feild2Value, feild3Value,feild1Name,feild2Name,feild3Name,feild1UpdateValue);
+	}
+	@Test
+	@Parameters( { "container","modle","entity","feild1Value", "feild2Value", "feild3Value","feild1Name","feild2Name","feild3Name"})
+	public void testUpdateRecord(String container,String modle,String entity,String feild1Value,String feild2Value,String feild3Value,String feild1Name,String feild2Name,String feild3Name) {
+		recordImpl.updateRecordImpl(container, modle, entity, feild1Value, feild2Value, feild3Value,feild1Name,feild2Name,feild3Name);
+	}
+	@Test
+	@Parameters( { "container","modle","entity","searchFeild", "opeartion", "value" })
+	public void testSearchRecordByValue(String container,String modle,String entity,String searchFeild,String opeartion,String value) {
+		recordImpl.SearchRecordByValueImpl(container, modle, entity, searchFeild,opeartion,value);
+	}
+	@Test
+	@Parameters( { "container","modle","entity","searchFeild", "opeartion", "value" })
+	public void testSearchRecordByString(String container,String modle,String entity,String searchFeild,String opeartion,String value) {
+		recordImpl.SearchRecordByStringImpl(container, modle, entity, searchFeild,opeartion,value);
+	}
+	
+	@Test
+	@Parameters( { "container","modle","entity","searchFeild", "opeartion", "value" })
+	public void testSearchRecordByDate(String container,String modle,String entity,String searchFeild,String opeartion,String value) {
+		recordImpl.SearchRecordByDateImpl(container, modle, entity, searchFeild,opeartion,value);
+	}
+	
+	
+	
+	
+}
---
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/impl/WelcomeImpl.java
@@ -0,0 +1,37 @@
+package org.talend.mdm.impl;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.talend.mdm.Base;
+import org.talend.mdm.modules.License;
+import org.talend.mdm.modules.User;
+import org.talend.mdm.modules.Welcome;
+import org.testng.Assert;
+
+
+public class WelcomeImpl extends Welcome{
+
+	public WelcomeImpl(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+
+	}
+	
+	public void reSortWindows(){
+		this.checkGetStartedWindow();
+		this.reSortWindowLayout("xpath.ui.window.getstarted.title",500,500);
+		this.checkAlertsWindow();
+		this.reSortWindowLayout("xpath.ui.window.alerts.title",500,500);
+		this.checkProcessWindow();
+		this.reSortWindowLayout("xpath.ui.window.processes.title", -800, 500);
+		this.checkSearchWindow();
+		this.reSortWindowLayout("xpath.ui.window.search.title", -800, 500);
+		this.checkTasksWindow();
+	}
+}
+
