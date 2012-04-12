8859cc73f8d71a307320469c1155ddf5f5815f73
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-12 05:58:51 +0000

    selenium:QAI-7909
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@81543 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/modules/WorkFlowTask.java
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/modules/WorkFlowTask.java
@@ -32,16 +32,21 @@ public class WorkFlowTask extends Base{
 
 	public void openMenuWorkFlowTask() {
 		
-		this.waitfor(By.id(locator.getString("id.menu.workflowtask")), WAIT_TIME_MIN);
-		this.clickElementById(locator.getString("id.menu.workflowtask"));
+		this.waitfor(By.id(locator.getString("id.menu.workflowtasks")), WAIT_TIME_MIN);
+		this.clickElementById(locator.getString("id.menu.workflowtasks"));
 		
+	}
+	
+	public void uncheckHideFinishedTask(){
+		this.clickElementByXpath("//label[text()='Hide Finished Tasks:']//ancestor::div[contains(@class,'x-form-item')]//div[contains(@class,'x-form-check-wrap-inner')]//img");
 	}
 	
 	public void sortWorkFlowTaskBydate(){
 		
 		this.moveToElement(driver.findElement(By.xpath("//div[text()='Ready Date']")));
 		this.clickElementByXpath("//div[contains(@class,'x-grid3-hd-inner x-grid3-hd-3')]//a[@class='x-grid3-hd-btn']");
-		
+		this.sleepCertainTime(2000);
+		this.clickElementByXpath("//a[text()='Sort Descending']");
 	}
 	
 	public void openAWorkTask(){
@@ -49,8 +54,16 @@ public class WorkFlowTask extends Base{
 	    this.doubleClick(this.getElementByXpath("//span[contains(@class,'x-panel-header-text') and text()='Workflow Tasks']//ancestor::div[contains(@id,'WorkflowTasksPanel')]//div[contains(@class,'x-grid3-body')]//div[contains(@class,'x-grid3-row ')][1]"));
 	}
 	
+	public void closeAWorkTask(){
+		this.clickElementByXpath("//span[contains(text(),'ApprouvePrix')]//ancestor::li//a[contains(@class,'x-tab-strip-close')]");
+	}
+	
 	public void openRelatedRecord(){
 		this.clickElementByXpath("//button[contains(@class,'wftask_bt_openRecord') and text()='Open Record']");
+	}
+	
+	public void closeRelatedRecord(String recordID){
+		this.clickElementByXpath(this.getString(locator, "xpath.workflowtask.openrelatedrecord.open.closeTab", recordID));
 	}
 	
 	public void approveBoxChecked(){
