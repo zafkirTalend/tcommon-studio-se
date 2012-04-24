b5dab630dbc221dcf0f03ba2e5899d7d64560005
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-24 08:49:16 +0000

    selenium:modify clickElementByXpath
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@82391 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/Base.java
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/Base.java
@@ -278,7 +278,7 @@ public class Base {
     	this.getElementByName(name).click();
     }
     public void clickElementByXpath(String xpath){
-    	Assert.assertTrue(this.waitfor(By.xpath(xpath), WAIT_TIME_MIN).isDisplayed());    
+    	Assert.assertTrue(this.waitfor(By.xpath(xpath), WAIT_TIME_MIN)!=null);    
     	this.driver.findElement(By.xpath(xpath)).click();
 //    	this.getElementByXpath(xpath).click();			
     	
