4c0978c2f47849d02206455ce5e370ac4d998c74
zpbai <zpbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-25 13:03:54 +0000

    Selenium MDM: set the timeout for Warning of Unresponsive script
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@82568 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/Login.java
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/Login.java
@@ -34,6 +34,9 @@ public class Login extends Base{
 	    firefoxProfile.setPreference("browser.download.manager.showWhenStarting",false);
 	    firefoxProfile.setPreference("browser.download.dir", this.getAbsoluteFolderPath("org/talend/mdm/download"));
 	    firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk","text/csv, application/vnd.ms-excel, application/zip, application/pdf");
+	    
+	    firefoxProfile.setPreference("dom.max_script_run_time", 60);
+	    
 
 	    driver = new FirefoxDriver(firefoxProfile);
 	    
