af0c7c7413167c5397b65cc81c9df736360c87d3
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-26 06:16:12 +0000

    Bug TDI-18366 fixed: Retrieve schema on hive connection throws null pointer exception
    git-svn-id: http://talendforge.org/svn/tos/trunk@82592 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/database/conn/version/EDatabaseVersion4Drivers.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/database/conn/version/EDatabaseVersion4Drivers.java
@@ -98,8 +98,8 @@ public enum EDatabaseVersion4Drivers {
 
     INGRES(new DbVersion4Drivers(EDatabaseTypeName.INGRES, "iijdbc.jar")), //$NON-NLS-1$
 
-    HIVE(new DbVersion4Drivers(EDatabaseTypeName.HIVE, new String[] { "hive-jdbc-0.7.0.jar", "hive-metastore-0.7.0.jar",
-            "hive-exec-0.7.0.jar", "hive-service-0.7.0.jar", "libfb303.jar", "hadoop-0.20.2-core.jar",
+    HIVE(new DbVersion4Drivers(EDatabaseTypeName.HIVE, new String[] { "hive-jdbc-0.7.1.jar", "hive-metastore-0.7.1.jar",
+            "hive-exec-0.7.1.jar", "hive-service-0.7.1.jar", "libfb303.jar", "hadoop-core-0.20.203.0.jar",
             "commons-logging-1.0.4.jar", "log4j-1.2.15.jar", "slf4j-api-1.6.1.jar", "slf4j-log4j12-1.6.1.jar" })),
 
     HBASE(new DbVersion4Drivers(EDatabaseTypeName.HBASE, new String[] { "hadoop-core-0.20.2-cdh3u0.jar",
