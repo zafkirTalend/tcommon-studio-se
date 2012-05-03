d0f646ea951c761982f0e39296df79df36a9cf45
zpbai <zpbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-03 01:47:11 +0000

    Selenium MDM: Enable Native Events of firefox profile
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@82741 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/Login.java
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/Login.java
@@ -38,6 +38,8 @@ public class Login extends Base{
 	    firefoxProfile.setPreference("dom.max_script_run_time", 0);
 	    firefoxProfile.setPreference("dom.max_chrome_script_run_time", 0);
 
+	    firefoxProfile.setEnableNativeEvents(true);
+	    
 	    driver = new FirefoxDriver(firefoxProfile);
 	    
 	    //set driver time out with TimeUnit
