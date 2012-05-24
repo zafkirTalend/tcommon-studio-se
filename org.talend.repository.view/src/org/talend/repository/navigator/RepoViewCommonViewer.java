e54840e4b9cc39e6e520bb45bed65b95013fe2a5
sgandon <sgandon@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-24 15:56:57 +0000

    Fix Bug TDI-21276 : Sometimes, the Folder icon will be disappeared after created a new folder http://jira.talendforge.org/browse/TDI-21276 Remove old unecessary code
    git-svn-id: http://talendforge.org/svn/tos/trunk@84122 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.view/src/org/talend/repository/navigator/RepoViewCommonViewer.java
+++ org.talend.repository.view/src/org/talend/repository/navigator/RepoViewCommonViewer.java
@@ -18,9 +18,7 @@ import java.util.Map;
 
 import org.eclipse.jface.util.LocalSelectionTransfer;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ITreeViewerListener;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.TreeExpansionEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DragSourceAdapter;
@@ -30,7 +28,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Item;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.internal.navigator.NavigatorActivationService;
 import org.eclipse.ui.navigator.CommonViewer;
 import org.eclipse.ui.navigator.INavigatorContentService;
@@ -46,7 +43,7 @@ import org.talend.repository.viewer.filter.RepositoryNodeFilterHelper;
  * $Id: talend.epf 55206 2011-02-15 17:32:14Z mhirt $
  * 
  */
-public class RepoViewCommonViewer extends CommonViewer implements ITreeViewerListener {
+public class RepoViewCommonViewer extends CommonViewer {
 
     private Map<String, Boolean> expanded = new HashMap<String, Boolean>();
 
@@ -98,71 +95,6 @@ public class RepoViewCommonViewer extends CommonViewer implements ITreeViewerLis
             repositoryNode = (RepositoryNode) data;
         }
         return repositoryNode;
-    }
-
-    @Override
-    protected boolean getExpanded(Item item) {
-        RepositoryNode repositoryNode = getRepositoryNode(item);
-        if (repositoryNode != null && repositoryNode.getId() != null) {
-            Boolean result = expanded.get(repositoryNode.getId());
-            if (result != null) {
-                if (item instanceof TreeItem) {
-                    TreeItem treeItem = (TreeItem) item;
-                    treeItem.setExpanded(result);
-                }
-            }
-        }
-        return super.getExpanded(item);
-    }
-
-    @Override
-    public void setExpandedState(Object elementOrTreePath, boolean expanded) {
-        if (expanded) {
-            internalExpand(elementOrTreePath);
-        } else {
-            internalCollapse(elementOrTreePath);
-        }
-        super.setExpandedState(elementOrTreePath, expanded);
-    }
-
-    @Override
-    public void treeCollapsed(TreeExpansionEvent event) {
-        Object element = event.getElement();
-        internalCollapse(element);
-    }
-
-    @Override
-    public void treeExpanded(TreeExpansionEvent event) {
-        Object element = event.getElement();
-        internalExpand(element);
-    }
-
-    private void internalCollapse(Object element) {
-        if (element instanceof RepositoryNode) {
-            RepositoryNode repositoryNode = (RepositoryNode) element;
-            if (idIsValid(repositoryNode)) {
-                expanded.put(repositoryNode.getId(), false);
-            }
-            emptyExpandedChildren(repositoryNode);
-        }
-    }
-
-    private void internalExpand(Object element) {
-        if (element instanceof RepositoryNode) {
-            RepositoryNode repositoryNode = (RepositoryNode) element;
-            if (idIsValid(repositoryNode)) {
-                expanded.put(repositoryNode.getId(), true);
-            }
-        }
-    }
-
-    private void emptyExpandedChildren(RepositoryNode repositoryNode) {
-        for (IRepositoryNode children : repositoryNode.getChildren()) {
-            if (idIsValid(children)) {
-                expanded.remove(children.getId());
-            }
-            emptyExpandedChildren((RepositoryNode) children);
-        }
     }
 
     private boolean idIsValid(IRepositoryNode repositoryNode) {
