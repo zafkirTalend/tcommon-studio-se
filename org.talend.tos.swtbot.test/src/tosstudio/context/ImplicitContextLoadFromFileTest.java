3597c3af79c6685c9cc5dd56c491e236e556e7ff
fzhong <fzhong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-15 08:46:37 +0000

    SWTBot DI: QAI-5286, TIS/TOS-1325:Implicit context load
    git-svn-id: http://talendforge.org/svn/tis_shared/branches/branch-5_0@85666 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.tos.swtbot.test/src/tosstudio/context/ImplicitContextLoadFromFileTest.java
@@ -0,0 +1,104 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package tosstudio.context;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditPart;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.swtbot.SWTBotTreeItemExt;
+import org.talend.swtbot.TalendSwtBotForTos;
+import org.talend.swtbot.Utilities;
+import org.talend.swtbot.helpers.JobHelper;
+import org.talend.swtbot.items.TalendJobItem;
+
+/**
+ * DOC fzhong class global comment. Detailled comment
+ */
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class ImplicitContextLoadFromFileTest extends TalendSwtBotForTos {
+
+    private TalendJobItem jobItem;
+
+    private static final String JOB_NAME = "jobTest";
+
+    @Before
+    public void initialisePrivateFields() {
+        repositories.add(ERepositoryObjectType.PROCESS);
+
+        jobItem = new TalendJobItem(JOB_NAME);
+        jobItem.create();
+    }
+
+    @Test
+    public void implicitContextLoadFromFileTest() throws IOException, URISyntaxException {
+        SWTBotGefEditor jobEditor = jobItem.getEditor();
+        String variableName = "source";
+        String variableValue = "local";
+
+        // create context for job
+        jobEditor.show();
+        gefBot.viewByTitle("Contexts(" + jobEditor.getTitle() + ")").show();
+        gefBot.cTabItem("Variables").activate();
+        gefBot.button(0).click(); // click add variable button
+        SWTBotTreeItemExt var = new SWTBotTreeItemExt(gefBot.tree(0).getTreeItem("new1").select());
+        var.click("Name");
+        gefBot.text().setText(variableName);
+        var.select();
+        gefBot.cTabItem("Values as table").activate();
+        var = new SWTBotTreeItemExt(gefBot.tree(0).getTreeItem(variableName).select());
+        var.click("Default");
+        gefBot.text().setText(variableValue);
+        var.select();
+
+        // use context in job
+        jobEditor.show();
+        Utilities.dndPaletteToolOntoJob(jobEditor, "tJava", new Point(100, 100));
+        SWTBotGefEditPart tJava = getTalendComponentPart(jobEditor, "tJava_1");
+        tJava.doubleClick();
+        gefBot.viewByTitle("Component").setFocus();
+        SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
+        gefBot.sleep(1000);
+        gefBot.styledText().selectCurrentLine();
+        String text = "System.out.println(context." + variableName + ");";
+        gefBot.styledText().typeText(text, 0);
+        jobEditor.save();
+
+        // implicit context load from file
+        gefBot.viewByTitle("Job(" + jobItem.getItemFullName() + ")").show();
+        selecteAllTalendTabbedPropertyListIndex(1);
+        gefBot.checkBox("Use Project Settings").deselect();
+        gefBot.checkBox("Implicit tContextLoad").select();
+        gefBot.radio("From File").click();
+        gefBot.sleep(1000);
+        gefBot.text(1)
+                .selectAll()
+                .typeText(
+                        "\"" + Utilities.getFileFromCurrentPluginSampleFolder("context.txt").getAbsolutePath().replace("\\", "/")
+                                + "\"", 0); // gefBot.textWithLabel("Field Separator")
+        gefBot.text(2).selectAll().typeText("\";\"", 0); // gefBot.textWithLabel("Field Separator")
+        JobHelper.runJob(jobEditor);
+        String result = JobHelper.execResultFilter(JobHelper.getExecutionResult());
+        Assert.assertEquals("get wrong context value from file", "file", result);
+    }
+
+}
