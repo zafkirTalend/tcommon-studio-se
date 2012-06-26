dde245bec201223454fb8ef248ead201dc9e4480
gliu <gliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-26 01:01:40 +0000

    [TESB-6260] fixed : there are some bugs when use "edit denpendencies"
    git-svn-id: http://talendforge.org/svn/tos/trunk@86285 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/IOsgiDependenciesService.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/IOsgiDependenciesService.java
@@ -15,5 +15,5 @@ public interface IOsgiDependenciesService extends IService{
 	 * key is {@link #IMPORT_PACKAGE} {@link #REQUIRE_BUNDLE} {@link #BUNDLE_CLASSPATH}
 	 * @return
 	 */
-	public Map<String, String> getBundleDependences(ProcessItem pi, EMap additionProperties);
+	public Map<String, String> getBundleDependences(ProcessItem pi, EMap<?, ?> additionProperties);
 }
