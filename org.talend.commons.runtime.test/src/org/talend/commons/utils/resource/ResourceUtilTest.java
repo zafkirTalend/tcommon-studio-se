dbf3aefdf13a12c28efc2c028b901474b6309906
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-03 14:40:06 +0000

    merge r86679 from trunk to branch 5.1 TDQ-5739 created an utility method for getting the path in the test plugins
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_1@86681 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.commons.runtime.test/src/org/talend/commons/utils/resource/ResourceUtilTest.java
@@ -0,0 +1,54 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons.utils.resource;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * created by scorreia on Jul 3, 2012 Detailled comment
+ * 
+ */
+public class ResourceUtilTest {
+
+    /**
+     * Test method for {@link org.talend.commons.utils.resource.ResourceUtil#convertResourceToFile(java.net.URL)}.
+     */
+    @Ignore("This test should be implemented later")
+    @Test
+    public void testConvertResourceToFile() {
+        fail("Not yet implemented"); //$NON-NLS-1$
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.commons.utils.resource.ResourceUtil#getFileFromResource(java.lang.Class, java.lang.String)}.
+     * 
+     * @throws URISyntaxException
+     * @throws IOException
+     */
+    @Test
+    public void testGetFileFromResource() throws IOException, URISyntaxException {
+        String filename = "/data/TextFile.txt"; //$NON-NLS-1$
+        File file = ResourceUtil.getFileFromResource(this.getClass(), filename);
+        Assert.assertNotNull(file);
+    }
+
+}
