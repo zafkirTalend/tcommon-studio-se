ef328a11573465f427813d4f629989ad8b43e183
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-03 14:34:25 +0000

    TDQ-5739 created an utility method for getting the path in the test plugins
    git-svn-id: http://talendforge.org/svn/tos/trunk@86679 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.runtime/src/org/talend/commons/utils/resource/ResourceUtil.java
+++ org.talend.commons.runtime/src/org/talend/commons/utils/resource/ResourceUtil.java
@@ -26,7 +26,7 @@ import org.eclipse.core.runtime.URIUtil;
  */
 public class ResourceUtil {
 
-    private static final String BUNDLERESOURCE = "bundleresource";
+    private static final String BUNDLERESOURCE = "bundleresource"; //$NON-NLS-1$
 
     public static File convertResourceToFile(URL resource) throws IOException, URISyntaxException {
         File fileDir = null;
@@ -40,4 +40,24 @@ public class ResourceUtil {
         return fileDir;
     }
 
+    /**
+     * Method "getFileFromResource" returns a File from the given path relative to the class. When the path starts with
+     * "/", the file can be located in a plugin (in the classpath of the plugin). This method must be used in plugin
+     * mode. It will return null when it's called as a java application.
+     * 
+     * @param clazz a class in the same plugin as the file to find
+     * @param path the path of the file.
+     * @return the file when found. Can return null. Otherwise will throw an exception
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public static File getFileFromResource(Class<?> clazz, String path) throws IOException, URISyntaxException {
+        URL url = clazz.getResource(path);
+        if (url == null) {
+            return null;
+        }
+        URL fileURL = FileLocator.toFileURL(url);
+        URI escapedUri = new URI(fileURL.getProtocol(), fileURL.getPath(), fileURL.getQuery());
+        return new File(escapedUri);
+    }
 }
