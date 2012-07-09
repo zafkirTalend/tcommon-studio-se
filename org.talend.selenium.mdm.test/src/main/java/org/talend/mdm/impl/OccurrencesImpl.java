21681e84561b85cd8ab84b66089795d10206d4ec
zcui <zcui@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-09 05:36:33 +0000

    selenium MDM:QAI-12164 ,update cases of occurrences
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@87011 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/impl/OccurrencesImpl.java
+++ org.talend.selenium.mdm.test/src/main/java/org/talend/mdm/impl/OccurrencesImpl.java
@@ -35,8 +35,7 @@ public class OccurrencesImpl extends Record{
 		chooseEntity(entity);	
 		maxDataBrowserBoard();
 		this.sleepCertainTime(6000);
-		int recordCount = Integer
-				.parseInt(this.getElementByXpath(locator.getString("xpath.record.search.record.count")).getText().split("of")[1].trim());
+		int recordCount = this.getTotalRecordNum();
 		logger.warn("before create ,total record num is:"+recordCount);
 		this.clickCreateRecord();
 		if(booLean){
@@ -90,8 +89,7 @@ public class OccurrencesImpl extends Record{
 	    
 	    this.clickElementByXpath(locator.getString("xpath.record.choose.create.input.save"));
 	    this.sleepCertainTime(10000);
-	    int recordCountAfter = Integer
-				.parseInt(this.getElementByXpath(locator.getString("xpath.record.search.record.count")).getText().split("of")[1].trim());
+	    int recordCountAfter = this.getTotalRecordNum();
 		logger.warn("after create ,total record num is:"+recordCountAfter);
 		Assert.assertTrue(recordCountAfter-recordCount==1);
 		
@@ -137,7 +135,7 @@ public class OccurrencesImpl extends Record{
 	    this.clickElementByXpath("//input[@name='Occurrences/enum2']//ancestor::tr//img[contains(@id,'Add')]");
 	    this.sleepCertainTime(3000);
 	    Assert.assertTrue(this.getElementsByXpath("//input[@name='Occurrences/enum2']").size()==2);
-	    this.seletDropDownList(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[19]//img[contains(@class,'x-form-trigger x-form-trigger-arrow')]"), enu2);
+	    this.seletDropDownList(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[19]//img[contains(@class,'x-form-trigger x-form-trigger-arrow')]"), enu2);
 	   
 	    Assert.assertTrue(this.getElementsByXpath("//input[@name='Occurrences/father2']").size()==1);
 	    this.clickElementByXpath("//input[@name='Occurrences/father2']//ancestor::tr//img[contains(@id,'Add')]");
@@ -150,35 +148,50 @@ public class OccurrencesImpl extends Record{
 	    Assert.assertTrue(this.getElementsByXpath("//div[text()='son2']").size()==2);
 	    
 	    //fill father2 just created
-	    Assert.assertEquals(this.getValueInput(By.xpath(" //div[contains(@id,'TreeDetail-root')]/div/div[24]//input[@name='Occurrences/father2']")), fater2Type);
-		this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[24]/table/tbody/tr/td[1]//img");
-		this.typeTextByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[24]//input[@name='Occurrences/father2/Name']", father2Name);
-		this.typeTextByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[24]//input[@name='Occurrences/father2/Address']", father2Add);
+	    Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[23]//input[@name='Occurrences/father2']")), fater2Type);
+		this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[23]/table/tbody/tr/td[1]//img");
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[23]//input[@name='Occurrences/father2/Name']", father2Name);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[23]//input[@name='Occurrences/father2/Address']", father2Add);
+	    
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]//input[@name='Occurrences/father2']")), fater2Type);
+		this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]/table/tbody/tr/td[1]//img");
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]//input[@name='Occurrences/father2/Name']", father2Name);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]//input[@name='Occurrences/father2/Address']", father2Add);
 		
 		//fill son2 just create
-		this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]/table/tbody/tr/td[1]//img");
+		this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[26]/table/tbody/tr/td[1]//img");
 		this.sleepCertainTime(3000);
-		this.typeTextByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]//input[@name='Occurrences/son2/Name']", son2Name);
-		this.typeTextByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]//input[@name='Occurrences/son2/Address']", son2Add);
-		this.typeTextByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]//input[@name='Occurrences/son2/School']", son2School);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[26]//input[@name='Occurrences/son2/Name']", son2Name);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[26]//input[@name='Occurrences/son2/Address']", son2Add);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[26]//input[@name='Occurrences/son2/School']", son2School);
+		 
+		
+		this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]/table/tbody/tr/td[1]//img");
+		this.sleepCertainTime(3000);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]//input[@name='Occurrences/son2/Name']", son2Name);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]//input[@name='Occurrences/son2/Address']", son2Add);
+		this.typeTextByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]//input[@name='Occurrences/son2/School']", son2School);
 		    
 		this.clickElementByXpath(locator.getString("xpath.record.choose.create.input.save"));
 		this.sleepCertainTime(20000);
-	
+		
+		Assert.assertTrue(this.getElementsByXpath("//input[@name='Occurrences/father2']").size()==2);
+		Assert.assertTrue(this.getElementsByXpath("//div[text()='son2']").size()==2);
+		
 		//verify enu2 just created
-		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[19]//input")), enu2);
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[19]//input")), enu2);
 			
 		//verify father2 just created
-		this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[24]/table/tbody/tr/td[1]//img");
-		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[24]//input[@name='Occurrences/father2/Name']")), father2Name);
-		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[24]//input[@name='Occurrences/father2/Address']")), father2Add);
+		this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]/table/tbody/tr/td[1]//img");
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]//input[@name='Occurrences/father2/Name']")), father2Name);
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[24]//input[@name='Occurrences/father2/Address']")), father2Add);
 		
 		//verify son2 just created
-		this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]/table/tbody/tr/td[1]//img");
+		this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]/table/tbody/tr/td[1]//img");
 		this.sleepCertainTime(3000);
-		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]//input[@name='Occurrences/son2/Name']")), son2Name);
-		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]//input[@name='Occurrences/son2/Address']")), son2Add);
-		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-root')]/div/div[27]//input[@name='Occurrences/son2/School']")), son2School);
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]//input[@name='Occurrences/son2/Name']")), son2Name);
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]//input[@name='Occurrences/son2/Address']")), son2Add);
+		Assert.assertEquals(this.getValueInput(By.xpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[27]//input[@name='Occurrences/son2/School']")), son2School);
 		  
 	}	
 	
@@ -190,8 +203,7 @@ public class OccurrencesImpl extends Record{
 		chooseEntity(entity);	
 		maxDataBrowserBoard();
 		this.sleepCertainTime(6000);
-		int recordCount = Integer
-				.parseInt(this.getElementByXpath(locator.getString("xpath.record.search.record.count")).getText().split("of")[1].trim());
+		int recordCount = this.getTotalRecordNum(); 
 		logger.warn("before create ,total record num is:"+recordCount);
 		this.clickCreateRecord();
 		if(booLean){
@@ -247,9 +259,8 @@ public class OccurrencesImpl extends Record{
 	    Assert.assertTrue(this.waitfor(By.xpath(locator.getString("xpath.occurrences.create.notinputallfieldsrequired.warning.info")),WAIT_TIME_MIN).isDisplayed());
 	    this.clickElementByXpath(locator.getString("xpath.occurrences.create.notinputallfieldsrequired.warning.ok.button"));
 	    this.sleepCertainTime(10000);
-	    int recordCountAfter = Integer
-				.parseInt(this.getElementByXpath(locator.getString("xpath.record.search.record.count")).getText().split("of")[1].trim());
-		logger.warn("after create ,total record num is:"+recordCountAfter);
+	    int recordCountAfter = this.getTotalRecordNum();
+	    logger.warn("after create ,total record num is:"+recordCountAfter);
 		Assert.assertTrue(recordCountAfter-recordCount==0);
 		
 	}
@@ -274,11 +285,11 @@ public class OccurrencesImpl extends Record{
 		Assert.assertEquals(this.getValueInput(By.xpath("//input[@name='Occurrences/datetime']")), dataTime);
 		Assert.assertEquals(this.getValueInput(By.xpath("//input[@name='Occurrences/datetime2']")),dataTime2);
 	
-	    //click '+' on 'enum2', 'father2', 'son2' fields, fill all the fields, save it
+	    //click '-' on 'enum2', 'father2', 'son2' fields, fill all the fields, save it
 	    Assert.assertTrue(this.getElementsByXpath("//input[@name='Occurrences/enum2']").size()==2);
 	    
 	    //remove an occurrence of enu2
-	    this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[19]//img[@id='Remove']");
+	    this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[19]//img[@id='Remove']");
 	    Assert.assertTrue(this.waitfor(By.xpath("//span[contains(text(),'Do you really want to delete the selected record?')]"), WAIT_TIME_MIN).isDisplayed());	   
 	    this.clickElementByXpath("//span[contains(text(),'Confirmation')]//ancestor::div[contains(@class,'x-window-plain x-window-dlg x-window x-component')]//button[contains(text(),'Yes')]");
 	    this.sleepCertainTime(3000);
@@ -286,7 +297,7 @@ public class OccurrencesImpl extends Record{
 	   
 	  //remove an occurrence of father2
 	    Assert.assertTrue(this.getElementsByXpath("//input[@name='Occurrences/father2']").size()==2);
-	    this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[23]//input[@name='Occurrences/father2']//ancestor::tr[1]//img[contains(@id,'Remove')]");
+	    this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[23]//input[@name='Occurrences/father2']//ancestor::tr[1]//img[contains(@id,'Remove')]");
 	    Assert.assertTrue(this.waitfor(By.xpath("//span[contains(text(),'Do you really want to delete the selected record?')]"), WAIT_TIME_MIN).isDisplayed());	   
 	    this.clickElementByXpath("//span[contains(text(),'Confirmation')]//ancestor::div[contains(@class,'x-window-plain x-window-dlg x-window x-component')]//button[contains(text(),'Yes')]");
 	    this.sleepCertainTime(3000);
@@ -294,13 +305,14 @@ public class OccurrencesImpl extends Record{
 	    
 	  //remove an occurrence of son2
 	    Assert.assertTrue(this.getElementsByXpath("//div[text()='son2']").size()==2);
-	    this.clickElementByXpath("//div[contains(@id,'TreeDetail-root')]/div/div[25]//div[contains(text(),'son2')]//ancestor::tr[1]//img[contains(@id,'Remove')]");
+	    this.clickElementByXpath("//div[contains(@id,'TreeDetail-tree')]/div[2]/div/div[25]//div[contains(text(),'son2')]//ancestor::tr[1]//img[contains(@id,'Remove')]");
 	    Assert.assertTrue(this.waitfor(By.xpath("//span[contains(text(),'Do you really want to delete the selected record?')]"), WAIT_TIME_MIN).isDisplayed());	   
 	    this.clickElementByXpath("//span[contains(text(),'Confirmation')]//ancestor::div[contains(@class,'x-window-plain x-window-dlg x-window x-component')]//button[contains(text(),'Yes')]");
 	    this.sleepCertainTime(3000);
 	    Assert.assertTrue(this.getElementsByXpath("//div[text()='son2']").size()==1);
 	    
 		this.clickElementByXpath(locator.getString("xpath.record.choose.create.input.save"));
+		Assert.assertTrue(this.waitfor(By.xpath(locator.getString("xpath.record.save.success.warning.info")), WAIT_TIME_MIN).isDisplayed());
 		this.sleepCertainTime(10000);
 	
 		Assert.assertTrue(this.getElementsByXpath("//input[@name='Occurrences/enum2']").size()==1);
