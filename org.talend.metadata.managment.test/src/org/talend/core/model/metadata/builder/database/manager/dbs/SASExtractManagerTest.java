d310a001aab0e70b44558e6315e31ece29e782a7
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-12 03:51:27 +0000

    TDI-21723: add the basic test for ExtractManager
    git-svn-id: http://talendforge.org/svn/tos/trunk@87261 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.metadata.managment.test/src/org/talend/core/model/metadata/builder/database/manager/dbs/SASExtractManagerTest.java
@@ -0,0 +1,47 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.metadata.builder.database.manager.dbs;
+
+import java.sql.Connection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.core.database.EDatabaseTypeName;
+
+/**
+ * created by ggu on Jul 11, 2012 Detailled comment
+ * 
+ */
+public class SASExtractManagerTest extends AbstractExtractmanagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+        init(EDatabaseTypeName.SAS);
+    }
+
+    @Override
+    @Test
+    public void testReturnColumns4DontCreateConnection() throws Exception {
+        // super.testReturnColumns4DontCreateConnection();
+        PTODO();
+    }
+
+    @Override
+    protected Connection mockConnection4ReturnColumns() throws Exception {
+        Connection conn = super.mockConnection4ReturnColumns();
+        // need care the FakeDatabaseMetaData for SAS
+        // PTODO
+        return conn;
+    }
+
+}
