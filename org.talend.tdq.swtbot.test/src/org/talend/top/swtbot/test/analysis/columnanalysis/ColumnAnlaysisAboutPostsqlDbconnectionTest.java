576c07d6403c8c30443541b933b6a877ab1ce2c5
yhbai <yhbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-13 08:28:43 +0000

    SWTBOT--TDQ：add the following cases about  QAI-9205  unfold all kinds of dbconnections to columns QAI-9185  QAI-9186 QAI-9187 QAI-9188 QAI-9193  QAI-9194  QAI-9195 QAI-9196  QAI-12182
    git-svn-id: http://talendforge.org/svn/tis_shared/trunk@87397 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.tdq.swtbot.test/src/org/talend/top/swtbot/test/analysis/columnanalysis/ColumnAnlaysisAboutPostsqlDbconnectionTest.java
@@ -0,0 +1,98 @@
+package org.talend.top.swtbot.test.analysis.columnanalysis;
+
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swtbot.swt.finder.matchers.WidgetOfType;
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.TimeoutException;
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.swtbot.test.commons.ContextMenuHelper;
+import org.talend.swtbot.test.commons.TalendSwtbotForTdq;
+import org.talend.swtbot.test.commons.TalendSwtbotTdqCommon;
+import org.talend.swtbot.test.commons.TalendSwtbotTdqCommon.TalendAnalysisTypeEnum;
+import org.talend.swtbot.test.commons.TalendSwtbotTdqCommon.TalendMetadataTypeEnum;
+
+public class ColumnAnlaysisAboutPostsqlDbconnectionTest extends TalendSwtbotForTdq{
+	
+	@Before
+	public void beforeClass(){
+		TalendSwtbotTdqCommon.createConnection(bot,
+				TalendMetadataTypeEnum.POSTGRESQL);
+		bot.editorByTitle(TalendMetadataTypeEnum.POSTGRESQL.toString()+" 0.1").close();
+		TalendSwtbotTdqCommon
+				.createAnalysis(bot, TalendAnalysisTypeEnum.COLUMN);
+		
+	}
+	
+	@Test
+	public void ColumnAnlaysisAboutPostsqlDbconnection(){
+		
+		String item = bot.tree().expandNode("Metadata","DB connections","POSTGRESQL", "shenze","optest","Tables (0)","test","Columns (2)").getNodes().get(1);
+		System.out.println("*******"+item+"********");
+		bot.editorByTitle(TalendAnalysisTypeEnum.COLUMN.toString() + " 0.1")
+				.show();
+		formBot.hyperlink("Select columns to analyze").click();
+		bot.waitUntil(Conditions.shellIsActive("Column Selection"));
+		SWTBotTree tree = new SWTBotTree((Tree) bot.widget(WidgetOfType
+				.widgetOfType(Tree.class)));
+		tree.expandNode("shenze").getNode("optest").expand().getNode(0).expand().select("test");
+		bot.table().getTableItem(item).check();
+		bot.button("OK").click();
+		formBot.ccomboBox(1).setSelection("Nominal");
+		bot.toolbarButtonWithTooltip("Save").click();
+		formBot.hyperlink("Select indicators for each column").click();
+		bot.waitUntil(Conditions.shellIsActive("Indicator Selection"));
+
+		tree = new SWTBotTree((Tree) bot.widget(WidgetOfType
+				.widgetOfType(Tree.class)));
+		tree.getTreeItem("Simple Statistics").click(1);
+		bot.checkBox().click();
+		bot.button("OK").click();
+		bot.toolbarButtonWithTooltip("Run").click();
+
+		try {
+			SWTBotShell shell = bot.shell("Run Analysis");
+			bot.waitUntil(Conditions.shellCloses(shell),180000);
+		} catch (TimeoutException e) {
+		}
+		
+		bot.viewByTitle("DQ Repository").setFocus();
+		tree = new SWTBotTree((Tree) bot.widget(
+				WidgetOfType.widgetOfType(Tree.class),
+				bot.viewByTitle("DQ Repository").getWidget()));
+		tree.expandNode("Metadata","DB connections",TalendMetadataTypeEnum.POSTGRESQL.toString(),"shenze","optest").
+		getNode(0).select();
+		ContextMenuHelper.clickContextMenu(tree, "Reload table list");
+		bot.waitUntil(Conditions.shellIsActive("Confirm reload"));
+	
+		bot.button("OK").click();
+		SWTBotShell shell = bot.shell("Progress Information");
+		bot.waitUntil(Conditions.shellIsActive("Analyzed element changed"));
+		bot.button("OK").click();
+	
+		bot.waitUntil(Conditions.shellCloses(shell));
+		tree.expandNode("Metadata","DB connections",TalendMetadataTypeEnum.POSTGRESQL.toString(),"shenze","optest").
+		getNode(0).expand().getNode("test").expand().getNode(0).select();
+		ContextMenuHelper.clickContextMenu(tree, "Reload column list");
+		bot.waitUntil(Conditions.shellIsActive("Confirm reload"));
+		bot.button("OK").click();
+		bot.waitUntil(Conditions.shellIsActive("Analyzed element changed"));
+		bot.button("OK").click();
+		bot.waitUntil(Conditions.shellCloses(shell));
+		bot.editorByTitle(TalendAnalysisTypeEnum.COLUMN.toString() + " 0.1").show();
+		bot.toolbarButtonWithTooltip("Run").click();
+
+		try {
+			SWTBotShell shell1 = bot.shell("Run Analysis");
+			bot.waitUntil(Conditions.shellCloses(shell1),180000);
+		} catch (TimeoutException e) {
+		}
+		bot.editorByTitle(TalendAnalysisTypeEnum.COLUMN.toString()+" 0.1").close();
+		
+	}
+		
+	
+
+}
