9bb477dc0e340be2cbfa53045d1e3c327279112b
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-13 02:50:34 +0000

    TDQ-5693 Fix an error "FileNotFoundException" ... TDQ-5721 Rename sql file, and rename sql folder modify:when rename a folder, change the path to relevent one. do not contain parent path like "TDQ_libraries/Source Files"
    git-svn-id: http://talendforge.org/svn/tos/trunk@87361 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.localprovider/src/main/java/org/talend/repository/localprovider/model/LocalRepositoryFactory.java
+++ org.talend.repository.localprovider/src/main/java/org/talend/repository/localprovider/model/LocalRepositoryFactory.java
@@ -156,6 +156,7 @@ import org.talend.repository.localprovider.i18n.Messages;
 import org.talend.repository.model.ERepositoryStatus;
 import org.talend.repository.model.IProxyRepositoryFactory;
 import org.talend.repository.model.RepositoryConstants;
+
 import orgomg.cwm.foundation.businessinformation.BusinessinformationPackage;
 
 /**
@@ -2713,8 +2714,11 @@ public class LocalRepositoryFactory extends AbstractEMFRepositoryFactory impleme
     private IPath getItemStatePath(ERepositoryObjectType type, IPath targetPath) {
         IPath itemStatePath = targetPath;
         if (ERepositoryObjectType.TDQ_PATTERN_REGEX.equals(type) || ERepositoryObjectType.TDQ_PATTERN_SQL.equals(type)
-                || ERepositoryObjectType.TDQ_USERDEFINE_INDICATORS.equals(type)) {
+                || ERepositoryObjectType.TDQ_USERDEFINE_INDICATORS.equals(type)
+                || ERepositoryObjectType.TDQ_SOURCE_FILE_ELEMENT.equals(type)) {
             itemStatePath = Path.fromOSString(type.getFolder()).removeFirstSegments(2).append(targetPath);
+        } else if (targetPath.toOSString().startsWith(ERepositoryObjectType.TDQ_SOURCE_FILE_ELEMENT.getFolder())) {
+            itemStatePath = targetPath.removeFirstSegments(2);
         }
         return itemStatePath;
     }
