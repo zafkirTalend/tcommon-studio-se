efb73d384156d9f33ac68aac65defe38ebc61ceb
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-23 02:45:15 +0000

    feature TDI-21417 added : Studio : Migration process should be refactored to allow different edition to work together
    git-svn-id: http://talendforge.org/svn/tos/trunk@87867 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.commons.runtime.test/src/org/talend/commons/utils/VersionUtilsTest.java
@@ -0,0 +1,32 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons.utils;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * DOC ycbai class global comment. Detailled comment
+ */
+public class VersionUtilsTest {
+
+    /**
+     * Test method for {@link org.talend.commons.utils.VersionUtils#getTalendVersion()}.
+     */
+    @Test
+    public void testGetTalendVersion() {
+        Assert.assertEquals(VersionUtils.getVersion(), VersionUtils.getTalendVersion());
+    }
+
+}
