8cfac5d2c397eb073a63452a8952344d12a8f798
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-22 10:48:03 +0000

    TDQ-5837 TDI-23300 - revert commit: 80747 - Use a new method to retrieve TdColumn list in ColumnHelper intead of using a deprecated method. - Check the db column name again for all the Node of currect process before persistence.
    git-svn-id: http://talendforge.org/svn/tos/trunk@92587 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/MetadataToolHelper.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/MetadataToolHelper.java
@@ -241,7 +241,7 @@ public final class MetadataToolHelper {
             for (int i = 0; i < originalColumnName.length(); i++) {
                 Character car = originalColumnName.charAt(i);
                 // MOD yyin TDQ-4929 20120330
-                if (car.toString().getBytes().length == 1 && !isAllowSpecific) {
+                if (car.toString().getBytes().length == 1 || !isAllowSpecific) {
                     // first character should have only a-z or A-Z or _
                     // other characters should have only a-z or A-Z or _ or 0-9
                     if (((car >= 'a') && (car <= 'z')) || ((car >= 'A') && (car <= 'Z')) || car == '_'
