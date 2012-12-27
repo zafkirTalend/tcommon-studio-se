d895657bef3efcdb961dda7c969e43452dcb1365
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-27 07:24:55 +0000

    TDQ-6485 fix: change the schema and after click checking , the reload did not popup which should be
    git-svn-id: http://talendforge.org/svn/tos/trunk@96004 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
+++ org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
@@ -2329,6 +2329,8 @@ public class DatabaseForm extends AbstractForm {
         if (originalURL != null) {
             if (!originalURL.equalsIgnoreCase(urlConnectionStringText.getText())) {
                 ConnectionHelper.setIsConnNeedReload(getConnection(), Boolean.TRUE);
+            } else if (!originalUischema.equalsIgnoreCase(schemaText.getText())) {
+                ConnectionHelper.setIsConnNeedReload(getConnection(), Boolean.TRUE);
             } else {
                 ConnectionHelper.setIsConnNeedReload(getConnection(), this.originalIsNeedReload);
             }
