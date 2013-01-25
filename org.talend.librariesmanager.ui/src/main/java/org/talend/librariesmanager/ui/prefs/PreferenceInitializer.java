f9b72490984f46c5c000dd50c145cba02d49203a
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-25 02:41:38 +0000

    bug  TUP-382: component librairies factorisation : Use component imports to look for libraries.
    git-svn-id: http://talendforge.org/svn/tos/trunk@97224 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/ui/prefs/PreferenceInitializer.java
@@ -0,0 +1,18 @@
+package org.talend.librariesmanager.ui.prefs;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.talend.librariesmanager.prefs.PreferencesUtilities;
+
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+    @Override
+    public void initializeDefaultPreferences() {
+        IPreferenceStore store = PreferencesUtilities.getPreferenceStore();
+        store.setDefault(PreferencesUtilities.EXTERNAL_LIB_PATH, Platform.getInstallLocation().getURL().getFile()
+                + "lib"); //$NON-NLS-1$
+        store.setDefault(PreferencesUtilities.EXTERNAL_LIB_PATH_MODE_SINGLE, true);
+    }
+
+}
---
+++ org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/ui/service/RoutineProviderManager.java
@@ -0,0 +1,84 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.librariesmanager.ui.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.routines.IRoutineProviderCreator;
+import org.talend.core.model.routines.IRoutinesProvider;
+import org.talend.librariesmanager.ui.i18n.Messages;
+
+/**
+ * DOC bqian class global comment. Detailled comment
+ */
+public class RoutineProviderManager {
+
+    private static Logger log = Logger.getLogger(RoutineProviderManager.class);
+
+    private static final RoutineProviderManager INSTANCE = new RoutineProviderManager();
+
+    private Collection<IRoutineProviderCreator> providerCreators;
+
+    private RoutineProviderManager() {
+    }
+
+    public static RoutineProviderManager getInstance() {
+        return INSTANCE;
+    }
+
+    public Collection<IRoutinesProvider> getProviders(ECodeLanguage lan) {
+        if (providerCreators == null) {
+            loadRoutinesProvidersFromExtension();
+        }
+
+        List<IRoutinesProvider> providers = new ArrayList<IRoutinesProvider>();
+        for (IRoutineProviderCreator creator : providerCreators) {
+            IRoutinesProvider provider = creator.createIRoutinesProviderByLanguage(lan);
+            if (provider != null) {
+                providers.add(provider);
+            }
+        }
+
+        return providers;
+    }
+
+    private void loadRoutinesProvidersFromExtension() {
+        providerCreators = new ArrayList<IRoutineProviderCreator>();
+
+        IExtensionRegistry extensionRegistry = Platform.getExtensionRegistry();
+        IExtensionPoint extensionPoint = extensionRegistry.getExtensionPoint("org.talend.core.routines_provider"); //$NON-NLS-1$
+        IExtension[] extensions = extensionPoint.getExtensions();
+        for (IExtension extension : extensions) {
+            IConfigurationElement[] configurationElements = extension.getConfigurationElements();
+            for (IConfigurationElement configurationElement : configurationElements) {
+                try {
+                    IRoutineProviderCreator componentsProviderCreator = (IRoutineProviderCreator) configurationElement
+                            .createExecutableExtension("class"); //$NON-NLS-1$
+                    providerCreators.add(componentsProviderCreator);
+                } catch (CoreException e) {
+                    log.error(Messages.getString("RoutineProviderManager.unableLoad"), e); //$NON-NLS-1$
+                }
+            }
+        }
+    }
+}
