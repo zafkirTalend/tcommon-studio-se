510c936a5d3afc9b78a89a58c83b2e2742f0edeb
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-03 00:14:15 +0000

    merge r100434 from trunk to branch 5.2
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@100516 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.registration/src/main/java/org/talend/repository/ui/wizards/license/LicenseWizardPage.java
+++ org.talend.registration/src/main/java/org/talend/repository/ui/wizards/license/LicenseWizardPage.java
@@ -19,16 +19,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Resource;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.exception.PersistenceException;
-import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.ui.branding.IBrandingService;
 import org.talend.repository.i18n.Messages;
@@ -43,6 +51,8 @@ public class LicenseWizardPage extends WizardPage {
 
     /** CLUF field. */
     private Text clufText;
+
+    private Browser clufBrowser;
 
     /**
      * Constructs a new LicenseWizardPage.
@@ -61,6 +71,7 @@ public class LicenseWizardPage extends WizardPage {
     /**
      * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
      */
+    @Override
     public void createControl(Composite parent) {
         ((GridData) parent.getLayoutData()).widthHint = 520;
         ((GridData) parent.getLayoutData()).heightHint = 280;
@@ -72,20 +83,62 @@ public class LicenseWizardPage extends WizardPage {
         Label subTitleLabel = new Label(container, SWT.NONE);
         subTitleLabel.setText(Messages.getString("LicenseWizard.subtitle")); //$NON-NLS-1$
 
-        clufText = new Text(container, SWT.MULTI | SWT.WRAP | SWT.V_SCROLL | SWT.H_SCROLL | SWT.LEFT | SWT.BORDER);
-        clufText.setBackground(new Color(null, 255, 255, 255));
-        clufText.setEditable(false);
-        String license = getLicense();
-        clufText.setText(license);
-
         GridData data = new GridData(GridData.FILL_BOTH);
-        clufText.setLayoutData(data);
+        IBrandingService brandingService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
+                IBrandingService.class);
+        boolean haveHtmlDesc = false;
+        String filePath = null;
+        try {
+            URL url = brandingService.getLicenseFile();
+            filePath = url.getFile();
+            IPath file = new Path(filePath).removeFileExtension();
+            file = file.addFileExtension("html"); //$NON-NLS-1$
+            if (new File(file.toPortableString()).exists()) {
+                haveHtmlDesc = true;
+                filePath = file.toPortableString();
+            }
+        } catch (IOException e) {
+            ExceptionHandler.process(e);
+        }
+
+        if (haveHtmlDesc && "yes".equalsIgnoreCase(System.getProperty("USE_BROWSER"))) { //$NON-NLS-1$//$NON-NLS-2$
+            clufBrowser = new Browser(container, SWT.BORDER);
+            clufBrowser.setText(getLicense(filePath));
+            clufBrowser.setLayoutData(data);
+        } else {
+            clufText = new Text(container, SWT.MULTI | SWT.WRAP | SWT.V_SCROLL | SWT.H_SCROLL | SWT.LEFT | SWT.BORDER);
+            clufText.setBackground(new Color(null, 255, 255, 255));
+            Font font = new Font(clufText.getDisplay(), "courier", 10, SWT.NONE); //$NON-NLS-1$
+            addResourceDisposeListener(clufText, font);
+            clufText.setFont(font);
+            clufText.setEditable(false);
+            if (filePath != null) {
+                clufText.setText(getLicense(filePath));
+            }
+            clufText.setLayoutData(data);
+        }
 
         // Label footerLabel = new Label(container, SWT.NONE);
         // footerLabel.setText(Messages.getString("LicenseWizard.footer")); //$NON-NLS-1$
 
         setControl(container);
         setPageComplete(true);
+    }
+
+    private void addResourceDisposeListener(final Control parent, final Resource res) {
+        if (parent != null) {
+            parent.addDisposeListener(new DisposeListener() {
+
+                @Override
+                public void widgetDisposed(DisposeEvent e) {
+                    if (res != null && !res.isDisposed()) {
+                        res.dispose();
+                    }
+                    parent.removeDisposeListener(this);
+                }
+            });
+        }
+
     }
 
     /**
@@ -96,14 +149,10 @@ public class LicenseWizardPage extends WizardPage {
      * @throws FileNotFoundException
      * @throws IOException
      */
-    private String getLicense() {
+    private String getLicense(String filePath) {
         String license = ""; //$NON-NLS-1$
         try {
-            IBrandingService brandingService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
-                    IBrandingService.class);
-            final URL url = brandingService.getLicenseFile();
-
-            FileReader fileReader = new FileReader(new File(url.getPath()));
+            FileReader fileReader = new FileReader(new File(filePath));
             BufferedReader in = new BufferedReader(fileReader);
 
             String licenseLine = ""; //$NON-NLS-1$
@@ -112,10 +161,8 @@ public class LicenseWizardPage extends WizardPage {
             }
 
         } catch (FileNotFoundException e) {
-            // e.printStackTrace();
             ExceptionHandler.process(e);
         } catch (IOException e) {
-            // e.printStackTrace();
             ExceptionHandler.process(e);
         }
         return license;
