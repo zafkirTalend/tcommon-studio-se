eaf22c5e973309cb5cf65f575c3cdafaa1c6e402
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-07 08:34:46 +0000

    bug TDI-25382 fixed : Wrong char (: should be ;) in JDBC connection string for Exasollution components, causes connection failure
    git-svn-id: http://talendforge.org/svn/tos/trunk@100721 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/database/conn/template/EDatabaseConnTemplate.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/database/conn/template/EDatabaseConnTemplate.java
@@ -109,7 +109,7 @@ public enum EDatabaseConnTemplate {
             "jdbc:teradata://<host>/<sid>,<property>")), //$NON-NLS-1$ //feature 0013719,hywang
 
     EXASOL(new DbConnStr(EDatabaseTypeName.EXASOL, //
-            "jdbc:exa:<host>:<port>:schema=<sid>", "8563")), //$NON-NLS-1$ //$NON-NLS-2$
+            "jdbc:exa:<host>:<port>;schema=<sid>", "8563")), //$NON-NLS-1$ //$NON-NLS-2$
 
     AS400(new DbConnStr(EDatabaseTypeName.AS400, //
             "jdbc:as400://<host>/<sid>;libraries=<sid>;<property>", //$NON-NLS-1$
