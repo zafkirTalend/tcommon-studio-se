f40eee8a3db5c9289fe08c4d2ba9f8ebed3e8ae4
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-16 02:20:19 +0000

    Fix Work Item TUP-618 : Try to have a cross platform crc check for text files since the first crc check is done on linux, it will avoid to re-calculate things when start the product on windows.
    git-svn-id: http://talendforge.org/svn/tos/trunk@101221 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.runtime/src/org/talend/commons/utils/io/IOUtils.java
+++ org.talend.commons.runtime/src/org/talend/commons/utils/io/IOUtils.java
@@ -13,6 +13,9 @@
 package org.talend.commons.utils.io;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.Adler32;
@@ -52,4 +55,54 @@ public class IOUtils {
         }
         return unitCRC;
     }
+
+    /**
+     * This function will avoid to re-calculate the CRC even if it was calculated in a different OS before. This will
+     * avoid each character of \r
+     * 
+     * @param in
+     * @return
+     */
+    public static long computeCRConTextFile(InputStream in) {
+        long unitCRC = 0;
+
+        BufferedInputStream bufferedInputStream = null;
+
+        try {
+            bufferedInputStream = new BufferedInputStream(in);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            DataInputStream dis = new DataInputStream(bufferedInputStream);
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while (((len = dis.read(buf))) != -1) {
+                baos.write(buf, 0, len);
+            }
+            dis.close();
+
+            String currentContent = baos.toString();
+            currentContent = currentContent.replace("\r", ""); //$NON-NLS-1$//$NON-NLS-2$ 
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(currentContent.getBytes());
+
+            // Compute Adler-32 checksum
+            CheckedInputStream cis = new CheckedInputStream(bis, new Adler32());
+            byte[] tempBuf = new byte[128];
+            while (cis.read(tempBuf) >= 0) {
+                // do nothing
+            }
+            unitCRC = cis.getChecksum().getValue();
+        } catch (IOException e) {
+            return -1;
+        } finally {
+            try {
+                bufferedInputStream.close();
+            } catch (Exception e) {
+                // ignore me even if i'm null
+            }
+        }
+        return unitCRC;
+    }
+
 }
