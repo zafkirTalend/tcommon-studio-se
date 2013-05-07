144a6b9eb7cae132d45ce830520a98cc999e9643
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-07 10:30:45 +0000

    TDI-25427 : remove duplicate display of ref project in the logon dialog.
    git-svn-id: http://talendforge.org/svn/tos/trunk@102398 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.localprovider/src/main/java/org/talend/repository/localprovider/model/LocalRepositoryFactory.java
+++ org.talend.repository.localprovider/src/main/java/org/talend/repository/localprovider/model/LocalRepositoryFactory.java
@@ -1002,6 +1002,7 @@ public class LocalRepositoryFactory extends AbstractEMFRepositoryFactory impleme
                         for (Project refP : ProjectManager.getInstance().getAllReferencedProjects()) {
                             if (refP.getTechnicalLabel().equals(p.getName())) {
                                 toReturn.add(refP);
+                                foundInRefs = true;
                                 break;
                             }
                         }
