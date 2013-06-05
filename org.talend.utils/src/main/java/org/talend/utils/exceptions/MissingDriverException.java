6ca1d721074c672a9e83599b998f4c28a8f49d87
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-05 10:35:01 +0000

    TDQ-7370 add some lost code and new case about column folder
    git-svn-id: http://talendforge.org/svn/tos/trunk@103795 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.utils/src/main/java/org/talend/utils/exceptions/MissingDriverException.java
@@ -0,0 +1,58 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.utils.exceptions;
+
+
+/**
+ * created by zshen on Jun 4, 2013
+ * Detailled comment
+ *
+ */
+public class MissingDriverException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private String errorMessage = ""; //$NON-NLS-1$
+
+    public MissingDriverException() {
+        super();
+    }
+
+    public MissingDriverException(String message) {
+        super(message);
+        errorMessage = message;
+    }
+
+    public MissingDriverException(Throwable cause, String message) {
+        super(cause);
+        errorMessage = message;
+    }
+
+    public MissingDriverException(Throwable cause) {
+        super(cause);
+        errorMessage = cause.getMessage();
+    }
+
+    /**
+     * Getter for errorMessage.
+     *
+     * @return the errorMessage
+     */
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
+}
