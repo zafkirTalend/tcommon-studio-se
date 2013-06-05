b0c7422b9e1ff14977ae35307322bdb99a1e4322
jzhao <jzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-05 09:29:56 +0000

    Fixed Bug TDI-26262: TSchemaCompliance failed to control date format https://jira.talendforge.org/browse/TDI-26262 Note: Merge to 5.1
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_1@103785 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/resources/java/routines/TalendDate.java
+++ org.talend.librariesmanager/resources/java/routines/TalendDate.java
@@ -8,7 +8,6 @@ package routines;
 import java.text.DateFormat;
 import java.text.FieldPosition;
 import java.text.ParseException;
-import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -207,16 +206,15 @@ public class TalendDate {
         sdf.setLenient(false);
 
         java.util.Date testDate = null;
-        ParsePosition pos = new ParsePosition(0);
 
-        testDate = sdf.parse(stringDate, pos);
-
-        if (testDate == null) {
+        try {
+            testDate = sdf.parse(stringDate);
+        } catch (ParseException e) {
             return false;
         }
 
         String formatDate = sdf.format(testDate);
-        if (formatDate.equalsIgnoreCase(stringDate) || pos.getIndex() == stringDate.length()) {
+        if (formatDate.equalsIgnoreCase(stringDate) || formatDate.length() == stringDate.length()) {
             return true;
         }
 
