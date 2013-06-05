2069506c511b0f3c599dfe2a8294174c89e11e63
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-05 08:27:20 +0000

    bug  TDI-26261 : Add url support for json wizard ,fix some problems for drag&drop and repository update
    git-svn-id: http://talendforge.org/svn/tos/trunk@103768 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core.runtime/src/main/java/org/talend/core/service/IJsonFileService.java
@@ -0,0 +1,27 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.service;
+
+import org.talend.core.IService;
+import org.talend.core.model.process.IElement;
+import org.talend.core.model.process.IElementParameter;
+
+/**
+ * created by wchen on 2013-6-5 Detailled comment
+ * 
+ */
+public interface IJsonFileService extends IService {
+
+    public boolean changeFilePathFromRepository(Object jsonConneciton, IElementParameter filePathParm, IElement elem, Object value);
+
+}
