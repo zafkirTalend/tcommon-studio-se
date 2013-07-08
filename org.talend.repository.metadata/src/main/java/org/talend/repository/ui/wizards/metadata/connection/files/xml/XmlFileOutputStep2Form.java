5061df7ac3ba74dd3b2eca02d574e35960f551b9
fwang <fwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-08 09:06:57 +0000

    bug TDI-26616 fixed:Metadata / XML File / Lost connection when schema is udpated
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@105015 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/files/xml/XmlFileOutputStep2Form.java
+++ org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/files/xml/XmlFileOutputStep2Form.java
@@ -537,6 +537,8 @@ public class XmlFileOutputStep2Form extends AbstractXmlFileStepForm {
             FOXTreeNode node = (FOXTreeNode) ex2.getDataItem();
             if (!cloumnNames.contains(metaColumn.getName())) {
                 node.setColumn(null);
+            } else {
+                node.setColumn(ConvertionHelper.convertToIMetaDataColumn(metaColumn));
             }
             node.setDataType(metaColumn.getTalendType());
         }
