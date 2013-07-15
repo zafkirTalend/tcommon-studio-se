03d7d6229fdb1391632f3a954a2e15407f8e1520
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-15 06:46:42 +0000

    TDI-26733 fixed:Need junit for TDI-26604 and TDI-26322
    git-svn-id: http://talendforge.org/svn/tos/trunk@105248 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.metadata.managment.test/src/org/talend/core/model/metadata/MetadataColumnTest.java
@@ -0,0 +1,38 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.metadata;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.talend.core.model.metadata.builder.connection.ConnectionFactory;
+import org.talend.core.model.metadata.builder.connection.MetadataColumn;
+
+/**
+ * DOC PLV class global comment. Detailled comment
+ */
+public class MetadataColumnTest {
+
+    @Test
+    public void testMetadataColumn() {
+        MetadataColumn column = ConnectionFactory.eINSTANCE.createMetadataColumn();
+        column.setLabel("a");
+        column.setName("b");
+        assertEquals("b", column.getName());
+        assertEquals("a", column.getLabel());
+        column.setName("b");
+        column.setLabel("a");
+        assertEquals(column.getLabel(), column.getName());
+    }
+
+}
