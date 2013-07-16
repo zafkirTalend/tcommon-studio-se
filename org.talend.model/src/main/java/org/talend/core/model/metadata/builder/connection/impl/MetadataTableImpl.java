bfe79d701fa0593c217198b3388d3526083d8dc8
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-16 11:27:17 +0000

    TDI-25774 Revert the original fix, using the this.label in setName() directly without a call to setLabel(), in order to avoid the stack over flow exception.
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@105308 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.model/src/main/java/org/talend/core/model/metadata/builder/connection/impl/MetadataTableImpl.java
+++ org.talend.model/src/main/java/org/talend/core/model/metadata/builder/connection/impl/MetadataTableImpl.java
@@ -7,6 +7,7 @@ package org.talend.core.model.metadata.builder.connection.impl;
 
 import java.util.Collection;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.common.util.EList;
@@ -1306,6 +1307,33 @@ public class MetadataTableImpl extends AbstractMetadataObjectImpl implements Met
         result.append(activatedCDC);
         result.append(')');
         return result.toString();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see orgomg.cwm.objectmodel.core.impl.ModelElementImpl#getName()
+     */
+    @Override
+    public String getName() {
+        if (super.getName() == null) {
+            return this.label;
+        } else {
+            return super.getName();
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.metadata.builder.connection.impl.AbstractMetadataObjectImpl#getLabel()
+     */
+    @Override
+    public String getLabel() {
+        if (StringUtils.isEmpty(this.label)) {
+            return getName();
+        }
+        return this.label;
     }
 
     public String getOriginalLabel() {
