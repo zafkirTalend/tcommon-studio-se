24522fbcdfb02c591b6ed25ca5a80377d5e0f7f7
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-24 08:59:44 +0000

    bug TDI-22849 : Display metadata schema columns in actual order
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_3@105727 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository.view/src/org/talend/repository/tester/SchemaColumnFolderNodeTester.java
@@ -0,0 +1,58 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.tester;
+
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.repository.model.IRepositoryNode;
+import org.talend.repository.model.RepositoryNode;
+
+/**
+ * created by ycbai on 2013-7-24 Detailled comment
+ * 
+ */
+public class SchemaColumnFolderNodeTester extends SubNodeTester {
+
+    private static final String IS_SCHEMA_COLUMN_FOLDER = "isSchemaColumnFolder"; //$NON-NLS-1$
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.repository.tester.AbstractNodeTester#testProperty(java.lang.Object, java.lang.String,
+     * java.lang.Object[], java.lang.Object)
+     */
+    @Override
+    protected Boolean testProperty(Object receiver, String property, Object[] args, Object expectedValue) {
+        if (receiver instanceof RepositoryNode) {
+            RepositoryNode repositoryNode = (RepositoryNode) receiver;
+            if (IS_SCHEMA_COLUMN_FOLDER.equals(property)) {
+                return isSchemaColumnFolder(repositoryNode);
+            }
+        }
+        return null;
+    }
+
+    public boolean isSchemaColumn(RepositoryNode repositoryNode) {
+        return isTypeNode(repositoryNode, ERepositoryObjectType.METADATA_CON_COLUMN);
+    }
+
+    public boolean isSchemaColumnFolder(RepositoryNode repositoryNode) {
+        if (repositoryNode.getType() == IRepositoryNode.ENodeType.STABLE_SYSTEM_FOLDER) {
+            if (repositoryNode.getChildren().size() > 0) {
+                return isSchemaColumn((RepositoryNode) repositoryNode.getChildren().get(0));
+            }
+            return isTypeNode(repositoryNode.getParent(), ERepositoryObjectType.METADATA_CON_TABLE);
+        }
+        return false;
+    }
+
+}
