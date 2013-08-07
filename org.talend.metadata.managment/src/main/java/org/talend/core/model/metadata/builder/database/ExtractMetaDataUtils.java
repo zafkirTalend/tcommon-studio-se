3bbc0b96716acc419fd1ccd25b53ac46e9118a99
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-07 03:31:57 +0000

    TDI-26869:Add combo in the wizard for Vertica 5.1
    git-svn-id: http://talendforge.org/svn/tos/trunk@106359 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataUtils.java
+++ org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataUtils.java
@@ -875,7 +875,8 @@ public class ExtractMetaDataUtils {
                 driverClassName = ExtractMetaDataUtils.getDriverClassByDbType(dbType);
                 // feature TDI-22108
                 if (EDatabaseTypeName.VERTICA.getXmlName().equals(dbType)
-                        && EDatabaseVersion4Drivers.VERTICA_6.getVersionValue().equals(dbVersion)) {
+                        && (EDatabaseVersion4Drivers.VERTICA_6.getVersionValue().equals(dbVersion) || EDatabaseVersion4Drivers.VERTICA_5_1
+                                .getVersionValue().equals(dbVersion))) {
                     driverClassName = EDatabase4DriverClassName.VERTICA2.getDriverClass();
                 }
             }
