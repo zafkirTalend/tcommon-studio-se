9fa9dd14bcd6d8bbf7dd27ae0d5ceb910b187613
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-23 09:27:31 +0000

    TDQ-7407: Only display the Cheat Sheet view on new startup of the studio
    git-svn-id: http://talendforge.org/svn/tos/trunk@107016 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.rcp/src/main/java/org/talend/rcp/intro/CheatSheetPerspectiveAdapter.java
+++ org.talend.rcp/src/main/java/org/talend/rcp/intro/CheatSheetPerspectiveAdapter.java
@@ -19,10 +19,11 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PerspectiveAdapter;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.PartPane;
+import org.eclipse.ui.internal.PartSite;
 import org.eclipse.ui.internal.cheatsheets.ICheatSheetResource;
 import org.eclipse.ui.internal.cheatsheets.views.CheatSheetView;
 import org.eclipse.ui.internal.cheatsheets.views.ViewUtilities;
@@ -36,6 +37,7 @@ public class CheatSheetPerspectiveAdapter extends PerspectiveAdapter {
     //public static final String DQ_CHEATSHEET_START_ID = "org.talend.dataprofiler.core.talenddataprofiler";//$NON-NLS-1$
 
     public static final String DQ_CHEATSHEET_START_ID = "org.talend.datacleansing.core.ui.dqcheatsheet";//$NON-NLS-1$
+
     protected String cheetSheetID;
 
     protected HashMap<String, Boolean> cheetSheetInPerspective = new HashMap<String, Boolean>();
@@ -44,6 +46,11 @@ public class CheatSheetPerspectiveAdapter extends PerspectiveAdapter {
 
     private static final String DQ_PERSPECTIVE_ID = "org.talend.dataprofiler.DataProfilingPerspective";//$NON-NLS-1$
 
+    /**
+     * CheatSheetPerspectiveAdapter constructor.
+     * 
+     * @param startId
+     */
     public CheatSheetPerspectiveAdapter(String startId) {
         CheatSheetView cheetSheet = findCheetSheet();
         if (cheetSheet != null) {
@@ -51,19 +58,30 @@ public class CheatSheetPerspectiveAdapter extends PerspectiveAdapter {
         }
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.PerspectiveAdapter#perspectivePreDeactivate(org.eclipse.ui.IWorkbenchPage,
+     * org.eclipse.ui.IPerspectiveDescriptor)
+     */
     @Override
     public void perspectivePreDeactivate(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
-
         CheatSheetView cheetSheet = findCheetSheet();
         if (null != cheetSheet) {
             cheetSheetID = cheetSheet.getCheatSheetID();
+            // Always hide cheatsheet first on switching perspective
+            hideCheetSheet(cheetSheet);
         }
         cheetSheetInPerspective.put(perspective.getId(), null != cheetSheet);
-        // Always hide cheatsheet first on switching perspective
-        hideCheetSheet();
         super.perspectivePreDeactivate(page, perspective);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.PerspectiveAdapter#perspectiveActivated(org.eclipse.ui.IWorkbenchPage,
+     * org.eclipse.ui.IPerspectiveDescriptor)
+     */
     @Override
     public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
 
@@ -80,27 +98,29 @@ public class CheatSheetPerspectiveAdapter extends PerspectiveAdapter {
         super.perspectiveActivated(page, perspective);
     }
 
-    private void hideCheetSheet() {
-        if (findCheetSheet() != null) {
-            try {
-                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().hideView(findCheetSheet());
-            } catch (Throwable t) {
-                log.warn(t, t);
-            }
+    /**
+     * hide the cheet sheet view.
+     * 
+     * @param cheetSheet
+     */
+    private void hideCheetSheet(CheatSheetView cheetSheet) {
+        try {
+            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().hideView(cheetSheet);
+        } catch (Throwable t) {
+            log.warn(t, t);
         }
     }
 
+    /**
+     * show the cheet sheet view.
+     */
     private void restoreCheetSheet() {
         Display.getDefault().asyncExec(new Runnable() {
 
+            @Override
             public void run() {
-                // code clean by gdbu 2011-4-18
-                // IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-                // unused variable
-                // IWorkbenchPage page = window.getActivePage();
-                // ~code clean
-                IWorkbenchPart activePart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
                 CheatSheetView view = ViewUtilities.showCheatSheetView();
+
                 // MOD klliu bug TDQ-4130 if studio is started by other user(DI/MDM),the cheetSheetID will not be
                 // initalized.
                 // Then it is changed to DQ,cheetSheetID will be null,but CheatSheetView is not null,so that will show
@@ -113,19 +133,24 @@ public class CheatSheetPerspectiveAdapter extends PerspectiveAdapter {
                         view.setInput(DQ_CHEATSHEET_START_ID);
                     }
                 }
-                // ~
-                if (null != activePart) {
-                    activePart.setFocus();
-                }
+
+                // ADD msjian TDQ-7407 2013-8-23: Only display the Cheat Sheet view on new startup of the studio
+                PartPane pane = ((PartSite) view.getSite()).getPane();
+                view.getSite().getPage().toggleZoom(pane.getPartReference());
+                // TDQ-7407~
             }
         });
     }
 
+    /**
+     * get the cheet sheet view.
+     * 
+     * @return CheatSheetView
+     */
     private CheatSheetView findCheetSheet() {
         IWorkbench workbench = PlatformUI.getWorkbench();
         IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
         IWorkbenchPage page = window.getActivePage();
-
         return (CheatSheetView) page.findView(ICheatSheetResource.CHEAT_SHEET_VIEW_ID);
     }
 }
