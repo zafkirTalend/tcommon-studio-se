73714cddca18e599f52cb0c19cd573a98b27b1e5
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-23 08:04:39 +0000

    Fix New Feature TDQ-7504 : Match Analysis Editor https://jira.talendforge.org/browse/TDQ-7504 implement result page of match analysis.
    git-svn-id: http://talendforge.org/svn/tos/trunk@107002 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/service/IMatchRuleUIService.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/service/IMatchRuleUIService.java
@@ -21,52 +21,50 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.talend.core.IService;
 
-
 /**
- * created by zshen on Aug 16, 2013
- * Detailled comment
- *
+ * created by zshen on Aug 16, 2013 Detailled comment
+ * 
  */
 public interface IMatchRuleUIService extends IService {
 
     /**
-     *
+     * 
      * DOC zshen Comment method "getImageByName".
-     *
+     * 
      * @param imageName can be found from {@link org.talend.dataquality.record.linkage.ui.composite.utils.ImageLib}
      * @return
      */
     public Image getImageByName(String imageName);
 
     /**
-     *
+     * 
      * get image which is like "Plus".
-     *
-     *
+     * 
+     * 
      * @return
      */
     public Image getAddImage();
 
     /**
-     *
+     * 
      * create MatchRuleCTabFolderRenderer instance
-     *
+     * 
      * @return
      */
     public CTabFolderRenderer createMatchRuleCTabFolderRenderer(CTabFolder configurationFolder);
 
     /**
-     *
+     * 
      * Create match rule table
-     *
+     * 
      * @return
      */
     public Composite createMatchRuleTable(SashForm parent, List<String> columnNames);
 
     /**
-     *
+     * 
      * create match rule chart
-     *
+     * 
      * @param parent
      * @param viewData the data for grouping result
      * @param columnNames the name of columns
@@ -76,18 +74,18 @@ public interface IMatchRuleUIService extends IService {
 
     /**
      * set the the value of count which need to display charts
-     *
+     * 
      * @param count if the count of someone grouping more of this parameter the data will be display
      */
     public void setNeedDisplayCount(int count, Composite chart);
 
     /**
-     *
+     * 
      * refresh the chart of match rule
-     *
+     * 
      * @param chart
      */
-    public void refreshMatchRuleChart(List<String[]> viewData, Composite chart);
+    public void refreshMatchRuleChart(List<String[]> viewData, Composite chart, List<String> columnNames);
 
     /**
      * do layout again for the chart
@@ -95,7 +93,7 @@ public interface IMatchRuleUIService extends IService {
     public void layoutMatchRuleChart(Composite chart);
 
     /**
-     *
+     * 
      * dispose current chart
      * 
      * @param chart
