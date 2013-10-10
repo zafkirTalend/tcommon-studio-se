264be1cc600c88994dcfe7868588bfcc1757d0b6
hcyi <hcyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-10 07:21:03 +0000

    bug TDI-27348 fixed : Database connection is failed. java.lang.RuntimeException: The service org.talend.repository.model.IMetadataService has not been registered .
    git-svn-id: http://talendforge.org/svn/tos/trunk@109054 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataUtils.java
+++ org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/builder/database/ExtractMetaDataUtils.java
@@ -392,16 +392,18 @@ public class ExtractMetaDataUtils {
     }
 
     private DatabaseMetaData createJtdsDatabaseMetaData(Connection conn) {
-        IService service = GlobalServiceRegister.getDefault().getService(IMetadataService.class);
-        if (service == null) {
-            try {
-                return conn.getMetaData();
-            } catch (SQLException e) {
-                log.error(e.toString());
-                throw new RuntimeException(e);
+        if (GlobalServiceRegister.getDefault().isServiceRegistered(IMetadataService.class)) {
+            IService service = GlobalServiceRegister.getDefault().getService(IMetadataService.class);
+            if (service != null) {
+                return ((IMetadataService) service).findCustomizedJTDSDBMetadata(conn);
             }
         }
-        return ((IMetadataService) service).findCustomizedJTDSDBMetadata(conn);
+        try {
+            return conn.getMetaData();
+        } catch (SQLException e) {
+            log.error(e.toString());
+            throw new RuntimeException(e);
+        }
     }
 
     /**
