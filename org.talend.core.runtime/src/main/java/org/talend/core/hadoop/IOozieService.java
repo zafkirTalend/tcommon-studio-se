455bfea79c4cdbc028bd68552b482fc3210d7eaa
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-17 06:24:57 +0000

    bug TDI-27716 fixed : Hadoop properties in oozie can not be imported when we use the oozie in Talend Oozie view setting.
    git-svn-id: http://talendforge.org/svn/tos/trunk@109510 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/hadoop/IOozieService.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/hadoop/IOozieService.java
@@ -1,5 +1,7 @@
 package org.talend.core.hadoop;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.talend.core.IService;
@@ -23,5 +25,7 @@ public interface IOozieService extends IService {
     public Map<String, Object> getOozieParamFromConnection(Connection connection);
 
     public boolean isOozieNode(RepositoryNode node);
+
+    public List<HashMap<String, Object>> getHadoopProperties(Connection connection);
 
 }
