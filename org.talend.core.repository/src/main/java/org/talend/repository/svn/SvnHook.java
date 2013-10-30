4feeee8ad7be4a3b5d613afbee0b2c98424b85a1
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-30 05:53:45 +0000

    bug TDI-27695 fixed: Migration:After do migration and logon old project, has error message in error log list
    git-svn-id: http://talendforge.org/svn/tos/trunk@110213 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core.repository/src/main/java/org/talend/repository/svn/SvnHook.java
@@ -0,0 +1,28 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.svn;
+
+import java.util.Collection;
+
+import org.eclipse.core.runtime.IPath;
+
+/**
+ * created by wchen on 2013-10-30 Detailled comment
+ * 
+ */
+public interface SvnHook {
+
+    public void unloadAndRemoveResources(Collection<String> changedPaths);
+
+    public Boolean accept(IPath projectRelativePath);
+}
