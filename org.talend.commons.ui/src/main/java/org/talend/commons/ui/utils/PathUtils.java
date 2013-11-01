6cf179c627234a6e1a71405d40f8acd149e6f0f1
hfchang <hfchang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-11-01 03:26:42 +0000

    bug TDI-26007 fixed : Should increase the display if item have long description put the method cutStr() from PathUtils to AbStractTreeToolTips
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_3@110344 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.ui/src/main/java/org/talend/commons/ui/utils/PathUtils.java
+++ org.talend.commons.ui/src/main/java/org/talend/commons/ui/utils/PathUtils.java
@@ -29,33 +29,4 @@ public class PathUtils {
     public static String getOSPath(String path) {
         return Path.fromPortableString(path).toOSString();
     }
-
-    /*
-     * author by hfchang
-     * 
-     * make a string line wrap autom
-     */
-    public static String cutStr(String str, int len) {
-        String s = "";
-        if (str.length() < len) {
-            s += str;
-        }
-        for (int i = 0; i < str.length(); i++) {
-            int r = i % len;
-            if (i != 0 && i <= len) {
-                if (r == 0) {
-                    s += str.substring(i - len, len) + "\n";
-                }
-            } else if (i > len) {
-                String laststr = str.substring(i - 1);
-                if (laststr.length() > len) {
-                    s += cutStr(laststr, len);
-                } else {
-                    s += laststr;
-                }
-                break;
-            }
-        }
-        return s;
-    }
 }
