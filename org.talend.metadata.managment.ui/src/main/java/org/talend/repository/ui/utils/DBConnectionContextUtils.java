28f70fdb036e5c05b94a28f69e1f39407653dd13
ldong <ldong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-11-18 09:46:04 +0000

    bug TDI-28124 fixed:tdb2input can't guess schema from join sql on system table.
    git-svn-id: http://talendforge.org/svn/tos/trunk@111061 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment.ui/src/main/java/org/talend/repository/ui/utils/DBConnectionContextUtils.java
+++ org.talend.metadata.managment.ui/src/main/java/org/talend/repository/ui/utils/DBConnectionContextUtils.java
@@ -500,6 +500,15 @@ public final class DBConnectionContextUtils {
             cloneConn.setURL(newURl);
             return cloneConn;
         }
+        // TDI-28124:tdb2input can't guess schema from join sql on system table
+        if (EDatabaseTypeName.IBMDB2.equals(EDatabaseTypeName.getTypeFromDbType(dbConn.getDatabaseType()))) {
+            String cursorForDb2 = ":cursorSensitivity=2;";
+            String database = sidOrDatabase + cursorForDb2;
+            String newURL = DatabaseConnStrUtil.getURLString(cloneConn.getDatabaseType(), dbConn.getDbVersionString(), server,
+                    username, password, port, database, filePath.toLowerCase(), datasource, dbRootPath, additionParam);
+            cloneConn.setURL(newURL);
+            return cloneConn;
+        }
 
         // Added 20130311 TDQ-7000, when it is context mode and not general jdbc, reset the url.
         if (contextType != null
