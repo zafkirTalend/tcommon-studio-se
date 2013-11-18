1225694165c5f5ff75bc9168b18f86f2366e5ef1
hfchang <hfchang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-11-18 08:12:12 +0000

    bug TDI-28084 fixed : paste columns button not working tFilterRow
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_3@111047 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/advanced/dataeditor/button/PastePushButtonForExtendedTable.java
+++ org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/advanced/dataeditor/button/PastePushButtonForExtendedTable.java
@@ -80,10 +80,21 @@ public abstract class PastePushButtonForExtendedTable extends PastePushButton im
         return (AbstractExtendedTableViewer) getExtendedControlViewer();
     }
 
+    @Override
     public boolean getEnabledState() {
         Object data = SimpleClipboard.getInstance().getData();
-        return super.getEnabledState() && data != null && data instanceof List
-                && !getExtendedControlViewer().isReadOnly();
+        boolean flag = true;
+        int a = CopyPushButtonForExtendedTable.colNum;
+        int colNum = getExtendedTableViewer().getTableViewerCreator().getTable().getColumnCount();
+        if (data != null) {
+            if (colNum == a) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+        }
+        return super.getEnabledState() && data != null && data instanceof List && !getExtendedControlViewer().isReadOnly()
+                && flag;
     }
 
 }
