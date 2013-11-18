819c295805e2f34d1f1c07c12e9354ea2ff882a2
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-11-18 09:20:25 +0000

    feature TUP-1326 : Need a common svn folder to share libraries between studios and commandline
    git-svn-id: http://talendforge.org/svn/tos/trunk@111055 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.repository/src/main/java/org/talend/core/repository/model/ProxyRepositoryFactory.java
+++ org.talend.core.repository/src/main/java/org/talend/core/repository/model/ProxyRepositoryFactory.java
@@ -153,8 +153,6 @@ public final class ProxyRepositoryFactory implements IProxyRepositoryFactory {
 
     private final ProjectManager projectManager;
 
-    public static final String TALEND_LIBRARY_PATH = "talend.library.path";
-
     @Override
     public synchronized void addPropertyChangeListener(PropertyChangeListener l) {
         if (l == null) {
@@ -1871,7 +1869,6 @@ public final class ProxyRepositoryFactory implements IProxyRepositoryFactory {
                         throw new OperationCanceledException(""); //$NON-NLS-1$
                     }
                 }
-                syblib(monitor);
                 fullLogonFinished = true;
             } finally {
                 TimeMeasure.end("logOnProject"); //$NON-NLS-1$
@@ -1891,35 +1888,6 @@ public final class ProxyRepositoryFactory implements IProxyRepositoryFactory {
             logOffProject();
             throw e;
         }
-    }
-
-    public void syblib(IProgressMonitor monitor) {
-        File libFile = new File(getLibrariesPath(ECodeLanguage.JAVA));
-        if (PluginChecker.isSVNProviderPluginLoaded()) {
-            ISVNProviderServiceInCoreRuntime service = (ISVNProviderServiceInCoreRuntime) GlobalServiceRegister.getDefault()
-                    .getService(ISVNProviderServiceInCoreRuntime.class);
-            if (service != null && service.isSvnLibSetupOnTAC() && !getRepositoryContext().isOffline()) {
-                boolean exist = libFile.exists();
-                boolean flag = service.isInSvn(libFile.getAbsolutePath());
-                if (exist && flag) { // means lib/java already link to svn
-                    service.doUpdateAndCommit(libFile.getAbsolutePath());
-                } else if (!exist) { // means lib/java not exist,should create lib/java and checkout
-                    service.createFolderAndLinkToSvn(libFile.getAbsolutePath());
-                } else if (exist && !flag) {// means lib/java is exist ,but not link to svn.
-                    service.synToSvn(libFile.getAbsolutePath());
-                }
-                // move the jar from project/libs to svnlib
-                service.synProjectLib(libFile.getAbsolutePath());
-            }
-        }
-    }
-
-    private static String getLibrariesPath(ECodeLanguage language) {
-        String libPath = System.getProperty(TALEND_LIBRARY_PATH);
-        if (libPath != null) {
-            return libPath;
-        }
-        return Platform.getInstallLocation().getURL().getFile() + "lib/java";
     }
 
     public void logOffProject() {
