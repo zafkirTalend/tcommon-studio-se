46f247eb7838e531e79571f55ed73d8dd5d2a53a
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-12-25 06:59:07 +0000

    TDQ-6788 "remove duplicate" job can not run well for oracle and sybase and IBM/DB2 https://jira.talendforge.org/browse/TDQ-6788 TDQ-6680 "Remove duplicate" job failed to run with IBM DB2 connection https://jira.talendforge.org/browse/TDQ-6680 if the database type is IBMDB2 and the name contain lowercase character, add quotes
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@112589 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
+++ org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
@@ -313,7 +313,9 @@ public class TalendTextUtils {
 
             boolean isH2 = EDatabaseTypeName.H2 == name;
 
-            if ((!matcher.matches() || matcher2.matches() || isSqlKeyword) && !isH2 && EDatabaseTypeName.SAS != name) {
+            // if the database type is IBMDB2 and the field name contain lowercase character, should add quotes
+            if (((!matcher.matches() || matcher2.matches() || isSqlKeyword) && !isH2 && EDatabaseTypeName.SAS != name)
+                    || isIBMDB2ContainLowerCase(dbType, fieldName)) {
                 isCheck = true; // contain other char
             }
 
@@ -328,6 +330,25 @@ public class TalendTextUtils {
 
         newFieldName = TalendQuoteUtils.addQuotesForSQLString(newFieldName, quote, simple);
         return newFieldName;
+    }
+
+    /**
+     * if the database type is IBM DB2 and the field name contains lowercase character return true otherwise return
+     * false.
+     * 
+     * @param dbType
+     * @param fieldName
+     * @return
+     */
+    private static boolean isIBMDB2ContainLowerCase(String dbType, String fieldName) {
+        if (dbType.equals(EDatabaseTypeName.IBMDB2.getDisplayName())
+                || dbType.equals(EDatabaseTypeName.IBMDB2ZOS.getDisplayName())) {
+            String temp = removeQuotes(fieldName);
+            Pattern pattern = Pattern.compile("^.*[a-z]+.*$"); //$NON-NLS-1$
+            Matcher matcher = pattern.matcher(temp);
+            return matcher.matches();
+        }
+        return false;
     }
 
     public static String getQuoteByDBType(EDatabaseTypeName name) {
