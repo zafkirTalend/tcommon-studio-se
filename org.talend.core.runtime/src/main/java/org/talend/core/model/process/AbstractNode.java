7cf612d3f049e34449b72fe909bc3d5da1117f4f
bchen <bchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-10 11:26:07 +0000

    Fix Work Item TDI-28064 : [MR] reuse Input for tSortRow to reduce one additional m/r job https://jira.talendforge.org/browse/TDI-28064 1. fixed the bug about Lookup and After_Run 2. reuse Input for tSortRow to reduce one additional m/r job when the input is [tMRInput,tHDFSInput] merge r112403 into branch5.3
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_3@113285 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/model/process/AbstractNode.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/model/process/AbstractNode.java
@@ -86,6 +86,10 @@ public abstract class AbstractNode implements INode {
 
     private boolean subProcessContainBreakpoint;
 
+    private boolean isThereLinkWithRef = Boolean.FALSE;
+
+    private List<INode> refNodes;
+
     private List<ModuleNeeded> modulesNeeded = new ArrayList<ModuleNeeded>();
 
     public String getComponentName() {
@@ -1012,5 +1016,41 @@ public abstract class AbstractNode implements INode {
     @Override
     public boolean isSubtreeStart() {
         return isDesignSubjobStartNode() && !isThereLinkWithHash();
+    }
+
+    /**
+     * Getter for isThereLinkWithRef.
+     * 
+     * @return the isThereLinkWithRef
+     */
+    public boolean isThereLinkWithRef() {
+        return this.isThereLinkWithRef;
+    }
+
+    /**
+     * Sets the isThereLinkWithRef.
+     * 
+     * @param isThereLinkWithRef the isThereLinkWithRef to set
+     */
+    public void setThereLinkWithRef(boolean isThereLinkWithRef) {
+        this.isThereLinkWithRef = isThereLinkWithRef;
+    }
+
+    /**
+     * Getter for refNodes.
+     * 
+     * @return the refNodes
+     */
+    public List<INode> getRefNodes() {
+        return this.refNodes;
+    }
+
+    /**
+     * Sets the refNodes.
+     * 
+     * @param refNodes the refNodes to set
+     */
+    public void setRefNodes(List<INode> refNodes) {
+        this.refNodes = refNodes;
     }
 }
