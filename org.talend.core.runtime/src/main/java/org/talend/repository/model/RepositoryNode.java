9cb005e07116ed3d2d7bf2f065461eb8a40201aa
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-15 10:46:48 +0000

    bug TDI-28571 fixed: BD:Many error logs when logon TOS_BD build or do "export item"
    git-svn-id: http://talendforge.org/svn/tos/trunk@113429 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/repository/model/RepositoryNode.java
+++ org.talend.core.runtime/src/main/java/org/talend/repository/model/RepositoryNode.java
@@ -417,6 +417,10 @@ public class RepositoryNode implements IRepositoryNode, IActionFilter {
             if (other.object != null) {
                 return false;
             }
+        } else if (other.object == null) {
+            if (this.object != null) {
+                return false;
+            }
         } else {
             if (object.getVersion() == null) {
                 if (other.object.getVersion() != null) {
