216b352025958daeb58b1de18618dd30d76d14cf
hfchang <hfchang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-12 06:47:05 +0000

    bug TDI-28602 fixed : Validation rules: can not do "paste" operation.
    git-svn-id: http://talendforge.org/svn/tos/trunk@114006 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.repository/src/main/java/org/talend/repository/ui/actions/PasteAction.java
+++ org.talend.core.repository/src/main/java/org/talend/repository/ui/actions/PasteAction.java
@@ -151,11 +151,14 @@ public class PasteAction extends AContextualAction {
         IProxyRepositoryFactory proxyFactory = ProxyRepositoryFactory.getInstance();
         IRepositoryViewObject object = target.getObject();
 
-        if (target.getContentType() == ERepositoryObjectType.JOBS || target.getContentType() == ERepositoryObjectType.JOBLETS
+        if (target.getContentType() == ERepositoryObjectType.JOBS
+                || target.getContentType() == ERepositoryObjectType.JOBLETS
                 || target.getContentType() == ERepositoryObjectType.GENERATED
                 || target.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.JOB_DOC
                 || target.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.JOBLET_DOC
-                || target.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.REFERENCED_PROJECTS) {
+                || target.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.REFERENCED_PROJECTS
+                || (target.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.METADATA_VALIDATION_RULES && target
+                        .getParent().getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.METADATA_VALIDATIONS_RULES_FOLDER)) {
             visible = false;
             enabled = false;
         } else if (object != null) {
