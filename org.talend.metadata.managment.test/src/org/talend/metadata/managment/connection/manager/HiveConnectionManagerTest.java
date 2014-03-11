1ad1474b0780c5067ff29cccdf75ec81fef0f2ea
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-11 08:35:57 +0000

    TDQ-8678 move junit about HiveHandlers from 'org.talend.cwm.management.test' to 'org.talend.metadata.management.test'
    git-svn-id: http://talendforge.org/svn/tos/trunk@114875 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.metadata.managment.test/src/org/talend/metadata/managment/connection/manager/HiveConnectionManagerTest.java
@@ -0,0 +1,88 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.metadata.managment.connection.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.core.database.conn.ConnParameterKeys;
+import org.talend.core.database.hbase.conn.version.EHBaseDistribution4Versions;
+import org.talend.core.model.metadata.builder.MetadataConnection;
+import org.talend.metadata.managment.hive.handler.CDH4YarnHandler;
+import org.talend.metadata.managment.hive.handler.CDH5YarnHandler;
+import org.talend.metadata.managment.hive.handler.HDP130Handler;
+import org.talend.metadata.managment.hive.handler.HDP200YarnHandler;
+import org.talend.metadata.managment.hive.handler.HiveConnectionHandler;
+import org.talend.metadata.managment.hive.handler.Mapr212Handler;
+
+/**
+ * created by qiongli on 2014-3-11 Detailled comment
+ * 
+ */
+public class HiveConnectionManagerTest {
+
+    /**
+     * DOC qiongli Comment method "setUp".
+     * 
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * DOC qiongli Comment method "tearDown".
+     * 
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.metadata.managment.connection.manager.HiveConnectionManager#createHandler(org.talend.core.model.metadata.IMetadataConnection)}
+     * .
+     */
+    @Test
+    public void testCreateHandler() {
+        MetadataConnection mc = new MetadataConnection();
+        mc.setParameter(ConnParameterKeys.CONN_PARA_KEY_HIVE_VERSION, EHBaseDistribution4Versions.HDP_1_3.getVersionValue());
+        HiveConnectionHandler createHandler = HiveConnectionManager.getInstance().createHandler(mc);
+        assertTrue(createHandler instanceof HDP130Handler);
+
+        mc.setParameter(ConnParameterKeys.CONN_PARA_KEY_HIVE_VERSION,
+                EHBaseDistribution4Versions.CLOUDERA_CDH4_YARN.getVersionValue());
+        createHandler = HiveConnectionManager.getInstance().createHandler(mc);
+        assertTrue(createHandler instanceof CDH4YarnHandler);
+
+        mc.setParameter(ConnParameterKeys.CONN_PARA_KEY_HIVE_VERSION, EHBaseDistribution4Versions.HDP_2_0.getVersionValue());
+        createHandler = HiveConnectionManager.getInstance().createHandler(mc);
+        assertTrue(createHandler instanceof HDP200YarnHandler);
+
+        mc.setParameter(ConnParameterKeys.CONN_PARA_KEY_HIVE_VERSION, EHBaseDistribution4Versions.MAPR_2_1_2.getVersionValue());
+        createHandler = HiveConnectionManager.getInstance().createHandler(mc);
+        assertTrue(createHandler instanceof Mapr212Handler);
+
+        mc.setParameter(ConnParameterKeys.CONN_PARA_KEY_HIVE_VERSION, EHBaseDistribution4Versions.MAPR_3_0_1.getVersionValue());
+        createHandler = HiveConnectionManager.getInstance().createHandler(mc);
+        assertTrue(createHandler instanceof Mapr212Handler);
+
+        mc.setParameter(ConnParameterKeys.CONN_PARA_KEY_HIVE_VERSION, EHBaseDistribution4Versions.CLOUDERA_CDH5.getVersionValue());
+        createHandler = HiveConnectionManager.getInstance().createHandler(mc);
+        assertTrue(createHandler instanceof CDH5YarnHandler);
+    }
+
+}
