f60b709ca4118eef384574e7dc1fb2ef36fc0e12
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-17 03:27:03 +0000

    bug TDI-28708 fixed : Garbled folder name after build jobs.
    git-svn-id: http://talendforge.org/svn/tos/trunk@115080 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.ui/src/main/java/org/talend/repository/documentation/TarFileExporterFullPath.java
+++ org.talend.core.ui/src/main/java/org/talend/repository/documentation/TarFileExporterFullPath.java
@@ -22,29 +22,29 @@ import java.io.InputStream;
 import java.util.zip.GZIPOutputStream;
 
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.ui.internal.wizards.datatransfer.TarEntry;
-import org.eclipse.ui.internal.wizards.datatransfer.TarOutputStream;
-
+import org.apache.tools.tar.TarOutputStream;
+import org.apache.tools.tar.TarEntry;
 
 /**
  * Exports resources to a .tar.gz file.
- *
+ * 
  * @since 3.1
  */
 public class TarFileExporterFullPath implements IFileExporterFullPath {
+
     private TarOutputStream outputStream;
+
     private GZIPOutputStream gzipOutputStream;
-    
 
     /**
-     *  Create an instance of this class.
-     *
-     *  @param filename java.lang.String
-     *  @param compress boolean
-     *  @exception java.io.IOException
+     * Create an instance of this class.
+     * 
+     * @param filename java.lang.String
+     * @param compress boolean
+     * @exception java.io.IOException
      */
     public TarFileExporterFullPath(String filename, boolean compress) throws IOException {
-        if(compress) {
+        if (compress) {
             gzipOutputStream = new GZIPOutputStream(new FileOutputStream(filename));
             outputStream = new TarOutputStream(new BufferedOutputStream(gzipOutputStream));
         } else {
@@ -53,36 +53,35 @@ public class TarFileExporterFullPath implements IFileExporterFullPath {
     }
 
     /**
-     *  Do all required cleanup now that we're finished with the
-     *  currently-open .tar.gz
-     *
-     *  @exception java.io.IOException
+     * Do all required cleanup now that we're finished with the currently-open .tar.gz
+     * 
+     * @exception java.io.IOException
      */
     public void finished() throws IOException {
         outputStream.close();
-        if(gzipOutputStream != null) {
+        if (gzipOutputStream != null) {
             gzipOutputStream.close();
         }
     }
 
     /**
-     *  Write the contents of the file to the tar archive.
-     *
-     *  @param entry
-     *  @param contents
-     *  @exception java.io.IOException
-     *  @exception org.eclipse.core.runtime.CoreException
+     * Write the contents of the file to the tar archive.
+     * 
+     * @param entry
+     * @param contents
+     * @exception java.io.IOException
+     * @exception org.eclipse.core.runtime.CoreException
      */
     private void write(TarEntry entry, String contents) throws IOException, CoreException {
-               
+
         java.io.File localFile = new File(contents);
-        
+
         if (!localFile.exists()) {
             throw new FileNotFoundException(contents);
         }
-               
+
         entry.setSize(localFile.length());
-        
+
         outputStream.putNextEntry(entry);
         InputStream contentStream = new FileInputStream(contents);
         try {
@@ -97,21 +96,20 @@ public class TarFileExporterFullPath implements IFileExporterFullPath {
             }
         }
 
-        outputStream.closeEntry();      
+        outputStream.closeEntry();
     }
 
     /**
-     *  Write the passed resource to the current archive.
-     *
-     *  @param resource org.eclipse.core.resources.IFile
-     *  @param destinationPath java.lang.String
-     *  @exception java.io.IOException
-     *  @exception org.eclipse.core.runtime.CoreException
+     * Write the passed resource to the current archive.
+     * 
+     * @param resource org.eclipse.core.resources.IFile
+     * @param destinationPath java.lang.String
+     * @exception java.io.IOException
+     * @exception org.eclipse.core.runtime.CoreException
      */
-    public void write(String resource, String destinationPath)
-            throws IOException, CoreException {
+    public void write(String resource, String destinationPath) throws IOException, CoreException {
 
-        TarEntry newEntry = new TarEntry(destinationPath);       
+        TarEntry newEntry = new TarEntry(destinationPath);
         write(newEntry, resource);
     }
 }
