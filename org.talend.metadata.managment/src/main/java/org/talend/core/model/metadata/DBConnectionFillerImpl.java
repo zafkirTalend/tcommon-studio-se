77fe3541397c4edb13c1014468bcea06eee15aa9
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-03 08:08:02 +0000

    bug TDI-29051 fixed: HSQLDB WebServer shutdown when do Check connection in studio
    git-svn-id: http://talendforge.org/svn/tos/trunk@115935 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/DBConnectionFillerImpl.java
+++ org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/DBConnectionFillerImpl.java
@@ -439,7 +439,7 @@ public class DBConnectionFillerImpl extends MetadataFillerImpl {
                                         EDatabaseTypeName.HSQLDB_IN_PROGRESS.getDisplayName())
                                 && !((DatabaseConnection) dbConn).getDatabaseType().equals(
                                         EDatabaseTypeName.HSQLDB_SERVER.getDisplayName())
-                                & !((DatabaseConnection) dbConn).getDatabaseType().equals(
+                                && !((DatabaseConnection) dbConn).getDatabaseType().equals(
                                         EDatabaseTypeName.HSQLDB_WEBSERVER.getDisplayName())) {
                             String databaseOnConnWizard = ((DatabaseConnection) dbConn).getSID();
                             // If the SID on ui is not empty, the catalog name should be same to this SID name.
