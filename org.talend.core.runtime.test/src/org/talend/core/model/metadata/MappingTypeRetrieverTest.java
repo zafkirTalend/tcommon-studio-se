f7cd959c14b6a400fe0cab6acd1d2976c84c5769
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-08 03:30:19 +0000

    bug TDI-29122 fixed: Fix junit in MappingTypeRetrieverTest.java
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_4@116081 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime.test/src/org/talend/core/model/metadata/MappingTypeRetrieverTest.java
+++ org.talend.core.runtime.test/src/org/talend/core/model/metadata/MappingTypeRetrieverTest.java
@@ -12,17 +12,11 @@
 // ============================================================================
 package org.talend.core.model.metadata;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
-import java.util.Map;
+import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.talend.core.model.metadata.types.DBTypeUtil;
-import org.talend.core.model.metadata.types.JavaTypesManager;
 
 /**
  * DOC Administrator class global comment. Detailled comment
@@ -55,26 +49,9 @@ public class MappingTypeRetrieverTest {
     public void testIsLengthIgnored() {
         String dbmsId = "id_MSSQL";
         String dbType = "int";
-        if (isExtensionLengthIgnored(dbmsId, dbType)) {
-            assertTrue(true);
-        }
-        Dbms dbms = MetadataTalendType.getDbms(dbmsId);
-        List ignoreLP = dbms.getIgnoreLengthPrecision();
-        String ignore = new String(""); //$NON-NLS-1$
-        for (int i = 0; i < ignoreLP.size(); i++) {
-            DbIgnoreLengthAndPrecision dbIgnore = (DbIgnoreLengthAndPrecision) ignoreLP.get(i);
-            if (dbIgnore.getDbType().equalsIgnoreCase(dbType)) {
-                ignore = dbIgnore.getIgnoreLength();
-                if (ignore == null) {
-                    assertFalse(false);
-                } else if (ignore.equals("true")) { //$NON-NLS-1$
-                    assertTrue(true);
-                } else {
-                    assertFalse(false);
-                }
-            }// end if
-        }// end for
-        assertFalse(false);
+        MappingTypeRetriever mappingType = MetadataTalendType.getMappingTypeRetriever(dbmsId);
+        assertTrue(mappingType.isLengthIgnored(dbmsId, dbType));
+
     }
 
     /**
@@ -86,61 +63,8 @@ public class MappingTypeRetrieverTest {
     public void testIsPrecisionIgnored() {
         String dbmsId = "id_MSSQL";
         String dbType = "int";
-        if (isExtensionPrecisionIgnored(dbmsId, dbType)) {
-            assertTrue(true);
-        }
-
-        Dbms dbms = MetadataTalendType.getDbms(dbmsId);
-        List ignoreLP = dbms.getIgnoreLengthPrecision();
-        String ignore = new String(""); //$NON-NLS-1$
-        for (int i = 0; i < ignoreLP.size(); i++) {
-            DbIgnoreLengthAndPrecision dbIgnore = (DbIgnoreLengthAndPrecision) ignoreLP.get(i);
-            if (dbIgnore.getDbType().equalsIgnoreCase(dbType)) {
-                ignore = dbIgnore.getIgnorePrecision();
-                if (ignore == null) {
-                    assertFalse(false);
-                } else if (ignore.equals("true")) { //$NON-NLS-1$
-                    assertTrue(true);
-                } else {
-                    assertFalse(false);
-                }
-            }// end if
-        }// end for
-        assertFalse(false);
-    }
-
-    private boolean isExtensionLengthIgnored(String dbmsId, String dbType) {
-        Map<String, Map<String, List<DBTypeUtil>>> javaTypeMappingFromExtension = JavaTypesManager
-                .getJavaTypeMappingFromExtension();
-        for (String id : javaTypeMappingFromExtension.keySet()) {
-            for (String mappingId : javaTypeMappingFromExtension.get(id).keySet()) {
-                if (dbmsId.equals(mappingId)) {
-                    for (DBTypeUtil dbTypeUtil : javaTypeMappingFromExtension.get(id).get(mappingId)) {
-                        if (dbTypeUtil.getDbTypeName().equals(dbType)) {
-                            return dbTypeUtil.isIgnoreLength();
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean isExtensionPrecisionIgnored(String dbmsId, String dbType) {
-        Map<String, Map<String, List<DBTypeUtil>>> javaTypeMappingFromExtension = JavaTypesManager
-                .getJavaTypeMappingFromExtension();
-        for (String id : javaTypeMappingFromExtension.keySet()) {
-            for (String mappingId : javaTypeMappingFromExtension.get(id).keySet()) {
-                if (dbmsId.equals(mappingId)) {
-                    for (DBTypeUtil dbTypeUtil : javaTypeMappingFromExtension.get(id).get(mappingId)) {
-                        if (dbTypeUtil.getDbTypeName().equals(dbType)) {
-                            return dbTypeUtil.isIgnorePrecision();
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+        MappingTypeRetriever mappingType = MetadataTalendType.getMappingTypeRetriever(dbmsId);
+        assertTrue(mappingType.isPrecisionIgnored(dbmsId, dbType));
     }
 
 }
