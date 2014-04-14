62592e73087d361695774f40e3976b5ad8467c5b
cmeng <cmeng@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-14 09:51:05 +0000

    bug TDI-29092 fixed : Some character are lost when modify context name
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_4@116325 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/core/model/utils/ParameterValueUtil.java
+++ org.talend.core.runtime/src/main/java/org/talend/core/model/utils/ParameterValueUtil.java
@@ -12,17 +12,17 @@
 // ============================================================================
 package org.talend.core.model.utils;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
 
 import org.apache.oro.text.regex.MalformedPatternException;
 import org.apache.oro.text.regex.Pattern;
 import org.apache.oro.text.regex.PatternCompiler;
-import org.apache.oro.text.regex.PatternMatcher;
 import org.apache.oro.text.regex.Perl5Compiler;
 import org.apache.oro.text.regex.Perl5Matcher;
 import org.apache.oro.text.regex.Perl5Substitution;
-import org.apache.oro.text.regex.Substitution;
 import org.apache.oro.text.regex.Util;
 import org.eclipse.emf.common.util.EList;
 import org.talend.core.model.context.UpdateContextVariablesHelper;
@@ -46,8 +46,9 @@ public final class ParameterValueUtil {
             return;
         }
         boolean flag = true;
-        if (param.getFieldType() == EParameterFieldType.MEMO_SQL)
+        if (param.getFieldType() == EParameterFieldType.MEMO_SQL) {
             flag = false;
+        }
         if (param.getValue() instanceof String) { // for TEXT / MEMO etc..
             String value = (String) param.getValue();
             if (value.contains(oldName)) {
@@ -100,7 +101,7 @@ public final class ParameterValueUtil {
             // replace
             String returnValue = "";
             if (value.contains(TalendQuoteUtils.getQuoteChar()) && !flag) {
-                returnValue = splitQueryData(matcher, pattern, substitution, value, Util.SUBSTITUTE_ALL);
+                returnValue = splitQueryData(oldName, newName, value);
             } else {
                 returnValue = Util.substitute(matcher, pattern, substitution, value, Util.SUBSTITUTE_ALL);
             }
@@ -111,29 +112,53 @@ public final class ParameterValueUtil {
 
     }
 
-    // for bug 12594 split ";
-    public static String splitQueryData(PatternMatcher matcher, Pattern pattern, Substitution sub, String value, int numSubs) {
-        String[] split = value.split("\"");
-        int i = 0;
-        String replace = "";
-        for (String s : split) {
-            if (i % 2 == 0) {
-                replace = s;
-                if (i != 0) {
-                    if (matcher.contains(value, pattern)) {
-                        replace = split[i].toString();
-                        split[i] = Util.substitute(matcher, pattern, sub, replace, numSubs);
-                    }
-                }
-            }
-            i++;
-        }
+    // for bug 12594 split "; for bug 29092(it has JUnits)
+    public static String splitQueryData(String oldName, String newName, String value) {
+        // example:"drop table "+context.oracle_schema+".\"TDI_26803\""
+        // >>>>>>>>_△_(const)__ _____△_(varible)_______ __△_(const)___
+        String regex = "\"\"|\".*?([^\\\\]\")";
+        // obtain all varibles
+        String[] split = value.split(regex);
+        Map<String, String> replacedStrings = new HashMap<String, String>();
         String returnValue = "";
-        for (int t = 1; t < split.length; t++) {
-            if (t % 2 == 0) {
-                returnValue += split[t];
-            } else {
-                returnValue += "\"" + split[t] + "\"";
+        // replace the variables & store both value of old and new
+        for (String s : split) {
+            if (s.contains(oldName)) {
+                replacedStrings.put(s, s.replaceAll("\\b" + oldName + "\\b", newName));
+            }
+        }
+        // obtain consts & concat the consts with the variables
+        java.util.regex.Pattern pattern = java.util.regex.Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(value);
+        if (matcher.find()) {
+            int x = matcher.start();
+            int y = matcher.end();
+            int curPos = 0;
+            int valueLength = value.length();
+            String oldFill = null;
+            String newFill = null;
+            while (true) {
+                if (curPos == valueLength) {
+                    break;
+                }
+                if (curPos < x) {
+                    oldFill = value.substring(curPos, x);
+                    if ((newFill = replacedStrings.get(oldFill)) != null) {
+                        returnValue += newFill;
+                    } else {
+                        returnValue += oldFill;
+                    }
+                    curPos = x;
+                    continue;
+                }
+                returnValue += matcher.group();
+                curPos = y;
+                if (!matcher.find()) {
+                    x = valueLength;
+                } else {
+                    x = matcher.start();
+                    y = matcher.end();
+                }
             }
         }
         return returnValue;
@@ -176,7 +201,7 @@ public final class ParameterValueUtil {
                 for (ElementValueType valueType : (List<ElementValueType>) elementValue) {
                     if (valueType.getValue() != null) { // cell is text so
                         // test data
-                        if (ParameterValueUtil.valueContains((String) valueType.getValue(), name)) {
+                        if (ParameterValueUtil.valueContains(valueType.getValue(), name)) {
                             return true;
                         }
                     }
