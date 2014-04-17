b65dd243e248b0c0ae5aa480aee831e37fe5f7ab
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-17 05:57:08 +0000

    bug  TDI-29121 : add junit test
    git-svn-id: http://talendforge.org/svn/tos/trunk@116506 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core.runtime.test/src/org/talend/core/model/process/ElementParameterParserTest.java
@@ -0,0 +1,64 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.process;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * created by wchen on 2014-4-16 Detailled comment
+ * 
+ */
+public class ElementParameterParserTest {
+
+    @Test
+    public void testCanEncrypt() {
+        String paramName = "__PASSWORD__";
+        // mock parameter
+        IElementParameter parameter = mock(IElementParameter.class);
+        when(parameter.getVariableName()).thenReturn(paramName);
+        when(parameter.getName()).thenReturn("PASSWORD");
+
+        // mock the node
+        IElement node = mock(IElement.class);
+        List elementParametersWithChildrens = new ArrayList();
+        elementParametersWithChildrens.add(parameter);
+        when(node.getElementParametersWithChildrens()).thenReturn(elementParametersWithChildrens);
+
+        // "ab"
+        when(parameter.getValue()).thenReturn("\"ab\"");
+        assertTrue(ElementParameterParser.canEncrypt(node, paramName));
+        // "a\"b"
+        when(parameter.getValue()).thenReturn("\"a\\\"b\"");
+        assertTrue(ElementParameterParser.canEncrypt(node, paramName));
+        // "a\\b"
+        when(parameter.getValue()).thenReturn("\"a\\\\b\"");
+        assertTrue(ElementParameterParser.canEncrypt(node, paramName));
+
+        // "a\\\\b"
+        when(parameter.getValue()).thenReturn("\"a\\\\\\\\b\"");
+        assertTrue(ElementParameterParser.canEncrypt(node, paramName));
+
+        // "test"+context.mypassword + "a"
+        when(parameter.getValue()).thenReturn("\"test\"+context.mypassword + \"a\"");
+        assertFalse(ElementParameterParser.canEncrypt(node, paramName));
+        // "a" + "b"
+        when(parameter.getValue()).thenReturn("\"a\" + \"b\"");
+        assertFalse(ElementParameterParser.canEncrypt(node, paramName));
+    }
+}
