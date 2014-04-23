da992f2169e408ce081a1563ea87d86e521329b6
sgandon <sgandon@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-23 13:49:26 +0000

    Fix Bug USV-69 : Migration tasks of Jobs/Joblets: provide an extension for additional ERepositoryObjectTypes https://jira.talendforge.org/browse/USV-69
    git-svn-id: http://talendforge.org/svn/tos/trunk@116812 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.core.runtime.test/src/org/talend/core/model/migration/ContextItem2JobDocExtContentHandler.java
@@ -0,0 +1,188 @@
+package org.talend.core.model.migration;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.ui.IWorkbench;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.image.IImage;
+import org.talend.core.model.metadata.builder.connection.MetadataTable;
+import org.talend.core.model.properties.ContextItem;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.Status;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.repository.IRepositoryContentHandler;
+import org.talend.core.model.repository.IRepositoryTypeProcessor;
+import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.repository.model.IRepositoryNode;
+import org.talend.repository.model.RepositoryNode;
+
+/**
+ * used to return the JOB_DOC_EXT RepoObjectType when an item of type ContextItem is used
+ * */
+public class ContextItem2JobDocExtContentHandler implements IRepositoryContentHandler {
+
+    public ContextItem2JobDocExtContentHandler() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Resource create(IProject project, Item item, int classifierID, IPath path) throws PersistenceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Resource createScreenShotResource(IProject project, Item item, int classifierID, IPath path)
+            throws PersistenceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Resource saveScreenShots(Item item) throws PersistenceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void copyScreenShotFile(Item originalItem, Item newItem) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Resource save(Item item) throws PersistenceException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IImage getIcon(ERepositoryObjectType type) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Item createNewItem(ERepositoryObjectType type) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isProcess(Item item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isRepObjType(ERepositoryObjectType type) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ERepositoryObjectType getProcessType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ERepositoryObjectType getCodeType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ERepositoryObjectType getRepositoryObjectType(Item item) {
+        return item instanceof ContextItem ? ERepositoryObjectType.getType("JOB_DOC_EXT") : null;
+    }
+
+    @Override
+    public void addNode(ERepositoryObjectType type, RepositoryNode recBinNode, IRepositoryViewObject repositoryObject,
+            RepositoryNode node) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addContents(Collection<EObject> collection, Resource resource) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public IImage getIcon(Item item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ERepositoryObjectType getHandleType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasSchemas() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public List<Status> getPropertyStatus(Item item) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hideAction(IRepositoryNode node, Class actionType) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isOwnTable(IRepositoryNode node, Class type) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public IWizard newWizard(IWorkbench workbench, boolean creation, RepositoryNode node, String[] existingNames) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IWizard newSchemaWizard(IWorkbench workbench, boolean creation, IRepositoryViewObject object,
+            MetadataTable metadataTable, String[] existingNames, boolean forceReadOnly) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteNode(IRepositoryViewObject repViewObject) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public IRepositoryTypeProcessor getRepositoryTypeProcessor(String repositoryType) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public URI getReferenceFileURI(Item item, String extension) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
