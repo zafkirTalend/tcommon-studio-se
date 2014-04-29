4d39c53957c15311df3bb8658b58c0697d03dc90
ldong <ldong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-29 08:45:22 +0000

    bug TDI-29305 fixed:First time open demo job,there is miss jar warning when these jars already exist in studio.
    git-svn-id: http://talendforge.org/svn/tos/trunk@117003 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/model/service/JavaLibrariesService.java
+++ org.talend.librariesmanager/src/main/java/org/talend/librariesmanager/model/service/JavaLibrariesService.java
@@ -177,10 +177,10 @@ public class JavaLibrariesService extends AbstractLibrariesService {
 
     @Override
     public void checkInstalledLibraries() {
+        List<ModuleNeeded> toCheck = ModulesNeededProvider.getModulesNeeded();
         Set<String> existLibraries = repositoryBundleService.list(false);
         Set<String> existDllLibraries = repositoryBundleService.listAllDllFiles();
         existLibraries.addAll(existDllLibraries);
-        List<ModuleNeeded> toCheck = ModulesNeededProvider.getModulesNeeded();
         for (ModuleNeeded current : toCheck) {
             if (existLibraries.contains(current.getModuleName())) {
                 current.setStatus(ELibraryInstallStatus.INSTALLED);
