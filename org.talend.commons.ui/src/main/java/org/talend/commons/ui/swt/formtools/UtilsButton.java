a552530ec5d3d2058657a937047d21a1b779e445
cmeng <cmeng@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-05-29 07:35:13 +0000

    bug TUP-2026 fixed : Small window to add the parameters when creating some database connection
    git-svn-id: http://talendforge.org/svn/tos/trunk@118067 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/formtools/UtilsButton.java
+++ org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/formtools/UtilsButton.java
@@ -21,6 +21,7 @@ import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 /**
  * Dimensionned Button ( defined width & height OR use FillLayout).
@@ -194,5 +195,9 @@ public class UtilsButton {
      */
     public String getText() {
         return button.getText();
+    }
+
+    public Control getControl() {
+        return button;
     }
 }
