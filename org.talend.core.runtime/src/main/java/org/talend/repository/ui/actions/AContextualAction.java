fe765e928929a72f8f5006d1b517ceb8995c6e64
ldong <ldong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-06-11 10:27:46 +0000

    bug TDI-29748 commited:Collaborative work problem when one user change the item's version.
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_5@118472 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.core.runtime/src/main/java/org/talend/repository/ui/actions/AContextualAction.java
+++ org.talend.core.runtime/src/main/java/org/talend/repository/ui/actions/AContextualAction.java
@@ -23,6 +23,7 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
@@ -272,7 +273,13 @@ public abstract class AContextualAction extends Action implements ITreeContextua
         }
         if (activePart instanceof IRepositoryView) {
             workbenchPart = activePart;
-            return ((IRepositoryView) activePart).getViewer().getSelection();
+            ISelection selection = ((IRepositoryView) activePart).getViewer().getSelection();
+            if (!selection.isEmpty()) {
+                return ((IRepositoryView) activePart).getViewer().getSelection();
+            } else {
+                selection = new StructuredSelection(node);
+                return selection;
+            }
         }
 
         if (workbenchPart != null) {
