e4f69d1e958beda04dd58c6c4a4ad935f5070fd9
Sébastien Gandon <sgandon@talend.com>
2014-06-22 14:47:56 +0200

    [GIT Migration] adding org.talend.testutils and test.all.test.suite to maintenance/5.1

---
+++ test/plugins/test.all.test.suite/src/org/talend/test/TalendJunitsTestCollector.java
@@ -0,0 +1,43 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.test;
+
+import test.common.BundleTestCollector;
+
+/**
+ * DOC nrousseau class global comment. Detailled comment
+ */
+public class TalendJunitsTestCollector extends TalendTestCollector {
+
+    @Override
+    public Class<?>[] getTests() {
+        Class<?>[] testsToExecute = super.getTests();
+        BundleTestCollector testCollector = new BundleTestCollector();
+        Class<?>[] allCollectedTestClasses = testCollector.collectTestsClasses(null, "org.talend.designer.codegen.test", "",
+                "org.talend.designer.codegen.junit", "JUnitStudioStartup", false);
+
+        // add a loop, but in all case there should be only one test executed here.
+
+        for (Class<?> clazz : allCollectedTestClasses) {
+            System.out.println("## To execute first:" + clazz.getName() + "\n");
+            Class<?>[] tempArray = new Class<?>[testsToExecute.length + 1];
+            tempArray[0] = clazz;
+            for (int i = 0; i < testsToExecute.length; i++) {
+                tempArray[i + 1] = testsToExecute[i];
+            }
+            testsToExecute = tempArray;
+        }
+        return testsToExecute;
+    }
+
+}
