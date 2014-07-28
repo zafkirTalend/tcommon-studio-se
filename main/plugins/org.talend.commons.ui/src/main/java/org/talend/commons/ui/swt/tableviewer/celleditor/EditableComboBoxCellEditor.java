6675ffcbda31672388d17f6a2dbb21e91b966b57
GGu <ggu@talend.com>
2014-07-28 17:36:09 +0800

    https://jira.talendforge.org/browse/TDI-30128: Add the EditableComboBoxCellEditor.

---
+++ main/plugins/org.talend.commons.ui/src/main/java/org/talend/commons/ui/swt/tableviewer/celleditor/EditableComboBoxCellEditor.java
@@ -0,0 +1,86 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons.ui.swt.tableviewer.celleditor;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * created by ggu on Jul 28, 2014 Detailled comment
+ *
+ */
+public class EditableComboBoxCellEditor extends ComboBoxCellEditor {
+
+    private CCombo comboList;
+
+    public EditableComboBoxCellEditor(Composite parent, String[] items) {
+        super(parent, items);
+    }
+
+    @Override
+    protected Control createControl(Composite parent) {
+        Control control = super.createControl(parent);
+        if (control instanceof CCombo) {
+            comboList = ((CCombo) control);
+            comboList.setEditable(true);
+        }
+        return control;
+    }
+
+    @Override
+    protected Object doGetValue() {
+        Object curValue = super.doGetValue();
+        if (curValue instanceof Integer) {
+            Integer index = (Integer) curValue;
+            if (index == -1) { // new value, so -1
+                curValue = comboList.getText();
+            } else { // in the list, try to find out.
+                String[] items = this.getItems();
+                if (items.length > index) {
+                    curValue = items[index];
+                }
+            }
+        }
+        return curValue;
+    }
+
+    @Override
+    protected void doSetValue(Object newvalue) {
+        // only deal with the String value.
+        Assert.isTrue(comboList != null && (newvalue instanceof String));
+        String value = newvalue.toString();
+        int index = findValueIndex(getItems(), value);
+        super.doSetValue(index); // make sure update the selection, even -1
+        if (index < 0) { // not found
+            comboList.setText(value);
+        }
+    }
+
+    private int findValueIndex(String[] items, String fValue) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].equals(fValue)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        comboList = null;
+    }
+}
