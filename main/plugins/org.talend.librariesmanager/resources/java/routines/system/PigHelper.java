89a2b8f55d12ae60c8cb660288de781561274142
remydub <rdubois.contact@gmail.com>
2014-08-04 11:33:35 +0200

    TBD-895: Add a method in the PigHelper to define functions

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/PigHelper.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/PigHelper.java
@@ -10,7 +10,6 @@ public class PigHelper {
 
     private java.util.List<String[]> pigLatins = new java.util.ArrayList<String[]>();
 
-    
     public java.util.List<String[]> getPigLatins() {
         return this.pigLatins;
     }
@@ -19,6 +18,14 @@ public class PigHelper {
         String[] pl = new String[2];
         pl[0] = type;
         pl[1] = pigLatin;
+        pigLatins.add(pl);
+    }
+
+    public void add(String type, String alias, String function) {
+        String[] pl = new String[3];
+        pl[0] = type;
+        pl[1] = alias;
+        pl[2] = function;
         pigLatins.add(pl);
     }
 
