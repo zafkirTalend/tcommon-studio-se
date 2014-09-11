ec5073e8741cd8a297ba3f2f1280f829644fdb73
hcyi <hcyi@talend.com>
2014-09-11 14:06:39 +0800

    TDI-30423:Slow to retrieve tables on AS400 on the wizard,fix a problem that When LIB empty, ALL libraries' tables should be retrieved.

--- main/plugins/org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/DBConnectionFillerImpl.java
+++ main/plugins/org.talend.metadata.managment/src/main/java/org/talend/core/model/metadata/DBConnectionFillerImpl.java
@@ -659,6 +659,7 @@ public class DBConnectionFillerImpl extends MetadataFillerImpl {
                 }
                 String pattern = ExtractMetaDataUtils.getInstance().retrieveSchemaPatternForAS400(
                         iMetadataCon.getAdditionalParams());
+                String sid = dbConnection.getSID();
                 if (pattern != null && !"".equals(pattern)) { //$NON-NLS-1$
                     String[] multiSchems = ExtractMetaDataUtils.getInstance().getMultiSchems(pattern);
                     if (multiSchems != null) {
@@ -668,8 +669,8 @@ public class DBConnectionFillerImpl extends MetadataFillerImpl {
                             }
                         }
                     }
-                } else {
-                    scheamFilterList.add(dbConnection.getSID());
+                } else if (sid != null && !"".equals(sid)) { //$NON-NLS-1$
+                    scheamFilterList.add(sid);
                 }
             }
         } else {
