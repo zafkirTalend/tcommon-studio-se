c882ed675c2859560fa659dfca0c6be6f5339d5f
ldong-talend <ldong@talend.com>
2014-10-10 17:47:23 +0800

    TDI-30715:fix TDI-30715 and TDI-30521 together and improved some code.

--- main/plugins/org.talend.utils/src/main/java/org/talend/utils/sql/ConnectionUtils.java
+++ main/plugins/org.talend.utils/src/main/java/org/talend/utils/sql/ConnectionUtils.java
@@ -364,6 +364,22 @@ public final class ConnectionUtils {
         return false;
     }
 
+    public static boolean isOracleForSid(DatabaseMetaData metadata, String oracleProduct) throws SQLException {
+        if (metadata != null && metadata.getDatabaseProductName() != null
+                && metadata.getDatabaseProductName().indexOf(oracleProduct) > -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isSqlite(DatabaseMetaData metadata, String sqliteProduct) throws SQLException {
+        if (metadata != null && metadata.getDatabaseProductName() != null
+                && metadata.getDatabaseProductName().toUpperCase().indexOf(sqliteProduct) > -1) {
+            return true;
+        }
+        return false;
+    }
+
     public static boolean isPostgresql(DatabaseMetaData metadata) {
         boolean result = false;
         try {
@@ -437,4 +453,5 @@ public final class ConnectionUtils {
         }
         return false;
     }
+
 }
