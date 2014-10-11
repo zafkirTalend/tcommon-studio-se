ce0825268deca60dc968d27b567d396d5253d291
zwzhao-talend <zwzhao@talend.com>
2014-10-11 18:20:08 +0800

    TDI-30280 : The DB Type is null and Length is max int value for Impala schema

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/database/EDatabaseTypeName.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/database/EDatabaseTypeName.java
@@ -113,8 +113,7 @@ public enum EDatabaseTypeName {
     REDSHIFT(
              "Redshift", "Redshift", Boolean.TRUE, "REDSHIFT", "REDSHIFT", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
-    IMPALA(
-           "IMPALA", "Impala", Boolean.TRUE, "CLOUDERA", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
+    IMPALA("IMPALA", "Impala", Boolean.TRUE, "HIVE", EDatabaseSchemaOrCatalogMapping.None, EDatabaseSchemaOrCatalogMapping.Schema), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
 
     HBASE(
           "HBase", "HBase", Boolean.FALSE, "HBASE", EDatabaseSchemaOrCatalogMapping.Sid, EDatabaseSchemaOrCatalogMapping.Column_Family, true); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
