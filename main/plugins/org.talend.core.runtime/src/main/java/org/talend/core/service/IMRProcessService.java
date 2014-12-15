b308d482ecdcc359f6fea2a7382a0b9d318ad143
hwang <hwang@talend.com>
2014-12-15 17:20:39 +0800

    TBD-1315:MR Job：Map icon lost after deactive MR job

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IMRProcessService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IMRProcessService.java
@@ -17,6 +17,7 @@ import java.util.List;
 import org.eclipse.ui.IEditorPart;
 import org.talend.core.IService;
 import org.talend.core.model.general.Project;
+import org.talend.core.model.process.INode;
 import org.talend.core.model.process.IProcess2;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.repository.IRepositoryViewObject;
@@ -46,4 +47,6 @@ public interface IMRProcessService extends IService {
     public void generateMRInfosParameter(IProcess2 process);
 
     public boolean isProgressBarNeeded(IProcess2 process);
+
+    public void refreshMRStatus(List<INode> nodeList);
 }
