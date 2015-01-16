882a15bb1958b2d2ae7ba71fe846822a9f3b01ac
ldong-talend <ldong@talend.com>
2015-01-16 15:44:09 +0800

    TDI-31692:Can't open add schema sub window when I click edit schema button for txxxInput for tos_bd.

---
+++ main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/utils/MetaDataDialogUtil.java
@@ -0,0 +1,38 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.ui.utils;
+
+import org.talend.core.model.metadata.IDatabaseConstant;
+import org.talend.core.model.process.EParameterFieldType;
+import org.talend.core.model.process.IElementParameter;
+import org.talend.core.model.process.INode;
+
+/**
+ * created by ldong on Jan 16, 2015 Detailled comment
+ * 
+ */
+public class MetaDataDialogUtil {
+
+    public static final String PROPERTY = "PROPERTY"; //$NON-NLS-1$
+
+    public static boolean isRedShiftNode(INode node) {
+        if (node != null) {
+            IElementParameter param = node.getElementParameter(PROPERTY);
+            if (param != null && param.getFieldType() == EParameterFieldType.PROPERTY_TYPE
+                    && IDatabaseConstant.REDSHIFT.equals(param.getRepositoryValue())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
