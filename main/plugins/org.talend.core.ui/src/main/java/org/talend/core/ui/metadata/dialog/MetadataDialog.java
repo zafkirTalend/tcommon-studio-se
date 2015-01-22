32eb940bb898271e56ebfceb822bae80391ec6e7
hcyi <hcyi@talend.com>
2015-01-22 18:06:25 +0800

    TDI-31708:SAP Bapi:NPE error when I click edit schema button for line if I change the line name

--- main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/metadata/dialog/MetadataDialog.java
+++ main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/metadata/dialog/MetadataDialog.java
@@ -338,7 +338,8 @@ public class MetadataDialog extends Dialog {
                         String key = (String) it.next();
                         if (key.equals("SAP_TABLE_NAME")) {
                             String value = (String) map.get(key);
-                            if (this.outputMetaTable.getLabel().equals(TalendQuoteUtils.removeQuotes(value))) {
+                            String tableLabel = this.outputMetaTable.getLabel();
+                            if (tableLabel != null && tableLabel.equals(TalendQuoteUtils.removeQuotes(value))) {
                                 if (map.containsKey("SCHEMA-TYPE") && map.containsValue("REPOSITORY")) {
                                     nodeModeFlag = true;
                                 }
