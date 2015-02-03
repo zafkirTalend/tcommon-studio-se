e4fa0c9200004a21b77343e826bf962b6298588e
pbailly <pbailly@talend.com>
2015-02-03 18:30:49 +0100

    AvroSchema: Multiple improvement: - AvroMetadaTable handle the element types:     "Object", "List", "Dynamic" and "Document"; - Improve type on the avroschema generated of the basic types; - Remove hand-written path; - Remove temporary dump of the avro schema; - Allow regeneration of the schema.

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/AvroMetadataTable.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/AvroMetadataTable.java
@@ -13,7 +13,7 @@
 package org.talend.core.model.metadata;
 
 import java.io.File;
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,6 +24,7 @@ import org.apache.avro.SchemaBuilder.BaseFieldTypeBuilder;
 import org.apache.avro.SchemaBuilder.FieldAssembler;
 import org.apache.avro.SchemaBuilder.FieldTypeBuilder;
 import org.apache.avro.compiler.specific.SpecificCompiler;
+import org.apache.avro.generic.GenericData.StringType;
 import org.talend.commons.exception.ExceptionHandler;
 
 /**
@@ -33,6 +34,25 @@ import org.talend.commons.exception.ExceptionHandler;
 public class AvroMetadataTable extends MetadataTable {
 
     private Schema schema = null;
+
+    private String filePath;
+
+    private String technicalProjectName;
+
+    private String jobName;
+
+    public AvroMetadataTable(String filePath, String technicalProjectName, String jobName, String jobVersion) {
+        super();
+        // First and last char are quotes.
+        this.filePath = filePath.substring(1, filePath.length() - 1);
+        // Fix path
+        this.filePath = this.filePath.substring(0, filePath.lastIndexOf("/")) + "/.Java/src/"; //$NON-NLS-1$
+
+        this.technicalProjectName = technicalProjectName.toLowerCase();
+
+        this.jobName = jobName + "_" + jobVersion.replace(".", "_"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        this.jobName = this.jobName.toLowerCase();
+    }
 
     @Override
     public IMetadataTable clone(boolean withCustoms) {
@@ -69,108 +89,75 @@ public class AvroMetadataTable extends MetadataTable {
     }
 
     public void setAvroSchema(String connectionName) {
-        if (schema == null) {
-            FieldAssembler<Schema> fieldAssembler = SchemaBuilder.record("AvroStruct" + connectionName)
-                    .prop(connectionName, connectionName)
-                    // TODO set this path generically. (get data from Process class)
-                    .namespace("emr24.s_dfs_0_1") //$NON-NLS-1$
-                    .fields();
 
-            for (IMetadataColumn column : super.getListColumns()) {
-                // Set field name
-                BaseFieldTypeBuilder<Schema> fieldTypeSchema = fieldAssembler.name(column.getLabel()).type();
+        System.out.println(technicalProjectName + "." + jobName);
+        FieldAssembler<Schema> fieldAssembler = SchemaBuilder.record("AvroStruct" + connectionName)
+                .prop(connectionName, connectionName).namespace(technicalProjectName + "." + jobName) //$NON-NLS-1$
+                .fields();
 
-                // Set Nullable
-                if (column.isNullable()) {
-                    fieldTypeSchema = ((FieldTypeBuilder<Schema>) fieldTypeSchema).nullable();
-                }
+        for (IMetadataColumn column : super.getListColumns()) {
+            // Set field name
+            BaseFieldTypeBuilder<Schema> fieldTypeSchema = fieldAssembler.name(column.getLabel()).type();
 
-                // Set field type
-                if ("id_Boolean".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.booleanType().noDefault();
-                } else if ("id_Byte".equals(column.getTalendType())) { //$NON-NLS-1$
-                    // TODO Does not work
-                    fieldTypeSchema.intBuilder().prop("java-class", "java.lang.Byte").endInt().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
-                } else if ("id_byte[]".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.bytesType().noDefault();
-                } else if ("id_Character".equals(column.getTalendType())) { //$NON-NLS-1$
-                    // TODO Does not work
-                    fieldTypeSchema.intBuilder().prop("java-class", "java.lang.Character").endInt().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
-                } else if ("id_Date".equals(column.getTalendType())) { //$NON-NLS-1$
-                    // TODO Does not work
-                    fieldTypeSchema.longBuilder().prop("java-class", "java.util.Date").endLong().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
-                } else if ("id_Double".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.doubleType().noDefault();
-                } else if ("id_Float".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.floatType().noDefault();
-                } else if ("id_BigDecimal".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.stringBuilder().prop("java-class", "java.math.BigDecimal").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
-                } else if ("id_Integer".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.intType().noDefault();
-                } else if ("id_Long".equals(column.getTalendType())) { //$NON-NLS-1$
-                    fieldTypeSchema.longType().noDefault();
-                } else if ("id_Short".equals(column.getTalendType())) { //$NON-NLS-1$
-                    // TODO Copy/past of
-                    // https://avro.apache.org/docs/1.7.6/api/java/org/apache/avro/reflect/package-summary.html
-                    // but does not work
-                    fieldTypeSchema.intBuilder().prop("java-class", "java.lang.Short").endInt().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
-                } else if ("id_String".equals(column.getTalendType())) { //$NON-NLS-1$
-                    // TODO double check string type.
-                    fieldTypeSchema.stringType().noDefault();
-                    // } else if ("id_List".equals(column.getTalendType())) {
-                    // // TODO
-                    // } else if ("id_Object".equals(column.getTalendType())) {
-                    // //TODO
-                }
+            // Set Nullable
+            if (column.isNullable()) {
+                fieldTypeSchema = ((FieldTypeBuilder<Schema>) fieldTypeSchema).nullable();
             }
-            schema = fieldAssembler.endRecord();
-            System.out.println(schema);
 
-            PrintWriter out;
-            try {
-                // Generate the schema output and dump it on a file
-                // TODO create /tmp/avsc on runtime
-                out = new PrintWriter("/tmp/avsc/AvroStruct" + connectionName + ".avsc");
-                out.println(schema);
-                out.close();
-
-                // Generate the java class from the schema
-                SpecificCompiler.compileSchema(new File("/tmp/avsc/AvroStruct" + connectionName + ".avsc"), new File(
-                        "/home/pbailly/yoxosluna/.metadata/.plugins/org.eclipse.pde.core/TP_BD_DQ/workspace2/.Java/src/"));
-
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+            // Set field type
+            if ("id_Boolean".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.booleanType().noDefault();
+            } else if ("id_Byte".equals(column.getTalendType())) { //$NON-NLS-1$
+                // TODO No native type byte
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.lang.Byte").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_byte[]".equals(column.getTalendType())) { //$NON-NLS-1$
+                // TODO is a byteBuffer
+                fieldTypeSchema.bytesType().noDefault();
+            } else if ("id_Character".equals(column.getTalendType())) { //$NON-NLS-1$
+                // TODO No native type char
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.lang.Character").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_Date".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.util.Date").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_Double".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.doubleType().noDefault();
+            } else if ("id_Float".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.floatType().noDefault();
+            } else if ("id_BigDecimal".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.math.BigDecimal").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_Integer".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.intType().noDefault();
+            } else if ("id_Long".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.longType().noDefault();
+            } else if ("id_Short".equals(column.getTalendType())) { //$NON-NLS-1$
+                // TODO No native type short
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.lang.Short").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_String".equals(column.getTalendType())) { //$NON-NLS-1$
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.lang.String").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_List".equals(column.getTalendType())) {
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.util.List").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_Document".equals(column.getTalendType())) {
+                fieldTypeSchema.stringBuilder().prop("java-class", "routines.system.Document").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else if ("id_Dynamic".equals(column.getTalendType())) {
+                fieldTypeSchema.stringBuilder().prop("java-class", "routines.system.Dynamic").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
+            } else { // Object
+                fieldTypeSchema.stringBuilder().prop("java-class", "java.lang.Object").endString().noDefault(); //$NON-NLS-1$  //$NON-NLS-2$
             }
         }
-    }
+        schema = fieldAssembler.endRecord();
 
-    @Override
-    public void setListColumns(List<IMetadataColumn> listColumns) {
-        super.setListColumns(listColumns);
-    }
+        // TODO remove System out
+        System.out.println(schema);
 
-    @Override
-    public void setLabel(String label) {
-        super.setLabel(label);
-        System.out.println(label);
-    }
-
-    /**
-     * Getter for schema.
-     * 
-     * @return the schema
-     */
-    public Schema getSchema() {
-        return this.schema;
-    }
-
-    /**
-     * Note: for a table with custom columns, the order for the test is really important. It should be
-     * outputMetadata.sameMetadataAs (inputMetadata).
-     */
-    @Override
-    public boolean sameMetadataAs(IMetadataTable input, int options) {
-        return super.sameMetadataAs(input, options);
+        try {
+            // Generate the java class from the schema
+            SpecificCompiler compiler = new SpecificCompiler(schema);
+            // Allow String java class
+            compiler.setStringType(StringType.String);
+            // No source, since we just want to parse the input schema
+            compiler.compileToDestination(null, new File(filePath));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
