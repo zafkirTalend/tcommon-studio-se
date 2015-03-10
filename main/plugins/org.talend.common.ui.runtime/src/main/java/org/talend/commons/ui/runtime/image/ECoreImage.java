effe8a335c3465927498dac9ac3f23ce8fb1462e
xhkong <xhkong@talend.com>
2015-03-10 11:08:39 +0800

    TUP-2712 new feature:Replace the icons of the repository https://jira.talendforge.org/browse/TUP-2712

--- main/plugins/org.talend.common.ui.runtime/src/main/java/org/talend/commons/ui/runtime/image/ECoreImage.java
+++ main/plugins/org.talend.common.ui.runtime/src/main/java/org/talend/commons/ui/runtime/image/ECoreImage.java
@@ -45,20 +45,20 @@ public enum ECoreImage implements IImage {
 
     RECYCLE_BIN_EMPTY_ICON("/icons1/recyclebinempty.png"), //$NON-NLS-1$
     RECYCLE_BIN_FULL_ICON("/icons1/recyclebinfull.png"), //$NON-NLS-1$
-    FOLDER_OPEN_ICON("/icons1/folder_open.gif"), //$NON-NLS-1$
+    FOLDER_OPEN_ICON("/icons1/folder_open.png"), //$NON-NLS-1$
     FOLDER_CLOSE_ICON("/icons1/folder_close.png"), //$NON-NLS-1$
     FOLDER_WIZ("/icons1/folder_wiz.gif"), //$NON-NLS-1$
 
     SVN_ROOT_ICON("/icons1/svn.gif"), //$NON-NLS-1$
-    BUSINESS_PROCESS_ICON("/icons1/businessProcess.gif"), //$NON-NLS-1$
+    BUSINESS_PROCESS_ICON("/icons1/businessProcess.png"), //$NON-NLS-1$
     BUSINESS_PROCESS_WIZ("/icons1/business_wiz.png"), //$NON-NLS-1$
-    PROCESS_ICON("/icons1/process_icon.gif"), //$NON-NLS-1$
+    PROCESS_ICON("/icons1/process_icon.png"), //$NON-NLS-1$
     PROCESS_WIZ("/icons1/process_wiz.png"), //$NON-NLS-1$
-    PROCESS_TEMPLATE_ICON("/icons1/process_icon.gif"), //$NON-NLS-1$
+    PROCESS_TEMPLATE_ICON("/icons1/process_icon.png"), //$NON-NLS-1$
     PROCESS_TEMPLATE_WIZ("/icons1/process_template_wiz.png"), //$NON-NLS-1$
     SERVICES_ICON("/icons1/services.png"), //$NON-NLS-1$
     JOBLET_ICON("/icons1/joblet_icon.png"), //$NON-NLS-1$
-    CONTEXT_ICON("/icons1/context_icon.gif"), //$NON-NLS-1$
+    CONTEXT_ICON("/icons1/context_icon.png"), //$NON-NLS-1$
     CONTEXT_CONF_ICON("/icons1/context_conf_icon.gif"), //$NON-NLS-1$
     CONTEXT_WIZ("/icons1/context_wiz.png"), //$NON-NLS-1$
     CODE_ICON("/icons1/code.png"), //$NON-NLS-1$
@@ -69,7 +69,7 @@ public enum ECoreImage implements IImage {
     ROUTINE_WIZ("/icons1/routine_wiz.png"), //$NON-NLS-1$
     SNIPPETS_ICON("/icons1/snippet.png"), //$NON-NLS-1$
     // SUBROUTINE_ICON("/icons1/subroutine.gif"),
-    DOCUMENTATION_ICON("/icons1/documentation.gif"), //$NON-NLS-1$
+    DOCUMENTATION_ICON("/icons1/documentation.png"), //$NON-NLS-1$
     DOCUMENTATION_SINGLE_ICON("/icons1/Documentation_feuillet.png"), //$NON-NLS-1$
     DOCUMENTATION_WIZ("/icons1/documentation_wiz.png"), //$NON-NLS-1$
 
