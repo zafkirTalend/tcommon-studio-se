5f65e240a8b57affa900e5f0849e0d80e591ccb5
hwang <hwang@talend.com>
2015-04-20 16:28:51 +0800

    TDI-32265:Impact Analysis is not available in context menu for JSON

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/ui/IJSONProviderService.java
@@ -0,0 +1,31 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.ui;
+
+import java.util.Map;
+
+import org.talend.core.IProviderService;
+import org.talend.core.model.process.INode;
+import org.talend.core.model.repository.ERepositoryObjectType;
+
+/**
+ * DOC wanghong class global comment. Detailled comment
+ */
+public interface IJSONProviderService extends IProviderService {
+
+    public boolean isJSONNode(final INode node);
+
+    public boolean isRepositorySchemaLine(INode node, Map<String, Object> lineValue);
+
+    public ERepositoryObjectType getJSONRepositoryType();
+}
