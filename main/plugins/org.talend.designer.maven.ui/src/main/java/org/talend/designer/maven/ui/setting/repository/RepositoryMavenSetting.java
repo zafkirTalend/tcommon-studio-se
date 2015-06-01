16727f13eae519d9d6ca8c4371a419e5eb956f0e
GGu <ggu@talend.com>
2015-06-01 13:26:43 +0800

    TUP-2840: refactor and enhance the codes.

--- main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/repository/RepositoryMavenSetting.java
+++ main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/repository/RepositoryMavenSetting.java
@@ -12,8 +12,18 @@
 // ============================================================================
 package org.talend.designer.maven.ui.setting.repository;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jface.preference.IPreferenceNode;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.designer.maven.ui.setting.repository.node.RepositoryPreferenceNode;
 import org.talend.designer.maven.ui.setting.repository.tester.IRepositorySettingTester;
+import org.talend.designer.maven.ui.utils.DesignerMavenUiHelper;
+import org.talend.repository.model.IRepositoryNode.ENodeType;
 import org.talend.repository.model.RepositoryNode;
 
 /**
@@ -63,5 +73,80 @@ public abstract class RepositoryMavenSetting {
         this.tester = tester;
     }
 
-    public abstract void createMavenScriptsChildren(final RepositoryPreferenceNode parentNode, final RepositoryNode node);
+    public IPreferenceNode createAndFindMavenFolderNode(final IPreferenceNode parentNode, final ILabelProvider labelProvider,
+            final RepositoryNode node) {
+        if (parentNode == null || labelProvider == null || node == null) {
+            return null;
+        }
+        ERepositoryObjectType contentType = node.getContentType();
+        // mabye it's not good for some sub nodes, for example, if for routines, supported type must containd
+        // CODE.
+        List<ERepositoryObjectType> supportedTypes = Arrays.asList(RepositoryMavenSettingManager.REGISTRY.getSupportTypes());
+        if (!supportedTypes.contains(contentType)) {
+            return null;
+        }
+        String id = buildFolderNodeId(parentNode, labelProvider, node);
+
+        IPreferenceNode foundNode = parentNode.findSubNode(id);
+        if (foundNode == null || foundNode != null && !(foundNode instanceof RepositoryPreferenceNode)) { // not found,
+            IPreferenceNode mavenFolderNode = createFolderNode(id, labelProvider, node);
+            if (mavenFolderNode != null) {
+                // must be front, when add other nodes. make sure the build id is right.
+                parentNode.add(mavenFolderNode);
+                return mavenFolderNode;
+            }
+        } else { // found, just use existed one.
+            return foundNode;
+        }
+
+        return null;
+    }
+
+    protected String buildFolderNodeId(final IPreferenceNode parentNode, final ILabelProvider labelProvider,
+            final RepositoryNode node) {
+        ERepositoryObjectType contentType = node.getContentType();
+        String id = DesignerMavenUiHelper.buildRepositoryPreferenceNodeId(parentNode.getId(), contentType.getType());
+        if (node.getType() == ENodeType.SIMPLE_FOLDER) {
+            String label = labelProvider.getText(node);
+            String parentId = parentNode.getId();
+            if (parentId != null && parentId.length() > 0) {
+                id = DesignerMavenUiHelper.buildRepositoryPreferenceNodeId(parentId, label);
+            } else {
+                id = DesignerMavenUiHelper.buildRepositoryPreferenceNodeId(contentType.getType(), label);
+            }
+        }
+        return id;
+    }
+
+    protected IPreferenceNode createFolderNode(final String id, final ILabelProvider labelProvider, final RepositoryNode node) {
+        ERepositoryObjectType contentType = node.getContentType();
+        ImageDescriptor imageDesc = null;
+        Image image = labelProvider.getImage(node);
+        if (image != null) {
+            imageDesc = ImageDescriptor.createFromImageData(image.getImageData());
+        }
+        String label = labelProvider.getText(node);
+        // label= node.getLabel(); //there is on bug for this label, so use provider directly.
+
+        RepositoryPreferenceNode mavenFolderNode = null;
+        if (!contentType.isResouce() || node.getType() == ENodeType.SYSTEM_FOLDER || node.getType() == ENodeType.SIMPLE_FOLDER) {
+            mavenFolderNode = new RepositoryPreferenceNode(id, label, imageDesc, node);
+
+        }
+        return mavenFolderNode;
+    }
+
+    public boolean needMavenScripts(RepositoryNode node) {
+        if (node != null) {
+            ERepositoryObjectType contentType = node.getContentType();
+            // must be resource item, for root node and simple folder nodes
+            if (contentType != null && contentType.isResourceItem()
+                    && (node.getType() == ENodeType.SYSTEM_FOLDER || node.getType() == ENodeType.SIMPLE_FOLDER)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public abstract void createMavenScriptsChildren(final IPreferenceNode parentNode, final RepositoryNode node);
 }
--- main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/repository/node/RepositoryPreferenceNode.java
+++ main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/repository/node/RepositoryPreferenceNode.java
@@ -111,4 +111,44 @@ public class RepositoryPreferenceNode extends PreferenceNode {
         return this.getId();
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof RepositoryPreferenceNode)) {
+            return false;
+        }
+        RepositoryPreferenceNode other = (RepositoryPreferenceNode) obj;
+        if (getId() == null) {
+            if (other.getId() != null) {
+                return false;
+            }
+        } else if (!getId().equals(other.getId())) {
+            return false;
+        }
+        return true;
+    }
+
 }
