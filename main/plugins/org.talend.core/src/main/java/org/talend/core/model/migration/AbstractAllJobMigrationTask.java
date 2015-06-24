7706846a3f975c86804a14e75427e2fe2eb77093
rdubois <rdubois.contact@gmail.com>
2015-06-24 19:23:21 +0200

    TDI-32416 - Support a new type of abstract migration task

---
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/migration/AbstractAllJobMigrationTask.java
@@ -0,0 +1,36 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.migration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.talend.core.model.repository.ERepositoryObjectType;
+
+/**
+ * DOC rdubois class global comment. Detailled comment
+ */
+public abstract class AbstractAllJobMigrationTask extends AbstractJobMigrationTask {
+
+    @Override
+    public List<ERepositoryObjectType> getTypes() {
+        List<ERepositoryObjectType> toReturn = new ArrayList<ERepositoryObjectType>();
+        toReturn.add(ERepositoryObjectType.PROCESS);
+        toReturn.add(ERepositoryObjectType.PROCESS_MR);
+        toReturn.add(ERepositoryObjectType.PROCESS_SPARK);
+        toReturn.add(ERepositoryObjectType.PROCESS_STORM);
+        toReturn.add(ERepositoryObjectType.PROCESS_SPARKSTREAMING);
+        toReturn.add(ERepositoryObjectType.JOBLET);
+        return toReturn;
+    }
+}
