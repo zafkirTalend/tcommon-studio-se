2d08b1984c02373355c9f79bc157ff02a241289a
wchen-talend <wchen@talend.com>
2015-06-25 11:41:26 +0800

    TUP-3048:Jars are duplicated in the local .m2

--- main/plugins/org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/utils/nexus/NexusDownloader.java
+++ main/plugins/org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/utils/nexus/NexusDownloader.java
@@ -12,17 +12,40 @@
 // ============================================================================
 package org.talend.librariesmanager.utils.nexus;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.ops4j.pax.url.mvn.MavenResolver;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Platform;
+import org.talend.commons.utils.workbench.resources.ResourceUtils;
 import org.talend.core.download.DownloadListener;
 import org.talend.core.download.IDownloadHelper;
+import org.talend.core.model.general.Project;
+import org.talend.core.nexus.NexusConstants;
+import org.talend.core.nexus.NexusServerBean;
+import org.talend.core.runtime.maven.MavenArtifact;
+import org.talend.core.runtime.maven.MavenConstants;
 import org.talend.core.runtime.maven.MavenUrlHelper;
+import org.talend.designer.maven.utils.PomUtil;
 import org.talend.librariesmanager.maven.ArtifactsDeployer;
 import org.talend.librariesmanager.maven.TalendLibsServerManager;
+import org.talend.repository.ProjectManager;
+import org.talend.utils.io.FilesUtils;
+import org.talend.utils.ssl.SSLUtils;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 /**
  * created by wchen on Apr 24, 2015 Detailled comment
@@ -42,18 +65,116 @@ public class NexusDownloader implements IDownloadHelper {
      * @see org.talend.core.download.IDownloadHelper#download(java.net.URL, java.io.File)
      */
     @Override
-    public void download(URL url, File destination) throws Exception {
-        fireDownloadStart(100);
-        MavenResolver mvnResolver = TalendLibsServerManager.getInstance().getMavenResolver();
-        if (mvnResolver != null) {
-            final String mavenUri = url.toExternalForm();
-            final File resolve = mvnResolver.resolve(mavenUri);
-            String snapshotUri = MavenUrlHelper.generateSnapshotMavenUri(mavenUri);
-            ArtifactsDeployer deployer = new ArtifactsDeployer();
-            deployer.deployToLocalMaven(resolve.getAbsolutePath(), snapshotUri);
+    public void download(URL url, File desc) throws Exception {
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
+        File tempFolder = null;
+        try {
+            TalendLibsServerManager manager = TalendLibsServerManager.getInstance();
+            final NexusServerBean talendlibServer = manager.getLibrariesNexusServer();
+            String mavenUri = url.toExternalForm();
+            MavenArtifact parseMvnUrl = MavenUrlHelper.parseMvnUrl(mavenUri);
+            if (parseMvnUrl != null) {
+                String reletivePath = PomUtil.getArtifactPath(parseMvnUrl);
+                String tempPath = getTmpFolderPath();
+                tempFolder = new File(tempPath + File.separator + File.createTempFile("talend_official", "").getName()); //$NON-NLS-1$ //$NON-NLS-2$
+                if (tempFolder.exists()) {
+                    tempFolder.delete();
+                }
+                tempFolder.mkdirs();
 
+                String name = parseMvnUrl.getArtifactId();
+                String type = parseMvnUrl.getType();
+                if (type == null || "".equals(type)) {
+                    type = MavenConstants.PACKAGING_JAR;
+                }
+                name = name + "." + type;
+                File destination = new File(tempFolder, name);
+                HttpURLConnection connection = getHttpURLConnection(talendlibServer.getServer(),
+                        talendlibServer.getRepositoryId(), reletivePath, talendlibServer.getUserName(),
+                        talendlibServer.getPassword());
+
+                InputStream inputStream = connection.getInputStream();
+                bis = new BufferedInputStream(inputStream);
+                bos = new BufferedOutputStream(new FileOutputStream(destination));
+                int contentLength = connection.getContentLength();
+                fireDownloadStart(contentLength);
+
+                int bytesDownloaded = 0;
+                byte[] buf = new byte[BUFFER_SIZE];
+                int bytesRead = -1;
+                while ((bytesRead = bis.read(buf)) != -1) {
+                    bos.write(buf, 0, bytesRead);
+                    fireDownloadProgress(bytesRead);
+                    bytesDownloaded += bytesRead;
+                    if (isCancel()) {
+                        return;
+                    }
+                }
+                bos.flush();
+                bos.close();
+                if (bytesDownloaded == contentLength) {
+                    String snapshotUri = MavenUrlHelper.generateSnapshotMavenUri(mavenUri);
+                    ArtifactsDeployer deployer = new ArtifactsDeployer();
+                    deployer.deployToLocalMaven(destination.getAbsolutePath(), snapshotUri);
+                }
+
+            }
+        } finally {
+            if (bis != null) {
+                bis.close();
+            }
+            if (bos != null) {
+                bos.close();
+            }
+            if (tempFolder != null) {
+                FilesUtils.deleteFile(tempFolder, true);
+            }
         }
+
         fireDownloadComplete();
+    }
+
+    private String getTmpFolderPath() {
+        Project project = ProjectManager.getInstance().getCurrentProject();
+        String tmpFolder;
+        try {
+            IProject physProject = ResourceUtils.getProject(project);
+            tmpFolder = physProject.getFolder("temp").getLocation().toPortableString(); //$NON-NLS-1$
+        } catch (Exception e) {
+            tmpFolder = System.getProperty("user.dir"); //$NON-NLS-1$
+        }
+        return tmpFolder;
+    }
+
+    private HttpURLConnection getHttpURLConnection(String nexusUrl, String repositoryId, String relativePath, String userName,
+            String password) throws Exception {
+        String path = nexusUrl;
+        if (path.endsWith(NexusConstants.SLASH)) {
+            path = path.substring(0, path.length() - 1);
+        }
+        path = path + NexusConstants.CONTENT_REPOSITORIES;
+        path = path + repositoryId + NexusConstants.SLASH;
+        URL url = new URL(path + relativePath);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        if (userName != null && !"".equals(userName)) {
+            urlConnection.setRequestProperty("Authorization", "Basic " + Base64.encode((userName + ":" + password).getBytes()));//$NON-NLS-1$ //$NON-NLS-2$
+        }
+        if (urlConnection instanceof HttpsURLConnection) {
+            String userDir = Platform.getInstallLocation().getURL().getPath();
+            final SSLSocketFactory socketFactory = SSLUtils.getSSLContext(userDir).getSocketFactory();
+            HttpsURLConnection httpsConnection = (HttpsURLConnection) urlConnection;
+            httpsConnection.setSSLSocketFactory(socketFactory);
+            httpsConnection.setHostnameVerifier(new HostnameVerifier() {
+
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+
+            });
+        }
+        return urlConnection;
     }
 
     /**
