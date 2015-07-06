b197a4fccf4e381dc870b9c7d1ec893b9791c969
hwang <hwang@talend.com>
2015-07-06 17:20:36 +0800

    TDI-32533:tFTPGet:The Encoding is not consistent with FTP connection

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/designerproperties/RepositoryToComponentProperty.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/metadata/designerproperties/RepositoryToComponentProperty.java
@@ -2570,6 +2570,11 @@ public class RepositoryToComponentProperty {
             }
             return connection.getMode().toUpperCase();
         }
+
+        if (value.equals("USE_ENCODING")) {
+            return connection.isSFTP();
+        }
+
         if (value.equals("ENCODING")) {
             if (isContextMode(connection, connection.getCustomEncode())) {
                 return connection.getCustomEncode();
