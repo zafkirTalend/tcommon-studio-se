e8e7fbb7995a7230eb35d75e4efc78ae31662520
rdubois <rdubois.contact@gmail.com>
2015-07-28 18:36:29 +0200

    TBD-1934 - Add Junit

---
+++ test/plugins/org.talend.core.runtime.test/src/org/talend/core/model/utils/NodeUtilTest.java
@@ -0,0 +1,64 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.talend.core.model.components.ComponentCategory;
+import org.talend.core.model.process.INode;
+import org.talend.designer.core.model.components.DummyComponent;
+import org.talend.designer.core.model.process.DataNode;
+
+/**
+ * created by rdubois on 17 juin 2015 Detailled comment
+ *
+ */
+public class NodeUtilTest {
+
+    @Test
+    public void testIsBigDataFrameworkNode() {
+        DummyComponent comp = Mockito.mock(DummyComponent.class);
+        Mockito.when(comp.getType()).thenReturn(ComponentCategory.CATEGORY_4_CAMEL.getName());
+        INode node = new DataNode(comp, ""); //$NON-NLS-1$
+
+        assertFalse(NodeUtil.isBigDataFrameworkNode(node));
+
+        Mockito.when(comp.getType()).thenReturn(ComponentCategory.CATEGORY_4_DI.getName());
+        node = new DataNode(comp, ""); //$NON-NLS-1$
+
+        assertFalse(NodeUtil.isBigDataFrameworkNode(node));
+
+        Mockito.when(comp.getType()).thenReturn(ComponentCategory.CATEGORY_4_MAPREDUCE.getName());
+        node = new DataNode(comp, ""); //$NON-NLS-1$
+
+        assertTrue(NodeUtil.isBigDataFrameworkNode(node));
+
+        Mockito.when(comp.getType()).thenReturn(ComponentCategory.CATEGORY_4_SPARK.getName());
+        node = new DataNode(comp, ""); //$NON-NLS-1$
+
+        assertTrue(NodeUtil.isBigDataFrameworkNode(node));
+
+        Mockito.when(comp.getType()).thenReturn(ComponentCategory.CATEGORY_4_SPARKSTREAMING.getName());
+        node = new DataNode(comp, ""); //$NON-NLS-1$
+
+        assertTrue(NodeUtil.isBigDataFrameworkNode(node));
+
+        Mockito.when(comp.getType()).thenReturn(ComponentCategory.CATEGORY_4_STORM.getName());
+        node = new DataNode(comp, ""); //$NON-NLS-1$
+
+        assertTrue(NodeUtil.isBigDataFrameworkNode(node));
+
+    }
+}
