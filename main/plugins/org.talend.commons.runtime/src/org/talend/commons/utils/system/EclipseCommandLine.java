3fdfc7568c950f5625145353b8b35ec482d34b13
Irene Wang <iwang@talend.com>
2015-08-05 20:12:39 +0200

    Update copyright year to 2015

--- main/plugins/org.talend.commons.runtime/src/org/talend/commons/utils/system/EclipseCommandLine.java
+++ main/plugins/org.talend.commons.runtime/src/org/talend/commons/utils/system/EclipseCommandLine.java
@@ -2,7 +2,7 @@
 //
 // Talend Community Edition
 //
-// Copyright (C) 2006-2012 Talend 锟�www.talend.com
+// Copyright (C) 2006-2015 Talend 锟�www.talend.com
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
