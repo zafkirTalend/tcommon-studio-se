873caaa5f26baff5107f5d12f3ce63e4e549dee5
wchen-talend <wchen@talend.com>
2015-08-14 11:33:25 +0800

    TUP-3381:Got NPE when generating documentation for jobs/routes

---
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/document/IGenerateAllDocumentation.java
@@ -0,0 +1,36 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.repository.document;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.talend.core.model.general.Project;
+import org.talend.repository.model.RepositoryNode;
+
+/**
+ * 
+ * created by wchen on 2015年8月14日 Detailled comment
+ *
+ */
+public interface IGenerateAllDocumentation {
+
+    public File generateAllDocListHTML();
+
+    public boolean generateAllItemsDocWithProgressMonitor(IProgressMonitor monitor) throws InterruptedException;
+
+    public void setProject(Project project);
+
+    public void setRootNode(RepositoryNode jobletNode);
+
+}
