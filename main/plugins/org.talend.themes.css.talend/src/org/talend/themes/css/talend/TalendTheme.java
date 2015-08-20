f2cbd78275ee832d062acd788fe8c06ff5e320cd
cmeng-talend <cmeng@talend.com>
2015-08-20 11:22:09 +0800

    Correct the plugin name

--- main/plugins/org.talend.themes.css.talend/src/org/talend/themes/css/talend/TalendTheme.java
+++ main/plugins/org.talend.themes.css.talend/src/org/talend/themes/css/talend/TalendTheme.java
@@ -8,43 +8,47 @@ import org.osgi.framework.BundleContext;
  */
 public class TalendTheme extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.talend.themes.talend_theme"; //$NON-NLS-1$
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.talend.themes.css.talend"; //$NON-NLS-1$
 
-	// The shared instance
-	private static TalendTheme plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public TalendTheme() {
-	}
+    // The shared instance
+    private static TalendTheme plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+    /**
+     * The constructor
+     */
+    public TalendTheme() {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static TalendTheme getDefault() {
-		return plugin;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static TalendTheme getDefault() {
+        return plugin;
+    }
 
 }
