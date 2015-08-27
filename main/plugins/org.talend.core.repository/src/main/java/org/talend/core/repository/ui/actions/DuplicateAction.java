a5ce90cd15c4a3eed92bfb4b3d3ec85f8c1c38f7
nrousseau <nrousseau@talend.com>
2015-08-27 12:51:58 +0800

    fix compilation issue

--- main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/ui/actions/DuplicateAction.java
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/ui/actions/DuplicateAction.java
@@ -832,7 +832,7 @@ public class DuplicateAction extends AContextualAction {
         if (codeGenService != null) {
             codeGenService.createCamelBeanSynchronizer().renameBeanClass(item);
             try {
-                codeGenService.createCamelBeanSynchronizer().syncBean(item, true);
+                codeGenService.createCamelBeanSynchronizer().syncRoutine((RoutineItem)item, true);
             } catch (SystemException e) {
                 ExceptionHandler.process(e);
             }
