6fe693fe394329c0a2b6c4ae4871c1b14a2c6b8e
hwang <hwang@talend.com>
2015-09-22 10:10:55 +0800

    TDI-33863:Import Excel file with references

--- main/plugins/org.talend.metadata.managment.ui/src/main/java/org/talend/metadata/managment/ui/preview/ShadowProcessPreview.java
+++ main/plugins/org.talend.metadata.managment.ui/src/main/java/org/talend/metadata/managment/ui/preview/ShadowProcessPreview.java
@@ -281,7 +281,9 @@ public class ShadowProcessPreview {
         }
 
         // scroll to show the first col and first row
-        table.showItem(table.getItem(0));
+        if (table.getItemCount() > 0) {
+            table.showItem(table.getItem(0));
+        }
         if (table.getColumns() != null && table.getColumns().length > 0) {
             table.showColumn(table.getColumn(0));
         }
