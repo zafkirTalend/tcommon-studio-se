cc72a08f731196c6acad799943bae49652895b15
hcyi <hcyi@talend.com>
2015-09-24 12:16:02 +0800

    TPS-1107:[5.6.2] Need some options to disable the internet access.

--- main/plugins/org.talend.registration/src/main/java/org/talend/repository/registeruser/RegisterManagement.java
+++ main/plugins/org.talend.registration/src/main/java/org/talend/repository/registeruser/RegisterManagement.java
@@ -35,6 +35,7 @@ import org.eclipse.update.core.SiteManager;
 import org.talend.commons.exception.BusinessException;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.commons.utils.VersionUtils;
+import org.talend.commons.utils.network.NetworkUtil;
 import org.talend.commons.utils.platform.PluginChecker;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.model.general.ConnectionBean;
@@ -448,6 +449,9 @@ public class RegisterManagement {
     }
 
     public void validateRegistration() {
+        if (!NetworkUtil.isNetworkValid()) {
+            return;
+        }
         IBrandingService brandingService = (IBrandingService) GlobalServiceRegister.getDefault().getService(
                 IBrandingService.class);
         if (!brandingService.getBrandingConfiguration().isUseProductRegistration()) {
