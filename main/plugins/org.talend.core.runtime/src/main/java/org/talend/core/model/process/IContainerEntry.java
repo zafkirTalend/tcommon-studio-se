aec3a356a814655601b38ee00bc6b00bb2522b96
hwang <hwang@talend.com>
2015-11-17 17:49:23 +0800

    TDI-34280:Run test case alway met the error log but not every time.

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/process/IContainerEntry.java
@@ -0,0 +1,32 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.process;
+
+import java.util.List;
+
+import org.eclipse.swt.graphics.Image;
+
+/**
+ * created by hwang on Nov 17, 2015 Detailled comment
+ *
+ */
+public interface IContainerEntry {
+
+    public String getLabel();
+
+    public boolean hasChildren();
+
+    public List getChildren();
+
+    public Image getImage();
+}
