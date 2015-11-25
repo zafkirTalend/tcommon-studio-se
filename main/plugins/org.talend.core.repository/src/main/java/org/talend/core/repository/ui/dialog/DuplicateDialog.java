2ffec828604d2175edfc393abd5bf4a264ee4763
amarkevich <amarkevich@talend.com>
2015-11-25 12:52:57 +0300

    also handle Routelet

--- main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/ui/dialog/DuplicateDialog.java
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/ui/dialog/DuplicateDialog.java
@@ -88,16 +88,18 @@ public class DuplicateDialog extends CustomInputDialog {
     private void init() {
         if (sourceNode != null) {
             // job type
-            if (sourceNode.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.PROCESS) {
+            final Object contentType = sourceNode.getProperties(EProperties.CONTENT_TYPE);
+            if (contentType == ERepositoryObjectType.PROCESS) {
                 jobTypeValue = JobType.STANDARD.getDisplayName();
                 jobTypeCombo.setText(jobTypeValue);
-            } else if (sourceNode.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.PROCESS_MR) {
+            } else if (contentType == ERepositoryObjectType.PROCESS_MR) {
                 jobTypeValue = JobType.BIGDATABATCH.getDisplayName();
                 jobTypeCombo.setText(jobTypeValue);
-            } else if (sourceNode.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.PROCESS_STORM) {
+            } else if (contentType == ERepositoryObjectType.PROCESS_STORM) {
                 jobTypeValue = JobType.BIGDATASTREAMING.getDisplayName();
                 jobTypeCombo.setText(jobTypeValue);
-            } else if (sourceNode.getProperties(EProperties.CONTENT_TYPE) == ERepositoryObjectType.PROCESS_ROUTE) {
+            } else if (contentType == ERepositoryObjectType.PROCESS_ROUTE
+                || contentType == ERepositoryObjectType.PROCESS_ROUTELET) {
                 jobTypeCombo.setVisible(false);
                 frameworkCombo.setVisible(false);
             }
