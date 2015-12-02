e35cad901fdfa7ea733a3330aec42e7807bd23c7
hwang <hwang@talend.com>
2015-12-02 20:44:53 +0800

    TDI-33988:Test case is not actually saved successfully when I check out for another version

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/ui/ITestContainerProviderService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/ui/ITestContainerProviderService.java
@@ -105,7 +105,7 @@ public interface ITestContainerProviderService extends IService {
     public IRepositoryEditorInput createTestCaseEditorInput(ProcessItem junitItem, boolean load, String originalJobID,
             List<INode> testNodes, Boolean lastVersion, Boolean readonly) throws PersistenceException;
 
-    public ProcessType loadSVNTestContainer(Item item, IProcess2 loadedProcess, Property loadProperty);
+    public ProcessType loadRemoteTestContainer(Item item, IProcess2 loadedProcess, Property loadProperty);
 
     public ERepositoryObjectType getTestCaseObjectType();
 }
