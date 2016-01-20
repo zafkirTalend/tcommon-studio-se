94a3907d9428a5d6c09f2fca13a0e7e74d60a639
cmeng-talend <cmeng@talend.com>
2016-01-20 17:36:14 +0800

    TDI-34005 bug: CPU usage won't go down after a job export https://jira.talendforge.org/browse/TDI-34005

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/prefs/ITalendCorePrefConstants.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/prefs/ITalendCorePrefConstants.java
@@ -179,4 +179,6 @@ public interface ITalendCorePrefConstants {
      * TAC connection timeout (seconds)
      */
     public static final String PERFORMANCE_TAC_CONNECTION_TIMEOUT = "PERFORMANCE_TAC_CONNECTION_TIMEOUT"; //$NON-NLS-1$
+
+    public static final String PERFORMANCE_JAVA_PROCESS_CODE_FORMATE_TIMEOUT = "PERFORMANCE_JAVA_PROCESS_CODE_FORMATE_TIMEOUT"; //$NON-NLS-1$
 }
