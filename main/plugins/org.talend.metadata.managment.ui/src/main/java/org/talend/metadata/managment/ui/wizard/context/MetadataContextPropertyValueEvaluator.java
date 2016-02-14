95aa0f9d805b15f15df32a4130e72beca0a6a35b
ycbai <ycbai@talend.com>
2016-02-14 14:52:34 +0800

    TUP-3782: Support Context

---
+++ main/plugins/org.talend.metadata.managment.ui/src/main/java/org/talend/metadata/managment/ui/wizard/context/MetadataContextPropertyValueEvaluator.java
@@ -0,0 +1,45 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.metadata.managment.ui.wizard.context;
+
+import org.talend.components.api.properties.Property;
+import org.talend.components.api.properties.PropertyValueEvaluator;
+import org.talend.core.model.metadata.builder.connection.Connection;
+import org.talend.core.model.utils.ContextParameterUtils;
+import org.talend.designer.core.model.utils.emf.talendfile.ContextType;
+import org.talend.metadata.managment.ui.utils.ConnectionContextHelper;
+
+/**
+ * created by ycbai on 2016年2月6日 Detailled comment
+ *
+ */
+public class MetadataContextPropertyValueEvaluator implements PropertyValueEvaluator {
+
+    private Connection connection;
+
+    public MetadataContextPropertyValueEvaluator(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public Object evaluate(Property property, Object storedValue) {
+        if (connection != null && connection.isContextMode() && storedValue != null) {
+            ContextType contextType = ConnectionContextHelper.getContextTypeForContextMode(null, connection, true);
+            if (contextType != null) {
+                return ContextParameterUtils.getOriginalValue(contextType, String.valueOf(storedValue));
+            }
+        }
+        return storedValue;
+    }
+
+}
