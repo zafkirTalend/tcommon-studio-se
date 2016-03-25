9a372e9f85e914b13f5c7a5c8996a9c5389842de
hcyi <hcyi@talend.com>
2016-03-25 10:48:12 +0800

    TUP-4152:Refactoring code about generic provider(TUP-4010)

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/process/IGenericProvider.java
@@ -0,0 +1,26 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.process;
+
+import java.util.List;
+
+/**
+ * created by hcyi on Mar 25, 2016 Detailled comment
+ *
+ */
+public interface IGenericProvider {
+
+    public void loadComponentsFromExtensionPoint();
+
+    public List<?> addPaletteEntry();
+}
