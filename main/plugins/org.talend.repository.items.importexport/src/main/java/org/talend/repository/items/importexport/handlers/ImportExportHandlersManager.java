41a825a99b625ebd3969aa09207456c1439d833f
hwang <hwang@talend.com>
2016-05-30 18:00:45 +0800

    TDI-36037:Can not import in items which export out from recycle bin.

--- main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/handlers/ImportExportHandlersManager.java
+++ main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/handlers/ImportExportHandlersManager.java
@@ -383,6 +383,7 @@ public class ImportExportHandlersManager {
                                 }
                             }
                             ImportCacheHelper.getInstance().checkDeletedFolders();
+                            ImportCacheHelper.getInstance().checkDeletedItems();
                             monitor.done();
 
                             TimeMeasure.step("importItemRecords", "before save"); //$NON-NLS-1$ //$NON-NLS-2$
