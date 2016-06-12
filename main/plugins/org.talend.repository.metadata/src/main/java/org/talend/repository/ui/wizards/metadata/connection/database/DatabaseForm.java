0fd9456a5b5731a857f1d6472d5d2a0bedcc954a
zwxue <zwxue@talend.com>
2016-06-12 18:17:58 +0800

    Revert "TDI-35531: Set driver jar LabelText read only."
    This reverts commit 33f701ce5305df6436bbc713bddffd2fd580fd8c.

--- main/plugins/org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
+++ main/plugins/org.talend.repository.metadata/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
@@ -2737,7 +2737,6 @@ public class DatabaseForm extends AbstractForm {
 
         generalJdbcDriverjarText = new LabelledText(generalDbCompositeParent, Messages.getString("DatabaseForm.general.jarfile"), //$NON-NLS-1$
                 1);
-        generalJdbcDriverjarText.setReadOnly(true);
 
         browseJarFilesButton = new Button(generalDbCompositeParent, SWT.NONE);
         browseJarFilesButton.setText("..."); //$NON-NLS-1$
