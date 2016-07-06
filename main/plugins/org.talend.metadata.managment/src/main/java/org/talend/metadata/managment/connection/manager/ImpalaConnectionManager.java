4ce0776638bc76d234b9e09759d97f82f467b829
hcyi <hcyi@talend.com>
2016-07-06 15:34:02 +0800

    TBD-3460:Unable to connect to kerberized Impala on CDH 5.5 on db connection metadata wizard.

--- main/plugins/org.talend.metadata.managment/src/main/java/org/talend/metadata/managment/connection/manager/ImpalaConnectionManager.java
+++ main/plugins/org.talend.metadata.managment/src/main/java/org/talend/metadata/managment/connection/manager/ImpalaConnectionManager.java
@@ -94,8 +94,9 @@ public class ImpalaConnectionManager extends DataBaseConnectionManager {
                 } else {
                     IHDistributionVersion impalaVersion = impalaDistribution.getHDVersion(version, false);
                     if (impalaVersion != null) {
+                        boolean isKeb = false;
                         DynamicClassLoader classLoader = ClassLoaderFactory.getClassLoader(impalaIndex
-                                + ClassLoaderFactory.KEY_SEPARATOR + impalaVersion.getVersion());
+                                + ClassLoaderFactory.KEY_SEPARATOR + impalaVersion.getVersion() + (isKeb ? "?USE_KRB" : ""));//$NON-NLS-1$//$NON-NLS-2$
 
                         // if not work for extension point, try modules from hadoop distribution
                         if (classLoader == null) {
