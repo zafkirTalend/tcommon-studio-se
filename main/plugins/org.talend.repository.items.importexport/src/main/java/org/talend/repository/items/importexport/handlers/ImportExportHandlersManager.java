d493f794ff7832a698fd972fcb1ce2c3ecc2b9d6
cmeng-talend <cmeng@talend.com>
2016-12-09 16:53:24 +0800

    TUP-16073 fix: Importing duplicated job causes wrong subjob referenced in tRunJob https://jira.talendforge.org/browse/TUP-16073
    Use new logic to change the id

--- main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/handlers/ImportExportHandlersManager.java
+++ main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/handlers/ImportExportHandlersManager.java
@@ -500,7 +500,7 @@ public class ImportExportHandlersManager {
                                         String oldId = itemRecord.getProperty().getId();
                                         itemRecord.getProperty().setId(id);
                                         try {
-                                            changeIdManager.mapNewId2OldId(id, oldId);
+                                            changeIdManager.mapOldId2NewId(oldId, id);
                                         } catch (Exception e) {
                                             ExceptionHandler.process(e);
                                         }
@@ -509,7 +509,6 @@ public class ImportExportHandlersManager {
                             }
 
                             try {
-                                changeIdManager.prepare(resManager);
                                 importItemRecordsWithRelations(monitor, resManager, checkedItemRecords, overwrite,
                                         allImportItemRecords, destinationPath, overwriteDeletedItems, idDeletedBeforeImport);
                             } catch (Exception e) {
@@ -594,6 +593,11 @@ public class ImportExportHandlersManager {
                             // post import
                             List<ImportItem> importedItemRecords = ImportCacheHelper.getInstance().getImportedItemRecords();
                             postImport(monitor, resManager, importedItemRecords.toArray(new ImportItem[0]));
+                            try {
+                                changeIdManager.changeIds();
+                            } catch (Exception e) {
+                                ExceptionHandler.process(e);
+                            }
                             unloadImportItems(allImportItemRecords);
                         }
 
@@ -643,12 +647,6 @@ public class ImportExportHandlersManager {
                                         }
                                         if (monitor.isCanceled()) {
                                             return;
-                                        }
-
-                                        try {
-                                            changeIdManager.changeId(itemRecord.getProperty().getId());
-                                        } catch (Exception e) {
-                                            ExceptionHandler.process(e);
                                         }
 
                                         // will import
