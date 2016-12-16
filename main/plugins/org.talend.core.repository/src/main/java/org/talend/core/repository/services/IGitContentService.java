eff577160f70ab3e85087e8629e5dbfa5c8d4f82
cmeng-talend <cmeng@talend.com>
2016-12-16 19:06:36 +0800

    TUP-16444 fix> JobScript: Cannot edit new jobscript on GIT Project(Manual, UnLock Items) https://jira.talendforge.org/browse/TUP-16444

--- main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/services/IGitContentService.java
+++ main/plugins/org.talend.core.repository/src/main/java/org/talend/core/repository/services/IGitContentService.java
@@ -1,5 +1,7 @@
 package org.talend.core.repository.services;
 
+import java.util.Collection;
+
 public interface IGitContentService {
 
     public abstract void setMenu(Object object);
@@ -9,4 +11,8 @@ public interface IGitContentService {
     public abstract void createDropdownCombo(Object composite);
 
     public abstract void configureCombo(String descriptor);
+
+    public abstract Collection<Object> frezzeEditors(Collection<String> modifiledFiles) throws Exception;
+
+    public abstract void defrezzeEditors(Collection<Object> frezzedEditors) throws Exception;
 }
