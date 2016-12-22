3b1b9d4f517c6c2f2bc1c17196dca92bf3069e95
Kevin Wang <kjwang@talend.com>
2016-12-22 14:32:40 +0800

    TUP-16262 Fix Opening schema is very slow using Talend studio 6.2 https://jira.talendforge.org/browse/TUP-16262

--- main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/metadata/editor/MetadataEmfTableEditorView.java
+++ main/plugins/org.talend.core.ui/src/main/java/org/talend/core/ui/metadata/editor/MetadataEmfTableEditorView.java
@@ -546,7 +546,7 @@ public class MetadataEmfTableEditorView extends AbstractMetadataTableEditorView<
     @Override
     protected void setTableViewerCreatorOptions(TableViewerCreator<MetadataColumn> newTableViewerCreator) {
         super.setTableViewerCreatorOptions(newTableViewerCreator);
-        // newTableViewerCreator.setLazyLoad(true);
+        newTableViewerCreator.setLazyLoad(true);
     }
 
     /*
