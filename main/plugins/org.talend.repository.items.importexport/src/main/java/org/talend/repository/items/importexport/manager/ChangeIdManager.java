8632665331e5cc100055cc030258adc64f639cf6
cmeng-talend <cmeng@talend.com>
2016-12-28 16:32:15 +0800

    TUP-16073 fix: Importing duplicated job causes wrong subjob referenced in tRunJob https://jira.talendforge.org/browse/TUP-16073
    Fix a compile error

--- main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/manager/ChangeIdManager.java
+++ main/plugins/org.talend.repository.items.importexport/src/main/java/org/talend/repository/items/importexport/manager/ChangeIdManager.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Priority;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.core.GlobalServiceRegister;
