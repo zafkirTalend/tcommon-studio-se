a91629fa2f19617c432fe245021ecececb25843d
hwang <hwang@talend.com>
2017-01-20 13:55:51 +0800

    TUP-16318:Password field in connection components should not mask global variables only strings

--- main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/core/model/utils/TalendTextUtils.java
@@ -497,6 +497,10 @@ public class TalendTextUtils {
         if (ContextParameterUtils.containContextVariables(password)) {
             return password;
         }
+        
+        if(!TalendQuoteUtils.isCommonString(password)){
+            return password;
+        }
 
         int length = password.length() + 2;
 
