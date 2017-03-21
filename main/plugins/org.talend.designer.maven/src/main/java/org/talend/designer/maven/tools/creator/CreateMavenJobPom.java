65eb6ff3a0f318ff31a002f03bf7f73ba4fa8b93
Pierre Bailly-Ferry <pbailly@talend.com>
2017-03-21 15:51:14 +0100

    feat(TBD-4707): Add Google Dataproc distribution and its context support (#914)
    * feat(TBD-4707): Add Google Dataproc distribution and its context support
    
    * feat(TBD-4707): Fix naming.

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/CreateMavenJobPom.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/CreateMavenJobPom.java
@@ -464,7 +464,7 @@ public class CreateMavenJobPom extends AbstractMavenProcessorPom {
     }
 
     private void setPomForHDLight(IProgressMonitor monitor) {
-        if (ProcessUtils.isHDInsight()) {
+        if (ProcessUtils.jarNeedToContainsContext()) {
             try {
                 Model model = MODEL_MANAGER.readMavenModel(getPomFile());
                 List<Plugin> plugins = new ArrayList<Plugin>(model.getBuild().getPlugins());
