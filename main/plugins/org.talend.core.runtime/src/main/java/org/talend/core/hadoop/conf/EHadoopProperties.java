9bcd9bbfe9c314b46c1c608f1e6a0e325a0070c1
Seloen <seloen@outlook.com>
2017-05-09 15:23:16 +0800

    bugfix(TUP-17592)No default value for HDInsight hadoop wizard (#1017)
    https://jira.talendforge.org/browse/TUP-17592

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/hadoop/conf/EHadoopProperties.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/hadoop/conf/EHadoopProperties.java
@@ -83,6 +83,22 @@ public enum EHadoopProperties {
     GOOGLE_REGION,
 
     GOOGLE_JARS_BUCKET,
+
+    HD_WEBHCAT_HOSTNAME,
+
+    HD_WEBHCAT_PORT,
+
+    HD_WEBHCAT_USERNAME,
+
+    HD_INSIGHT_USERNAME,
+
+    HD_AZURE_HOSTNAME,
+
+    HD_AZURE_CONTAINER,
+
+    HD_AZURE_USERNAME,
+
+    HD_AZURE_DEPLOYBOLB,
     ;
 
     public String getName() {
